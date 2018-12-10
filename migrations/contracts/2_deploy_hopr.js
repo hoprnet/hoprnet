7196ddce30f5521ae8a2d98b05f66ce6f4bf4a0e
Matt Swezey <matt@mattswezey.net>
2018-12-10 08:32:30 -0600

    Updated comment on migration file

--- migrations/contracts/2_deploy_hopr.js
+++ migrations/contracts/2_deploy_hopr.js
@@ -1,8 +1,6 @@
 /* eslint-disable prefer-destructuring */
 /**
- * Migration - ICO & Token
- * Deploys a Token contract, then the ICO contract passing the address of the previously deployed token.
- * Adds minter role to ico contract. Renounces minter role for the original token contract deployer (accounts[0])
+ * Migration
  */
 const cnf = require('../../config/contract-hopr.json');
 const HoprChannel = artifacts.require('./HoprChannel.sol');
