79b0b6a25f73835b4c6ac92d291dae8e6c16b272
Robert Kiel <robert.kiel@validitylabs.org>
2019-01-10 10:49:37 +0100

    now using OZ 2.1 directly

--- contracts/HoprChannel.sol
+++ contracts/HoprChannel.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 // import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
-import "./math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 /* solhint-disable max-line-length */
