9834fe15f210d3878a5817a86dbc0c47e5099187
Robert Kiel <robert.kiel@validitylabs.org>
2019-01-28 13:17:06 +0100

    ✨ add several plausibility checks & better err handling

--- src/paymentChannels/handlers/open.js
+++ src/paymentChannels/handlers/open.js
@@ -6,7 +6,7 @@ const { PROTOCOL_PAYMENT_CHANNEL } = require('../../constants')
 const pull = require('pull-stream')
 const lp = require('pull-length-prefixed')
 const { sign } = require('secp256k1')
-const { deepCopy, pubKeyToEthereumAddress, numberToBuffer, bufferToNumber, pubKeyToPeerId } = require('../../utils')
+const { deepCopy, pubKeyToEthereumAddress, numberToBuffer, bufferToNumber, pubKeyToPeerId, log } = require('../../utils')
 const { waterfall } = require('neo-async')
 const { BN } = require('web3').utils
 
@@ -17,70 +17,68 @@ module.exports = (node) => node.handle(PROTOCOL_PAYMENT_CHANNEL, (protocol, conn
     lp.decode(),
     pull.asyncMap((data, cb) => {
         if (data.length !== Transaction.SIZE)
-            cb(true)
+            return cb()
 
         const restoreTx = Transaction.fromBuffer(data)
 
         if (bufferToNumber(restoreTx.index) !== 1)
-            cb(true, null)
+            return cb()
 
         const counterparty = restoreTx.counterparty
 
         waterfall([
+            // checking whether the information provided by libp2p network
+            // stack is plausible
             (cb) => conn.getPeerInfo(cb),
             (peerInfo, cb) => {
                 if (peerInfo.id.pubKey) {
-                    if (peerInfo.id.pubKey.marshal().compare(counterparty) === 0) {
-                        cb(null, restoreTx)
-                    } else {
-                        throw Error('Something went wrong with the signature?')
-                        cb(null, null)
-                    }
+                    if (peerInfo.id.pubKey.marshal().compare(counterparty) !== 0)
+                        return cb(Error('Invalid public key.'))
+
+                    return cb()
                 } else {
                     pubKeyToPeerId(counterparty, (err, peerId) => {
-                        if (peerInfo.id.pubKey.bytes.compare(peerId.pubKey.bytes) === 0) {
-                            cb(null, restoreTx)
-                        } else {
-                            throw Error('Something went wrong with the signature?')
-                            cb(null, null)
-                        }
+                        if (peerInfo.id.pubKey.bytes.compare(peerId.pubKey.bytes) !== 0)
+                            return cb(Error('Invalid public key.'))
+
+                        return cb()
                     })
                 }
             },
-            // (cb) => node.paymentChannels.contract.methods.states(pubKeyToEthereumAddress(counterparty)).call(cb),
-            // (state, cb) => {
-            //     if ((new BN(state.stakedEther)).lt(new BN(restoreTx.value))) {
-            //         cb(true)
-            //     } else {
-            //         cb(null, restoreTx)
-            //     }
-            // }
-        ], cb)
-    }),
-    (read) => (end, reply) => waterfall([
-        (cb) => {
-            if (end) {
-                cb(end, null)
-            } else {
-                read(end, cb)
+            // Check whether the counterparty has staked enough money to open
+            // the payment channel
+            (cb) => node.paymentChannels.contract.methods.states(pubKeyToEthereumAddress(counterparty)).call(cb),
+            (state, cb) => {
+                if ((new BN(state.stakedEther)).lt(new BN(restoreTx.value)))
+                    return cb(Error('Too less staked money.'))
+
+                return cb()
+            },
+            // Save channel information
+            (cb) => node.paymentChannels.setChannel({
+                restoreTx: restoreTx,
+                tx: deepCopy(restoreTx, Transaction),
+                index: restoreTx.index,
+                currentValue: restoreTx.value,
+                totalBalance: (new BN(restoreTx.value)).imuln(2).toBuffer('be', Transaction.VALUE_LENGTH)
+            }, cb),
+            // Register event handler
+            (cb) => {
+                node.paymentChannels.setSettlementListener(restoreTx.getChannelId(node.peerInfo.id))
+
+                const sigRestore = sign(restoreTx.hash, node.peerInfo.id.privKey.marshal())
+                cb(null, Buffer.concat([sigRestore.signature, numberToBuffer(sigRestore.recovery, 1)], SIGNATURE_LENGTH + 1))
             }
-        },
-        (restoreTx, cb) => node.paymentChannels.setChannel({
-            restoreTx: restoreTx,
-            tx: deepCopy(restoreTx, Transaction),
-            index: restoreTx.index,
-            currentValue: restoreTx.value,
-            totalBalance: (new BN(restoreTx.value)).imuln(2).toBuffer('be', Transaction.VALUE_LENGTH)
-        }, (err) => {
-            if (err)
-                throw err
+        ], (err, signature) => {
+            if (err) {
+                log(node.peerInfo.id, err.message)
+                return cb(null, null)
+            }
 
-            node.paymentChannels.setSettlementListener(restoreTx.getChannelId(node.peerInfo.id))
-
-            const sigRestore = sign(restoreTx.hash, node.peerInfo.id.privKey.marshal())
-            cb(null, Buffer.concat([sigRestore.signature, numberToBuffer(sigRestore.recovery, 1)], SIGNATURE_LENGTH + 1))
+            return cb(null, signature)
         })
-    ], reply),
+    }),
+    pull.filter(data => Buffer.isBuffer(data)),
     lp.encode(),
     conn
 ))
--- src/paymentChannels/handlers/settle.js
+++ src/paymentChannels/handlers/settle.js
@@ -13,12 +13,12 @@ module.exports = (node) => node.handle(PROTOCOL_SETTLE_CHANNEL, (protocol, conn)
     lp.decode(),
     pull.filter((data) => data.length > 0 && data.length === CHANNEL_ID_LENGTH),
     pull.asyncMap((channelId, cb) => node.paymentChannels.getChannel(channelId, (err, record) => {
-        if (!record) {
-            cb(null, null)
-        } else {
-            cb(null, channelId)
-        } 
+        if (!record)
+            return cb()
+
+        return cb(null, channelId)
     })),
+    pull.filter(data => Buffer.isBuffer(data)),
     pull.drain((channelId) => {
         log(node.peerInfo.id, `Asked to settle channel \x1b[33m${channelId.toString('hex')}\x1b[0m.`)
         node.paymentChannels.requestClose(channelId)
