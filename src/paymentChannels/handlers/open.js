7aa82dae2ef06ec6ef2247092ee027922a5f23ec
Robert Kiel <robert.kiel@validitylabs.org>
2019-02-11 18:12:26 +0100

    ♻️ remove unnecessary checks, add more significant ones

--- src/paymentChannels/handlers/open.js
+++ src/paymentChannels/handlers/open.js
@@ -6,8 +6,9 @@ const { PROTOCOL_PAYMENT_CHANNEL } = require('../../constants')
 const pull = require('pull-stream')
 const lp = require('pull-length-prefixed')
 const { sign } = require('secp256k1')
-const { deepCopy, pubKeyToEthereumAddress, numberToBuffer, bufferToNumber, pubKeyToPeerId, log } = require('../../utils')
-const { waterfall } = require('neo-async')
+const { deepCopy, pubKeyToEthereumAddress, numberToBuffer, bufferToNumber, log } = require('../../utils')
+const { applyEachSeries, applyEach } = require('neo-async')
+const { fromWei } = require('web3-utils')
 const BN = require('bn.js')
 
 const { SIGNATURE_LENGTH } = require('../../transaction')
@@ -26,37 +27,44 @@ module.exports = (node) => node.handle(PROTOCOL_PAYMENT_CHANNEL, (protocol, conn
 
         const counterparty = restoreTx.counterparty
 
-        waterfall([
-            // checking whether the information provided by libp2p network
-            // stack is plausible
-            (cb) => conn.getPeerInfo(cb),
-            (peerInfo, cb) => node.getPubKey(peerInfo, cb),
-            (peerInfo, cb) => {
-                if (peerInfo.id.pubKey) {
-                    if (peerInfo.id.pubKey.marshal().compare(counterparty) !== 0)
-                        return cb(Error('Invalid public key.'))
+        applyEachSeries([
+            (cb) => applyEach([
+                // Check whether the counterparty has staked enough money to open
+                // the payment channel
+                (cb) => node.paymentChannels.contract.methods.states(pubKeyToEthereumAddress(counterparty)).call({
+                    from: pubKeyToEthereumAddress(node.peerInfo.id.pubKey.marshal())
+                }, 'latest', (err, state) => {
+                    if (err)
+                        return cb(err)
+
+                    if (!state.isSet)
+                        return cb(Error(`Rejecting payment channel opening request because counterparty hasn't staked any funds.`))
+                
+                    const stakedEther = new BN(state.stakedEther)
+                    const claimedFunds = new BN(restoreTx.value)
+                    if (stakedEther.lt(claimedFunds))
+                        return cb(Error(`Rejecting payment channel opening request due to ${fromWei(claimedFunds.sub(stakedEther), 'ether')} ETH too less staked funds.`))
 
                     return cb()
-                } else {
-                    pubKeyToPeerId(counterparty, (err, peerId) => {
-                        if (peerInfo.id.pubKey.bytes.compare(peerId.pubKey.bytes) !== 0)
-                            return cb(Error('Invalid public key.'))
+                }),
+                // Check whether there is already such a channel registered in the
+                // smart contract
+                (cb) => {
+                    const channelId = restoreTx.getChannelId(node.peerInfo.id)
+                    node.paymentChannels.contract.methods.channels(channelId).call({
+                        from: pubKeyToEthereumAddress(node.peerInfo.id.pubKey.marshal())
+                    }, (err, channel) => {
+                        if (err)
+                            return cb(err)
+
+                        const state = parseInt(channel.state)
+                        if (!Number.isInteger(state) || state < 0)
+                            return cb(Error(`Invalid state. Got ${state.toString()} instead.`))
 
                         return cb()
                     })
                 }
-            },
-            // Check whether the counterparty has staked enough money to open
-            // the payment channel
-            (cb) => node.paymentChannels.contract.methods.states(pubKeyToEthereumAddress(counterparty)).call({
-                from: pubKeyToEthereumAddress(node.peerInfo.id.pubKey.marshal())
-            }, 'latest', cb),
-            (state, cb) => {
-                if ((new BN(state.stakedEther)).lt(new BN(restoreTx.value)))
-                    return cb(Error('Too less staked money.'))
-
-                return cb()
-            },
+            ], cb),
             // Save channel information
             (cb) => node.paymentChannels.setChannel({
                 restoreTx: restoreTx,
@@ -65,20 +73,16 @@ module.exports = (node) => node.handle(PROTOCOL_PAYMENT_CHANNEL, (protocol, conn
                 currentValue: restoreTx.value,
                 totalBalance: (new BN(restoreTx.value)).imuln(2).toBuffer('be', Transaction.VALUE_LENGTH)
             }, cb),
-            // Register event handler
-            (cb) => {
-                node.paymentChannels.setSettlementListener(restoreTx.getChannelId(node.peerInfo.id))
-
-                const sigRestore = sign(restoreTx.hash, node.peerInfo.id.privKey.marshal())
-                cb(null, Buffer.concat([sigRestore.signature, numberToBuffer(sigRestore.recovery, 1)], SIGNATURE_LENGTH + 1))
-            }
-        ], (err, signature) => {
+        ], (err) => {
             if (err) {
                 log(node.peerInfo.id, err.message)
-                return cb(null, null)
+                return cb()
             }
 
-            return cb(null, signature)
+            node.paymentChannels.setSettlementListener(restoreTx.getChannelId(node.peerInfo.id))
+
+            const sigRestore = sign(restoreTx.hash, node.peerInfo.id.privKey.marshal())
+            return cb(null, Buffer.concat([sigRestore.signature, numberToBuffer(sigRestore.recovery, 1)], SIGNATURE_LENGTH + 1))
         })
     }),
     pull.filter(data => Buffer.isBuffer(data)),
