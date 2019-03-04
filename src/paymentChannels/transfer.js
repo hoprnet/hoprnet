93c77419a4df7adb7c7372081e20e1f6ec7e7487
Robert Kiel <robert.kiel@validitylabs.org>
2019-03-04 10:25:30 +0100

    ✨adding a FIFO queue to process transaction and channel opening in order

--- src/paymentChannels/transfer.js
+++ src/paymentChannels/transfer.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { waterfall } = require('neo-async')
+const { waterfall, queue } = require('neo-async')
 const BN = require('bn.js')
 
 const { isPartyA, getId, pubKeyToEthereumAddress, bufferToNumber, numberToBuffer, deepCopy } = require('../utils')
@@ -8,64 +8,84 @@ const { isPartyA, getId, pubKeyToEthereumAddress, bufferToNumber, numberToBuffer
 const Transaction = require('../transaction')
 
 module.exports = (self) => {
+    const queues = new Map()
 
+    const foo = (options, cb) => waterfall([
+        (cb) => self.getChannel(options.channelId, cb),
+        (record, cb) => {
+            if (typeof record === 'function') {
+                cb = record
+                record = null
+            }
+
+            if (record)
+                return cb(null, record)
+
+            self.open(options.to, cb)
+
+        },
+        (record, cb) => {
+            const currentValue = new BN(record.currentValue)
+
+            const partyA = isPartyA(
+                pubKeyToEthereumAddress(self.node.peerInfo.id.pubKey.marshal()),
+                pubKeyToEthereumAddress(options.to.pubKey.marshal()))
+
+            if (partyA) {
+                currentValue.isub(options.amount)
+                if (currentValue.isNeg())
+                    return cb(Error(`Insufficient funds. Please equip the payment channel with at least ${currentValue.abs().toString()} additional wei.`))
+            } else {
+                currentValue.iadd(options.amount)
+
+                const totalBalance = new BN(record.totalBalance)
+                if (currentValue.gt(totalBalance))
+                    return cb(Error(`Insufficient funds. Please equip the payment channel with at least ${currentValue.sub(totalBalance).toString()} additional wei.`))
+            }
+
+            const newTx = deepCopy(record.tx, Transaction)
+
+            newTx.value = currentValue.toBuffer('be', Transaction.VALUE_LENGTH)
+            newTx.index = numberToBuffer(bufferToNumber(record.index) + 1, Transaction.INDEX_LENGTH)
+            newTx.sign(self.node.peerInfo.id)
+
+            self.setChannel({
+                index: newTx.index,
+                currentValue: newTx.value
+            }, { channelId: options.channelId }, (err) => {
+                if (err)
+                    return cb(err)
+
+                options.done(null, newTx)
+
+                setImmediate(cb)
+            })
+        }
+    ], cb)
 
     return (amount, to, cb) => {
         const channelId = getId(
             pubKeyToEthereumAddress(self.node.peerInfo.id.pubKey.marshal()),
             pubKeyToEthereumAddress(to.pubKey.marshal())
         )
-    
-        waterfall([
-            (cb) => self.getChannel(channelId, cb),
-            (record, cb) => {
-                if (typeof record === 'function') {
-                    cb = record
-                    record = null
-                }
-    
-                if (record) 
-                    return cb(null, record)
-                    
-                self.open(to, cb)
-                
-            },
-            (record, cb) => {
-                const currentValue = new BN(record.currentValue)
-    
-                const partyA = isPartyA(
-                    pubKeyToEthereumAddress(self.node.peerInfo.id.pubKey.marshal()),
-                    pubKeyToEthereumAddress(to.pubKey.marshal()))
-    
-                if (partyA) {
-                    currentValue.isub(amount)
-                    if (currentValue.isNeg())
-                        return cb(Error(`Insufficient funds. Please equip the payment channel with at least ${currentValue.abs().toString()} additional wei.`))
-                } else {
-                    currentValue.iadd(amount)
-    
-                    const totalBalance = new BN(record.totalBalance)
-                    if (currentValue.gt(totalBalance))
-                        return cb(Error(`Insufficient funds. Please equip the payment channel with at least ${currentValue.sub(totalBalance).toString()} additional wei.`))
-                }
-    
-                const newTx = deepCopy(record.tx, Transaction)
-    
-                newTx.value = currentValue.toBuffer('be', Transaction.VALUE_LENGTH)
-    
-                newTx.index = numberToBuffer(bufferToNumber(record.index) + 1, Transaction.INDEX_LENGTH)
-                newTx.sign(self.node.peerInfo.id)
-    
-                self.setChannel({
-                    index: newTx.index,
-                    currentValue: newTx.value
-                }, channelId, (err) => {
-                    if (err)
-                        return cb(err)
-    
-                    return cb(null, newTx)
-                })
-            }
-        ], cb)
+
+        let pendingJobs = queues.get(channelId.toString('base64'))
+        if (!pendingJobs) {
+            pendingJobs = queue(foo)
+            pendingJobs.push({
+                amount: amount,
+                to: to,
+                channelId: channelId,
+                done: cb
+            })
+            queues.set(channelId, pendingJobs)
+        } else {
+            pendingJobs.push({
+                amount: amount,
+                to: to,
+                channelId: channelId,
+                done: cb
+            })
+        }
     }
-} 
+}
