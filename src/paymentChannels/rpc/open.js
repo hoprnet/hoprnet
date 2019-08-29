b9f70d48cd8818fa745a16e707aa0abd74852a22
Robert Kiel <robert.kiel@validitylabs.org>
2019-08-29 10:39:35 +0200

    🐛 use timeout and proper handling of connection errors

--- src/paymentChannels/rpc/open.js
+++ src/paymentChannels/rpc/open.js
@@ -3,6 +3,8 @@
 const pull = require('pull-stream')
 const lp = require('pull-length-prefixed')
 
+const chalk = require('chalk')
+
 const { randomBytes } = require('crypto')
 const { toWei } = require('web3-utils')
 const BN = require('bn.js')
@@ -12,10 +14,17 @@ const { bufferToNumber, numberToBuffer } = require('../../utils')
 const { PROTOCOL_PAYMENT_CHANNEL } = require('../../constants')
 const Transaction = require('../../transaction')
 
+const TWENTY_SECONDS = 20 * 1000
+
 module.exports = self => to =>
     new Promise(async (resolve, reject) => {
         let restoreTx, conn
 
+        const timeout = setTimeout(() =>
+            reject(
+                Error(`Unable to open a payment channel because counterparty ${chalk.blue(to.toB58String())} is not answering with an appropriate response.`)
+            ), TWENTY_SECONDS
+        )
         try {
             conn = await self.node.peerRouting.findPeer(to).then(peerInfo => self.node.dialProtocol(peerInfo, PROTOCOL_PAYMENT_CHANNEL))
         } catch (err) {
@@ -46,32 +55,37 @@ module.exports = self => to =>
                         .recover(restoreTx.hash, data.slice(0, Transaction.SIGNATURE_LENGTH), bufferToNumber(data.slice(Transaction.SIGNATURE_LENGTH)))
                         .compare(to.pubKey.marshal()) === 0
             ),
-            pull.collect(async (err, signatures) => {
-                if (err) return reject(err)
-
-                if (signatures.length !== 1) return reject(Error(`Invalid response. To: ${to.toB58String()}`))
-
-                restoreTx.signature = signatures[0].slice(0, Transaction.SIGNATURE_LENGTH)
-                restoreTx.recovery = signatures[0].slice(Transaction.SIGNATURE_LENGTH)
+            pull.drain(signature => {
+                restoreTx.signature = signature.slice(0, Transaction.SIGNATURE_LENGTH)
+                restoreTx.recovery = signature.slice(Transaction.SIGNATURE_LENGTH)
 
                 const channelId = restoreTx.getChannelId(self.node.peerInfo.id)
-
-                await self.node.db.put(self.node.paymentChannels.StashedRestoreTransaction(channelId), restoreTx.toBuffer(), { sync: true })
 
                 self.registerSettlementListener(channelId)
                 self.registerOpeningListener(channelId)
 
-                self.once(`opened ${channelId.toString('base64')}`, resolve)
+                let promise
 
-                self.contractCall(
-                    self.contract.methods.createFunded(
-                        restoreTx.nonce,
-                        new BN(restoreTx.value).toString(),
-                        restoreTx.signature.slice(0, 32),
-                        restoreTx.signature.slice(32, 64),
-                        bufferToNumber(restoreTx.recovery) + 27
+                self.once(`opened ${channelId.toString('base64')}`, () => {
+                    clearTimeout(timeout)
+                    resolve(promise)
+                })
+
+                promise = Promise.all([
+                    self.node.db.put(self.node.paymentChannels.StashedRestoreTransaction(channelId), restoreTx.toBuffer(), { sync: true }),
+                    self.contractCall(
+                        self.contract.methods.createFunded(
+                            restoreTx.nonce,
+                            new BN(restoreTx.value).toString(),
+                            restoreTx.signature.slice(0, 32),
+                            restoreTx.signature.slice(32, 64),
+                            bufferToNumber(restoreTx.recovery) + 27
+                        )
                     )
-                )
+                ])
+
+                // Closes the stream
+                return false
             })
         )
     })
