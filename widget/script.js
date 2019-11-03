a5b08e67d6c7d9dbd1099b2ef4dfaa54557946dd
nionis <stivennoni77@gmail.com>
2019-11-03 19:35:12 +0200

    feat/implement-widget-UI

---
+++ widget/script.js
@@ -0,0 +1,36 @@
+let views = null;
+
+const changeTab = name => {
+  // if views object doesn't exist create it
+  if (!views) {
+    views = {
+      stats: {
+        container: document.getElementById("stats-container"),
+        tab: document.getElementById("stats-tab")
+      },
+      stake: {
+        container: document.getElementById("stake-container"),
+        tab: document.getElementById("stake-tab")
+      },
+      votes: {
+        container: document.getElementById("votes-container"),
+        tab: document.getElementById("votes-tab")
+      }
+    };
+  }
+
+  // loop through views and update the styles
+  for (const tabName in views) {
+    const { container, tab } = views[tabName];
+
+    if (tabName === name) {
+      container.classList.remove("hidden");
+      tab.classList.add("tab-active");
+    } else {
+      container.classList.add("hidden");
+      tab.classList.remove("tab-active");
+    }
+  }
+};
+
+const onTabClick = changeTab;
