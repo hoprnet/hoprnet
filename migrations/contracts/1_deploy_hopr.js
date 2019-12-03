8f7ad85df8915e0ae6e95645bb128f30c944bf08
Robert Kiel <robert.kiel@validitylabs.org>
2019-12-03 15:16:32 +0100

    ♻︎ cleanup

--- migrations/contracts/1_deploy_hopr.js
+++ migrations/contracts/1_deploy_hopr.js
@@ -2,7 +2,6 @@
 /**
  * Migration
  */
-const cnf = require('../../config/contract-hopr.json');
 const HoprChannel = artifacts.require('./HoprChannel.sol');
 
 module.exports = function (deployer, network, accounts) { // eslint-disable-line
