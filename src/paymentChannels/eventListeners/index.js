433588d7b19b4b499219e11f9d2ae52469c969d5
Robert Kiel <robert.kiel@validitylabs.org>
2019-12-18 16:30:52 +0100

    ♻︎ move Ethereum connector into own repository

--- src/paymentChannels/eventListeners/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = self => ({
-    closingListener: require('./close')(self),
-    openingListener: require('./open')(self),
-    openedForListener: require('./openedFor')(self)
-})
--- src/paymentChannels/handlers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const registerOpeningRequest = require('./open')
-const registerSettleRequest = require('./settle')
-
-module.exports = (node) => {
-    registerOpeningRequest(node)
-    registerSettleRequest(node)
-}
