a6efaf59bebc3bcfd6b70733f1b63159dbc206d0
Robert Kiel <robert.kiel@validitylabs.org>
2020-01-13 18:38:13 +0100

    ✨ add utils.hash()

--- src/utils.d.ts
+++ src/utils.d.ts
@@ -5,3 +5,5 @@ export function isPartyA(self: AccountId, counterparty: AccountId): boolean
 export function getId(self: AccountId, counterparty: AccountId, api?: any): Promise<Hash>
 
 export function pubKeytToAccountId(pubkey: Uint8Array, ...args: any[]): Promise<AccountId>
+
+export function hash(msg: Uint8Array): Uint8Array
