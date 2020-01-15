30b9b3aeefbe1d04b6859d6375b96665e7df650d
Robert Kiel <robert.kiel@validitylabs.org>
2020-01-15 15:42:49 +0100

    🐛 export static functions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,30 +1,19 @@
 import { LevelUp } from 'levelup'
 import BN from 'bn.js'
 
-import Utils from './utils'
+import IUtils from './utils'
 import Channel from './channel'
-import Constructors, {Types} from './types'
+import Constructors, { Types } from './types'
 
-export { Utils, Types, Channel }
+export { IUtils, Types, Channel }
 
-export default class HoprCoreConnector {
+declare class HoprCoreConnector {
   private constructor(...props: any[])
 
   readonly started: boolean
   readonly self: any
   readonly db: LevelUp
   readonly nonce: Promise<number>
-
-  static readonly utils: Utils
-  static readonly channel: Channel
-  static readonly types: Constructors
-
-  /**
-   * Creates an uninitialised instance.
-   *
-   * @param db database instance
-   */
-  static create(db: LevelUp, keyPair: any, uri?: string): Promise<HoprCoreConnector>
 
   start(): Promise<void>
 
@@ -35,3 +24,15 @@ export default class HoprCoreConnector {
   checkFreeBalance(newBalance: any): Promise<void>
 }
 
+declare namespace HoprCoreConnector {
+  /**
+   * Creates an uninitialised instance.
+   *
+   * @param db database instance
+   */
+  function create(db: LevelUp, keyPair: any, uri?: string): Promise<HoprCoreConnector>
+
+  const Utils: IUtils
+  const channel: Channel
+  const types: Constructors
+}
