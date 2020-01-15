0fbd4daf41b88a1ec7b95a06867e37810e5c9d2e
Robert Kiel <robert.kiel@validitylabs.org>
2020-01-15 11:53:21 +0100

    ♻︎ properly specify interface

--- src/utils.d.ts
+++ src/utils.d.ts
@@ -1,9 +1,8 @@
 import Types from './types'
 
 export default interface Utils {
-  isPartyA: (self: Types['AccountId'], counterparty: Types['AccountId']) => boolean
-  getId: (self: Types['AccountId'], counterparty: Types['AccountId'], ...props: any[]) => Promise<Types['Hash']>
-
-  pubKeyToAccountId: (pubkey: Uint8Array, ...args: any[]) => Promise<Types['AccountId']>
-  hash: (msg: Uint8Array) => Promise<Types['Hash']>
+  isPartyA(self: Types['AccountId'], counterparty: Types['AccountId']): boolean
+  getId(self: Types['AccountId'], counterparty: Types['AccountId'], ...props: any[]): Promise<Types['Hash']>
+  pubKeyToAccountId(pubkey: Uint8Array, ...args: any[]): Promise<Types['AccountId']>
+  hash(msg: Uint8Array): Promise<Types['Hash']>
 }
