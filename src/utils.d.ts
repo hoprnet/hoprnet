b5057e45ec407ba3956e6c61f7c6fbfd562aaed7
Robert Kiel <robert.kiel@validitylabs.org>
2020-01-16 15:23:05 +0100

    ✨ add signature functions

--- src/utils.d.ts
+++ src/utils.d.ts
@@ -5,6 +5,11 @@ type Utils = {
   getId(self: TypeClasses.AccountId, counterparty: TypeClasses.AccountId, ...props: any[]): Promise<TypeClasses.Hash>
   pubKeyToAccountId(pubkey: Uint8Array, ...args: any[]): Promise<TypeClasses.AccountId>
   hash(msg: Uint8Array): Promise<TypeClasses.Hash>
+  sign(msg: Uint8Array, privKey: Uint8Array): Promise<{
+    signature: Uint8Array,
+    recovery: number
+  }>
+  verify(msg: Uint8Array, signature: Uint8Array, pubkey: Uint8Array): Promise<boolean>
 }
 
 export default Utils
