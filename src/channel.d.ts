851e751f5c67c75008b42258045c9bf6a8468142
Robert Kiel <robert.kiel@validitylabs.org>
2020-01-17 11:33:43 +0100

    🎨 add documentation and more appropriate variable name

--- src/channel.d.ts
+++ src/channel.d.ts
@@ -15,23 +15,66 @@ export interface ChannelClass {
 
   readonly currentBalanceOfCounterparty: Promise<TypeClasses.Balance>
 
+  /**
+   * Creates a ticket.
+   * @param secretKey the private key of the issuer
+   * @param amount how many funds are included
+   * @param challenge the challenge that need to resolved to redeem the ticket
+   * @param winProb the winning probability
+   */
   createTicket(secretKey: Uint8Array, amount: TypeClasses.Balance, challenge: TypeClasses.Hash, winProb: TypeClasses.Hash): Promise<TypeClasses.SignedTicket>
 
+  /**
+   * Check `signedTicket` for its validity.
+   * @param signedTicket the ticket to check
+   */
   verifyTicket(signedTicket: TypeClasses.SignedTicket): Promise<boolean>
 
+  /**
+   * Initiates a settlement for this channel.
+   */
   initiateSettlement(): Promise<void>
 
+  /**
+   * Submits a signed to the blockchain.
+   * @param signedTicket a signed ticket
+   */
   submitTicket(signedTicket: TypeClasses.SignedTicket): Promise<void>
 
+  /**
+   * Fetches all unresolved, previous challenges from the database that
+   * have occured in this channel.
+   */
   getPreviousChallenges(): Promise<TypeClasses.Hash>
 }
 
 export default interface Channel {
-  fromDatabase(props: any): Promise<ChannelClass>
+  /**
+   * Creates a Channel instance from the database.
+   * @param props additional arguments
+   */
+  create(...props: any[]): Promise<ChannelClass>
 
+  /**
+   * Opens a new payment channel and initializes the on-chain data.
+   * @param amount how much should be staked
+   * @param signature a signature over channel state
+   * @param props additional arguments
+   */
   open(amount: TypeClasses.Balance, signature: Promise<Uint8Array>, ...props: any[]): Promise<ChannelClass>
 
-  getAllChannels<T, R>(onData: (channelId: TypeClasses.Hash, state: TypeClasses.State, ...props: any[]) => T, onEnd: (promises: Promise<T>[]) => R): Promise<R>
+  /**
+   * Fetches all channel instances from the database and applies first `onData` and
+   * then `onEnd` on the received nstances.
+   * @param onData applied on all channel instances
+   * @param onEnd composes at the end the received data
+   */
+  getAllChannels<T, R>(onData: (channel: ChannelClass, ...props: any[]) => T, onEnd: (promises: Promise<T>[]) => R): Promise<R>
 
+  /**
+   * Fetches all channel instances from the database and initiates a settlement on
+   * each of them.
+   * @param props additional arguments
+   */
   closeChannels(...props: any[]): Promise<TypeClasses.Balance>
 }
