4ed487b4a9679609a460043cf6ac500057e4d446
nionis <stivennoni77@gmail.com>
2020-03-18 23:27:37 +0200

    🔧 remove minting in migrations

--- migrations/1_hopr_token.ts
+++ migrations/1_hopr_token.ts
@@ -1,14 +1,5 @@
 const HoprToken = artifacts.require("HoprToken");
 
-module.exports = (async (deployer, network, [owner]) => {
+module.exports = (async deployer => {
   await deployer.deploy(HoprToken);
-
-  if (network === "development") {
-    const hoprToken = await HoprToken.deployed();
-
-    // mint tokens for owner
-    await hoprToken.mint(owner, web3.utils.toWei("100", "ether"), {
-      from: owner
-    });
-  }
 }) as Truffle.Migration;
