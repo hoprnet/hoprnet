ecea978901a863c072e670f81990a0a43618541c
nionis <stivennoni77@gmail.com>
2020-03-26 19:29:30 +0200

    🔧 experiment with postinstall

--- scripts/typechain.ts
+++ scripts/typechain.ts
@@ -4,13 +4,14 @@ import { TypeChain } from 'typechain/dist/TypeChain'
 
 async function main() {
   const cwd = path.join(__dirname, '..')
+  console.log(cwd)
 
   await tsGenerator(
     { cwd },
     new TypeChain({
       cwd,
       rawConfig: {
-        files: './node_modules/@hoprnet/hopr-ethereum/build/extracted/abis/*.json',
+        files: path.join(root, 'node_modules/@hoprnet/hopr-ethereum/build/extracted/abis/*.json'),
         outDir: './src/tsc/web3',
         target: 'web3-v1'
       }
