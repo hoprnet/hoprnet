2f028393940b250cf54a2b6e4923fcde117f0c9f
nionis <stivennoni77@gmail.com>
2020-04-13 18:24:18 +0300

    ♻️ use hopr-utils

--- migrations/2_hopr_channels.ts
+++ migrations/2_hopr_channels.ts
@@ -1,11 +1,11 @@
-// import { time } from "@openzeppelin/test-helpers";
-// using @openzeppelin/test-helpers "time" causes tests to fail
+import {durations} from "@hoprnet/hopr-utils";
+
 const HoprChannels = artifacts.require("HoprChannels");
 const HoprToken_3 = artifacts.require("HoprToken");
 
 module.exports = (async deployer => {
   const token = await HoprToken_3.deployed();
-  const secsClosure = 172800; // 2 days
+  const secsClosure = durations.days(2) / 1e3;
 
   await deployer.deploy(HoprChannels, token.address, secsClosure);
 }) as Truffle.Migration;
--- migrations/3_hopr_minter.ts
+++ migrations/3_hopr_minter.ts
@@ -1,16 +1,12 @@
-// import { time } from "@openzeppelin/test-helpers";
-// using @openzeppelin/test-helpers "time" causes tests to fail
+import {durations} from "@hoprnet/hopr-utils";
+
 const HoprMinter = artifacts.require("HoprMinter");
 const HoprToken_4 = artifacts.require("HoprToken");
 
 module.exports = (async deployer => {
   const token = await HoprToken_4.deployed();
   const maxAmount = web3.utils.toWei("100000000", "ether");
-  const duration = web3.utils
-    .toBN("365")
-    .mul(web3.utils.toBN("24"))
-    .mul(web3.utils.toBN("60"))
-    .mul(web3.utils.toBN("60"));
+  const duration = durations.days(365) / 1e3;
 
   await deployer.deploy(HoprMinter, token.address, maxAmount, duration);
 }) as Truffle.Migration;
