c5b815e7152d6fcace06a80924ef6381693305f4
nionis <stivennoni77@gmail.com>
2020-04-14 11:56:40 +0300

    ✨ print native balance

--- chat/commands/printBalance.ts
+++ chat/commands/printBalance.ts
@@ -2,24 +2,36 @@ import type HoprCoreConnector from '@hoprnet/hopr-core-connector-interface'
 import type Hopr from '../../src'
 import type AbstractCommand from './abstractCommand'
 
+import { moveDecimalPoint } from '@hoprnet/hopr-utils'
 import chalk from 'chalk'
-import BN from 'bn.js'
 
 export default class PrintBalance implements AbstractCommand {
-    constructor(public node: Hopr<HoprCoreConnector>) { }
+  constructor(public node: Hopr<HoprCoreConnector>) {}
 
-    /**
-     * Prints the balance of our account.
-     * @notice triggered by the CLI
-     */
-    async execute(): Promise<void> {
-        console.log(
-            `Account Balance: ${
-                chalk.magenta((await this.node.paymentChannels.accountBalance).div(new BN(10).pow(new BN(this.node.paymentChannels.types.Balance.DECIMALS))).toString())} ${this.node.paymentChannels.types.Balance.SYMBOL}`
-        )
-    }
+  /**
+   * Prints the balance of our account.
+   * @notice triggered by the CLI
+   */
+  async execute(): Promise<void> {
+    const { paymentChannels } = this.node
+    const { Balance, NativeBalance } = paymentChannels.types
 
-    complete(line: string, cb: (err: Error | undefined, hits: [string[], string]) => void): void {
-        cb(undefined, [[''], line])
-    }
+    const balance = await paymentChannels.accountBalance.then(b => {
+      return moveDecimalPoint(b.toString(), Balance.DECIMALS * -1)
+    })
+    const nativeBalance = await paymentChannels.accountNativeBalance.then(b => {
+      return moveDecimalPoint(b.toString(), NativeBalance.DECIMALS * -1)
+    })
+
+    console.log(
+      [
+        `Account Balance: ${chalk.magenta(balance)} ${Balance.SYMBOL}`,
+        `Account Native Balance: ${chalk.magenta(nativeBalance)} ${NativeBalance.SYMBOL}`,
+      ].join('\n')
+    )
+  }
+
+  complete(line: string, cb: (err: Error | undefined, hits: [string[], string]) => void): void {
+    cb(undefined, [[''], line])
+  }
 }
