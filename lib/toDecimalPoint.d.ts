de97c949a90e35057bfce55c91bc06502e52e637
nionis <stivennoni77@gmail.com>
2020-04-14 11:04:10 +0300

    🐛 improve toDecimalPoint

--- lib/toDecimalPoint.d.ts
+++ lib/toDecimalPoint.d.ts
@@ -1,2 +1,2 @@
 import BigNumber from 'bignumber.js';
-export declare const toDecimalPoint: (amount: BigNumber.Value, decimalPoint: number) => string;
+export declare const toDecimalPoint: (amount: string | number | BigNumber, decimalPoint: number) => string;
--- src/toDecimalPoint.ts
+++ src/toDecimalPoint.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js'
 
-export const toDecimalPoint = (amount: ConstructorParameters<typeof BigNumber>[0], decimalPoint: number): string => {
+export const toDecimalPoint = (amount: BigNumber | string | number, decimalPoint: number): string => {
   return new BigNumber(amount).multipliedBy(new BigNumber(10).pow(decimalPoint)).toString()
 }
