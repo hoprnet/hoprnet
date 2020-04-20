c78bd2967522f6bf133b51a28423d453b2acae2a
robertkiel <robert.kiel@validitylabs.org>
2020-04-20 19:28:04 +0200

    ✨ -v option

--- chat/index.ts
+++ chat/index.ts
@@ -27,28 +27,29 @@ const SPLIT_OPERAND_QUERY_REGEX: RegExp = /([\w\-]+)(?:\s+)?([\w\s\-.]+)?/
 
 // Allowed keywords
 export const keywords: string[][] = [
-    ['open', 'opens a payment channel'],
-    ['send', 'sends a message to another party'],
-    ['quit', 'stops the node and terminates the process'],
-    ['crawl', 'crawls the network and tries to find other nodes'],
-    ['openChannels', 'lists all currently open channels'],
-    ['closeAll', 'closes all payment channel of this node'],
-    ['myAddress', 'shows the address of this node'],
-    ['balance', 'shows our current balance'],
-    ['listConnectors', 'lists all installed blockchain connectors'],
-    ['ping', 'pings another node to check its availability'],
-    ['help', 'shows this help page']
-].sort((a, b) => a[0].localeCompare(b[0], 'en', {sensitivity: 'base'}))
+  ['open', 'opens a payment channel'],
+  ['send', 'sends a message to another party'],
+  ['quit', 'stops the node and terminates the process'],
+  ['crawl', 'crawls the network and tries to find other nodes'],
+  ['openChannels', 'lists all currently open channels'],
+  ['closeAll', 'closes all payment channel of this node'],
+  ['myAddress', 'shows the address of this node'],
+  ['balance', 'shows our current balance'],
+  ['listConnectors', 'lists all installed blockchain connectors'],
+  ['ping', 'pings another node to check its availability'],
+  ['help', 'shows this help page'],
+].sort((a, b) => a[0].localeCompare(b[0], 'en', { sensitivity: 'base' }))
 
 // Allowed CLI options
 export const cli_options: string[][] = [
-    ['-b', '--bootstrapNode, bootstrap', 'starts HOPR as a bootstrap node'],
-    ['-n <connector>', '--network', 'starts HOPR with blockchain connector <connector>'],
-    ['-h', '--help', 'shows this help page'],
-    ['-l', '--listConnectors', 'shows all available connectors'],
-    ['-p <password>', '--password', 'start HOPR with <password>'],
-    ['<ID>', undefined, 'starts HOPR with a demo ID']
-].sort((a, b) => a[0].localeCompare(b[0], 'en', {sensitivity: 'base'}))
+  ['-b', '--bootstrapNode, bootstrap', 'starts HOPR as a bootstrap node'],
+  ['-n <connector>', '--network', 'starts HOPR with blockchain connector <connector>'],
+  ['-h', '--help', 'shows this help page'],
+  ['-l', '--listConnectors', 'shows all available connectors'],
+  ['-p <password>', '--password', 'start HOPR with <password>'],
+  ['-v', '--verbose', 'show debug info'],
+  ['<ID>', undefined, 'starts HOPR with a demo ID'],
+].sort((a, b) => a[0].localeCompare(b[0], 'en', { sensitivity: 'base' }))
 
 // Name our process 'hopr'
 process.title = 'hopr'
@@ -57,9 +58,9 @@ process.title = 'hopr'
  * Alphabetical list of known connectors.
  */
 export const knownConnectors = [
-    /* prettier-ignore */
-    ['@hoprnet/hopr-core-ethereum', 'ethereum'],
-    ['@hoprnet/hopr-core-polkadot', 'polkadot']
+  /* prettier-ignore */
+  ['@hoprnet/hopr-core-ethereum', 'ethereum'],
+  ['@hoprnet/hopr-core-polkadot', 'polkadot'],
 ]
 
 let node: Hopr<HoprCoreConnector>
@@ -71,181 +72,183 @@ let node: Hopr<HoprCoreConnector>
  * @param cb to returns the suggestions
  */
 function tabCompletion(commands: Commands) {
-    return async (line: string, cb: (err: Error | undefined, hits: [string[], string]) => void) => {
-        if (line == null || line == '') {
-            return cb(undefined, [keywords.map(entry => entry[0]), line])
-        }
-
-        const [command, query]: (string | undefined)[] = line
-            .trim()
-            .split(SPLIT_OPERAND_QUERY_REGEX)
-            .slice(1)
-
-        if (command == null || command === '') {
-            return cb(undefined, [keywords.map(entry => entry[0]), line])
-        }
-
-        switch (command.trim()) {
-            case 'send':
-                await commands.sendMessage.complete(line, cb, query)
-                break
-            case 'open':
-                await commands.openChannel.complete(line, cb, query)
-                break
-            case 'close':
-                await commands.closeChannel.complete(line, cb, query)
-                break
-            case 'ping': {
-                await commands.ping.complete(line, cb, query)
-            }
-            default:
-                const hits = keywords.reduce((acc: string[], keyword: [string, string]) => {
-                    if (keyword[0].startsWith(line)) {
-                        acc.push(keyword[0])
-                    }
-
-                    return acc
-                }, [])
-
-                return cb(undefined, [hits.length ? hits : keywords.map(keyword => keyword[0]), line])
-        }
+  return async (line: string, cb: (err: Error | undefined, hits: [string[], string]) => void) => {
+    if (line == null || line == '') {
+      return cb(undefined, [keywords.map(entry => entry[0]), line])
     }
+
+    const [command, query]: (string | undefined)[] = line
+      .trim()
+      .split(SPLIT_OPERAND_QUERY_REGEX)
+      .slice(1)
+
+    if (command == null || command === '') {
+      return cb(undefined, [keywords.map(entry => entry[0]), line])
+    }
+
+    switch (command.trim()) {
+      case 'send':
+        await commands.sendMessage.complete(line, cb, query)
+        break
+      case 'open':
+        await commands.openChannel.complete(line, cb, query)
+        break
+      case 'close':
+        await commands.closeChannel.complete(line, cb, query)
+        break
+      case 'ping': {
+        await commands.ping.complete(line, cb, query)
+      }
+      default:
+        const hits = keywords.reduce((acc: string[], keyword: [string, string]) => {
+          if (keyword[0].startsWith(line)) {
+            acc.push(keyword[0])
+          }
+
+          return acc
+        }, [])
+
+        return cb(undefined, [hits.length ? hits : keywords.map(keyword => keyword[0]), line])
+    }
+  }
 }
 
 async function runAsRegularNode() {
-    const commands = new Commands(node)
+  const commands = new Commands(node)
 
-    let rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        completer: tabCompletion(commands)
-    })
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    completer: tabCompletion(commands),
+  })
 
-    rl.on('SIGINT', async () => {
-        const question = `Are you sure you want to exit? (${chalk.green('y')}, ${chalk.red('N')}): `
+  rl.on('SIGINT', async () => {
+    const question = `Are you sure you want to exit? (${chalk.green('y')}, ${chalk.red('N')}): `
 
-        const answer = await new Promise<string>(resolve => rl.question(question, resolve))
+    const answer = await new Promise<string>(resolve => rl.question(question, resolve))
 
-        if (answer.match(/^y(es)?$/i)) {
-            clearString(question, rl)
-            await commands.stopNode.execute()
-            return
-        } 
-        rl.prompt()
-    });
+    if (answer.match(/^y(es)?$/i)) {
+      clearString(question, rl)
+      await commands.stopNode.execute()
+      return
+    }
+    rl.prompt()
+  })
 
-    rl.once('close', async () => {
+  rl.once('close', async () => {
+    await commands.stopNode.execute()
+    return
+  })
+
+  console.log(`Connecting to bootstrap node${node.bootstrapServers.length == 1 ? '' : 's'}...`)
+
+  rl.on('line', async (input: string) => {
+    if (input == null || input == '') {
+      console.log(chalk.red('Unknown command!'))
+      rl.prompt()
+      return
+    }
+
+    const [command, query]: (string | undefined)[] = input
+      .trim()
+      .split(SPLIT_OPERAND_QUERY_REGEX)
+      .slice(1)
+
+    if (command == null) {
+      console.log(chalk.red('Unknown command!'))
+      rl.prompt()
+      return
+    }
+
+    switch (command.trim()) {
+      case 'balance':
+        await commands.printBalance.execute()
+        break
+      case 'close':
+        await commands.closeChannel.execute(query)
+        break
+      case 'crawl':
+        await commands.crawl.execute()
+        break
+      case 'help':
+        commands.listCommands.execute()
+        break
+      case 'quit':
         await commands.stopNode.execute()
-        return
-    })
+        break
+      case 'openChannels':
+        await commands.listOpenChannels.execute()
+        break
+      case 'open':
+        await commands.openChannel.execute(rl, query)
+        break
+      case 'send':
+        await commands.sendMessage.execute(rl, query)
+        break
 
-    console.log(`Connecting to bootstrap node${node.bootstrapServers.length == 1 ? '' : 's'}...`)
-
-    rl.on('line', async (input: string) => {
-        if (input == null || input == '') {
-            console.log(chalk.red('Unknown command!'))
-            rl.prompt()
-            return
-        }
-
-        const [command, query]: (string | undefined)[] = input
-            .trim()
-            .split(SPLIT_OPERAND_QUERY_REGEX)
-            .slice(1)
-
-        if (command == null) {
-            console.log(chalk.red('Unknown command!'))
-            rl.prompt()
-            return
-        }
-
-        switch (command.trim()) {
-            case 'balance':
-                await commands.printBalance.execute()
-                break
-            case 'close':
-                await commands.closeChannel.execute(query)
-                break
-            case 'crawl':
-                await commands.crawl.execute()
-                break
-            case 'help':
-                commands.listCommands.execute()
-                break
-            case 'quit':
-                await commands.stopNode.execute()
-                break
-            case 'openChannels':
-                await commands.listOpenChannels.execute()
-                break
-            case 'open':
-                await commands.openChannel.execute(rl, query)
-                break
-            case 'send':
-                await commands.sendMessage.execute(rl, query)
-                break
-
-            case 'listConnectors':
-                await commands.listConnectors.execute()
-                break
-            case 'myAddress':
-                await commands.printAddress.execute()
-                break
-            case 'ping':
-                await commands.ping.execute(query)
-                break
-            default:
-                console.log(chalk.red('Unknown command!'))
-                break
-        }
-
-        rl.prompt()
-    })
+      case 'listConnectors':
+        await commands.listConnectors.execute()
+        break
+      case 'myAddress':
+        await commands.printAddress.execute()
+        break
+      case 'ping':
+        await commands.ping.execute(query)
+        break
+      default:
+        console.log(chalk.red('Unknown command!'))
+        break
+    }
 
     rl.prompt()
+  })
+
+  rl.prompt()
 }
 
-
-
 function runAsBootstrapNode() {
-    console.log(`... running as bootstrap node!.`)
+  console.log(`... running as bootstrap node!.`)
 
-    node.on('peer:connect', (peer: PeerInfo) => {
-        console.log(`Incoming connection from ${chalk.blue(peer.id.toB58String())}.`)
-    })
+  node.on('peer:connect', (peer: PeerInfo) => {
+    console.log(`Incoming connection from ${chalk.blue(peer.id.toB58String())}.`)
+  })
 }
 
 async function main() {
-    clear()
-    console.log(
-        figlet.textSync('HOPRnet.eth', {
-            horizontalLayout: 'fitted'
-        })
-    )
-    console.log(`Welcome to ${chalk.bold('HOPR')}!\n`)
+  clear()
+  console.log(
+    figlet.textSync('HOPRnet.eth', {
+      horizontalLayout: 'fitted',
+    })
+  )
+  console.log(`Welcome to ${chalk.bold('HOPR')}!\n`)
 
-    let options: HoprOptions 
-    try {
-        options = await parseOptions()
-    } catch (err) {
-        console.log(err.message + '\n')
-        return
-    }
+  let options: HoprOptions
+  try {
+    options = await parseOptions()
+  } catch (err) {
+    console.log(err.message + '\n')
+    return
+  }
 
-    try {
-        node = await Hopr.createNode(options)
-    } catch (err) {
-        console.log(chalk.red(err.message))
-        process.exit(1)
-    }
+  try {
+    node = await Hopr.createNode(options)
+  } catch (err) {
+    console.log(chalk.red(err.message))
+    process.exit(1)
+  }
 
-    console.log(`\nAvailable under the following addresses:\n ${node.peerInfo.multiaddrs.toArray().join('\n ')}\n`)
+  console.log(
+    `\nAvailable under the following addresses:\n ${node.peerInfo.multiaddrs
+      .toArray()
+      .join('\n ')}\n`
+  )
 
-    if (options.bootstrapNode) {
-        runAsBootstrapNode()
-    } else {
-        runAsRegularNode()
-    }
+  if (options.bootstrapNode) {
+    runAsBootstrapNode()
+  } else {
+    runAsRegularNode()
+  }
 }
 
 main()
--- chat/utils/parseOptions.ts
+++ chat/utils/parseOptions.ts
@@ -23,139 +23,160 @@ const listConnectors = new ListConnctor()
  * @returns
  */
 export async function parseOptions(): Promise<HoprOptions> {
-    const unknownOptions: string[] = []
+  const unknownOptions: string[] = []
 
-    let cli_options = getopts(process.argv.slice(2), {
-        boolean: ['bootstrapNode', 'help', 'listConnectors'],
-        string: ['network', 'password'],
-        alias: {
-            l: 'listConnectors',
-            p: 'password',
-            bootstrap: 'bootstrapNode',
-            b: 'bootstrapNode',
-            h: 'help',
-            n: 'network'
-        },
-        default: {
-            network: 'ethereum',
-            bootstrapNode: false
-        },
-        unknown: (option: string) => {
-            unknownOptions.push(option)
-            return false
-        }
-    })
+  let cli_options = getopts(process.argv.slice(2), {
+    boolean: ['bootstrapNode', 'help', 'listConnectors', 'verbose'],
+    string: ['network', 'password'],
+    alias: {
+      l: 'listConnectors',
+      p: 'password',
+      bootstrap: 'bootstrapNode',
+      b: 'bootstrapNode',
+      h: 'help',
+      n: 'network',
+      v: 'verbose',
+    },
+    default: {
+      network: 'ethereum',
+      bootstrapNode: false,
+    },
+    unknown: (option: string) => {
+      unknownOptions.push(option)
+      return false
+    },
+  })
 
-    if (cli_options._.length > 1) {
-        console.log(`Found more than the allowed options. Got ${chalk.yellow(cli_options._.join(', '))}\n`)
-        displayHelp()
-        process.exit(0)
-    }
+  if (cli_options._.length > 1) {
+    console.log(
+      `Found more than the allowed options. Got ${chalk.yellow(cli_options._.join(', '))}\n`
+    )
+    displayHelp()
+    process.exit(0)
+  }
 
-    let id: number
-    for (let i = 0; i < cli_options._.length; i++) {
-        try {
-            const int = parseInt(cli_options._[i])
-
-            if (isFinite(int)) {
-                id = int
-            }
-        } catch { 
-            console.log(chalk.yellow(`Got unknown option '${cli_options._[i]}'.`))
-            displayHelp()
-            process.exit(0)
-        }
-    }
-
-
-
-    if (unknownOptions.length > 0) {
-        console.log(chalk.yellow(`Got unknown option${unknownOptions.length == 1 ? '' : 's'} [${unknownOptions.join(', ')}]\n`))
-        displayHelp()
-        process.exit(0)
-    }
-
-    if (cli_options.help) {
-        displayHelp()
-        process.exit(0)
-    }
-
-    if (cli_options.listConnectors) {
-        await listConnectors.execute()
-        process.exit()
-    }
-
-
-
-    if (!knownConnectors.some(connector => connector[1] == cli_options.network)) {
-        console.log(`Unknown network! <${chalk.red(cli_options.network)}>\n`)
-        await listConnectors.execute()
-        return
-    }
-
-    let connector: typeof HoprCoreConnector
+  let id: number
+  for (let i = 0; i < cli_options._.length; i++) {
     try {
-        connector = (await import(`@hoprnet/hopr-core-${cli_options.network}`)).default as typeof HoprCoreConnector
-    } catch (err) {
-        console.log(`Could not find <${chalk.red(`@hoprnet/hopr-core-${cli_options.network}`)}>. Please make sure it is available under ./node_modules!\n`)
-        await listConnectors.execute()
-        return
+      const int = parseInt(cli_options._[i])
+
+      if (isFinite(int)) {
+        id = int
+      }
+    } catch {
+      console.log(chalk.yellow(`Got unknown option '${cli_options._[i]}'.`))
+      displayHelp()
+      process.exit(0)
+    }
+  }
+
+  if (unknownOptions.length > 0) {
+    console.log(
+      chalk.yellow(
+        `Got unknown option${unknownOptions.length == 1 ? '' : 's'} [${unknownOptions.join(
+          ', '
+        )}]\n`
+      )
+    )
+    displayHelp()
+    process.exit(0)
+  }
+
+  if (cli_options.verbose) {
+    require('debug').enable('*')
+  }
+
+  if (cli_options.help) {
+    displayHelp()
+    process.exit(0)
+  }
+
+  if (cli_options.listConnectors) {
+    await listConnectors.execute()
+    process.exit()
+  }
+
+  if (!knownConnectors.some(connector => connector[1] == cli_options.network)) {
+    console.log(`Unknown network! <${chalk.red(cli_options.network)}>\n`)
+    await listConnectors.execute()
+    return
+  }
+
+  let connector: typeof HoprCoreConnector
+  try {
+    connector = (await import(`@hoprnet/hopr-core-${cli_options.network}`))
+      .default as typeof HoprCoreConnector
+  } catch (err) {
+    console.log(
+      `Could not find <${chalk.red(
+        `@hoprnet/hopr-core-${cli_options.network}`
+      )}>. Please make sure it is available under ./node_modules!\n`
+    )
+    await listConnectors.execute()
+    return
+  }
+
+  if (!cli_options.bootstrapNode && process.env.BOOTSTRAP_SERVERS == null) {
+    console.log(chalk.red('Cannot start HOPR without a bootstrap node'))
+  }
+
+  const bootstrapAddresses = process.env.BOOTSTRAP_SERVERS.split(',')
+
+  if (bootstrapAddresses.length == 0) {
+    console.log(chalk.red('Invalid bootstrap servers. Cannot start HOPR without a bootstrap node'))
+  }
+
+  let addr: Multiaddr,
+    bootstrapServerMap = new Map<string, PeerInfo>()
+  for (let i = 0; i < bootstrapAddresses.length; i++) {
+    addr = Multiaddr(bootstrapAddresses[i].trim())
+
+    let peerInfo = bootstrapServerMap.get(addr.getPeerId())
+    if (peerInfo == null) {
+      peerInfo = await PeerInfo.create(PeerId.createFromB58String(addr.getPeerId()))
     }
 
-    if (!cli_options.bootstrapNode && process.env.BOOTSTRAP_SERVERS == null) {
-        console.log(chalk.red('Cannot start HOPR without a bootstrap node'))
-    }
+    peerInfo.multiaddrs.add(addr)
+    bootstrapServerMap.set(addr.getPeerId(), peerInfo)
+  }
 
-    const bootstrapAddresses = process.env.BOOTSTRAP_SERVERS.split(',')
+  if (process.env[`${cli_options.network.toUpperCase()}_PROVIDER`] == null) {
+    throw Error(
+      `Could not find any connector for ${chalk.magenta(
+        cli_options.network
+      )}. Please specify ${chalk.yellow(
+        `${cli_options.network.toUpperCase()}_PROVIDER`
+      )} in ${chalk.yellow('.env')}.`
+    )
+  }
 
-    if (bootstrapAddresses.length == 0) {
-        console.log(chalk.red('Invalid bootstrap servers. Cannot start HOPR without a bootstrap node'))
-    }
+  let options: HoprOptions = {
+    bootstrapNode: cli_options.bootstrapNode,
+    network: cli_options.network,
+    connector,
+    bootstrapServers: [...bootstrapServerMap.values()],
+    provider: process.env[`${cli_options.network.toUpperCase()}_PROVIDER`],
+    output(encoded: Uint8Array) {
+      const { latency, msg } = decodeMessage(encoded)
 
-    let addr: Multiaddr, bootstrapServerMap = new Map<string, PeerInfo>()
-    for (let i = 0; i < bootstrapAddresses.length; i++) {
-        addr = Multiaddr(bootstrapAddresses[i].trim())
+      let str = `\n`
 
-        let peerInfo = bootstrapServerMap.get(addr.getPeerId())
-        if (peerInfo == null) {
-            peerInfo = await PeerInfo.create(PeerId.createFromB58String(addr.getPeerId()))
-        }
+      str += `===== New message ======\n`
+      str += `Message: ${chalk.yellow(msg.toString())}\n`
+      str += `Latency: ${chalk.green(latency.toString())} ms\n`
+      str += `========================\n`
 
-        peerInfo.multiaddrs.add(addr)
-        bootstrapServerMap.set(addr.getPeerId(), peerInfo)
-    }
+      console.log(str)
+    },
+  }
 
-    if (process.env[`${cli_options.network.toUpperCase()}_PROVIDER`] == null) {
-        throw Error(`Could not find any connector for ${chalk.magenta(cli_options.network)}. Please specify ${chalk.yellow(`${cli_options.network.toUpperCase()}_PROVIDER`)} in ${chalk.yellow('.env')}.`)
-    }
+  if (id != null) {
+    options.id = id
+  }
 
-    let options: HoprOptions = {
-        bootstrapNode: cli_options.bootstrapNode,
-        network: cli_options.network,
-        connector,
-        bootstrapServers: [...bootstrapServerMap.values()],
-        provider: process.env[`${cli_options.network.toUpperCase()}_PROVIDER`],
-        output(encoded: Uint8Array) {
-            const { latency, msg } = decodeMessage(encoded)
-    
-            let str = `\n`
-    
-            str += `===== New message ======\n`
-            str += `Message: ${chalk.yellow(msg.toString())}\n`
-            str += `Latency: ${chalk.green(latency.toString())} ms\n`
-            str += `========================\n`
-    
-            console.log(str)
-        }
-    }
+  if (cli_options.password) {
+    options.password = cli_options.password
+  }
 
-    if (id != null) {
-        options.id = id
-    }
-
-    if (cli_options.password) {
-        options.password = cli_options.password
-    }
-
-    return options
+  return options
 }
