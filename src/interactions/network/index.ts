c2b41cfb4f21e1472fff7b20aef3123843f2f2f2
Robert Kiel <robert.kiel@hoprnet.io>
2020-06-03 15:15:13 +0200

    remove old packet relaying code

--- src/interactions/network/index.ts
+++ src/interactions/network/index.ts
@@ -3,17 +3,14 @@ import HoprCoreConnector from '@hoprnet/hopr-core-connector-interface'
 import Hopr from '../..'
 
 import { Crawler } from './crawler'
-import { ForwardPacketInteraction } from './forwardPacket'
 import { Heartbeat } from './heartbeat'
 
 class NetworkInteractions<Chain extends HoprCoreConnector> {
   crawler: Crawler<Chain>
-  forwardPacket: ForwardPacketInteraction<Chain>
   heartbeat: Heartbeat<Chain>
 
   constructor(node: Hopr<Chain>) {
     this.crawler = new Crawler(node)
-    this.forwardPacket = new ForwardPacketInteraction(node)
     this.heartbeat = new Heartbeat(node)
   }
 }
