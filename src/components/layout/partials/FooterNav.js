f50f305e48c8b50ce29db76d27cb740c59e1cfdc
nionis <stivennoni77@gmail.com>
2020-06-04 16:26:57 +0300

    ✨ attach urls to footer

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -9,16 +9,24 @@ const FooterNav = ({ className, ...props }) => {
     <nav {...props} className={classes}>
       <ul className="list-reset">
         <li>
-          <Link to="/contact/">Contact</Link>
+          <Link to="/HOPR/#contact" target="_blank" rel="noopener noreferrer">
+            Contact
+          </Link>
         </li>
         <li>
-          <Link to="/about-us/">About us</Link>
+          <Link to="/HOPR/#about" target="_blank" rel="noopener noreferrer">
+            About us
+          </Link>
         </li>
+        {/* <li>
+          <Link to="/faqs/" target="_blank" rel="noopener noreferrer">
+            FAQ's
+          </Link>
+        </li> */}
         <li>
-          <Link to="/faqs/">FAQ's</Link>
-        </li>
-        <li>
-          <Link to="/support/">Support</Link>
+          <Link to="/for_you/#for_you" target="_blank" rel="noopener noreferrer">
+            Support
+          </Link>
         </li>
       </ul>
     </nav>
