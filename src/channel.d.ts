6a4ae957598d2ae1681f4f6d3a771e43e73bc023
Robert Kiel <robert.kiel@hoprnet.io>
2020-06-08 11:45:28 +0200

    move channel ticket functionality into channel factory

--- src/channel.d.ts
+++ src/channel.d.ts
@@ -81,7 +81,7 @@ declare namespace Channel {
       channel: ChannelType
       signature?: Signature
     }
-  ): Promise<SignedChannel> 
+  ): Promise<SignedChannel>
 }
 
 declare interface Channel {
@@ -104,7 +104,42 @@ declare interface Channel {
 
   readonly currentBalanceOfCounterparty: Promise<Balance>
 
-  readonly ticket: typeof Ticket
+  readonly ticket: {
+    /**
+     * Constructs a ticket to use in a probabilistic payment channel.
+     * @param amount amount of funds to include
+     * @param challenge a challenge that has to be solved be the redeemer
+     */
+    create(
+      amount: Balance,
+      challenge: Hash,
+      arr?: {
+        bytes: ArrayBuffer
+        offset: number
+      }
+    ): Promise<SignedTicket>
+
+    /**
+     * Checks a previously issued ticket for its validity.
+     * @param signedTicket a previously issued ticket to check
+     * @param props additional arguments
+     */
+    verify(signedTicket: SignedTicket): Promise<boolean>
+
+    /**
+     * BIG TODO
+     * Aggregate previously issued tickets. Still under active development!
+     * @param tickets array of tickets to aggregate
+     * @param props additional arguments
+     */
+    // aggregate(channel: any, tickets: Ticket[], ...props: any[]): Promise<Ticket>
+
+    /**
+     * Submits a signed to the blockchain.
+     * @param signedTicket a signed ticket
+     */
+    submit(signedTicket: SignedTicket): Promise<void>
+  }
 
   readonly counterparty: AccountId
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -140,41 +140,6 @@ declare interface State {
 
 declare namespace Ticket {
   const SIZE: number
-
-  /**
-   * Constructs a ticket to use in a probabilistic payment channel.
-   * @param amount amount of funds to include
-   * @param challenge a challenge that has to be solved be the redeemer
-   */
-  function create(
-    amount: Balance,
-    challenge: Hash,
-    arr?: {
-      bytes: ArrayBuffer
-      offset: number
-    }
-  ): Promise<SignedTicket>
-
-  /**
-   * Checks a previously issued ticket for its validity.
-   * @param signedTicket a previously issued ticket to check
-   * @param props additional arguments
-   */
-  function verify(signedTicket: SignedTicket): Promise<boolean>
-
-  /**
-   * BIG TODO
-   * Aggregate previously issued tickets. Still under active development!
-   * @param tickets array of tickets to aggregate
-   * @param props additional arguments
-   */
-  // aggregate(channel: any, tickets: Ticket[], ...props: any[]): Promise<Ticket>
-
-  /**
-   * Submits a signed to the blockchain.
-   * @param signedTicket a signed ticket
-   */
-  function submit(signedTicket: SignedTicket): Promise<void>
 }
 declare interface Ticket {
   channelId: Hash
