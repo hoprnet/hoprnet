cfdc05750a1d1e24fd031fb451c394b63cae8bf5
Steve <stivennoni77@gmail.com>
2020-06-16 12:15:30 +0300

    🔖 Release v0.3.5 (#84)
    * 🔖 Deploying v0.3.5
    
    * v0.3.5-dev.be1aea6

--- scripts/addresses.ts
+++ scripts/addresses.ts
@@ -6,7 +6,7 @@ export const HOPR_TOKEN: { [key in Networks]: string } = {
   ropsten: undefined,
   rinkeby: undefined,
   goerli: undefined,
-  kovan: '0xedEaeA1BaAc154D47dd20eC0b8952dB0a4224dd0',
+  kovan: '0xc499E80d455cdF0F7926c89E500786f114C070de',
   private: '0x66DB78F4ADD912a6Cb92b672Dfa09028ecc3085E',
 }
 
@@ -16,7 +16,7 @@ export const HOPR_CHANNELS: { [key in Networks]: string } = {
   ropsten: undefined,
   rinkeby: undefined,
   goerli: undefined,
-  kovan: '0xFA4530CAEF7f19AEa4c3a7A1880AB2edB5803f31',
+  kovan: '0xcB16f5dC7c6b0EF4e665b8344FAE4642717F3B98',
   private: '0x902602174a9cEb452f60c09043BE5EBC52096200',
 }
 
@@ -36,6 +36,6 @@ export const HOPR_FAUCET: { [key in Networks]: string } = {
   ropsten: undefined,
   rinkeby: undefined,
   goerli: undefined,
-  kovan: '0x827557073244d7d73d73d3Bf3cD3c7C546867f28',
+  kovan: '0x42C549678F71611Fd5aC05B30Fc256f054af4cdC',
   private: '0x6c97048D67c39ADCe38bbB228fc1bA415fc8f096',
 }
