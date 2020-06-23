4b7d0b9f75ff32c250719fa8dc01993b6b078c72
Steve <stivennoni77@gmail.com>
2020-06-23 11:53:00 +0300

    🐛 re-enable contract verification (#93)

--- scripts/operations/migrate.ts
+++ scripts/operations/migrate.ts
@@ -4,8 +4,7 @@ import { bash, isLocalNetwork, getContractNames } from './utils'
 export default async (network: string = 'development') => {
   await bash(`npx truffle migrate --network ${network}`)
 
-  // verification library currently doesn't work for solidity v6
-  // if (!isLocalNetwork(network)) {
-  //   await verify(network, ...getContractNames())
-  // }
+  if (!isLocalNetwork(network)) {
+    await verify(network, ...getContractNames())
+  }
 }
