22e40961da9837b813b8788f018ca0e327f7b1fc
Robert Kiel <robert.kiel@hoprnet.io>
2020-06-30 16:31:30 +0200

    linting

--- src/network/transport/handshake.spec.ts
+++ src/network/transport/handshake.spec.ts
@@ -45,30 +45,54 @@ describe('test handshake stream implementation', function () {
     const streamAlice = myHandshake(Alice, webRTCsendAlice, webRTCrecvAlice)
     const streamBob = myHandshake(Bob, webRTCsendBob, webRTCrecvBob)
 
-    pipe(Alice, streamAlice.webRtcStream)
+    pipe(
+      // prettier-ignore
+      Alice,
+      streamAlice.webRtcStream
+    )
 
-    pipe(streamBob.webRtcStream, Bob)
+    pipe(
+      // prettier-ignore
+      streamBob.webRtcStream,
+      Bob
+    )
 
-    pipe(Bob, streamBob.webRtcStream)
+    pipe(
+      // prettier-ignore
+      Bob,
+      streamBob.webRtcStream
+    )
 
-    pipe(streamAlice.webRtcStream, Alice)
+    pipe(
+      // prettier-ignore
+      streamAlice.webRtcStream,
+      Alice
+    )
 
     setTimeout(() => {
       webRTCrecvBob.end()
       webRTCrecvAlice.end()
     }, 100)
 
-    const pipePromiseBobAlice = pipe(webRTCrecvAlice, async (source: AsyncIterable<Uint8Array>) => {
-      for await (const msg of source) {
-        console.log('from webRTC recv buffer', msg)
+    const pipePromiseBobAlice = pipe(
+      // prettier-ignore
+      webRTCrecvAlice,
+      async (source: AsyncIterable<Uint8Array>) => {
+        for await (const msg of source) {
+          console.log('from webRTC recv buffer', msg)
+        }
       }
-    })
+    )
 
-    const pipePromiseAliceBob = pipe(webRTCrecvBob, async (source: AsyncIterable<Uint8Array>) => {
-      for await (const msg of source) {
-        console.log('from webRTC recv buffer', msg)
+    const pipePromiseAliceBob = pipe(
+      // prettier-ignore
+      webRTCrecvBob,
+      async (source: AsyncIterable<Uint8Array>) => {
+        for await (const msg of source) {
+          console.log('from webRTC recv buffer', msg)
+        }
       }
-    })
+    )
 
     webRTCsendAlice.push(new Uint8Array([23, 27]))
     webRTCsendAlice.push(new Uint8Array([24, 28]))
@@ -78,24 +102,40 @@ describe('test handshake stream implementation', function () {
 
     await Promise.all([pipePromiseBobAlice, pipePromiseAliceBob])
 
-    pipe([new Uint8Array([0, 4, 5, 6])], streamAlice.relayStream)
+    pipe(
+      // prettier-ignore
+      [new Uint8Array([0, 4, 5, 6])],
+      streamAlice.relayStream
+    )
 
-    pipe(streamBob.relayStream, async (source: Uint8Array) => {
-      for await (const msg of source) {
-        console.log(`Bob received:`, msg)
+    pipe(
+      // prettier-ignore
+      streamBob.relayStream,
+      async (source: Uint8Array) => {
+        for await (const msg of source) {
+          console.log(`Bob received:`, msg)
+        }
       }
-    })
+    )
 
-    pipe([new Uint8Array([0, 1, 2, 3])], streamBob.relayStream)
+    pipe(
+      // prettier-ignore
+      [new Uint8Array([0, 1, 2, 3])],
+      streamBob.relayStream
+    )
 
-    pipe(streamAlice.relayStream, async (source: Uint8Array) => {
-      for await (const msg of source) {
-        console.log(`Alice received:`, msg)
+    pipe(
+      // prettier-ignore
+      streamAlice.relayStream,
+      async (source: Uint8Array) => {
+        for await (const msg of source) {
+          console.log(`Alice received:`, msg)
+        }
       }
-    })
+    )
   })
 
-  it('should create a stream and upgrade it', async function () {
+  it('should create a downgraded stream without any further WebRTC interaction', async function () {
     const AliceBob = pushable<Uint8Array>()
     const BobAlice = pushable<Uint8Array>()
 
@@ -130,24 +170,40 @@ describe('test handshake stream implementation', function () {
     const streamAlice = myHandshake(Alice, undefined, undefined)
     const streamBob = myHandshake(Bob, undefined, undefined)
 
-    pipe([new Uint8Array([0, 4, 5, 6])], streamAlice.relayStream)
+    pipe(
+      // prettier-ignore
+      [new Uint8Array([0, 4, 5, 6])],
+      streamAlice.relayStream
+    )
 
-    pipe(streamBob.relayStream, async (source: Uint8Array) => {
-      for await (const msg of source) {
-        console.log(`Bob received:`, msg)
+    pipe(
+      // prettier-ignore
+      streamBob.relayStream,
+      async (source: Uint8Array) => {
+        for await (const msg of source) {
+          console.log(`Bob received:`, msg)
+        }
       }
-    })
+    )
 
-    pipe([new Uint8Array([0, 1, 2, 3])], streamBob.relayStream)
+    pipe(
+      // prettier-ignore
+      [new Uint8Array([0, 1, 2, 3])],
+      streamBob.relayStream
+    )
 
-    pipe(streamAlice.relayStream, async (source: Uint8Array) => {
-      for await (const msg of source) {
-        console.log(`Alice received:`, msg)
+    pipe(
+      // prettier-ignore
+      streamAlice.relayStream,
+      async (source: Uint8Array) => {
+        for await (const msg of source) {
+          console.log(`Alice received:`, msg)
+        }
       }
-    })
+    )
   })
 
-  it('should create a stream and upgrade it', async function () {
+  it('should create a stream that uses on one side WebRTC buffer and downgrade that stream to the base stream', async function () {
     const AliceBob = pushable<Uint8Array>()
     const BobAlice = pushable<Uint8Array>()
 
@@ -185,28 +241,52 @@ describe('test handshake stream implementation', function () {
     const streamAlice = myHandshake(Alice, webRTCsendAlice, webRTCrecvAlice)
     const streamBob = myHandshake(Bob, undefined, undefined)
 
-    pipe(Alice, streamAlice.webRtcStream)
+    pipe(
+      // prettier-ignore
+      Alice,
+      streamAlice.webRtcStream
+    )
 
-    pipe(streamAlice.webRtcStream, Alice)
+    pipe(
+      // prettier-ignore
+      streamAlice.webRtcStream,
+      Alice
+    )
 
     setTimeout(() => {
       webRTCrecvAlice.end()
     }, 100)
 
-    pipe([new Uint8Array([0, 4, 5, 6])], streamAlice.relayStream)
+    pipe(
+      // prettier-ignore
+      [new Uint8Array([0, 4, 5, 6])],
+      streamAlice.relayStream
+    )
 
-    pipe(streamBob.relayStream, async (source: Uint8Array) => {
-      for await (const msg of source) {
-        console.log(`Bob received:`, msg)
+    pipe(
+      // prettier-ignore
+      streamBob.relayStream,
+      async (source: Uint8Array) => {
+        for await (const msg of source) {
+          console.log(`Bob received:`, msg)
+        }
       }
-    })
+    )
 
-    pipe([new Uint8Array([0, 1, 2, 3])], streamBob.relayStream)
+    pipe(
+      // prettier-ignore
+      [new Uint8Array([0, 1, 2, 3])],
+      streamBob.relayStream
+    )
 
-    pipe(streamAlice.relayStream, async (source: Uint8Array) => {
-      for await (const msg of source) {
-        console.log(`Alice received:`, msg)
+    pipe(
+      // prettier-ignore
+      streamAlice.relayStream,
+      async (source: Uint8Array) => {
+        for await (const msg of source) {
+          console.log(`Alice received:`, msg)
+        }
       }
-    })
+    )
   })
 })
