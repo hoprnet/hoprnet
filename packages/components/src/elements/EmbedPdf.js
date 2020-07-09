35e61b814147e614544626b7e9b6a3d2a82961e2
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2020-07-09 15:48:48 +0200

    ✨ Reformatted pdf to be class

--- packages/components/src/elements/EmbedPdf.js
+++ packages/components/src/elements/EmbedPdf.js
@@ -3,22 +3,31 @@ import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-const EmbedPdf = ({ src, ...props }) => {
-  const [numPages, setNumPages] = React.useState(null);
-  const [pageNumber, setPageNumber] = React.useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
+class EmbedPdf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageNumber: 1,
+      numPages: null
+    }
+    this.onDocumentLoadSuccess = this.onDocumentLoadSuccess.bind(this);
   }
 
-  return (
-    <Document
-      file={src}
-      onLoadSuccess={onDocumentLoadSuccess}
-    >
-      <Page pageNumber={pageNumber} />
-    </Document>
-  )
+  onDocumentLoadSuccess({ numPages }) {
+    this.setState({ numPages });
+  }
+
+  render() {
+    const { src } = this.props;
+    return (
+      <Document
+        file={src}
+        onLoadSuccess={this.onDocumentLoadSuccess}
+      >
+        <Page pageNumber={this.state.pageNumber} />
+      </Document>
+    )
+  }
 }
 
 export default EmbedPdf
