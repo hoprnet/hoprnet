9b300e4393e65691ca7eef2e68178c3c5ecee396
Peter Braden <peter.braden@hoprnet.org>
2020-08-04 16:27:03 +0200

    Migrate server (#255)
    * Server was moved to hoprnet/hopr-server
    
    * adjust workflows

--- server/src/system/system.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { SystemService } from './system.service'
-
-@Module({
-  providers: [SystemService],
-  exports: [SystemService],
-})
-export class SystemModule {}
