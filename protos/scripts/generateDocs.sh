a482621c11b8146d5f6419f02ed9441bc35ebdae
Peter Braden <peter.braden@hoprnet.org>
2020-08-13 17:17:29 +0200

    Protos migration (#254)
    * move protos out into separate repo
    
    * remove protos action
    
    * Spelling error (#253)
    
    * ♻️ use @grpc/grpc-web to generate web stubs (#257)
    
    * ✨  implement grpc/grpc-web compatible stubs
    
    * ♻️ remove bundling step
    
    * 🔖 v0.0.12 (#237)
    
    * Re enable tests re #231 (#256)
    
    * Re enable tests re #231
    
    * test signing
    
    * Migrate server (#255)
    
    * Server was moved to hoprnet/hopr-server
    
    * adjust workflows
    
    * Bump node versions (#262)
    
    * bump node versions
    
    * Check on node versions
    
    * use yarn of node version
    
    * Bump versions
    
    * remove 14 from matrix
    
    * Make tests quieter
    
    * Run test STUN server on unused port
    
    * move protos out into separate repo
    
    * remove protos action
    
    Co-authored-by: Steve <stivennoni77@gmail.com>

--- protos/scripts/generateDocs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-parent_path=$( cd "$(dirname "${BASH_SOURCE[1]}")" ; pwd -P )
-
-docker run --rm \
-  -v "$parent_path/docs":/out \
-  -v "$parent_path/protos":/protos \
-  pseudomuto/protoc-gen-doc --doc_opt=markdown,protos.md
