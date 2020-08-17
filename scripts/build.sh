d381f4f92628855d49a1d5f103104f17c5b78a83
Robert Kiel <robert.kiel@hoprnet.io>
2020-08-17 15:18:18 +0200

    Version bump (#90)
    * fixes and cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
-[ ! -d "/src/ethereum" ] && mkdir ./src/ethereum
-[ ! -d "/src/ethereum/abi" ] && mkdir ./src/ethereum/abi
+[ ! -d "./src/ethereum" ] && mkdir ./src/ethereum
+[ ! -d "./src/ethereum/abi" ] && mkdir ./src/ethereum/abi
 
 # Make the smart contract ABI available to our Typescript sources
 cp -R ./node_modules/@hoprnet/hopr-ethereum/build/extracted/abis/*.json ./src/ethereum/abi;
