18edf2aa3de40a9eb6b4e86a5d14fa043daae28d
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-08-17 13:48:00 +0200

    Use bootstrap node loading

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import { encode, decode } from 'rlp'
 import Multihash from 'multihashes'
 import bs58 from 'bs58'
 import { addPubKey } from '@hoprnet/hopr-core/lib/utils'
+import { getBootstrapAddresses } from "@hoprnet/hopr-utils"
 
 const CRAWL_TIMEOUT = 100_000 // ~15 mins
 
@@ -97,10 +98,6 @@ const network = process.env.HOPR_NETWORK || "ETHEREUM";
 const provider =
   process.env.HOPR_ETHEREUM_PROVIDER ||
   "wss://kovan.infura.io/ws/v3/f7240372c1b442a6885ce9bb825ebc36";
-const bootstrapAddresses =
-  (process.env.HOPR_BOOTSTRAP_SERVERS ? process.env.HOPR_BOOTSTRAP_SERVERS.split(',') :
-  ["/ip4/34.65.82.167/tcp/9091/p2p/16Uiu2HAm6VH37RG1R4P8hGV1Px7MneMtNc6PNPewNxCsj1HsDLXW",
-    "/ip4/34.65.111.179/tcp/9091/p2p/16Uiu2HAmPyq9Gw93VWdS3pgmyAWg2UNnrgZoYKPDUMbKDsWhzuvb"]);
 const host = process.env.HOPR_HOST || "0.0.0.0:9091"; // Default IPv4
 
 // TODO this should probably be shared between chat and this, and live in a
@@ -150,7 +147,6 @@ function setupAdminServer(logs: LogStream, onMessage: (msg: string) => void){
 
 async function main() {
   let addr: Multiaddr;
-  let bootstrapServerMap = new Map<string, PeerInfo>();
   let logs = new LogStream()
 
   async function onMessage(message: string){
@@ -177,22 +173,6 @@ async function main() {
 
   setupAdminServer(logs, onMessage);
 
-  if (bootstrapAddresses.length == 0) {
-    throw new Error(
-      "Invalid bootstrap servers. Cannot start HOPR without a bootstrap node"
-    );
-  }
-  for (let i = 0; i < bootstrapAddresses.length; i++) {
-    addr = Multiaddr(bootstrapAddresses[i].trim());
-    let peerInfo = bootstrapServerMap.get(addr.getPeerId());
-    if (peerInfo == null) {
-      peerInfo = await PeerInfo.create(
-        PeerId.createFromB58String(addr.getPeerId())
-      );
-    }
-    peerInfo.multiaddrs.add(addr);
-    bootstrapServerMap.set(addr.getPeerId(), peerInfo);
-  }
   
   function logMessageToNode(msg: Uint8Array){
     logs.log("#### NODE RECEIVED MESSAGE ####")
@@ -209,7 +189,7 @@ async function main() {
   let options: HoprOptions = {
     debug: Boolean(process.env.DEBUG),
     network,
-    bootstrapServers: [...bootstrapServerMap.values()],
+    bootstrapServers: [... (await getBootstrapAddresses()).values()],
     provider,
     hosts: parseHosts(),
     output: logMessageToNode,
@@ -218,7 +198,7 @@ async function main() {
 
   logs.log('Creating HOPR Node')
   logs.log('- network : ' + network);
-  logs.log('- bootstrapServers : ' + bootstrapAddresses.join(', '));
+  logs.log('- bootstrapServers : ' + Array.from(options.bootstrapServers!.keys()).join(','));
 
   NODE = await Hopr.create(options);
   logs.log('Created HOPR Node')
