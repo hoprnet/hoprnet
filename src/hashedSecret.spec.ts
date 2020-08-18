b35f33042dd97a2b8365539784f9c963ed9d69e2
Robert Kiel <robert.kiel@hoprnet.io>
2020-08-18 11:14:39 +0200

    add tests for deterministic on-chain debug secrets

--- src/hashedSecret.spec.ts
+++ src/hashedSecret.spec.ts
@@ -21,9 +21,8 @@ describe('test hashedSecret management', function () {
   this.timeout(durations.seconds(7))
   const ganache = new Ganache()
   let connector: HoprEthereum
-  let preImage: PreImage
 
-  async function generateConnector(): Promise<HoprEthereum> {
+  async function generateConnector(debug?: boolean): Promise<HoprEthereum> {
     let web3 = new Web3(configs.DEFAULT_URI)
     const chainId = await Utils.getChainId(web3)
     const network = Utils.getNetworkName(chainId) as addresses.Networks
@@ -37,7 +36,7 @@ describe('test hashedSecret management', function () {
       utils: Utils,
       types: Types,
       options: {
-        debug: false,
+        debug,
       },
     } as unknown) as HoprEthereum
 
@@ -79,84 +78,145 @@ describe('test hashedSecret management', function () {
     }
   }
 
-  before(async function () {
-    this.timeout(60e3)
-    await ganache.start()
-    await migrate()
-    await fund(1)
+  context('random pre-image', function () {
+    before(async function () {
+      this.timeout(60e3)
+      await ganache.start()
+      await migrate()
+      await fund(1)
 
-    connector = await generateConnector()
+      connector = await generateConnector()
+    })
+
+    after(async function () {
+      await ganache.stop()
+    })
+
+    it('should publish a hashed secret', async function () {
+      await connector.hashedSecret.check()
+
+      await connector.hashedSecret.submit()
+
+      let onChainHash = new Types.Hash(
+        stringToU8a(
+          (await connector.hoprChannels.methods.accounts((await connector.account.address).toHex()).call()).hashedSecret
+        )
+      )
+
+      let preImage = await connector.hashedSecret.getPreimage(onChainHash)
+
+      assert(u8aEquals((await connector.utils.hash(preImage.preImage)).slice(0, HASHED_SECRET_WIDTH), onChainHash))
+
+      await connector.utils.waitForConfirmation(
+        (
+          await connector.signTransaction(connector.hoprChannels.methods.setHashedSecret(preImage.preImage.toHex()), {
+            from: (await connector.account.address).toHex(),
+            to: connector.hoprChannels.options.address,
+            nonce: await connector.account.nonce,
+          })
+        ).send()
+      )
+      let updatedOnChainHash = new Types.Hash(
+        stringToU8a(
+          (await connector.hoprChannels.methods.accounts((await connector.account.address).toHex()).call()).hashedSecret
+        )
+      )
+
+      assert(!u8aEquals(onChainHash, updatedOnChainHash), `new and old onChainSecret must not be the same`)
+
+      let updatedPreImage = await connector.hashedSecret.getPreimage(updatedOnChainHash)
+
+      assert(!u8aEquals(preImage.preImage, updatedPreImage.preImage), `new and old pre-image must not be the same`)
+
+      assert(
+        u8aEquals(
+          (await connector.utils.hash(updatedPreImage.preImage)).slice(0, HASHED_SECRET_WIDTH),
+          updatedOnChainHash
+        )
+      )
+    })
+
+    // Commented due expensive operations
+    // it('should generate a hashed secret and recover a pre-Image', async function () {
+    //   this.timeout(durations.seconds(22))
+    //   await connector.hashedSecret.create()
+
+    //   for (let i = 0; i < TOTAL_ITERATIONS / GIANT_STEP_WIDTH; i++) {
+    //     assert(
+    //       (await connector.db.get(Buffer.from(connector.dbKeys.OnChainSecretIntermediary(i * GIANT_STEP_WIDTH)))) != null
+    //     )
+    //   }
+
+    //   const masterSecret = await connector.db.get(Buffer.from(connector.dbKeys.OnChainSecretIntermediary(0)))
+
+    //   await checkIndex(1, masterSecret, false)
+
+    //   await checkIndex(randomInteger(1, TOTAL_ITERATIONS), masterSecret, false)
+
+    //   await checkIndex(TOTAL_ITERATIONS, masterSecret, false)
+
+    //   await checkIndex(0, masterSecret, true)
+
+    //   await checkIndex(TOTAL_ITERATIONS + 1, masterSecret, true)
+    // })
   })
 
-  after(async function () {
-    await ganache.stop()
+  context('deterministic debug pre-image', function () {
+    before(async function () {
+      this.timeout(60e3)
+      await ganache.start()
+      await migrate()
+      await fund(1)
+
+      connector = await generateConnector(true)
+    })
+
+    after(async function () {
+      await ganache.stop()
+    })
+
+    it('should publish a hashed secret', async function () {
+      await connector.hashedSecret.check()
+
+      await connector.hashedSecret.submit()
+
+      let onChainHash = new Types.Hash(
+        stringToU8a(
+          (await connector.hoprChannels.methods.accounts((await connector.account.address).toHex()).call()).hashedSecret
+        )
+      )
+
+      let preImage = await connector.hashedSecret.getPreimage(onChainHash)
+
+      assert(u8aEquals((await connector.utils.hash(preImage.preImage)).slice(0, HASHED_SECRET_WIDTH), onChainHash))
+
+      await connector.utils.waitForConfirmation(
+        (
+          await connector.signTransaction(connector.hoprChannels.methods.setHashedSecret(preImage.preImage.toHex()), {
+            from: (await connector.account.address).toHex(),
+            to: connector.hoprChannels.options.address,
+            nonce: await connector.account.nonce,
+          })
+        ).send()
+      )
+      let updatedOnChainHash = new Types.Hash(
+        stringToU8a(
+          (await connector.hoprChannels.methods.accounts((await connector.account.address).toHex()).call()).hashedSecret
+        )
+      )
+
+      assert(!u8aEquals(onChainHash, updatedOnChainHash), `new and old onChainSecret must not be the same`)
+
+      let updatedPreImage = await connector.hashedSecret.getPreimage(updatedOnChainHash)
+
+      assert(!u8aEquals(preImage.preImage, updatedPreImage.preImage), `new and old pre-image must not be the same`)
+
+      assert(
+        u8aEquals(
+          (await connector.utils.hash(updatedPreImage.preImage)).slice(0, HASHED_SECRET_WIDTH),
+          updatedOnChainHash
+        )
+      )
+    })
   })
-
-  it('should publish a hashed secret', async function () {
-    await connector.hashedSecret.check()
-
-    await connector.hashedSecret.submit()
-
-    let onChainHash = new Types.Hash(
-      stringToU8a(
-        (await connector.hoprChannels.methods.accounts((await connector.account.address).toHex()).call()).hashedSecret
-      )
-    )
-
-    let preImage = await connector.hashedSecret.getPreimage(onChainHash)
-
-    assert(u8aEquals((await connector.utils.hash(preImage.preImage)).slice(0, HASHED_SECRET_WIDTH), onChainHash))
-
-    await connector.utils.waitForConfirmation(
-      (
-        await connector.signTransaction(connector.hoprChannels.methods.setHashedSecret(preImage.preImage.toHex()), {
-          from: (await connector.account.address).toHex(),
-          to: connector.hoprChannels.options.address,
-          nonce: await connector.account.nonce,
-        })
-      ).send()
-    )
-    let updatedOnChainHash = new Types.Hash(
-      stringToU8a(
-        (await connector.hoprChannels.methods.accounts((await connector.account.address).toHex()).call()).hashedSecret
-      )
-    )
-
-    assert(!u8aEquals(onChainHash, updatedOnChainHash), `new and old onChainSecret must not be the same`)
-
-    let updatedPreImage = await connector.hashedSecret.getPreimage(updatedOnChainHash)
-
-    assert(!u8aEquals(preImage.preImage, updatedPreImage.preImage), `new and old pre-image must not be the same`)
-
-    assert(
-      u8aEquals(
-        (await connector.utils.hash(updatedPreImage.preImage)).slice(0, HASHED_SECRET_WIDTH),
-        updatedOnChainHash
-      )
-    )
-  })
-
-  // Commented due expensive operations
-  // it('should generate a hashed secret and recover a pre-Image', async function () {
-  //   this.timeout(durations.seconds(22))
-  //   await connector.hashedSecret.create()
-
-  //   for (let i = 0; i < TOTAL_ITERATIONS / GIANT_STEP_WIDTH; i++) {
-  //     assert(
-  //       (await connector.db.get(Buffer.from(connector.dbKeys.OnChainSecretIntermediary(i * GIANT_STEP_WIDTH)))) != null
-  //     )
-  //   }
-
-  //   const masterSecret = await connector.db.get(Buffer.from(connector.dbKeys.OnChainSecretIntermediary(0)))
-
-  //   await checkIndex(1, masterSecret, false)
-
-  //   await checkIndex(randomInteger(1, TOTAL_ITERATIONS), masterSecret, false)
-
-  //   await checkIndex(TOTAL_ITERATIONS, masterSecret, false)
-
-  //   await checkIndex(0, masterSecret, true)
-
-  //   await checkIndex(TOTAL_ITERATIONS + 1, masterSecret, true)
-  // })
 })
