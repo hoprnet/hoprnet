f668aa5d80bbf21a98597ee949c573853da5bde4
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-09-01 11:00:56 +0200

    Start porting across

--- hopr-admin/pages/index.js
+++ hopr-admin/pages/index.js
@@ -1,19 +1,91 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+function browser(){
+  var prevLog = ""
+
+  var appendLog = function(msg){
+    var logs = document.querySelector('#log')
+    logs.textContent = logs.textContent + '\n'+ msg
+    logs.scrollIntoView({block: 'end', behaviour: 'smooth'});
+  }
+
+  var connect = function(){
+    var logs = document.querySelector('.logs')
+    console.log('Connecting ...')
+    var client = new WebSocket('ws://' + window.location.host);
+    console.log('Web socket created')
+
+    client.onopen = function(){
+      console.log('Web socket opened')
+      logs.classList.remove('connecting')
+      document.querySelector('#command').disabled = false
+      document.querySelector('#command').focus()
+
+      document.querySelector('#command').onkeydown = function(e) {
+        if (e.keyCode == 13 ) { // enter 
+          var text = e.target.value 
+          console.log("Command: ", text)
+          if (text.length > 0) {
+            client.send(text)
+            prevLog = text
+            e.target.value = ""
+          }
+        }
+        if (e.keyCode == 38) { // Up Arrow
+          e.target.value = prevLog
+        }
+      }
+
+    }
+
+    client.onmessage = function(event) {
+      appendLog(event.data)
+      console.log(event)
+    }
+
+    client.onerror = function(error){
+      console.log('Connection error:', error)
+    }
+
+    client.onclose = function(){
+      console.log('Web socket closed')
+      logs.classList.add('connecting')
+      document.querySelector('#command').disabled = true
+      appendLog(' --- < Lost Connection, attempting to reconnect... > ---')
+      setTimeout(function(){
+        try {
+          connect()
+          console.log('connection')
+        } catch (e){
+          console.log('Error connecting', e)
+        }
+      }, 1000);
+    }
+  }
+
+  window.onload = function(){
+    connect();
+  }
+}
+
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-      </main>
-
-      <footer className={styles.footer}>
-      </footer>
+      <div className='logo'>
+      </div>
+      <h1>HOPR Logs [TESTNET NODE]</h1>
+      <div className='logs connecting'>
+        <pre>
+          <code id='log'>Connecting...</code>
+        </pre>
+      </div>
+      <div className='send'>
+        <input id="command"
+          type="text"
+          disabled={true}
+          placeholder="type 'help' for full list of commands" /> 
+      </div>
     </div>
   )
 }
