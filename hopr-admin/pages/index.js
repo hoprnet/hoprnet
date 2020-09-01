ac78bb2d12368c64815d68fe50e5e658d9958ed2
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-09-01 10:47:46 +0200

    move and delete cruft

---
+++ hopr-admin/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
