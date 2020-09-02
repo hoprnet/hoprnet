2a3d68e7b221a0500fbf8369c5821feea2764026
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-09-02 14:28:44 +0200

    balls

--- hopr-admin/components/log.js
+++ hopr-admin/components/log.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState, useRef } from "react";
 import styles from '../styles/log.module.css'
+import dynamic from "next/dynamic";
+
+const Jazzicon = dynamic(() => import("../components/jazzicon"), { ssr: false });
 
 const ID_REGEX = /(\w{53})/g
 
 export function LogLine(props){
   let raw = props.value.msg
   let msg = []
+  let ids = []
   let match
 
   let lastIndex = 0
   while ((match = ID_REGEX.exec(raw)) !== null){
+    ids.push(match[0])
     msg.push(match.input.slice(lastIndex, match.index))
     msg.push(<abbr title={match[0]}>{match[0].slice(48)}</abbr>)
     lastIndex = match.index + match[0].length
@@ -22,6 +27,17 @@ export function LogLine(props){
     <div key={props.value.ts} className={styles.logline}>
       <time>{ props.value.ts.slice(11) }</time>
       <pre>{ msg }</pre>
+      <div className={styles.loglineicons}>
+        {ids.map(x => 
+                <Jazzicon
+                  key={x}
+                  diameter={15}
+                  address={x}
+                  />
+              )
+        }
+        &nbsp;
+      </div>
     </div>
   )
 }
