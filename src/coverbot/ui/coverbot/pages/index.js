b5cae46e16311a13aaa33ac6893b6d76924555a9
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-09-03 17:09:33 +0200

    UI

--- src/coverbot/ui/coverbot/pages/index.js
+++ src/coverbot/ui/coverbot/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Logo from '../components/logo'
 
 export default function Home() {
   return (
@@ -9,19 +10,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <header>
+        <Logo />
         <h1 className={styles.title}>
           HOPR Incentivized Testnet on xDAI
         </h1>
+      </header>
 
+      <main className={styles.main}>
         <h2>Instructions</h2>
         <ol>
           <li>Download <a href="#">HOPR Node Saentis</a> and run it.</li>
           <li>Send 10 xDAI to your node</li>
-          <li><a href="#">Tweet</a> your HOPR node address with the tag #HOPRNetwork and @hoprnet</li>
+          <li><a href="#">Tweet</a> your HOPR node address with the tag <strong>#HOPRNetwork</strong> and <strong>@hoprnet</strong></li>
           <li>Send a message with your tweet to the Cover Node address</li>
           <li>Wait for the Cover bot to send you a message</li>
-          <li>You have received xHOPR tokens</li>
+          <li>You have received xHOPR tokens!</li>
         </ol>
 
       </main>
