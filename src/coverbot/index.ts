9df0c919fa273da757a8fa13c239507398442fa5
Jose Aguinaga <me@jjperezaguinaga.com>
2020-09-07 16:16:10 +0200

    ♻️  Storing eth addresses as well, namespacing debugs

--- src/coverbot/index.ts
+++ src/coverbot/index.ts
@@ -10,11 +10,13 @@ import fs from 'fs'
 
 
 //@TODO: Move this to an environment variable or read from a contract
-const XDAI_THRESHOLD = 0.001
-const VERIFICATION_CYCLE_IN_MS =30000
+const COVERBOT_XDAI_THRESHOLD = 0.001
+const COVERBOT_VERIFICATION_CYCLE_IN_MS =30000
+const COVERBOT_DEBUG_MODE = true
 
 type HoprNode = {
   id: string,
+  address: string,
   tweetId: string,
   tweetUrl: string
 }
@@ -39,7 +41,7 @@ const BotResponses = {
     Welcome to the xHOPR incentivized network.
 
     1. Post a tweet with your HOPR Address and the tag #HOPRNetwork
-    2. Load ${XDAI_THRESHOLD} xDAI into your HOPR Ethereum Address
+    2. Load ${COVERBOT_XDAI_THRESHOLD} xDAI into your HOPR Ethereum Address
     3. Send me the link to your tweet (don‘t delete it!)
     4. Keep your tweet and node alive, and I'll slowly send xHOPR to you.
     
@@ -69,11 +71,11 @@ const NodeStateResponses = {
     Your tweet has succeeded verification. Please do no delete this tweet, as we will
     use it multiple times to verify and connect to your node.
 
-    We’ll now proceed to check that your HOPR Ethereum address has at least ${XDAI_THRESHOLD} xDAI.
+    We’ll now proceed to check that your HOPR Ethereum address has at least ${COVERBOT_XDAI_THRESHOLD} xDAI.
     If you need xDAI, you always swap DAI to xDAI using https://dai-bridge.poa.network/.
   `,
   [NodeStates.xdaiBalanceFailed]: (xDaiBalance: number) => `\n
-    Your node does not have at least ${XDAI_THRESHOLD} xDAI. Currently, your node has ${xDaiBalance} xDAI.
+    Your node does not have at least ${COVERBOT_XDAI_THRESHOLD} xDAI. Currently, your node has ${xDaiBalance} xDAI.
 
     To participate in our incentivized network, please make sure to add the missing amount of xDAI.
   `,
@@ -120,20 +122,22 @@ export class Coverbot implements Bot {
 
     this.ethereumAddress = null;
     this.xdaiWeb3 = new Web3(new Web3.providers.HttpProvider('https://dai.poa.network'));
-    this.verificationTimeout = setInterval(this._verificationCycle.bind(this), VERIFICATION_CYCLE_IN_MS)
+    this.verificationTimeout = setInterval(this._verificationCycle.bind(this), COVERBOT_VERIFICATION_CYCLE_IN_MS)
 
     this.verifiedHoprNodes = new Map<string, HoprNode>()
+  }
+
+  private async _getEthereumAddressFromHOPRAddress(hoprAddress: string): Promise<string> {
+    const pubkey = await convertPubKeyFromB58String(hoprAddress)
+    const ethereumAddress = u8aToHex(await Utils.pubKeyToAccountId(pubkey.marshal()))
+    return ethereumAddress;
   }
 
   protected async dumpData() {
     //@TODO: Ideally we move this to a more suitable place.
     if(!this.ethereumAddress) {
-      const pubkey = await convertPubKeyFromB58String(this.address)
-      const ethereumAddress = u8aToHex(await Utils.pubKeyToAccountId(pubkey.marshal()))
-      this.ethereumAddress = ethereumAddress;
+      this.ethereumAddress = await this._getEthereumAddressFromHOPRAddress(this.address)
     }
-
-    console.log('Checking dump...', Array.from(this.verifiedHoprNodes.values()))
 
     const state = {
       address: this.address,
@@ -145,10 +149,8 @@ export class Coverbot implements Bot {
       refreshed: new Date().toISOString()
     }
 
-    console.log('Storing nodes...')
     let pth = process.env.STATS_FILE
     fs.writeFileSync(pth, JSON.stringify(state), 'utf8')
-    console.log("Stored", pth)
   }
 
   protected async _sendMessageOpeningChannels(recipient, message, intermediatePeers) {
@@ -159,42 +161,43 @@ export class Coverbot implements Bot {
   }
 
   protected async _verificationCycle() {
-    console.log(`${VERIFICATION_CYCLE_IN_MS}ms has passed. Verifying nodes...`)
+    console.log(`${COVERBOT_VERIFICATION_CYCLE_IN_MS}ms has passed. Verifying nodes...`)
 
     this.dumpData()
     const _verifiedNodes = Array.from(this.verifiedHoprNodes.values());
-    console.log('Verified nodes', _verifiedNodes);
     const randomIndex = Math.floor(Math.random() * _verifiedNodes.length);
-    console.log('Random index', randomIndex);
     const hoprNode: HoprNode = _verifiedNodes[randomIndex]
-    console.log('Trying to verify:', hoprNode);
     if (!hoprNode) {
       return;
     }
 
     try {
       const tweet = new TweetMessage(hoprNode.tweetUrl)
-      await tweet.fetch({ mock: true })
+      await tweet.fetch({ mock: COVERBOT_DEBUG_MODE })
       const _hoprNodeAddress = tweet.getHOPRNode()
-      console.log('HoprNode Address', _hoprNodeAddress);
       if (_hoprNodeAddress.length === 0) {
         // We got no HOPR Node here.
         this.verifiedHoprNodes.delete(hoprNode.id)
       } else {
         this._sendMessageFromBot(_hoprNodeAddress, BotResponses[BotCommands.verify])
-        //@TODO: We need to actually be able to get a message back from the user instead of waiting.
+        /*
+        * @TODO: Ideally we wait until we get the packet back before we try to open the payment channel.
+        * Right now we are “sending and forgetting”, as we do not wait for the message back.
+        * To fix this, under the `handle` message we can detect messages that came from us but were
+        * relayed as a way to verify connection. Also, it would be important to ensure the content sent
+        * could only be generated by us. For the time being, we are just “hoping” the messages get by.
+        */
         setTimeout(async () => {
           console.log('Sending messages to node...')
           this._sendMessageFromBot(_hoprNodeAddress, NodeStateResponses[NodeStates.verifiedNode])
-          this._sendMessageOpeningChannels(this.address, `Packet relayed by ${_hoprNodeAddress}`, [_hoprNodeAddress])
-        }, VERIFICATION_CYCLE_IN_MS/2)
+          this._sendMessageOpeningChannels(this.address, ` Packet relayed by ${_hoprNodeAddress}`, [_hoprNodeAddress])
+        }, COVERBOT_VERIFICATION_CYCLE_IN_MS/2)
       }
     } catch (err) {
       console.log('Err:', err);
       // Something failed. We better remove node.
       this.verifiedHoprNodes.delete(hoprNode.id)
     }
-    console.log("Checking logs", Array.from(this.verifiedHoprNodes.values()));
   }
 
   protected _sendMessageFromBot(recipient, message) {
@@ -211,7 +214,7 @@ export class Coverbot implements Bot {
     const weiBalance = await this.xdaiWeb3.eth.getBalance(nodeEthereumAddress)
     const balance = +Web3.utils.fromWei(weiBalance)
 
-    return balance >= XDAI_THRESHOLD ? [balance, NodeStates.xdaiBalanceSucceeded] : [balance, NodeStates.xdaiBalanceFailed]
+    return balance >= COVERBOT_XDAI_THRESHOLD ? [balance, NodeStates.xdaiBalanceSucceeded] : [balance, NodeStates.xdaiBalanceFailed]
   }
 
   protected async _verifyTweet(message: IMessage): Promise<[TweetMessage, NodeStates]> {
@@ -224,7 +227,7 @@ export class Coverbot implements Bot {
     * content, so if something isn't in the first 100 characters, it might
     * be left out of the parser.
     */
-    await tweet.fetch({ mock: true })
+    await tweet.fetch({ mock: COVERBOT_DEBUG_MODE })
 
     if (tweet.hasTag('hoprnetwork')) {
       tweet.status.hasTag = true
@@ -232,7 +235,7 @@ export class Coverbot implements Bot {
     if(tweet.hasMention('hoprnet')) {
       tweet.status.hasMention = true
     }
-    if(tweet.hasSameHOPRNode(message.from)) {
+    if(tweet.hasSameHOPRNode(message.from) || COVERBOT_DEBUG_MODE ) {
       tweet.status.sameNode = true
     }
     return tweet.status.isValid() ? [tweet, NodeStates.tweetVerificationSucceeded] : [tweet, NodeStates.tweetVerificationFailed]
@@ -265,7 +268,12 @@ export class Coverbot implements Bot {
             break;
           case NodeStates.xdaiBalanceSucceeded:
             //@TODO Add this to a persistent store
-            this.verifiedHoprNodes.set(message.from, {id: message.from, tweetId: tweet.id, tweetUrl: tweet.url })
+            this.verifiedHoprNodes.set(message.from, {
+              id: message.from,
+              tweetId: tweet.id,
+              tweetUrl: tweet.url,
+              address: await this._getEthereumAddressFromHOPRAddress(message.from)
+            })
             this._sendMessageFromBot(message.from, NodeStateResponses[xDaiBalanceNodeState](balance))
             break;
         }
