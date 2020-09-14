74470ade34c61ddef70fbe98f5d93c708638545f
nionis <stivennoni77@gmail.com>
2020-09-14 10:20:17 +0300

    🐛 fix HOPR_ENVIRONMENT

--- src/coverbot/ui/coverbot/utils/env.js
+++ src/coverbot/ui/coverbot/utils/env.js
@@ -1,1 +1,2 @@
-export const HOPR_ENVIRONMENT = process.env['HOPR_ENVIRONMENT'] || 'develop'
+export const HOPR_ENVIRONMENT = process.env['NEXT_PUBLIC_HOPR_ENVIRONMENT'] || 'develop'
+console.log(HOPR_ENVIRONMENT)
