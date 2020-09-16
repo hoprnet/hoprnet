2021e2df12b4a56e6b6a126fcaf72023716851f3
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-09-16 14:51:38 +0200

    remove unused

--- src/network/transport/index.ts
+++ src/network/transport/index.ts
@@ -36,7 +36,6 @@ import upgradeToWebRTC from './webrtc'
 
 import Relay from './relay'
 
-const PRIVATE_NETS = /(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/
 /**
  * @class TCP
  */
