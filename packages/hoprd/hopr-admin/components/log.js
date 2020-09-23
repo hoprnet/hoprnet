2ee3473662328510387769036bd306607de620be
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-09-23 16:03:44 +0200

    duplicate line

--- packages/hoprd/hopr-admin/components/log.js
+++ packages/hoprd/hopr-admin/components/log.js
@@ -3,7 +3,6 @@ import styles from '../styles/log.module.css'
 import dynamic from "next/dynamic";
 
 const Jazzicon = dynamic(() => import("../components/jazzicon"), { ssr: false });
-const Jazzicon = dynamic(() => import("../components/jazzicon"), { ssr: false });
 
 export function AbbreviatedId({id}){
   let [expanded, setExpanded] = useState(false)
