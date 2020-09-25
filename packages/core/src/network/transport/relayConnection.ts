df6f011ed119ddd06ae4e6a73c1e00eb7518c0c1
Robert Kiel <robert.kiel@hoprnet.io>
2020-09-25 19:24:45 +0200

    WIP: relay connection

--- packages/core/src/network/transport/relayConnection.ts
+++ packages/core/src/network/transport/relayConnection.ts
@@ -1,9 +1,13 @@
-import { Connection, Stream } from './types'
+import { Stream } from './types'
 import Multiaddr from 'multiaddr'
 import BL from 'bl'
 import { MultiaddrConnection } from './types'
 import Defer, { DeferredPromise } from 'p-defer'
 import { RELAY_PAYLOAD_PREFIX, RELAY_STATUS_PREFIX, STOP } from './constants'
+import { u8aEquals } from '@hoprnet/hopr-utils'
+
+import Debug from 'debug'
+const error = Debug('hopr-core:transport:error')
 
 class RelayConnection implements MultiaddrConnection {
   private _defer: DeferredPromise<void>
@@ -32,14 +36,14 @@ class RelayConnection implements MultiaddrConnection {
       let msgReceived = false
       let itDone = false
 
-      let msg: Promise<IteratorResult<Uint8Array, void>>
+      let msg: Promise<IteratorResult<Uint8Array, Uint8Array | void>>
 
       this._defer.promise.then(() => {
         itDone = true
       })
 
       while (true) {
-        msg = (this._stream.source as AsyncGenerator<Uint8Array, void>).next()
+        msg = (this._stream.source as AsyncGenerator<Uint8Array, Uint8Array | void>).next()
 
         await Promise.race([
           msg.then(({ done }) => {
@@ -55,10 +59,29 @@ class RelayConnection implements MultiaddrConnection {
         if (msgReceived) {
           msgReceived = false
 
-          if (itDone) {
-            return (await msg).value
+          const _received = (await msg).value
+
+          if (_received == null) {
+            // change this to continue to accept **empty** messages
+            return
+          }
+
+          const received = (_received as Uint8Array).slice()
+
+          if (u8aEquals(received.slice(0, 1), RELAY_PAYLOAD_PREFIX)) {
+            if (itDone) {
+              return received.slice(1)
+            } else {
+              yield received.slice(1)
+            }
+          } else if (u8aEquals(received.slice(0, 1), RELAY_STATUS_PREFIX)) {
+            if (u8aEquals(received.slice(1), STOP)) {
+              return
+            } else {
+                error(`Received invalid status message ${received.slice(1)}. Dropping message.`)
+            }
           } else {
-            yield (await msg).value
+            error(`Received invalid prefix <${received.slice(1)}. Dropping message.`)
           }
         }
 
@@ -69,14 +92,16 @@ class RelayConnection implements MultiaddrConnection {
     }.call(this)
   }
 
-  async sink(source: AsyncIterable<Uint8Array>) {
-    return (async function* () {
-      for await (const msg of source) {
-        yield (new BL([(RELAY_PAYLOAD_PREFIX as unknown) as BL, (msg as unknown) as BL]) as unknown) as Uint8Array
-      }
+  sink(source: AsyncIterable<Uint8Array>): Promise<void> {
+    return this._stream.sink(
+      (async function* () {
+        for await (const msg of source) {
+          yield (new BL([(RELAY_PAYLOAD_PREFIX as unknown) as BL, (msg as unknown) as BL]) as unknown) as Uint8Array
+        }
 
-      yield (new BL([(RELAY_STATUS_PREFIX as unknown) as BL, (STOP as unknown) as BL]) as unknown) as Uint8Array
-    })()
+        yield (new BL([(RELAY_STATUS_PREFIX as unknown) as BL, (STOP as unknown) as BL]) as unknown) as Uint8Array
+      })()
+    )
   }
 
   async close(err?: Error) {
