4878981e02c521c67fe617efd21ee2e10746fe1b
nionis <stivennoni77@gmail.com>
2020-09-30 10:54:48 +0300

    ✨ check HOPR balance before increaseFunds attempt

--- packages/core-ethereum/src/channel/index.ts
+++ packages/core-ethereum/src/channel/index.ts
@@ -15,6 +15,7 @@ import {
 } from '../types'
 import { ChannelStatus } from '../types/channel'
 import { waitForConfirmation, getId, events, pubKeyToAccountId, sign, isPartyA } from '../utils'
+import { ERRORS } from '../constants'
 
 import type HoprEthereum from '..'
 import Channel from './channel'
@@ -38,6 +39,11 @@ class ChannelFactory {
 
   async increaseFunds(counterparty: AccountId, amount: Balance): Promise<void> {
     try {
+      const balance = await this.coreConnector.account.balance
+      if (balance.isZero()) {
+        throw Error(ERRORS.OOF_HOPR)
+      }
+
       await waitForConfirmation(
         (
           await this.coreConnector.signTransaction(
