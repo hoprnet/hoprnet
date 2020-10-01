6f70d51e3ba51ff43f94eee62b13e59c7afa83b1
Robert Kiel <robert.kiel@hoprnet.io>
2020-10-01 17:22:26 +0200

    restore tests

--- packages/core/src/network/transport/relayContext.spec.ts
+++ packages/core/src/network/transport/relayContext.spec.ts
@@ -1,34 +1,33 @@
 import { RelayContext } from './relayContext'
 
-// describe('test overwritable connection', function () {
-//   let iteration = 0
-//   function getGenerator(): AsyncGenerator<Uint8Array> {
-//     return (async function* () {
-//       let i = 0
-//       for (; i < 23; i++) {
-//         yield new TextEncoder().encode(`iteration ${iteration} - msg no. ${i}`)
-//         await new Promise((resolve) => setTimeout(resolve, 12))
-//       }
+describe('test overwritable connection', function () {
+  let iteration = 0
+  function getGenerator(): AsyncGenerator<Uint8Array> {
+    return (async function* () {
+      let i = 0
+      for (; i < 23; i++) {
+        yield new TextEncoder().encode(`iteration ${iteration} - msg no. ${i}`)
+        await new Promise((resolve) => setTimeout(resolve, 12))
+      }
 
-//       return `iteration ${iteration} - msg no. ${i + 1}`
-//     })()
-//   }
+      return `iteration ${iteration} - msg no. ${i + 1}`
+    })()
+  }
 
-//   it('should create a connection and overwrite it', async function () {
-// /*
-//     const ctx = new RelayContext(getGenerator())
+  it('should create a connection and overwrite it', async function () {
+    /*
+    const ctx = new RelayContext(getGenerator())
 
-//     let i = setInterval(() => {
-//       ctx.update(getGenerator())
-//       iteration++
-//     }, 123)
+    let i = setInterval(() => {
+      ctx.update(getGenerator())
+      iteration++
+    }, 123)
 
-//     for await (const msg of ctx.source) {
-//       console.log(new TextDecoder().decode(msg))
-//     }
+    for await (const msg of ctx.source) {
+      console.log(new TextDecoder().decode(msg))
+    }
 
-//     clearInterval(i)
-// */
-
-//   })
-// })
+    clearInterval(i)
+*/
+  })
+})
