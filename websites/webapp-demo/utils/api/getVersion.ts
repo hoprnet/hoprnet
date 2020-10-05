eefcc4e6ca449d646bdc6a60f5e494058a3085d4
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-10-05 15:08:49 +0200

    Lint webapp demo

--- websites/webapp-demo/utils/api/getVersion.ts
+++ websites/webapp-demo/utils/api/getVersion.ts
@@ -1,10 +1,8 @@
-import { VersionPromiseClient } from "@hoprnet/hopr-protos/web/version_grpc_web_pb";
-import { VersionRequest } from "@hoprnet/hopr-protos/web/version_pb";
+import { VersionPromiseClient } from '@hoprnet/hopr-protos/web/version_grpc_web_pb'
+import { VersionRequest } from '@hoprnet/hopr-protos/web/version_pb'
 
 export const getVersion = async (apiUrl: string): Promise<string> => {
-  const client = new VersionPromiseClient(apiUrl);
+  const client = new VersionPromiseClient(apiUrl)
 
-  return client
-    .getVersion(new VersionRequest(), undefined)
-    .then((res) => res.getVersion());
-};
+  return client.getVersion(new VersionRequest(), undefined).then((res) => res.getVersion())
+}
