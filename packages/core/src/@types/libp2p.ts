faa9b5f9437faeeaa074a63225cad34f20b82c2e
Steve <stivennoni77@gmail.com>
2020-10-09 12:41:35 +0300

    experimental fix for the building process (#239)
    * 🐛 various build fixes
    
    * 🚧 add temporary scripts
    
    * 🚧 experiment with different building flow
    
    * 🚧 to link or not to link
    
    * 🎨 order devDeps
    
    * ♻️ change setup workflow
    
    * ♻️ regenerating yarn.lock files

--- packages/core/src/@types/libp2p.ts
+++ packages/core/src/@types/libp2p.ts
@@ -2,7 +2,7 @@ declare module 'libp2p' {
   type PeerId = import('peer-id')
   type PeerInfo = import('peer-info')
   type Multiaddr = import('multiaddr')
-  type Handler = import('../network/transport/types').Handler
+  type Handler = import('./transport').Handler
 
   export type PeerStore = {
     has(peerInfo: PeerId): boolean
