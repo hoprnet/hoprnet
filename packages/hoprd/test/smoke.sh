2038c74340b2f59c887ea5ae757331d27e3301a8
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-10-09 08:32:35 +0200

    open

--- packages/hoprd/test/smoke.sh
+++ packages/hoprd/test/smoke.sh
@@ -40,7 +40,7 @@ echo "Charlie running as pid $CHARLIE as $CHARLIE on $CHARLIE_ADDR:$CHARLIE_PORT
 hoprd --data='alice' --password="opensesame" --run="crawl; ping $CHARLIE"
 
 # Open channel alice -> bob and send a-b-c
-DEBUG=hopr* hoprd --data='alice' --password="opensesame" --run="crawl; open $BOB; send $CHARLIE hi"
+DEBUG=hopr* hoprd --data='alice' --password="opensesame" --run="crawl; open $BOB 0.1; send $CHARLIE hi"
 
 # Multihop send self via Bob and Charlie
 #DEBUG=hopr* hoprd --data='alice' --password="opensesame" --settings="{\"route\":\"$BOB,$CHARLIE\"}" --run="crawl; send $ALICE hi"
