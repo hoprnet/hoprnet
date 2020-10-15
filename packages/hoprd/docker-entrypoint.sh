62ef05c4688ac744ea98949a39ce89ac661bfbc7
nionis <stivennoni77@gmail.com>
2020-10-15 18:58:38 +0300

    🐛 remove args from process.yaml

--- packages/hoprd/docker-entrypoint.sh
+++ packages/hoprd/docker-entrypoint.sh
@@ -3,7 +3,10 @@ set -e
 
 # if no arguments passed on runtime, start in default mode
 if [ "$#" -eq 0 ]; then
-  exec pm2-runtime process.yaml --env "replaced_by_dockerfile_network" --admin
+  exec pm2-runtime process.yaml \
+    --password="open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0" \
+    --env "replaced_by_dockerfile_network" \
+    --admin
 else
   exec pm2-runtime process.yaml "${@:1}"
 fi
