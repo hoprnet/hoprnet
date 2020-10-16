035610aeebf05a7e162566b95b69ae1210faeaca
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-10-16 10:56:52 +0200

    Remove PeerInfo from crawler

--- packages/core/src/network/index.ts
+++ packages/core/src/network/index.ts
@@ -1,11 +1,13 @@
 import type { HoprOptions } from '..'
-import type PeerInfo from 'peer-info'
 import type { Interactions } from '../interactions'
 import type { LibP2P } from '../index'
 import { Crawler } from './crawler'
 import Heartbeat from './heartbeat'
 import PeerStore from './peerStore'
 import Stun from './stun'
+import Multiaddr from 'multiaddr'
+import PeerId from 'peer-id'
+import PeerInfo from 'peer-info'
 
 type TestOpts = {
   crawl?: { timeoutIntentionally?: boolean }
@@ -17,13 +19,25 @@ class Network {
   public stun?: Stun
 
   constructor(node: LibP2P, interactions: Interactions<any>, private options: HoprOptions, testingOptions?: TestOpts) {
+
+    // These are temporary, and will be replaced by accessors to the addressBook
+    const putPeer = (ma: Multiaddr) => {
+      const pinfo = new PeerInfo(PeerId.createFromB58String(ma.getPeerId()))
+      pinfo.multiaddrs.add(ma)
+      node.peerStore.put(pinfo)
+    }
+    const getPeer = (id: PeerId): Multiaddr[] => {
+      return node.peerStore.get(id).multiaddrs.toArray()
+    }
+
     this.peerStore = new PeerStore(node.peerStore.peers.values())
     this.heartbeat = new Heartbeat(this.peerStore, interactions.network.heartbeat, node.hangUp)
     this.crawler = new Crawler(
       node.peerInfo.id,
       this.peerStore,
       interactions.network.crawler,
-      node.peerStore,
+      getPeer,
+      putPeer,
       testingOptions?.crawl
     )
 
@@ -36,6 +50,7 @@ class Network {
       this.stun = new Stun(options.hosts)
     }
   }
+
 
   async start() {
     if (this.options.bootstrapNode) {
