60fc8d31bc3431acdb8316389e0d88c8f41b04d7
Beto Salas <alberto.salas@obsidiansoft.io>
2020-10-19 14:37:16 -0500

    Was added a new package

--- service/utils/db.js
+++ service/utils/db.js
@@ -1,9 +1,5 @@
-import debug from "debug";
 import fetch from "isomorphic-fetch";
 import { HOPR_DATABASE_URL } from "./env";
-
-const log = debug("hopr-leaderboard:firebase");
-const error = debug("hopr-leaderboard:firebase:error");
 
 export const FirebaseNetworkSchema = {
   basodino: 'basodino',
@@ -24,37 +20,37 @@ class FirebaseDatabase {
       const json = await response
         .json()
         .catch((err) =>
-          error(
+          console.error(
             `- resolveResponse | json :: Error parsing data from response`,
             err
           )
         );
-      log(`- resolveResponse | Retrieved json ${JSON.stringify(json)}`);
+      console.log(`- resolveResponse | Retrieved json ${JSON.stringify(json)}`);
       return { data: json, status: 200 };
     } else {
-      error(`- resolveResponse | Failed to retrieve data.`);
+      console.error(`- resolveResponse | Failed to retrieve data.`);
       return { data: null, status: 500 };
     }
   }
 
   async getSchema(schema) {
     try {
-      log(`- getSchema | Retrieving schema ${schema} from ${this.databaseUrl}`);
+      console.log(`- getSchema | Retrieving schema ${schema} from ${this.databaseUrl}`);
       const response = await fetch(
         `${this.databaseUrl}${schema}.json`
       ).catch((err) =>
-        error(`- getSchema | fetch :: Error retrieve data from database`, err)
+        console.error(`- getSchema | fetch :: Error retrieve data from database`, err)
       );
       return this.resolveResponse(response);
     } catch (err) {
-      error(`- getSchema | catch :: Error retrieving data`, err);
+      console.error(`- getSchema | catch :: Error retrieving data`, err);
       return { data: null, status: 500 };
     }
   }
 
   async getTable(schema, table) {
     try {
-      log(
+      console.log(
         `- getTable | Retrieving table ${table} within schema ${schema} from ${this.databaseUrl}`
       );
       const response = await fetch(
@@ -62,7 +58,7 @@ class FirebaseDatabase {
       );
       return this.resolveResponse(response);
     } catch (err) {
-      error(`- getTable | catch :: Error retrieving data`, err);
+      console.error(`- getTable | catch :: Error retrieving data`, err);
       return { data: null, status: 500 };
     }
   }
