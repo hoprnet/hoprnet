6bd4fef605e8c5eba98796c39a0372ad0354fc17
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-10-20 12:05:19 +0200

    Start updating dependencies and types

--- packages/core/src/@types/libp2p.ts
+++ packages/core/src/@types/libp2p.ts
@@ -1,15 +1,24 @@
+
 declare module 'libp2p' {
   type PeerId = import('peer-id')
-  type PeerInfo = import('peer-info')
   type Multiaddr = import('multiaddr')
   type Handler = import('./transport').Handler
 
+  export type PeerInfo = { 
+    id: PeerId;
+    addresses: Array<Multiaddr>;
+    metadata: Map<string, Buffer>;
+    protocols: Array<string>
+  }
+
   export type PeerStore = {
-    has(peerInfo: PeerId): boolean
-    get(peerId: PeerId): PeerInfo | undefined
-    put(peerInfo: PeerInfo, options?: { silent: boolean }): PeerInfo
+    get(peerId: PeerId):  PeerInfo | undefined
     peers: Map<string, PeerInfo>
-    remove(peer: PeerId): void
+    delete(peer: PeerId): void
+
+    addressBook: {
+      add(id: PeerId, addr: Multiaddr)
+    }
   }
 
   export default class LibP2P {
@@ -17,18 +26,17 @@ declare module 'libp2p' {
     static create(options: any): any
     // @TODO add libp2p types
     emit: (event: string, ...args: any[]) => void
-    dial: (addr: Multiaddr | PeerInfo | PeerId, options?: { signal: AbortSignal }) => Promise<Handler>
+    dial: (addr: Multiaddr | PeerId, options?: { signal: AbortSignal }) => Promise<Handler>
     dialer: any // TODO
     dialProtocol: (
-      addr: Multiaddr | PeerInfo | PeerId,
+      addr: Multiaddr | PeerId,
       protocol: string,
       options?: { signal: AbortSignal }
     ) => Promise<Handler>
-    hangUp: (addr: PeerInfo | PeerId | Multiaddr | string) => Promise<void>
-    peerInfo: PeerInfo
+    hangUp: (addr: PeerId | Multiaddr | string) => Promise<void>
     peerStore: PeerStore
     peerRouting: {
-      findPeer: (addr: PeerId) => Promise<PeerInfo>
+      findPeer: (addr: PeerId) => Promise<{ id: PeerId, multiaddrs: Multiaddr[] }>
     }
     handle: (protocol: string | string[], handler: (struct: { connection: any; stream: any }) => void) => void
     on: (str: string, handler: (...props: any[]) => void) => void
