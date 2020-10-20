9dc65bac8e1cdabf4a70f7a3c0e7b3c92555da55
Steve <stivennoni77@gmail.com>
2020-10-20 11:53:49 +0300

    add leaderboard-new (#380)
    * Initial commit
    
    * base to mobile view ONLY
    
    * ?up^
    
    * update navegation
    
    * get the url pathname
    
    * views and urls good wk..
    
    * modal copy --- help
    
    * chg,, nam..Root,, twitter link
    
    * desktop version base
    
    * API consume,
    
    * Was added the API's
    
    * Fix unused var
    
    * Remove the console log
    
    * update
    
    * Was added a new package
    
    * API test V1
    
    * Modify the environment
    
    * Added stats api
    
    * fix css styles
    
    * fix API connect data-box
    
    * Added functionality to return all data, score and stats
    
    * add, fix & crea. sty.. but w've a probl. on _ tab
    
    * error on index.js
    
    * Was added validation to check if data exist
    
    * add fix
    
    * clean styles and components
    
    * fix style
    
    * fix modal
    
    * maybe is good idea create table hand
    
    * remove rc-table
    
    * a review is uploaded
    
    * updated table fix mb
    
    * fix tbl...
    
    * fix discord link
    
    * Was added functionality to show the top 5 of more score users
    
    * add styles and tables
    
    * update Style
    
    * Updated network
    
    * ✨ add fathom & small UI changes
    
    * ✨ update leaderboard workflow
    
    * 🐛 prettier ignore leaderboard-new
    
    Co-authored-by: Luis David Gutierrez Velazquez <david.gutierrez@obsidiansoft.io>
    Co-authored-by: David Lopez <david@Davids-Mac-mini.local>
    Co-authored-by: Beto Salas <alberto.salas@obsidiansoft.io>
    Co-authored-by: lodi <lodi.dev@outlook.com>
    Co-authored-by: hopr-jjperezaguinaga <jose.aguinaga@hoprnet.org>
    Co-authored-by: Steven Nonis <steven.noni@hoprnet.org>

---
+++ websites/leaderboard-new/service/components/layout/left-side.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import "../../styles/main.scss";
+
+const useUser = () => ({ user: null, loading: false });
+
+const LeftSide = () => {
+  const router = useRouter();
+  const [hash, setHash] = useState(
+    "16Uiu2HAmRE4fVtp8dF6H62NzRcx6LGUTL5fBRTdnAfZXjveP5Kz9"
+  );
+  const [modal, setModal] = useState(false);
+  const copyCodeToClipboard = () => {
+    navigator.clipboard.writeText(hash);
+    setModal(true);
+    setTimeout(() => {
+      setModal(false);
+    }, 4000);
+  };
+
+  return (
+    <section className="area-left-desktop">
+      <div className="menu-desktop">
+        <Link href="/">
+          <div
+            className={
+              "menu-item-desktop " + [router.pathname == "/" ? "active" : ""]
+            }
+          >
+            <img src="/assets/icons/home.svg" alt="hopr HOME" />
+            <p>HOME</p>
+          </div>
+        </Link>
+        <Link href="/top-assets">
+          <div
+            className={
+              "menu-item-desktop " +
+              [router.pathname == "/top-assets" ? "active" : ""]
+            }
+          >
+            <img src="/assets/icons/top.svg" alt="hopr Top ASSETS" />
+            <p>TOP ASSETS</p>
+          </div>
+        </Link>
+
+        <div className="menu-item-desktop ">
+          <a
+            target="_blank"
+            href="https://discord.com/invite/wUSYqpD"
+            rel="noopener noreferrer"
+          >
+            <img src="/assets/icons/discord.svg" alt="hopr DISCORD" />
+            <p>DISCORD</p>
+          </a>
+        </div>
+
+        <Link href="/help">
+          <div
+            className={
+              "menu-item-desktop " +
+              [router.pathname == "/help" ? "active" : ""]
+            }
+          >
+            <img src="/assets/icons/help.svg" alt="hopr HELP" />
+            <p>HELP</p>
+          </div>
+        </Link>
+      </div>
+      {/*  */}
+
+      <div className="twitter-line-menu">
+        <div>
+          <a href="https://twitter.com/hoprnet" target="_blank">
+            <img src="/assets/icons/twitter.svg" alt="twitter" />
+            <p>@hoprnet</p>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fsaentis.hoprnet.org%2F&amp;ref_src=twsrc%5Etfw&amp;related=hoprnet&amp;text=Signing%20up%20to%20earn%20%24HOPR%20on%20the%20%23HOPRnetwork.%20My%20%40hoprnet%20address%20is%3A%20&amp;tw_p=tweetbutton"
+            target="_blank"
+          >
+            <img src="/assets/icons/twitter.svg" alt="twitter" />
+            <p>#basodino</p>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LeftSide;
---
+++ websites/leaderboard-new/service/utils/env.js
@@ -0,0 +1,7 @@
+const HOPR_NETWORK = process.env["NEXT_PUBLIC_HOPR_NETWORK"] || "basodino-develop";
+const HOPR_DATABASE_URL = process.env["NEXT_PUBLIC_HOPR_DATABASE_URL"] || "hopr-coverbot";
+
+module.exports = {
+  HOPR_NETWORK,
+  HOPR_DATABASE_URL
+};
