7e82954989e357dd48cb3830087ed104ddb79f9f
nionis <stivennoni77@gmail.com>
2020-10-20 15:08:24 +0300

    ✨ update HOPR_NETWORK

--- service/utils/env.js
+++ service/utils/env.js
@@ -1,7 +1,8 @@
-const HOPR_NETWORK = process.env["NEXT_PUBLIC_HOPR_NETWORK"] || "basodino-develop";
-const HOPR_DATABASE_URL = process.env["NEXT_PUBLIC_HOPR_DATABASE_URL"] || "hopr-coverbot";
+const HOPR_NETWORK = process.env["NEXT_PUBLIC_HOPR_NETWORK"] || "basodino";
+const HOPR_DATABASE_URL =
+  process.env["NEXT_PUBLIC_HOPR_DATABASE_URL"] || "hopr-coverbot";
 
 module.exports = {
   HOPR_NETWORK,
-  HOPR_DATABASE_URL
+  HOPR_DATABASE_URL,
 };
