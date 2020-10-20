1132848f193dce46358af04c5caaa90ccb434791
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-10-20 10:38:21 +0200

    bump timeout

--- packages/core/src/interactions/network/heartbeat.spec.ts
+++ packages/core/src/interactions/network/heartbeat.spec.ts
@@ -14,7 +14,7 @@ import { EventEmitter } from 'events'
 import * as constants from '../../constants';
 
 // @ts-ignore
-constants.HEARTBEAT_TIMEOUT = 30
+constants.HEARTBEAT_TIMEOUT = 300
 
 describe('check heartbeat mechanism', function () {
   async function generateNode(options?: { timeoutIntentionally?: boolean }) {
