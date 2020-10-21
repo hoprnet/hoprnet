eb06bb042ac58bed25ff4d870798daeadc7fee6f
David Lopez <david@Davids-Mac-mini.local>
2020-10-21 11:22:02 -0500

    add link to medium post

--- websites/leaderboard-new/service/components/layout/right-side.js
+++ websites/leaderboard-new/service/components/layout/right-side.js
@@ -7,20 +7,16 @@ const RightSide = () => {
   return (
     <section className="right-side only-desktop-view">
       <p className="paragraph">
-        Welcome to <span>HOPR Basòdino testnet!</span> Visit HELP in the menu
+        Welcome to  <a  
+        className="aux-link-out"
+          target="_blank"
+          href="https://medium.com/hoprnet"
+          rel="noopener noreferrer"><span>HOPR Basòdino testnet!</span></a> Visit HELP in the menu
         for instructions. Registration is open. CoverBot will begin relaying
         data on <span>21st Oct</span> at <span>3pm CET</span>. The{" "}
         <span>200</span> highest scorers will win a share of{" "}
         <span>200,000 HOPR</span>. The testnet will run until{" "}
-        <span>Nov 4th</span>.{" "}
-        <a  
-        className="aux-link-out"
-          target="_blank"
-          href="https://medium.com/hoprnet"
-          rel="noopener noreferrer">
-          
-           follow us on <span>[<img src="/assets/icons/medium.svg" alt="medium" />] medium.</span>
-        </a>
+        <span>Nov 4th</span>. 
       </p>
       <hr />
       <DataBoxCloud />
