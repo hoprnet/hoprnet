52557555588b117b7bfc9357b51c7819e2a1485f
Robert Kiel <robert.kiel@hoprnet.io>
2020-10-23 11:08:40 +0200

    reenable protocol

--- packages/core/src/network/transport/index.ts
+++ packages/core/src/network/transport/index.ts
@@ -134,17 +134,17 @@ class TCP {
 
       log(`####### inside reconnect #######`)
 
-      // newStream.sink((async function * () {
-      //   let i = 0
-      //   while (i < 7) {
-      //     yield new TextEncoder().encode(`message #${i++}`)
-      //     await new Promise(resolve => setTimeout(resolve, 20))
-      //   }
-      // })())
+      newStream.sink((async function * () {
+        let i = 0
+        while (i < 7) {
+          yield new TextEncoder().encode(`message #${i++}`)
+          await new Promise(resolve => setTimeout(resolve, 20))
+        }
+      })())
 
-      // for await (const msg of newStream.source) {
-      //   log(`received in reconnect:`, msg?.slice())
-      // }
+      for await (const msg of newStream.source) {
+        log(`received in reconnect:`, msg?.slice())
+      }
       try {
         this._upgrader.upgradeInbound(newStream).then((conn: Connection) => this.connHandler?.(conn))
       } catch (err) {
