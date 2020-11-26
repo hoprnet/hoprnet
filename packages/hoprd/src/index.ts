08f77c1fdac909516f9946da414fbe4082f6f8b9
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-11-26 16:43:36 +0100

    bump commit

--- packages/hoprd/src/index.ts
+++ packages/hoprd/src/index.ts
@@ -14,6 +14,7 @@ import { LogStream } from './logs'
 import { AdminServer } from './admin'
 import * as yargs from 'yargs'
 
+
 /**
  * TEMPORARY HACK - copy pasted from
  * https://github.com/hoprnet/hopr-chat/blob/master/utils/checkPeerId.ts
