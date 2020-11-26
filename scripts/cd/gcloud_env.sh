af8feffb4262b937618def1bb4dc6540f513a16f
jjperezaguinaga <me@jjperezaguinaga.com>
2020-11-26 05:25:36 +0000

    Better naming and structure

--- scripts/cd/gcloud_env.sh
+++ scripts/cd/gcloud_env.sh
@@ -11,6 +11,10 @@ fi
 
 if [[ $(gcloud compute instances list | grep ${ENV}-bootstrap) ]]; then
   echo "GCLOUD_UPDATE_CONTAINER=true"
+  echo "GCLOUD_CREATE_CONTAINER=false"
 else
+  echo "GCLOUD_UPDATE_CONTAINER=false"
   echo "GCLOUD_CREATE_CONTAINER=true"
 fi
+
+echo "GCLOUD_VM_NAME=${ENV}-bootstrap"
