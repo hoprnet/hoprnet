ab5f6b20f6e6bf11e86143c14e039a62e8125eb8
jjperezaguinaga <me@jjperezaguinaga.com>
2020-11-26 05:29:16 +0000

    Simplified action process

--- scripts/cd/gcloud_env.sh
+++ scripts/cd/gcloud_env.sh
@@ -10,11 +10,9 @@ else
 fi
 
 if [[ $(gcloud compute instances list | grep ${ENV}-bootstrap) ]]; then
-  echo "GCLOUD_UPDATE_CONTAINER=true"
-  echo "GCLOUD_CREATE_CONTAINER=false"
+  echo "GCLOUD_ACTION_CONTAINER=update"
 else
-  echo "GCLOUD_UPDATE_CONTAINER=false"
-  echo "GCLOUD_CREATE_CONTAINER=true"
+  echo "GCLOUD_ACTION_CONTAINER=create"
 fi
 
 echo "GCLOUD_VM_NAME=${ENV}-bootstrap"
