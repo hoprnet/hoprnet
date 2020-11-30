ae5d005b34861dce224364dcb9c7a6050efd915b
nionis <stivennoni77@gmail.com>
2020-11-30 10:23:52 +0200

    do not print uninitialized channels

--- packages/chat/commands/listOpenChannels.ts
+++ packages/chat/commands/listOpenChannels.ts
@@ -104,6 +104,10 @@ export default class ListOpenChannels extends AbstractCommand {
             balance_a
           }))
 
+          const status = channelData.status
+          // do not print UNINITIALISED channels
+          if (status === 'UNINITIALISED') continue
+
           const selfIsPartyA = utils.isPartyA(self, counterParty)
           const totalBalance = moveDecimalPoint(channelData.balance.toString(), types.Balance.DECIMALS * -1)
           const myBalance = moveDecimalPoint(
@@ -111,7 +115,6 @@ export default class ListOpenChannels extends AbstractCommand {
             types.Balance.DECIMALS * -1
           )
           const peerId = (await pubKeyToPeerId(channelData.offChainCounterparty)).toB58String()
-          const status = channelData.status
 
           result.push(
             this.generateOutput({
