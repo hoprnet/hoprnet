051876440aa0f039fa38a49ee4562040baf1f56f
Robert Kiel <robert.kiel@hoprnet.io>
2020-12-01 18:56:41 +0100

    minor improvements

--- packages/utils/src/u8a/xor.spec.ts
+++ packages/utils/src/u8a/xor.spec.ts
@@ -6,7 +6,7 @@ describe('testing XORing Uint8Array', function () {
     let a = new Uint8Array([0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0])
     let b = new Uint8Array([255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255])
 
-    let aXORb = new Uint8Array([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255])
+    let aXORb = new Uint8Array(15).fill(255)
 
     assert.deepStrictEqual(u8aXOR(false, a, b), aXORb)
 
@@ -17,9 +17,9 @@ describe('testing XORing Uint8Array', function () {
   it('should XOR more than two arrays', function () {
     let a = new Uint8Array([0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0])
     let b = new Uint8Array([255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255])
-    let c = new Uint8Array([15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15])
+    let c = new Uint8Array(15).fill(15)
 
-    let aXORbXORc = new Uint8Array([240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240])
+    let aXORbXORc = new Uint8Array(15).fill(240)
 
     assert.deepStrictEqual(u8aXOR(false, a, b, c), aXORbXORc)
 
--- packages/utils/src/u8a/xor.ts
+++ packages/utils/src/u8a/xor.ts
@@ -5,11 +5,13 @@
  * @param list arrays to XOR
  */
 export function u8aXOR(inPlace: boolean = false, ...list: Uint8Array[]): Uint8Array {
-  if (!list.slice(1).every((array) => array.length == list[0].length)) {
+  const list0length = list[0].length
+
+  if (!list.slice(1).every((array) => array.length == list0length)) {
     throw Error(`Uint8Arrays must not have different sizes`)
   }
 
-  const result = inPlace ? list[0] : new Uint8Array(list[0].length)
+  const result = inPlace ? list[0] : new Uint8Array(list0length)
 
   if (list.length == 2) {
     let index = 0
@@ -19,19 +21,19 @@ export function u8aXOR(inPlace: boolean = false, ...list: Uint8Array[]): Uint8Ar
 
     let list1 = new DataView(list[1].buffer)
 
-    for (; index + 8 <= list[0].length; index += 8) {
+    for (; index + 8 <= list0length; index += 8) {
       resultView.setBigUint64(index, list0.getBigUint64(index) ^ list1.getBigUint64(index))
     }
 
-    for (; index + 4 <= list[0].length; index += 4) {
+    for (; index + 4 <= list0length; index += 4) {
       resultView.setUint32(index, list0.getUint32(index) ^ list1.getUint32(index))
     }
 
-    for (; index + 2 <= list[0].length; index += 2) {
+    for (; index + 2 <= list0length; index += 2) {
       resultView.setUint16(index, list0.getUint16(index) ^ list1.getUint16(index))
     }
 
-    if (index < list[0].length) {
+    if (index < list0length) {
       resultView.setUint8(index, list0.getUint8(index) ^ list1.getUint8(index))
     }
   } else {
@@ -40,31 +42,33 @@ export function u8aXOR(inPlace: boolean = false, ...list: Uint8Array[]): Uint8Ar
     let listView = list.map((u8a) => new DataView(u8a.buffer))
     let resultView = inPlace ? listView[0] : new DataView(result.buffer)
 
-    for (; index + 8 <= list[0].length; index += 8) {
+    const listLength = list.length
+
+    for (; index + 8 <= list0length; index += 8) {
       resultView.setBigUint64(index, listView[0].getBigUint64(index) ^ listView[1].getBigUint64(index))
-      for (let j = 2; j < list.length; j++) {
+      for (let j = 2; j < listLength; j++) {
         resultView.setBigUint64(index, resultView.getBigUint64(index) ^ listView[j].getBigUint64(index))
       }
     }
 
-    for (; index + 4 <= list[0].length; index += 4) {
+    for (; index + 4 <= list0length; index += 4) {
       resultView.setUint32(index, listView[0].getUint32(index) ^ listView[1].getUint32(index))
-      for (let j = 2; j < list.length; j++) {
+      for (let j = 2; j < listLength; j++) {
         resultView.setUint32(index, resultView.getUint32(index) ^ listView[j].getUint32(index))
       }
     }
 
-    for (; index + 2 <= list[0].length; index += 2) {
+    for (; index + 2 <= list0length; index += 2) {
       resultView.setUint16(index, listView[0].getUint16(index) ^ listView[1].getUint16(index))
-      for (let j = 2; j < list.length; j++) {
+      for (let j = 2; j < listLength; j++) {
         resultView.setUint16(index, resultView.getUint16(index) ^ listView[j].getUint16(index))
       }
     }
 
-    if (index < list[0].length) {
+    if (index < list0length) {
       resultView.setUint8(index, listView[0].getUint8(index) ^ listView[1].getUint8(index))
 
-      for (let j = 2; j < list.length; j++) {
+      for (let j = 2; j < listLength; j++) {
         resultView.setUint8(index, resultView.getUint8(index) ^ listView[j].getUint8(index))
       }
     }
