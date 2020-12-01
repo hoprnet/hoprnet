26ebaf83a2a5da8d1316c754482ba3ea1e4dbfc8
HOPR Linting robot <noreply@hoprnet.org>
2020-12-01 12:36:21 +0000

    Fix linting

--- test/e2e/set-up.js
+++ test/e2e/set-up.js
@@ -1,29 +1,22 @@
-const util = require('util');
-const execFile = util.promisify(require('child_process').execFile);
+const util = require('util')
+const execFile = util.promisify(require('child_process').execFile)
 
 const releaseEnvScript = './scripts/cd/release_env.sh'
 const addressEnvScript = './scripts/cd/gcloud_env.sh'
 
-const parseEnvs = (stdout) => 
-  stdout.split('\n').map(line => 
-    line.length > 0 && line.split('='))
+const parseEnvs = (stdout) => stdout.split('\n').map((line) => line.length > 0 && line.split('='))
 
-const parseStdout = (stdout, envMap) => 
+const parseStdout = (stdout, envMap) =>
   parseEnvs(stdout)
-  .filter(Boolean)
-  .map(envs => (([env, value]) => envMap[env] = value)(envs))
+    .filter(Boolean)
+    .map((envs) => (([env, value]) => (envMap[env] = value))(envs))
 
 const main = async () => {
   try {
     const envMap = {}
-    const envPromises = [
-      execFile(releaseEnvScript),
-      execFile(addressEnvScript)
-    ]
+    const envPromises = [execFile(releaseEnvScript), execFile(addressEnvScript)]
     const resolvedEnvPromises = await Promise.all(envPromises)
-    resolvedEnvPromises.map(
-      ({stdout, stderr}) => parseStdout(stdout, envMap)
-    )
+    resolvedEnvPromises.map(({ stdout, stderr }) => parseStdout(stdout, envMap))
     console.log(envMap)
   } catch (e) {
     console.error('Error', e)
@@ -31,4 +24,3 @@ const main = async () => {
 }
 
 main()
-
