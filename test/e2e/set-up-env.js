fff0be5a9302da4795cc042c68eb951d9dfc1248
HOPR Linting robot <noreply@hoprnet.org>
2020-12-02 05:49:19 +0000

    Fix linting

--- test/e2e/set-up-env.js
+++ test/e2e/set-up-env.js
@@ -9,14 +9,13 @@ const DIR_NAME = './scripts/cd/'
 const releaseEnvScript = DIR_NAME + 'release_env.sh'
 const gcloudEnvScript = DIR_NAME + 'gcloud_env.sh'
 
-
 const main = async () => {
   try {
     const envMap = {}
     envMap.RELEASE_VERSION = RELEASE_VERSION
     envMap.BS_PASSWORD = BS_PASSWORD
     await Promise.all([execFile(releaseEnvScript), execFile(gcloudEnvScript)]).then(utils.parsePromises(envMap))
-    return envMap;
+    return envMap
   } catch (e) {
     console.error('Error', e)
     return {}
--- test/e2e/set-up.js
+++ test/e2e/set-up.js
@@ -1,5 +1,5 @@
 const execFile = require('child_process').execFile
-const setupEnv = require('./set-up-env');
+const setupEnv = require('./set-up-env')
 
 const DIR_NAME = './scripts/cd/'
 
@@ -7,17 +7,17 @@ const gcloudEnvScript = DIR_NAME + 'gcloud_vm.sh'
 
 const main = async () => {
   try {
-    const env = await setupEnv();
-    console.log(env);
+    const env = await setupEnv()
+    console.log(env)
     const child = execFile(gcloudEnvScript, [], { env })
     child.stdout.on('data', function (data) {
-      console.log(data.toString());
-    });
+      console.log(data.toString())
+    })
     child.stderr.on('data', function (data) {
-      console.log(data.toString());
-    });
+      console.log(data.toString())
+    })
   } catch (e) {
-    console.error('Error', e);
+    console.error('Error', e)
   }
 }
 
