b85c4b1b2bb6a6d30b0b777721cb4f1c8a938171
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-12-04 11:01:24 +0100

    Version

--- packages/chat/commands/version.ts
+++ packages/chat/commands/version.ts
@@ -1,44 +1,16 @@
 import { AbstractCommand } from './abstractCommand'
-import pkg from '../package.json'
-import { styleValue, getPaddingLength } from '../utils'
+import { FULL_VERSION } from '@hoprnet/hopr-core'
 
 export default class Version extends AbstractCommand {
-  private items: {
-    name: string
-    version: string
-  }[] = [
-    {
-      name: 'hopr-chat',
-      version: pkg.version
-    },
-    {
-      name: 'hopr-core',
-      version: pkg.dependencies['@hoprnet/hopr-core']
-    }
-  ]
-
   public name() {
     return 'version'
   }
 
   public help() {
-    return 'Displays the versions for `hopr-chat` and `hopr-core`'
+    return 'Displays the version'
   }
 
   public async execute(): Promise<string> {
-    const items = this.items.map((item) => ({
-      name: item.name + ': ',
-      version: styleValue(item.version, 'number')
-    }))
-    const paddingLength = getPaddingLength(
-      items.map((item) => item.name),
-      false
-    )
-
-    return items
-      .map((item) => {
-        return `${item.name.padEnd(paddingLength)}${item.version}`
-      })
-      .join('\n')
+    return FULL_VERSION
   }
 }
