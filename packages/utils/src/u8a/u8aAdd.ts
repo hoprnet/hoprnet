9cf9cf666c5b6e149d8a4d48ce7a0f6c76103eb8
Robert Kiel <robert.kiel@hoprnet.org>
2020-12-08 11:20:03 +0100

    Feature/improve u8a functions (#815)
    * include hopr-connect
    
    * housekeeping
    
    * remove unnecessary dependency
    
    * hopr-connect upgrade
    
    * hopr-connect upgrade
    
    * Fix linting
    
    * update hopr-connect
    
    * update hopr-connect
    
    * Fix linting
    
    * update hopr-connect
    
    * remove transport code:
    
    * remove unnecessary packages
    
    * recover declaration file
    
    * improved u8a functions
    
    * Fix linting
    
    * Fix linting
    
    * little optimisation
    
    * bugfixes
    
    * minor improvements
    
    * fix edge cases
    
    * Fix linting
    
    * fix types
    
    Co-authored-by: Robert Kiel <robert.kiel@hoprnet.io>
    Co-authored-by: HOPR Linting robot <noreply@hoprnet.org>

--- packages/utils/src/u8a/u8aAdd.ts
+++ packages/utils/src/u8a/u8aAdd.ts
@@ -1,7 +1,11 @@
 /**
  * Adds the contents of two arrays together while ignoring the final overflow.
- * Computes a + b mod ( 2^(8 * a.length) - 1)
+ * Computes `a + b % ( 2 ** (8 * a.length) - 1)`
  *
+ * @example
+ * u8aAdd(false, new Uint8Array([1], new Uint8Array([2])) // Uint8Array([3])
+ * u8aAdd(false, new Uint8Array([1], new Uint8Array([255])) // Uint8Array([0])
+ * u8aAdd(false, new Uint8Array([0, 1], new Uint8Array([0, 255])) // Uint8Array([1, 0])
  * @param inplace result is stored in a if set to true
  * @param a first array
  * @param b second array
@@ -15,7 +19,7 @@ export function u8aAdd(inplace: boolean, a: Uint8Array, b: Uint8Array): Uint8Arr
   for (let offset = a.length; offset > 0; offset--) {
     tmp = a[offset - 1] + b[offset - 1] + overflow
     overflow = tmp >> 8
-    result.set([tmp], offset - 1)
+    result[offset - 1] = tmp
   }
 
   return result
