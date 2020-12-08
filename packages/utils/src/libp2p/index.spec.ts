f7bee82943179ec87e689b76934f5834c2e94e7e
Robert Kiel <robert.kiel@hoprnet.io>
2020-12-08 12:49:28 +0100

    cleanup

--- packages/utils/src/libp2p/index.spec.ts
+++ packages/utils/src/libp2p/index.spec.ts
@@ -2,8 +2,6 @@ import assert from 'assert'
 import PeerId from 'peer-id'
 import { convertPubKeyFromPeerId, convertPubKeyFromB58String, hasB58String, getB58String } from '.'
 import { validTweetsMap } from './tweets'
-// @ts-ignore
-import * as multihashes from 'multihashes'
 
 describe(`test convertPubKeyFromPeerId`, function () {
   it(`should equal to a newly created pubkey from PeerId`, async function () {
