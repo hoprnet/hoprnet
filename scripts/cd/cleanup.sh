a539b0c30e3fcd8e8d7827f97c7c2dbdd7b778a6
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-12-14 11:20:16 +0100

    remove old instances

--- scripts/cd/cleanup.sh
+++ scripts/cd/cleanup.sh
@@ -6,7 +6,6 @@ fi
 
 OLD_RELEASES='zurich zug luzern basodino'
 
-
 cleanup_ips() {
   local IPS=$(gcloud compute addresses list)
   for x in $OLD_RELEASES
@@ -14,10 +13,24 @@ cleanup_ips() {
     local IP=$(echo "$IPS" | grep $x | awk '{ print $2 }')
     if [ $IP ]; then
       local NAME=$(echo "$IPS" | grep $x | awk '{ print $1 }')
-      echo "releasing - $NAME ($IP)"
+      echo "- releasing - $NAME ($IP)"
       gcloud compute addresses delete $NAME $REGION --quiet
     fi
   done  
 }
 
-cleanup_ips
+cleanup_instances() {
+  local INSTANCES=$(gcloud compute instances list)
+  for old in $OLD_RELEASES
+  do
+    echo "$INSTANCES" | grep $old | grep 'RUNNING' | while read -r instance; do
+      local name=$(echo "$instance" | awk '{ print $1 }')
+      echo "- stopping $name"
+      gcloud compute instances stop $name $ZONE
+    done
+  done
+}
+
+#cleanup_ips
+cleanup_instances
+
