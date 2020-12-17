89a9c75661e953ae74a8be96a0cd1eeb1d293381
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-12-17 09:49:48 +0100

    wip on scripts

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -7,7 +7,7 @@ source scripts/cleanup.sh
 # ----- Nightly integration / network test. --------
 
 if [ -z "$RPC" ]; then
-  RPC=https://rpc-mainnet.matic.network
+  RPC=https://bsc-dataseed.binance.org/
 fi
 
 # Get version from package.json
@@ -15,11 +15,13 @@ RELEASE=$(node -p -e "require('./packages/hoprd/package.json').version")
 IMG="gcr.io/hoprassociation/hoprd:$RELEASE"
 
 source scripts/dependencies.sh
-echo "Cleaning up devops before running nightly testnet"
-cleanup
+#echo "Cleaning up devops before running nightly testnet"
+#cleanup
 echo "Starting nightly testnet"
 start_testnet nightly 2 $IMG
+echo "Testnet up and running. Leaving it for 20 mins"
 sleep 72000 # 20mins
+echo "Testnet has run for 20m, time to kill it."
 gcloud_get_logs nightly-bootstrap $IMG > bootstrap-logs.txt 
 gcloud_get_logs nightly-node-2 $IMG > node-2.txt 
 cat bootstrap-logs.txt
---
+++ scripts/send-hopr.js
@@ -0,0 +1,13 @@
+// TODO - addresses are hardcoded.
+
+(accounts[0]
+  .getAddress()
+  .then(address => (
+    new ethers.Contract(
+      "0x4F88909ecc41eC032162683aa56c2384a0630f8B", 
+      ["function balanceOf(address owner) view returns (uint256)"],
+      new ethers.providers.JsonRpcProvider("https://rpc-mainnet.matic.network"))
+    ).balanceOf(address)
+    .then(b => formatEther(b))
+  )
+)
