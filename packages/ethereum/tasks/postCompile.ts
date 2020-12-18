f22d81fc9c589f98ad7db71cf883296aa7e89136
nionis <stivennoni77@gmail.com>
2020-12-18 12:59:29 +0200

    fix postCompile

--- packages/ethereum/tasks/postCompile.ts
+++ packages/ethereum/tasks/postCompile.ts
@@ -1,32 +1,47 @@
 import type { HardhatRuntimeEnvironment, RunSuperFunction } from 'hardhat/types'
-import type { Export } from 'hardhat-deploy/types'
+import type { MultiExport } from 'hardhat-deploy/types'
 import { join } from 'path'
 import { promises, existsSync } from 'fs'
 
-const { readFile, writeFile, mkdir } = promises
-const CHAIN_DIR = join(__dirname, '..', 'chain')
-const ABIS_DIR = join(CHAIN_DIR, 'abis')
+const { mkdir, writeFile } = promises
+const OUTPUT_DIR = join(__dirname, '..', 'chain', 'abis')
 
 /**
- * Updates chain/abis folder after compilation.
+ * Updates 'chain/abis/X.json' with the latest ABI
  */
 async function main(_params, { run, config }: HardhatRuntimeEnvironment, _runSuper: RunSuperFunction<any>) {
-  const fileDir = join(config.paths.cache, 'deployed_contracts.json')
+  const inputDir = join(config.paths.cache, 'export_all.json')
 
   // use hardhat-deploy export to get data about the contracts
-  await run('export', { export: fileDir })
-
+  await run('export', { exportAll: inputDir })
   // read exported data of this network
-  const result: Export = JSON.parse(await readFile(fileDir, { encoding: 'utf-8' }))
+  const exportResult: MultiExport = require(inputDir)
 
-  if (!existsSync(ABIS_DIR)) {
-    await mkdir(ABIS_DIR, { recursive: true })
+  // ensure chain folder is created
+  if (!existsSync(OUTPUT_DIR)) {
+    await mkdir(OUTPUT_DIR, { recursive: true })
   }
 
-  // store abi files
+  const abis: {
+    [contractName: string]: any[]
+  } = {}
+
+  // loop through exported result and retrieve what we need
+  for (const perChainId of Object.values(exportResult)) {
+    for (const { name: networkName, contracts } of Object.values(perChainId)) {
+      // no need to do this for hardhat network
+      if (networkName === 'hardhat') continue
+
+      for (const [contractName, { abi }] of Object.entries(contracts)) {
+        abis[contractName] = abi
+      }
+    }
+  }
+
+  // store updated data.json
   return Promise.all(
-    Object.entries(result.contracts).map(([contractName, { abi }]) => {
-      return writeFile(join(ABIS_DIR, `${contractName}.json`), JSON.stringify(abi, null, 2))
+    Object.entries(abis).map(([contractName, abi]) => {
+      return writeFile(join(OUTPUT_DIR, `${contractName}.json`), JSON.stringify(abi, null, 2))
     })
   )
 }
