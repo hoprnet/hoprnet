9405607ebd9b6368b4ddb9380d0105aef5c86ada
Robert Kiel <robert.kiel@hoprnet.org>
2021-01-12 09:04:54 +0100

    proper import

--- packages/core/src/interactions/packet/acknowledgement.ts
+++ packages/core/src/interactions/packet/acknowledgement.ts
@@ -20,11 +20,10 @@ import type { Handler } from 'libp2p'
 import EventEmitter from 'events'
 
 import { PROTOCOL_ACKNOWLEDGEMENT } from '../../constants'
-import { u8aToHex, durations, u8aConcat, toU8a, u8aToNumber, pubKeyToPeerId } from '@hoprnet/hopr-utils'
+import { u8aToHex, durations, u8aConcat, toU8a, u8aToNumber, pubKeyToPeerId, u8aAdd } from '@hoprnet/hopr-utils'
 import { UnacknowledgedTicket } from '../../messages/ticket'
 
 import { ACKNOWLEDGED_TICKET_INDEX_LENGTH } from '../../dbKeys'
-import { u8aAdd } from '../../../../utils/lib'
 
 const ONE = toU8a(1, 8)
 
