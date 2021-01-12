c64b9e87dd1ac71c7677cbe2d82287f7232ae204
nionis <stivennoni77@gmail.com>
2021-01-12 15:59:24 +0200

    add index.rst

--- docs/hopr-documentation-sphinx/conf.py
+++ docs/hopr-documentation-sphinx/conf.py
@@ -2,3 +2,17 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath('.'))
+
+numfig = True
+
+extensions = []
+extensions.append('sphinx.ext.todo')
+extensions.append('sphinx.ext.autodoc')
+# extensions.append('sphinx.ext.autosummary')
+extensions.append('sphinx.ext.intersphinx')
+extensions.append('sphinx.ext.mathjax')
+extensions.append('sphinx.ext.viewcode')
+extensions.append('sphinx.ext.graphviz')
+
+autosummary_generate = True
+html_theme = 'default'
