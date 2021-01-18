7f986def7750dbf172ae7865bbff4d6beb0a2736
HOPR Linting robot <noreply@hoprnet.org>
2021-01-18 09:22:41 +0000

    Fix linting

--- packages/hoprd/src/admin.ts
+++ packages/hoprd/src/admin.ts
@@ -95,19 +95,19 @@ export class AdminServer {
     })
 
     this.node.on('hopr:warning:unfunded', (addr) => {
-      const min = (new node.paymentChannels.types.Balance(0)).toFormattedString.apply(MIN_NATIVE_BALANCE)
+      const min = new node.paymentChannels.types.Balance(0).toFormattedString.apply(MIN_NATIVE_BALANCE)
       this.logs.log(
         `- The account associated with this node has no funds,\n` +
-        `  in order to send messages, or open channels, you will need to send` +
-        `  at least ${min} to ${addr}`
+          `  in order to send messages, or open channels, you will need to send` +
+          `  at least ${min} to ${addr}`
       )
     })
 
     this.node.on('hopr:warning:unfundedNative', (addr) => {
       this.logs.log(
         `- The account associated with this node has no BNB,\n` +
-        `  in order to fund gas for protocol overhead you will need to send\n` +
-        `  0.025 BNB to ${addr}`
+          `  in order to fund gas for protocol overhead you will need to send\n` +
+          `  0.025 BNB to ${addr}`
       )
     })
 
