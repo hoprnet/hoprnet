99aa8efbc9a2eed4bd6d7f63b4b773e1fe36a39a
nionis <stivennoni77@gmail.com>
2021-01-21 10:49:43 +0200

    reverse one-off MAX_HOPS change

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -29,7 +29,7 @@ export const HEARTBEAT_TIMEOUT = durations.seconds(3)
 
 export const MAX_PACKET_DELAY = 200
 
-export const MAX_HOPS = 1
+export const MAX_HOPS = 2
 export const PATH_RANDOMNESS = 0.1
 export const MAX_PATH_ITERATIONS = 100
 export const NETWORK_QUALITY_THRESHOLD = 0.5
