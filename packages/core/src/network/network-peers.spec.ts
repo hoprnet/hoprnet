3b4be02c849cd07ba1347e2ba5dbeeae0b35f55f
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-01-25 10:02:34 +0100

    Fix tests

--- packages/core/src/network/network-peers.spec.ts
+++ packages/core/src/network/network-peers.spec.ts
@@ -1,5 +1,5 @@
 import assert from 'assert'
-//import { NETWORK_QUALITY_THRESHOLD as Q } from '../constants'
+import { NETWORK_QUALITY_THRESHOLD as Q } from '../constants'
 import { fakePeerId } from '../test-utils'
 import PeerStore from './network-peers'
 
@@ -20,27 +20,28 @@ describe('test PeerStore', async function () {
     const networkPeers = new PeerStore(IDS)
     assert(networkPeers.randomSubset(3).length == 3)
   })
-  /*
 
   it('should _ping_ peers', async function () {
     const id = fakePeerId(5)
     const networkPeers = new PeerStore([])
     assert(networkPeers.length() == 0, 'networkPeers must be empty')
-    await networkPeers.pingOldest(() => {
-      throw new Error('Empty networkPeers in ping')
-    })
+    assert(networkPeers.pingSince(123).length === 0, 'no peers yet')
+
     networkPeers.register(id)
     assert(networkPeers.qualityOf(id) < Q, 'initial peers have low quality')
     assert(networkPeers.length() === 1)
 
-    await networkPeers.pingOldest(() => Promise.resolve(true))
-    assert(networkPeers.qualityOf(id) > Q, 'after first successful ping, peer is good quality')
-    await networkPeers.pingOldest(() => Promise.resolve(false))
-    assert(networkPeers.qualityOf(id) <= Q, 'after 50% failed pings, peer is bad quality')
+    await networkPeers.ping(id, () => Promise.resolve(true)) // 0.3
+    await networkPeers.ping(id, () => Promise.resolve(true)) // 0.4
+    await networkPeers.ping(id, () => Promise.resolve(true)) // 0.5
+    await networkPeers.ping(id, () => Promise.resolve(true)) // 0.6
+    assert(networkPeers.qualityOf(id) > Q, 'after 4 successful ping, peer is good quality')
 
-    await networkPeers.pingOldest(() => Promise.resolve(true))
-    await networkPeers.pingOldest(() => Promise.resolve(true))
-    assert(networkPeers.qualityOf(id) > Q, 'after 25% failed pings, peer is good again')
+    await networkPeers.ping(id, () => Promise.resolve(false)) //0.5
+    assert(networkPeers.qualityOf(id) <= Q, 'after 1 failed pings, peer is bad quality')
+
+    await networkPeers.ping(id, () => Promise.resolve(true))
+    await networkPeers.ping(id, () => Promise.resolve(true))
+    assert(networkPeers.qualityOf(id) > Q, 'after 2 more pings, peer is good again')
   })
-  */
 })
