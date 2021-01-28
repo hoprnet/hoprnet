4cb15ed9c4bd33ed16e89a511ba263c074c06bea
Robert Kiel <robert.kiel@hoprnet.org>
2021-01-28 10:53:21 +0100

    better typing

--- packages/core/src/utils/libp2p/dialHelper.ts
+++ packages/core/src/utils/libp2p/dialHelper.ts
@@ -25,12 +25,10 @@ export async function dialHelper(
   protocols: string[],
   options:
     | {
-        timeout?: number
         signal: AbortSignal
       }
     | {
         timeout: number
-        signal?: AbortSignal
       }
 ): Promise<Handler | void> {
   // Prevent us from dialing ourself
