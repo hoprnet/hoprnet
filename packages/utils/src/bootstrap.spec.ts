ad589b3df3d81cb59b3c6853c781f3d2f16fb004
jjperezaguinaga <me@jjperezaguinaga.com>
2021-01-29 15:23:20 +0000

    fix: Improved tests to verify dns call but not value

--- packages/utils/src/bootstrap.spec.ts
+++ packages/utils/src/bootstrap.spec.ts
@@ -1,13 +1,35 @@
 import assert from 'assert'
-import { getBootstrapAddresses } from './bootstrap'
+import rewiremock from 'rewiremock';
+import sinon from 'sinon'
+
+let getBootstrapAddresses : any = null;
+//let mockPromises = sinon.fake() as any
+
+const mockMultiAddrss = '/ip4/34.65.75.45/tcp/9091/p2p/16Uiu2HAm2cjqsDMmprtN2QKaq3LJrq3YK7vtdbQQFsxGrhLRoYsy'
+const resolveTxt = sinon.fake.returns(
+  [mockMultiAddrss]
+)
 
 describe('getBootstrapAddresses', function () {
+  beforeEach(async() => {
+    rewiremock('dns')
+    .with({
+      promises: { resolveTxt }
+    });
+    rewiremock.enable();
+    const BootstrapUtil = await import("./bootstrap");
+    getBootstrapAddresses = BootstrapUtil.getBootstrapAddresses;
+  });
+
   it('passed addresses resolved first', async function () {
-    assert(
-      await getBootstrapAddresses('/ip4/34.65.75.45/tcp/9091/p2p/16Uiu2HAm2cjqsDMmprtN2QKaq3LJrq3YK7vtdbQQFsxGrhLRoYsy')
-    )
+    assert(await getBootstrapAddresses(mockMultiAddrss))
+    assert(!resolveTxt.calledOnce)
   })
   it('falls back to dns', async function () {
     assert(await getBootstrapAddresses())
+    assert(resolveTxt.calledOnce)
   })
+
+  afterEach( () => rewiremock.disable() );
+
 })
