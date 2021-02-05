b35eb878c7a0329c83a70065fae4b24f34dab3fe
Robert Kiel <robert.kiel@hoprnet.org>
2021-02-05 15:52:25 +0100

    remove obsolete interactions

--- packages/core/src/interactions/index.ts
+++ packages/core/src/interactions/index.ts
@@ -2,18 +2,15 @@ import type HoprCoreConnector from '@hoprnet/hopr-core-connector-interface'
 import type Hopr from '..'
 import PeerId from 'peer-id'
 
-import { PaymentInteractions } from './payments'
 import { NetworkInteractions } from './network'
 import { PacketInteractions } from './packet'
 import { Mixer } from '../mixer'
 
 class Interactions<Chain extends HoprCoreConnector> {
-  public payments: PaymentInteractions<Chain>
   public network: NetworkInteractions
   public packet: PacketInteractions<Chain>
 
   constructor(node: Hopr<Chain>, mixer: Mixer<Chain>, heartbeat: (remotePeer: PeerId) => void) {
-    this.payments = new PaymentInteractions(node)
     this.network = new NetworkInteractions(node._libp2p, heartbeat)
     this.packet = new PacketInteractions(node, mixer)
   }
