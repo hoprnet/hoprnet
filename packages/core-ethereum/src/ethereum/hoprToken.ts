c007285faa3fa23b5ecfece0b2a17db76a991e60
HOPR Linting robot <noreply@hoprnet.org>
2021-02-19 16:17:29 +0000

    Fix linting

--- packages/core-ethereum/src/ethereum/hoprToken.ts
+++ packages/core-ethereum/src/ethereum/hoprToken.ts
@@ -1,6 +1,6 @@
-import type { HoprToken } from "../tsc/web3/HoprToken"
-import type { AccountId } from "../types"
-import { Balance } from "../types"
+import type { HoprToken } from '../tsc/web3/HoprToken'
+import type { AccountId } from '../types'
+import { Balance } from '../types'
 
 export const getBalance = async (hoprToken: HoprToken, account: AccountId): Promise<Balance> => {
   const result = await hoprToken.methods.balanceOf(account.toHex()).call()
--- packages/core-ethereum/src/ethereum/index.ts
+++ packages/core-ethereum/src/ethereum/index.ts
@@ -1,8 +1,8 @@
-import type Web3 from "web3"
-import type { AccountId } from "../types"
-import { NativeBalance } from "../types"
+import type Web3 from 'web3'
+import type { AccountId } from '../types'
+import { NativeBalance } from '../types'
 
-export * from "./hoprToken"
+export * from './hoprToken'
 
 export const getNativeBalance = async (web3: Web3, account: AccountId): Promise<NativeBalance> => {
   const result = await web3.eth.getBalance(account.toHex())
