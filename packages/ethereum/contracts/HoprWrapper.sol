ac13f72152151dd135ff9d5e67b090ac122c3fdc
nionis <stivennoni77@gmail.com>
2021-02-23 17:03:29 +0200

    fix styling

--- packages/ethereum/contracts/HoprWrapper.sol
+++ packages/ethereum/contracts/HoprWrapper.sol
@@ -13,41 +13,41 @@ import "./HoprToken.sol";
 
 
 contract HoprWrapper is IERC777Recipient, ERC1820Implementer, ReentrancyGuard {
-	using SafeMath for uint256;
+    using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-	// setup ERC1820
-	IERC1820Registry internal constant _ERC1820_REGISTRY = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
-	bytes32 public constant TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
+    // setup ERC1820
+    IERC1820Registry internal constant _ERC1820_REGISTRY = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    bytes32 public constant TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
 
-	// xHOPR token
-	IERC20 public xHOPR;
-	// wrapped xHOPR address
-	HoprToken public wxHOPR;
+    // xHOPR token
+    IERC20 public xHOPR;
+    // wrapped xHOPR address
+    HoprToken public wxHOPR;
 
-	// total amount of xHOPR collected in this contract
-	uint256 total_xHOPR = 0;
+    // total amount of xHOPR collected in this contract
+    uint256 total_xHOPR = 0;
 
     event Wrapped(address indexed account, uint256 amount);
     event Unwrapped(address indexed account, uint256 amount);
 
-	constructor(IERC20 _xHOPR, HoprToken _wxHOPR) public {
-		xHOPR = _xHOPR;
-		wxHOPR = _wxHOPR;
+    constructor(IERC20 _xHOPR, HoprToken _wxHOPR) public {
+        xHOPR = _xHOPR;
+        wxHOPR = _wxHOPR;
 
         _ERC1820_REGISTRY.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
-	}
+    }
 
     /**
-     * A hook triggered when xHOPR or wxHOPR tokens are send to this contract.
-     *
-     * @param operator address operator requesting the transfer
-     * @param from address token holder address
-     * @param to address recipient address
-     * @param amount uint256 amount of tokens to transfer
-     * @param userData bytes extra information provided by the token holder (if any)
-     * @param operatorData bytes extra information provided by the operator (if any)
-     */
+        * A hook triggered when xHOPR or wxHOPR tokens are send to this contract.
+        *
+        * @param operator address operator requesting the transfer
+        * @param from address token holder address
+        * @param to address recipient address
+        * @param amount uint256 amount of tokens to transfer
+        * @param userData bytes extra information provided by the token holder (if any)
+        * @param operatorData bytes extra information provided by the operator (if any)
+        */
     function tokensReceived(
         address operator,
         address from,
@@ -58,20 +58,20 @@ contract HoprWrapper is IERC777Recipient, ERC1820Implementer, ReentrancyGuard {
         // solhint-disable-next-line no-unused-vars
         bytes calldata operatorData
     ) external override nonReentrant {
-		// must be xHOPR or wxHOPR
+        // must be xHOPR or wxHOPR
         require(msg.sender == address(xHOPR) || msg.sender == address(wxHOPR), "Invalid token");
 
-		if (msg.sender == address(xHOPR)) {
-			total_xHOPR = total_xHOPR.add(amount);
-			wxHOPR.mint(from, amount, "", "");
+        if (msg.sender == address(xHOPR)) {
+            total_xHOPR = total_xHOPR.add(amount);
+            wxHOPR.mint(from, amount, "", "");
 
             emit Wrapped(from, amount);
-		} else {
-			total_xHOPR = total_xHOPR.sub(amount);
-			wxHOPR.burn(amount, "");
+        } else {
+            total_xHOPR = total_xHOPR.sub(amount);
+            wxHOPR.burn(amount, "");
             xHOPR.safeTransfer(from, amount);
 
             emit Unwrapped(from, amount);
-		}
+        }
     }
 }
