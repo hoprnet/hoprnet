0f13da40be98cb0ac96a0be752dec15728fde044
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-03-24 19:17:29 +0100

    Cherry pick utils from channel refactor

--- packages/utils/src/u8a/index.ts
+++ packages/utils/src/u8a/index.ts
@@ -16,3 +16,31 @@ export * from './u8aToNumber'
 
 export * from './equals'
 export * from './u8aCompare'
+
+export type U8aAndSize = [Uint8Array, number]
+
+export function serializeToU8a(items: U8aAndSize[]): Uint8Array {
+  const totalSize = items.map((x) => x[1]).reduce((x, y) => x + y, 0)
+  const arr = new Uint8Array(totalSize)
+  let i = 0
+  items.forEach((item) => {
+    if (item[0].length != item[1]) {
+      throw new Error(`Error serializing - expected item of length ${item[1]}, got ${item[0].length}`)
+    }
+    arr.set(item[0], i)
+    i += item[1]
+  })
+  return arr
+}
+
+export function u8aSplit(u8a: Uint8Array, sizes: number[]): Uint8Array[] {
+  let totalSize = sizes.reduce((x, y) => x + y, 0)
+  if (u8a.length !== totalSize) {
+    throw new Error('U8a cannot be split: length != sum(sizes)')
+  }
+  let i = 0
+  return sizes.map((s) => {
+    i += s
+    return u8a.slice(i - s, i)
+  })
+}
