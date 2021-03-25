f5206dbb3ec0b6fcaa567b8deb869524c366813a
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-03-25 17:10:24 +0100

    move solidity types

---
+++ packages/core-ethereum/src/types/solidity.ts
@@ -0,0 +1,14 @@
+import { BNE } from '../../types/extended'
+
+class UINT256 extends BNE {
+  toU8a(): Uint8Array {
+    return super.toU8a(UINT256.SIZE)
+  }
+
+  static get SIZE(): number {
+    return 32
+  }
+}
+
+export { UINT256 }
+
--- packages/core-ethereum/src/types/solidity/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as UINT256 } from './uint256'
