6414f3c9e0be4b9c1c30cd89d8f3473793bd984b
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-04-08 14:15:08 +0200

    Simplify acknowledgement db

--- packages/core/src/interactions/index.ts
+++ packages/core/src/interactions/index.ts
@@ -1,16 +1,19 @@
 import type Hopr from '..'
 import PeerId from 'peer-id'
 
-import { PacketInteractions } from './packet'
 import { Heartbeat } from './network/heartbeat'
+import { PacketForwardInteraction } from './packet/forward'
+import { PacketAcknowledgementInteraction } from './packet/acknowledgement'
 
 class Interactions {
   heartbeat: Heartbeat
-  public packet: PacketInteractions
+  acknowledgment: PacketAcknowledgementInteraction
+  forward: PacketForwardInteraction
 
   constructor(node: Hopr, heartbeat: (remotePeer: PeerId) => void) {
     this.heartbeat = new Heartbeat(node._libp2p, heartbeat)
-    this.packet = new PacketInteractions(node)
+    this.acknowledgment = new PacketAcknowledgementInteraction(node._libp2p, node.db, node.paymentChannels)
+    this.forward = new PacketForwardInteraction(node)
   }
 }
 
