68a511a7e4e5d93160eac60c167dba7f5e799bbc
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-04-08 11:43:01 +0200

    remove unused payment interactions

--- packages/core/src/interactions/index.ts
+++ packages/core/src/interactions/index.ts
@@ -1,17 +1,14 @@
 import type Hopr from '..'
 import PeerId from 'peer-id'
 
-import { PaymentInteractions } from './payments'
 import { PacketInteractions } from './packet'
 import { Heartbeat } from './network/heartbeat'
 
 class Interactions {
-  public payments: PaymentInteractions
   heartbeat: Heartbeat
   public packet: PacketInteractions
 
   constructor(node: Hopr, heartbeat: (remotePeer: PeerId) => void) {
-    this.payments = new PaymentInteractions(node)
     this.heartbeat = new Heartbeat(node._libp2p, heartbeat)
     this.packet = new PacketInteractions(node)
   }
