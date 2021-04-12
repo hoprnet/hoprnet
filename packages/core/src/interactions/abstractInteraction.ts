96c552bdc5328810d1493e2db1bb124e10b4fa8e
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-04-12 14:59:03 +0200

    Remove unused

--- packages/core/src/interactions/abstractInteraction.ts
+++
@@ -1,9 +0,0 @@
-import type { Connection, MuxedStream } from 'libp2p'
-
-interface AbstractInteraction {
-  protocols: string[]
-  handler(struct: { connection: Connection; stream: MuxedStream; protocol: string }): void
-  interact(...props: any[]): any
-}
-
-export { AbstractInteraction }
