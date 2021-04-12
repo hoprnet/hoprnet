3fd24ad2f9fcc195abbc9dbc339ea49d16e60e0f
jjperezaguinaga <me@jjperezaguinaga.com>
2021-04-12 15:14:32 +0000

    feat: Fixes #1464 - Enabled faucet via hardhat

--- packages/ethereum/tasks/faucet.ts
+++ packages/ethereum/tasks/faucet.ts
@@ -1,8 +1,11 @@
 import type { HardhatRuntimeEnvironment, RunSuperFunction } from 'hardhat/types'
 import { NODE_SEEDS } from '@hoprnet/hopr-demo-seeds'
 import { HoprToken__factory } from '../types'
+import { promisify } from 'util';
+import { stat, readFile } from 'fs'
 
-
+const statAsync = promisify(stat);
+const readFileAsync = promisify(readFile);
 const send = (signer, txparams) =>
   signer.sendTransaction(txparams, (error, transactionHash) => {
     if (error) {
@@ -10,6 +13,16 @@ const send = (signer, txparams) =>
     }
     console.log(`transactionHash: ${transactionHash}`);
   });
+
+const getHoprTokenAddress = async (addressesFile) => {
+  try {
+    const deployedContracts = await readFileAsync(addressesFile, 'utf8');
+    return JSON.parse(deployedContracts).contracts['HoprToken'].address;
+  } catch(err) {
+    console.log('⛔️ Error when obtaining local address', err)
+    return;
+  }
+}
 
 /**
  * Faucets HOPR and ETH tokens to a local account with HOPR
@@ -19,29 +32,35 @@ async function main(
   { ethers, network }: HardhatRuntimeEnvironment,
   _runSuper: RunSuperFunction<any>
 ) {
-  console.log('🚰 Starting faucet task', {
-    address,
-    network: network.name
-  })
-  const etherAmount = '1'
+  if (network.name !== 'localhost') {
+    console.error('🌵 Faucet is only valid in localhost network');
+    return;
+  }
+  const addressesFile = __dirname + "/../hardhat/cache/deployed_contracts.json";
+  if (!await statAsync(addressesFile)) {
+    console.error("⛓  You need to ensure the network deployed the contracts");
+    return;
+  }
+
+  console.log('🚰 Starting local faucet task')
+  const etherAmount = '1.0'
   const signer = ethers.provider.getSigner()
+  const minterWallet = new ethers.Wallet(NODE_SEEDS[0], ethers.provider)
   const tx = {
     to: address,
     value: ethers.utils.parseEther(etherAmount)
   };
-  const minterWallet = new ethers.Wallet(NODE_SEEDS[0], ethers.provider)
-  const hoprToken = HoprToken__factory.connect(address, ethers.provider).connect(minterWallet)
+  const hoprTokenAddress = await getHoprTokenAddress(addressesFile);
+  const hoprToken = HoprToken__factory.connect(hoprTokenAddress, ethers.provider).connect(minterWallet)
 
   console.log(`💧💰 Sending ${etherAmount} ETH to ${address} on network ${network.name}`);
   await send(signer, tx);
 
-  console.log(`💧🟡 Sending ${amount} HOPR to ${address} on network ${network.name}`);
+  console.log(`💧🟡 Sending ${ethers.utils.formatEther(amount)} HOPR to ${address} on network ${network.name}`);
   await hoprToken.mint(address, amount, ethers.constants.HashZero, ethers.constants.HashZero, {
     from: minterWallet.getAddress(),
     gasLimit: 200e3
   })
-
-  await hoprToken.balanceOf(address).then(console.log)
 }
 
 export default main
