18f44fa41e37999c6c9234bd786b4909387e67e3
Robert Kiel <robert.kiel@hoprnet.io>
2021-04-13 15:36:15 +0200

    refactor challenge

--- packages/core/src/messages/packet/challenge.spec.ts
+++ packages/core/src/messages/packet/challenge.spec.ts
@@ -1,35 +1,20 @@
-import assert from 'assert'
 import { Challenge } from './challenge'
-import { Hash } from '@hoprnet/hopr-core-ethereum'
-import BN from 'bn.js'
+import { sampleGroupElement } from '@hoprnet/hopr-utils'
 import PeerId from 'peer-id'
-import { randomBytes } from 'crypto'
-import { randomInteger, u8aEquals, u8aToHex } from '@hoprnet/hopr-utils'
+import assert from 'assert'
 
 describe('test creation & verification of a challenge', function () {
   it('should create a verifiable challenge', async function () {
-    for (let i = 0; i < 10; i++) {
-      const secret = randomBytes(32)
+    const peerId = await PeerId.create({ keyType: 'secp256k1' })
 
-      const peerId = await PeerId.create({
-        keyType: 'secp256k1'
-      })
+    const [exponent, ackChallenge] = sampleGroupElement(true)
 
-      const challenge = await Challenge.create(new Hash(secret), new BN(0)).sign(peerId)
+    const challenge = Challenge.create(ackChallenge, peerId)
 
-      assert(await challenge.verify(peerId), `Previously generated signature should be valid.`)
+    assert(challenge.serialize().length == Challenge.SIZE, `Size must be correct`)
 
-      assert(u8aEquals(await challenge.counterparty, peerId.pubKey.marshal()), `recovered pubKey should be equal.`)
+    const deserializedChallenge = Challenge.deserialize(challenge.serialize(), ackChallenge, peerId)
 
-      let exponent = randomInteger(0, 7)
-      let index = randomInteger(0, challenge.length - 1)
-
-      challenge[index] = challenge[index] ^ (1 << exponent)
-
-      if (await challenge.verify(peerId)) {
-        // @TODO change to assert.fail
-        console.log(`found invalid signature, <${u8aToHex(challenge)}>, byte #${index}, bit #${exponent}`)
-      }
-    }
+    assert(deserializedChallenge.solve(exponent), `Challenge must be solvable`)
   })
 })
--- packages/core/src/messages/packet/challenge.ts
+++ packages/core/src/messages/packet/challenge.ts
@@ -1,170 +1,62 @@
-import secp256k1 from 'secp256k1'
-import PeerId from 'peer-id'
-import { Signature, Hash, PublicKey } from '@hoprnet/hopr-core-ethereum'
+import { u8aEquals, SECP256K1 } from '@hoprnet/hopr-utils'
+import { HASH_ALGORITHM } from './constants'
+import { ecdsaSign, ecdsaVerify, publicKeyCreate } from 'secp256k1'
+import { createHash } from 'crypto'
 
-import BN from 'bn.js'
+import type PeerId from 'peer-id'
 
-/**
- * The purpose of this class is to give the relayer the opportunity to claim
- * the proposed funds in case the the next downstream node responds with an
- * inappropriate acknowledgement.
- */
-class Challenge extends Uint8Array {
-  // private : Uint8Array
-  private _hashedKey: Hash
-  private _fee: BN
-  private _counterparty: Uint8Array
+export class Challenge {
+  private constructor(private ackChallenge: Uint8Array, private signature: Uint8Array) {}
 
-  constructor(
-    arr?: {
-      bytes: ArrayBuffer
-      offset: number
-    },
-    struct?: {
-      signature: Signature
+  static get SIZE(): number {
+    return SECP256K1.SIGNATURE_LENGTH
+  }
+
+  static deserialize(preArray: Uint8Array | Buffer, ackChallenge: Uint8Array, pubKey: PeerId): Challenge {
+    if (preArray.length != SECP256K1.SIGNATURE_LENGTH) {
+      throw Error(`Invalid arguments`)
     }
-  ) {
-    if (arr == null) {
-      super(Challenge.SIZE())
+
+    let arr: Uint8Array
+    if (typeof Buffer !== 'undefined' && Buffer.isBuffer(preArray)) {
+      arr = Uint8Array.from(preArray)
     } else {
-      super(arr.bytes, arr.offset, Challenge.SIZE())
+      arr = preArray
     }
 
-    if (struct != null) {
-      super(struct.signature.serialize())
+    if (!verifyChallenge(pubKey, arr, ackChallenge)) {
+      throw Error(`General error.`)
     }
+
+    return new Challenge(ackChallenge, arr)
   }
 
   serialize(): Uint8Array {
-    return this // TODO
+    return Uint8Array.from(this.signature)
   }
 
-  static deserialize(arr: Uint8Array) {
-    return new Challenge({ bytes: arr.buffer, offset: arr.byteOffset }) // TODO
-  }
-
-  get challengeSignatureOffset(): number {
-    return this.byteOffset
-  }
-
-  get challengeSignature(): Signature {
-    return Signature.deserialize(new Uint8Array(this.buffer, this.challengeSignatureOffset, Signature.SIZE))
-  }
-
-  get signatureHash(): Promise<Hash> {
-    return new Promise(async (resolve) => {
-      resolve(Hash.create(this.challengeSignature.serialize()))
-    })
-  }
-
-  static SIZE(): number {
-    return Signature.SIZE
-  }
-
-  get hash(): Hash {
-    if (this._hashedKey == null) {
-      throw Error(`Challenge was not set yet.`)
+  static create(ackChallenge: Uint8Array, privKey: PeerId): Challenge {
+    console.log(ackChallenge.length, SECP256K1.COMPRESSED_PUBLIC_KEY_LENGTH)
+    if (ackChallenge.length != SECP256K1.COMPRESSED_PUBLIC_KEY_LENGTH || privKey.privKey == null) {
+      throw Error(`Invalid arguments`)
     }
 
-    return this._hashedKey
+    const toSign = hashChallenge(ackChallenge)
+
+    const signature = ecdsaSign(toSign, privKey.privKey.marshal())
+
+    return new Challenge(ackChallenge, signature.signature)
   }
 
-  subarray(begin: number = 0, end: number = Challenge.SIZE()): Uint8Array {
-    return new Uint8Array(this.buffer, begin + this.byteOffset, end - begin)
-  }
-
-  getCopy(): Challenge {
-    const arrCopy = new Uint8Array(Challenge.SIZE())
-
-    arrCopy.set(this)
-
-    const copiedChallenge = new Challenge({
-      bytes: arrCopy.buffer,
-      offset: arrCopy.byteOffset
-    })
-
-    copiedChallenge._hashedKey = this._hashedKey
-    copiedChallenge._fee = this._fee
-
-    return copiedChallenge
-  }
-
-  /**
-   * Uses the derived secret and the signature to recover the public
-   * key of the signer.
-   */
-  get counterparty(): Promise<Uint8Array> {
-    if (this._hashedKey == null) {
-      return Promise.reject(Error(`Challenge was not set yet.`))
-    }
-
-    if (this._counterparty != null) {
-      return Promise.resolve(this._counterparty)
-    }
-
-    return new Promise<Uint8Array>(async (resolve) => {
-      const challengeSignature = await this.challengeSignature
-      resolve(secp256k1.ecdsaRecover(challengeSignature.signature, challengeSignature.recovery, this.hash.serialize()))
-    })
-  }
-
-  /**
-   * Signs the challenge and includes the transferred amount of money as
-   * well as the ethereum address of the signer into the signature.
-   *
-   * @param peerId that contains private key and public key of the node
-   */
-  async sign(peerId: PeerId): Promise<Challenge> {
-    const signature = Signature.create(this.hash.serialize(), peerId.privKey.marshal())
-    this.set(signature.serialize(), this.challengeSignatureOffset - this.byteOffset)
-    return this
-  }
-
-  /**
-   * Creates a challenge object.
-   *
-   * @param hashedKey that is used to generate the key half
-   * @param fee
-   */
-  static create(
-    hashedKey: Hash,
-    fee: BN,
-    arr?: {
-      bytes: ArrayBuffer
-      offset: number
-    }
-  ): Challenge {
-    if (arr == null) {
-      const tmp = new Uint8Array(this.SIZE())
-
-      arr = {
-        bytes: tmp.buffer,
-        offset: tmp.byteOffset
-      }
-    }
-
-    const challenge = new Challenge(arr)
-
-    challenge._hashedKey = hashedKey
-    challenge._fee = fee
-
-    return challenge
-  }
-
-  /**
-   * Verifies the challenge by checking whether the given public matches the
-   * one restored from the signature.
-   *
-   * @param peerId PeerId instance that contains the public key of
-   * the signer
-   * @param secret the secret that was used to derive the key half
-   */
-  async verify(peerId: PeerId): Promise<boolean> {
-    if (!peerId.pubKey) {
-      throw Error('Unable to verify challenge without a public key.')
-    }
-    return this.challengeSignature.verify(this.hash.serialize(), new PublicKey(peerId.pubKey.marshal()))
+  solve(secret: Uint8Array): boolean {
+    return u8aEquals(publicKeyCreate(secret), this.ackChallenge)
   }
 }
 
-export { Challenge }
+function verifyChallenge(pubKey: PeerId, signature: Uint8Array, challenge: Uint8Array): boolean {
+  return ecdsaVerify(signature, hashChallenge(challenge), pubKey.pubKey.marshal())
+}
+
+function hashChallenge(ackChallenge: Uint8Array): Uint8Array {
+  return createHash(HASH_ALGORITHM).update(ackChallenge).digest()
+}
