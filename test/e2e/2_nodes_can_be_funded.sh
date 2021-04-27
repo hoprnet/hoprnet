590b0e5d16297acc8a2ada24abfaab0f212b6e6b
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-04-27 17:35:33 +0200

    e2e

--- test/e2e/2_nodes_can_be_funded.sh
+++ test/e2e/2_nodes_can_be_funded.sh
@@ -5,24 +5,24 @@ source 'test/e2e/0_configuration.sh'
 
 # Running RPC
 rpc_network
-# Running bootstrap server
-bootstrap_node
+# Running server
+start_node
 
-echo "🤖 (BS) Requesting bootstrap address"
-BOOTSTRAP_HOPR_ADDRESS=$(curl -s http://127.0.0.1:3001/api/v1/address/hopr)
-echo "⛓  HOPR Address: $BOOTSTRAP_HOPR_ADDRESS"
+echo "🤖 (BS) Requesting address"
+HOPR_ADDRESS=$(curl -s http://127.0.0.1:3001/api/v1/address/hopr)
+echo "⛓  HOPR Address: $HOPR_ADDRESS"
 
-fund_node $BOOTSTRAP_HOPR_ADDRESS
-BOOTSTRAP_BALANCE=$(curl -s --data balance http://127.0.0.1:3001/api/v1/command)
+fund_node $HOPR_ADDRESS
+BALANCE=$(curl -s --data balance http://127.0.0.1:3001/api/v1/command)
 
-ETH_BALANCE=$(echo -e $BOOTSTRAP_BALANCE | grep -c "1\ xDAI" || true)
-HOPR_BALANCE=$(echo -e $BOOTSTRAP_BALANCE | grep -c "1\ HOPR" || true)
+ETH_BALANCE=$(echo -e $BALANCE | grep -c "1\ xDAI" || true)
+HOPR_BALANCE=$(echo -e $BALANCE | grep -c "1\ HOPR" || true)
 
 if [[ $ETH_BALANCE == 1 && $HOPR_BALANCE == 1 ]]; then
   echo "✅ Node holds balance after being funded."
   exit 0
 else
   echo "⛔️ Node has an invalid balance:"
-  echo -e "$BOOTSTRAP_BALANCE"
+  echo -e "$BALANCE"
   exit 1 
 fi
