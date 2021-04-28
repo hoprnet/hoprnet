63a3746310e71a8b60825a5fd33e35b7883a7de7
nionis <stivennoni77@gmail.com>
2021-04-28 18:12:54 +0300

    dont floor

--- packages/core-ethereum/src/types/ticket.ts
+++ packages/core-ethereum/src/types/ticket.ts
@@ -129,7 +129,7 @@ class Ticket {
 
   static floatToUint256(float: number): UINT256 {
     if (float > 1) throw Error('Float cannot be larger than 1')
-    const percent = Math.floor(float * 100)
+    const percent = float * 100
     return new UINT256(new BN(ethers.constants.MaxUint256.mul(percent).div(100).toString()))
   }
 
