c04c9b42c38beccba13f918ef2b1db5ea9f36975
HOPR Linting robot <noreply@hoprnet.org>
2021-05-04 15:05:49 +0000

    Fix linting

--- packages/core/src/utils/tickets.spec.ts
+++ packages/core/src/utils/tickets.spec.ts
@@ -6,7 +6,16 @@ import chaiAsPromised from 'chai-as-promised'
 import chai, { expect } from 'chai'
 import sinon from 'sinon'
 import { Channel } from '@hoprnet/hopr-core-ethereum'
-import { Address, Balance, PublicKey, UINT256, Ticket, ChannelEntry, validateUnacknowledgedTicket, validateCreatedTicket } from '@hoprnet/hopr-utils'
+import {
+  Address,
+  Balance,
+  PublicKey,
+  UINT256,
+  Ticket,
+  ChannelEntry,
+  validateUnacknowledgedTicket,
+  validateCreatedTicket
+} from '@hoprnet/hopr-utils'
 
 chai.use(chaiAsPromised)
 
