cc4e7c782cb15094ba9726e9fc358611c55bdf0d
Robert Kiel <robert.kiel@hoprnet.io>
2021-05-04 11:06:06 +0200

    add description to padding methods

--- packages/utils/src/crypto/packet/padding.ts
+++ packages/utils/src/crypto/packet/padding.ts
@@ -5,6 +5,13 @@ import { u8aEquals } from '../../u8a'
 export const PADDING_TAG = Uint8Array.from([72, 79, 80, 82]) // "HOPR"
 export const PADDING_TAG_LENGTH = 4
 
+/**
+ * Adds a deterministic padding to a given payload.
+ * @dev payloads that do not include the correct padding are
+ * considered invalid
+ * @param msg the payload to pad
+ * @returns the padded payload
+ */
 export function addPadding(msg: Uint8Array) {
   const msgLength = msg.length
 
@@ -15,6 +22,13 @@ export function addPadding(msg: Uint8Array) {
   return Uint8Array.from([...new Uint8Array(PAYLOAD_SIZE - PADDING_TAG_LENGTH - msgLength), ...PADDING_TAG, ...msg])
 }
 
+/**
+ * Removes the padding from a given payload and fails if
+ * the padding does not exist or if the payload has the
+ * wrong size.
+ * @param decoded a padded payload
+ * @returns the message without the padding
+ */
 export function removePadding(decoded: Uint8Array) {
   if (decoded.length != PAYLOAD_SIZE) {
     throw new CryptoError(`Incorrect message length. Dropping message.`)
