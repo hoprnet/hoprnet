89751937703de0a2ff620b7f9cc983db7fe10d97
Robert Kiel <robert.kiel@hoprnet.io>
2021-05-05 10:21:43 +0200

    better comment

--- packages/utils/src/crypto/sampleGroupElement.ts
+++ packages/utils/src/crypto/sampleGroupElement.ts
@@ -3,27 +3,28 @@ import { randomFillSync } from 'crypto'
 import { SECP256K1_CONSTANTS } from './constants'
 
 /**
- * Generates a secp256k1 keypair used for an
- * offline Diffie-Hellman key exchange.
- * @returns a secp256k1 keypair
+ * Samples a valid exponent and returns the exponent
+ * and the product of exponent and base-point.
+ * @dev can be used to derive a secp256k1 keypair
+ * @returns [ exponent, groupElement]
  */
 export function sampleGroupElement(compressed: boolean = false): [exponent: Uint8Array, groupElement: Uint8Array] {
-  let privKey = new Uint8Array(SECP256K1_CONSTANTS.PRIVATE_KEY_LENGTH)
-  let pubKey: Uint8Array
+  let exponent = new Uint8Array(SECP256K1_CONSTANTS.PRIVATE_KEY_LENGTH)
+  let groupElement: Uint8Array
 
   do {
-    randomFillSync(privKey, 0, SECP256K1_CONSTANTS.PRIVATE_KEY_LENGTH)
+    randomFillSync(exponent, 0, SECP256K1_CONSTANTS.PRIVATE_KEY_LENGTH)
 
-    if (!privateKeyVerify(privKey)) {
+    if (!privateKeyVerify(exponent)) {
       continue
     }
 
-    pubKey = publicKeyCreate(privKey, false)
-  } while (!publicKeyVerify(pubKey))
+    groupElement = publicKeyCreate(exponent, false)
+  } while (!publicKeyVerify(groupElement))
 
   if (compressed) {
-    return [privKey, publicKeyConvert(pubKey)]
+    return [exponent, publicKeyConvert(groupElement)]
   } else {
-    return [privKey, pubKey]
+    return [exponent, groupElement]
   }
 }
