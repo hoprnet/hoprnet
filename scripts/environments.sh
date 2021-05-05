011d4e931a7da596d6190a35e3b126ab95caee2e
Tino Breddin <tolbrino@users.noreply.github.com>
2021-05-05 17:08:30 +0200

    Improve bash script syntax, no semantic changes (#1592)
    * Add yarn cache directory to Github workflow
    * Minor cleanup in github actions workflows
    * Add new entry to SSH authorized keys
    * Add link to video guide on YT to Readme

--- scripts/environments.sh
+++ scripts/environments.sh
@@ -1,31 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 source scripts/utils.sh
 
 # These will be cleaned up and machines stopped
+# shellcheck disable=SC2034
 OLD_RELEASES='zurich zug luzern larnaca queretaro basodino saentis debug-dbg nightly internal'
 
 # ===== Load env variables for the current github ref =====
 # Takes:
 # - GITHUB_REF
 # - RELEASE
-# Sets: 
+# Sets:
 # - RELEASE_NAME
-# - RELEASE_IP deprecated
 # - VERSION_MAJ_MIN
 get_environment() {
-  BRANCH=$(echo "$GITHUB_REF" | sed -e "s#refs/heads/##g") # Removing `refs/heads`
+  BRANCH=${GITHUB_REF#refs/heads} # Removing `refs/heads`
 
   if [ "$BRANCH" == 'master' ]; then
     RELEASE_NAME='master'
-    RELEASE_IP='34.65.102.152'
     VERSION_MAJ_MIN='prerelease'
     return
   fi
 
   case "$BRANCH" in release/*)
-    VERSION_MAJ_MIN=$(get_version_maj_min $RELEASE) 
-    
+    VERSION_MAJ_MIN=$(get_version_maj_min "$RELEASE")
+
     if [ "$VERSION_MAJ_MIN" == '1.70' ]; then
       RELEASE_NAME='jungfrau'
       return
@@ -61,34 +64,29 @@ get_environment() {
     fi
     if [ "$VERSION_MAJ_MIN" == '1.59' ]; then
       RELEASE_NAME='mainz'
-      # From this release on, RELEASE_IP is deprecated
       return
     fi
 
     if [ "$VERSION_MAJ_MIN" == '1.58' ]; then
       RELEASE_NAME='queretaro'
-      RELEASE_IP='34.65.207.39'
       return
     fi
 
     if [ "$VERSION_MAJ_MIN" == '1.57' ]; then
       RELEASE_NAME='larnaca'
-      RELEASE_IP='unknown'
       return
     fi
     if [ "$VERSION_MAJ_MIN" == '1.56' ]; then
       RELEASE_NAME='luzern'
-      RELEASE_IP='34.65.5.42'
       return
     fi
     if [ "$VERSION_MAJ_MIN" == '1.55' ]; then
       RELEASE_NAME='zug'
-      RELEASE_IP='34.65.158.118'
       return
     fi
     if [ "$VERSION_MAJ_MIN" == '1.54' ]; then
+      # shellcheck disable=SC2034
       RELEASE_NAME='zurich'
-      RELEASE_IP='unknown'
       return
     fi
     echo "Unknown version: $VERSION_MAJ_MIN"
@@ -96,8 +94,4 @@ get_environment() {
 
   echo "Unknown release / environment: '$BRANCH'"
   exit 1
-  RELEASE_NAME='debug'
-  RELEASE_IP='34.65.56.229'
-  VERSION_MAJ_MIN='dbg'
 }
-
