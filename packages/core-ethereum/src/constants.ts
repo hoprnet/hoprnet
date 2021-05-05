883d5062264690cfdadb3c09998b07c35353211c
Steve <stivennoni77@gmail.com>
2021-05-05 12:34:26 +0300

    Update constants.ts

--- packages/core-ethereum/src/constants.ts
+++ packages/core-ethereum/src/constants.ts
@@ -4,5 +4,5 @@
  */
 export const PROVIDER_CACHE_TTL = 30e3 // 30 seconds
 export const PROVIDER_DEFAULT_URI = 'ws://127.0.0.1:8545/'
-export const INDEXER_MAX_CONFIRMATIONS = 1
+export const INDEXER_MAX_CONFIRMATIONS = 8
 export const INDEXER_BLOCK_RANGE = 2000
