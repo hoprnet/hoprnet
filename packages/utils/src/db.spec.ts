31576336a0812c8b0deb564ac5af7491087ebf4e
Robert Kiel <robert.kiel@hoprnet.io>
2021-05-07 19:05:05 +0200

    check ticket workflow

--- packages/utils/src/db.spec.ts
+++ packages/utils/src/db.spec.ts
@@ -1,11 +1,30 @@
 import { HoprDB } from './db'
+import { PublicKey, UnacknowledgedTicket, Ticket } from '.'
 import { randomBytes } from 'crypto'
+
 import assert from 'assert'
+import { AcknowledgedTicket, Address, Balance, Hash, UINT256 } from './types'
+import BN from 'bn.js'
 
+function createMockedTicket() {
+  return Ticket.create(
+    new Address(randomBytes(Address.SIZE)),
+    PublicKey.fromPrivKey(randomBytes(32)),
+    UINT256.fromString('0'),
+    UINT256.fromString('0'),
+    new Balance(new BN(0)),
+    UINT256.fromProbability(1),
+    UINT256.fromString('1'),
+    randomBytes(32)
+  )
+}
 describe(`database tests`, function () {
-  const db = HoprDB.createMock()
+  let db: HoprDB
 
-  after(async function () {
+  beforeEach(function () {
+    db = HoprDB.createMock()
+  })
+  afterEach(async function () {
     await db.close()
   })
 
@@ -19,5 +38,32 @@ describe(`database tests`, function () {
     const secondTry = await db.checkAndSetPacketTag(packetTag)
 
     assert(secondTry == true, `Packet tag must be set`)
+  })
+
+  it('ticket workflow', async function () {
+    const key = PublicKey.fromPrivKey(randomBytes(32))
+    await db.storeUnacknowledgedTickets(
+      key,
+      new UnacknowledgedTicket(createMockedTicket(), new Hash(new Uint8Array(Hash.SIZE)))
+    )
+
+    assert((await db.getTickets()).length == 1, `DB should find one ticket`)
+
+    const ticket = await db.getUnacknowledgedTicketsByKey(key)
+    assert(ticket != null)
+
+    await db.replaceTicketWithAcknowledgement(
+      key,
+      new AcknowledgedTicket(ticket.ticket, new Hash(randomBytes(Hash.SIZE)), new Hash(randomBytes(Hash.SIZE)))
+    )
+
+    assert((await db.getTickets()).length == 1, `DB should find one ticket`)
+
+    const empty = await db.getUnacknowledgedTicketsByKey(key)
+
+    console.log(empty)
+    assert(empty == undefined)
+
+    // const ackedTicket = await db.getA
   })
 })
