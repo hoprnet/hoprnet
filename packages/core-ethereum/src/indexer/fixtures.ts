f5b66c203bbdac563f289e79ec7386402d822428
nionis <stivennoni77@gmail.com>
2021-05-07 11:27:25 +0300

    fix tests

--- packages/core-ethereum/src/indexer/fixtures.ts
+++ packages/core-ethereum/src/indexer/fixtures.ts
@@ -17,7 +17,6 @@ export const partyAMultiAddr = Multiaddr(
 export const expectAccountsToBeEqual = (actual: AccountEntry, expected: AccountEntry) => {
   assert.strictEqual(actual.address.toString(), expected.address.toString(), 'address')
   assert.strictEqual(actual.getPublicKey().toString(), expected.getPublicKey().toString(), 'publicKey')
-  assert.strictEqual(actual.updatedBlock.toString(), expected.updatedBlock.toString(), 'updatedBlock')
 }
 
 export const expectChannelsToBeEqual = (actual: ChannelEntry, expected: ChannelEntry) => {
