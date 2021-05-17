f04a728b3b2046286dc3d46c63c5a2daca31881b
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-05-17 10:10:50 +0200

    split further

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -27,4 +27,16 @@ start_testnet $TESTNET_NAME $TESTNET_SIZE "gcr.io/hoprassociation/hoprd:$RELEASE
 # TODO FUND ADDRESS
 
 echo "Running integration test against testnet: $TESTNET_NAME"
+NODE1="$(vm_name 'node-1' $TESTNET_NAME)"
+NODE2="$(vm_name 'node-2' $TESTNET_NAME)"
+NODE3="$(vm_name 'node-3' $TESTNET_NAME)"
+IP1="$(gcloud_get_ip $NODE1)"
+IP2="$(gcloud_get_ip $NODE2)"
+IP3="$(gcloud_get_ip $NODE3)"
+ETH_ADDRESS1="$(get_eth_address $IP1)"
+ETH_ADDRESS2="$(get_eth_address $IP2)"
+ETH_ADDRESS3="$(get_eth_address $IP3)"
 source ./test/integration_test.sh
+
+#echo "Cleaning up after deploy"
+#cleanup
--- test/integration-test.sh
+++ test/integration-test.sh
@@ -1,24 +1,13 @@
+# -- Integration test --
+# We assume the existence of a test network with three nodes:
+# NODE1, IP1, ETH_ADDRESS1 etc.
 #
 #
-#
-#
-
 source scripts/testnet.sh
 
-NODE1="$(vm_name 'node-1' $TESTNET_NAME)"
-NODE2="$(vm_name 'node-2' $TESTNET_NAME)"
-NODE3="$(vm_name 'node-3' $TESTNET_NAME)"
-IP1="$(gcloud_get_ip $NODE1)"
-IP2="$(gcloud_get_ip $NODE2)"
-IP3="$(gcloud_get_ip $NODE3)"
-ETH_ADDRESS1="$(get_eth_address $IP1)"
-ETH_ADDRESS2="$(get_eth_address $IP2)"
-ETH_ADDRESS3="$(get_eth_address $IP3)"
 echo "Node 1: $NODE1 IP: $IP1, ETH: $ETH_ADDRESS1"
 echo "Node 2: $NODE2 IP: $IP2, ETH: $ETH_ADDRESS2"
 echo "Node 3: $NODE3 IP: $IP3, ETH: $ETH_ADDRESS3"
-
-
 
 echo "Query node-1"
 echo "$(run_command $IP1 'balance')"
