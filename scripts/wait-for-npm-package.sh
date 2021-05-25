818235641eddbff9a27f0e833053e590009b3506
Tino Breddin <t@tolbrino.com>
2021-05-25 10:53:33 +0200

    Move some scripts into lib/

--- scripts/wait-for-npm-package.sh
+++ scripts/wait-for-npm-package.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
+# exit on errors, undefined variables, ensure errors in pipes are not hidden
+set -euo pipefail
 
+# prevent souring of this script, only allow execution
+$(return >/dev/null 2>&1)
+test "$?" -eq "0" && (echo "This script should only be executed."; exit 1)
+
+usage() {
+  echo "Usage: $0 <package-name> <package-version>" >&2
+  echo
+  exit 0
+}
+
+# return early with help info when requested
+([ "${1:-}" = "-h" ] || [ "${1:-}" = "--help" ]) && usage
+
+# verify parameters
+[ -z "${1:-}" ] && (echo "Missing first parameter <package-name>"; usage; exit 1)
+[ -z "${2:-}" ] && (echo "Missing second parameter <package-version>"; usage; exit 1)
+
+# do work
 declare mydir
 declare npm_package
-
-test -z "${HOPR_PACKAGE:-}" && (echo "Missing environment variable HOPR_PACKAGE"; exit 1)
-test -z "${HOPR_PACKAGE_VERSION:-}" && (echo "Missing environment variable HOPR_PACKAGE_VERSION"; exit 1)
 
 mydir=$(dirname $(readlink -f $0))
 
