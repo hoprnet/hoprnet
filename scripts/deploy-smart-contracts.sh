b523e6394270b6ff14f4f23e849133066e4ce7f2
nionis <stivennoni77@gmail.com>
2021-05-25 11:51:17 +0300

    various changes

---
+++ scripts/deploy-smart-contracts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+test -z "${PRIVATE_KEY:-}" && (echo "Missing environment variable PRIVATE_KEY"; exit 1)
+test -z "${QUIKNODE_KEY:-}" && (echo "Missing environment variable QUIKNODE_KEY"; exit 1)
+test -z "${NETWORK:-}" && (echo "Missing environment variable NETWORK"; exit 1)
+
+lerna exec --scope @hoprnet/hopr-ethereum -- "PRIVATE_KEY=${PRIVATE_KEY}" "QUIKNODE_KEY=${QUIKNODE_KEY}" npx hardhat deploy --network "${NETWORK}"
