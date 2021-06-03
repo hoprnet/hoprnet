bd245abdc20f7c3190aaeb76c88590f0f134198d
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-06-03 16:54:33 +0200

    certified addresses go first

--- packages/utils/src/libp2p/localAddressesFirst.spec.ts
+++ packages/utils/src/libp2p/localAddressesFirst.spec.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai'
 import { localAddressesFirst } from './localAddressesFirst'
 
 describe(`test localAddressesFirst`, function () {
-    it(`should sort addresses, local first`, async function () {
+    it(`should put local addresses first`, async function () {
         const addresses = [            
             {
               multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'),
@@ -22,5 +22,36 @@ describe(`test localAddressesFirst`, function () {
     
         const sortedAddresses = localAddressesFirst(addresses)
         expect(sortedAddresses[0].multiaddr.equals(new Multiaddr('/ip4/127.0.0.1/tcp/4000'))).to.eql(true)
+    })
+
+    it('should sort public certified addresses first', () => {
+        const addresses = [
+          {
+            multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'),
+            isCertified: false
+          },
+          {
+            multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'),
+            isCertified: true
+          },
+          {
+            multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'),
+            isCertified: false
+          },
+          {
+            multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'),
+            isCertified: true
+          }
+        ]
+    
+        const sortedAddresses = localAddressesFirst(addresses)
+        expect(sortedAddresses).to.eql(
+            [
+                { multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'), isCertified: true },
+                { multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'), isCertified: false },
+                { multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'), isCertified: true },
+                { multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'), isCertified: false }
+            ]
+        )
     })
 })
