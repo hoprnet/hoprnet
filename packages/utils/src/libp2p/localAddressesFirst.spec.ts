a5f52c7657ec3597cc5d209700748f9156770077
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-06-03 16:47:32 +0200

    working implementation

--- packages/utils/src/libp2p/localAddressesFirst.spec.ts
+++ packages/utils/src/libp2p/localAddressesFirst.spec.ts
@@ -8,17 +8,19 @@ describe(`test localAddressesFirst`, function () {
         const addresses = [            
             {
               multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'),
+              isCertified: true,
             },
             {
               multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'),
+              isCertified: true,
             },
             {
               multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'),        
+              isCertified: true,
             }
         ]
     
         const sortedAddresses = localAddressesFirst(addresses)
-        expect(sortedAddresses[0].multiaddr.equals(new Multiaddr('/ip4/30.0.0.1/tcp/4000'))).to.eql(true)
-        expect(sortedAddresses[1].multiaddr.equals(new Multiaddr('/ip4/31.0.0.1/tcp/4000'))).to.eql(true)
-        expect(sortedAddresses[2].multiaddr.equals(new Multiaddr('/ip4/127.0.0.1/tcp/4000'))).to.eql(true)        })
+        expect(sortedAddresses[0].multiaddr.equals(new Multiaddr('/ip4/127.0.0.1/tcp/4000'))).to.eql(true)
+    })
 })
--- packages/utils/src/libp2p/localAddressesFirst.ts
+++ packages/utils/src/libp2p/localAddressesFirst.ts
@@ -1,3 +1,25 @@
-export function localAddressesFirst (addresses) {
-    return [...addresses].sort()
+import { Address } from 'libp2p/src/peer-store'
+import isPrivate from 'libp2p-utils/src/multiaddr/is-private'
+
+function addressesLocalFirstCompareFunction (a: Address, b: Address) {
+    const isAPrivate = isPrivate(a.multiaddr)
+    const isBPrivate = isPrivate(b.multiaddr)
+  
+    if (isAPrivate && !isBPrivate) {
+      return -1
+    } else if (!isAPrivate && isBPrivate) {
+      return 1
+    }
+    
+    if (a.isCertified && !b.isCertified) {
+      return -1
+    } else if (!a.isCertified && b.isCertified) {
+      return 1
+    }
+  
+    return 0
+  }
+  
+export function localAddressesFirst (addresses: Address[]) {
+    return [...addresses].sort(addressesLocalFirstCompareFunction)
   }
