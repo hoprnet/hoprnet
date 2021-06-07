e67aecfebe59cf09dc12b6edb2edd0aaebba505d
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-06-07 14:12:54 +0200

    publicAddressesFirst

--- packages/utils/src/libp2p/addressSorters.spec.ts
+++ packages/utils/src/libp2p/addressSorters.spec.ts
@@ -1,7 +1,7 @@
 import { Multiaddr } from 'multiaddr'
 import { expect } from 'chai'
 
-import { localAddressesFirst } from './addressSorters'
+import { localAddressesFirst, publicAddressesFirst } from './addressSorters'
 
 describe(`test localAddressesFirst`, function () {
   it(`should put local addresses first`, async function () {
@@ -28,7 +28,7 @@ describe(`test localAddressesFirst`, function () {
     ])
   })
 
-  it('should sort public certified addresses first', () => {
+  it('should put certified addresses first', () => {
     const addresses = [
       {
         multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'),
@@ -54,6 +54,61 @@ describe(`test localAddressesFirst`, function () {
       { multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'), isCertified: false },
       { multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'), isCertified: true },
       { multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'), isCertified: false }
+    ])
+  })
+})
+
+describe(`test publicAddressesFirst`, function () {
+  it(`should put public addresses first`, async function () {
+    const addresses = [
+      {
+        multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'),
+        isCertified: true
+      },
+      {
+        multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'),
+        isCertified: true
+      },
+      {
+        multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'),
+        isCertified: true
+      }
+    ]
+
+    const sortedAddresses = publicAddressesFirst(addresses)
+    expect(sortedAddresses).to.eql([
+      { multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'), isCertified: true },
+      { multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'), isCertified: true },
+      { multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'), isCertified: true }      
+    ])
+  })
+
+  it('should put certified addresses first', () => {
+    const addresses = [
+      {
+        multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'),
+        isCertified: false
+      },
+      {
+        multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'),
+        isCertified: true
+      },
+      {
+        multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'),
+        isCertified: false
+      },
+      {
+        multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'),
+        isCertified: true
+      }
+    ]
+
+    const sortedAddresses = publicAddressesFirst(addresses)
+    expect(sortedAddresses).to.eql([
+      { multiaddr: new Multiaddr('/ip4/31.0.0.1/tcp/4000'), isCertified: true },
+      { multiaddr: new Multiaddr('/ip4/30.0.0.1/tcp/4000'), isCertified: false },
+      { multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'), isCertified: true },
+      { multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4000'), isCertified: false }
     ])
   })
 })
--- packages/utils/src/libp2p/addressSorters.ts
+++ packages/utils/src/libp2p/addressSorters.ts
@@ -20,8 +20,31 @@ function addressesLocalFirstCompareFunction(a: Address, b: Address) {
   return 0
 }
 
+function addressesPublicFirstCompareFunction(a: Address, b: Address) {
+  const isAPrivate = isPrivate(a.multiaddr)
+  const isBPrivate = isPrivate(b.multiaddr)
+
+  if (isAPrivate && !isBPrivate) {
+    return 1
+  } else if (!isAPrivate && isBPrivate) {
+    return -1
+  }
+
+  if (a.isCertified && !b.isCertified) {
+    return -1
+  } else if (!a.isCertified && b.isCertified) {
+    return 1
+  }
+
+  return 0
+}
+
 export function localAddressesFirst(addresses: Address[]): Address[] {
   return [...addresses].sort(addressesLocalFirstCompareFunction)
+}
+
+export function publicAddressesFirst(addresses: Address[]): Address[] {
+  return [...addresses].sort(addressesPublicFirstCompareFunction)
 }
 
 export declare type AddressSorter = (input: Address[]) => Address[]
