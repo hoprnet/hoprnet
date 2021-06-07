c2e0a1ca211bdb5ffe9c84b03eff72ceb43bcf92
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-06-07 14:43:56 +0200

    fix

--- packages/hoprd/src/commands/listCommands.ts
+++ packages/hoprd/src/commands/listCommands.ts
@@ -1,6 +1,30 @@
 import { AbstractCommand } from './abstractCommand'
-import { getOptions } from './utils'
+import { getPaddingLength, styleValue } from './utils'
 
+function getOptions(
+  options: { value: any; description?: string }[],
+  style: 'compact' | 'vertical' = 'compact'
+): string[] {
+  if (style === 'compact') {
+    return [`Options: ${options.map((o) => String(o.value)).join('|')}`, '\n']
+  } else {
+    const padding = getPaddingLength(options.map((o) => String(o.value)))
+
+    return [
+      'Options:',
+      ...options.map((option) => {
+        return [
+          // needed to preperly format the array
+          '\n',
+          '- ',
+          styleValue(String(option.value).padEnd(padding), 'highlight'),
+          option.description
+        ].join('')
+      }),
+      '\n'
+    ]
+  }
+}
 export default class ListCommands extends AbstractCommand {
   constructor(private getCommands: () => AbstractCommand[]) {
     super()
--- packages/hoprd/src/commands/utils/index.ts
+++ packages/hoprd/src/commands/utils/index.ts
@@ -1,2 +1,12 @@
 export * from './checkPeerId'
 export * from './message'
+
+const EXTRA_PADDING = 2
+
+export function styleValue(value: any, _type?: any): string {
+  return value // no-op[
+}
+export function getPaddingLength(items: string[], addExtraPadding: boolean = true): number {
+  return Math.max(...items.map((str) => str.length)) + (addExtraPadding ? EXTRA_PADDING : 0)
+}
+
