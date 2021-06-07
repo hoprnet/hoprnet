8b5a20399c376f44ed4029aedddffe1a693866a5
Tino Breddin <tino@hoprnet.org>
2021-06-07 16:05:02 +0200

    Add CI job to update hardhat contracts

--- scripts/deploy-smart-contracts.sh
+++ scripts/deploy-smart-contracts.sh
@@ -4,12 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-test -z "${DEPLOYER_WALLET_PRIVATE_KEY:-}" && (echo "Missing environment variable DEPLOYER_WALLET_PRIVATE_KEY"; exit 1)
 test -z "${NETWORK:-}" && (echo "Missing environment variable NETWORK"; exit 1)
 
 if [ $NETWORK == "xdai" ]; then
+  test -z "${DEPLOYER_WALLET_PRIVATE_KEY:-}" && (echo "Missing environment variable DEPLOYER_WALLET_PRIVATE_KEY"; exit 1)
   test -z "${QUIKNODE_KEY:-}" && (echo "Missing environment variable QUIKNODE_KEY"; exit 1)
 elif [ $NETWORK == "goerli" ]; then
+  test -z "${DEPLOYER_WALLET_PRIVATE_KEY:-}" && (echo "Missing environment variable DEPLOYER_WALLET_PRIVATE_KEY"; exit 1)
   test -z "${INFURA_KEY:-}" && (echo "Missing environment variable INFURA_KEY"; exit 1)
   test -z "${ETHERSCAN_KEY:-}" && (echo "Missing environment variable ETHERSCAN_KEY"; exit 1)
 fi
@@ -19,4 +20,4 @@ dir=$(dirname $(readlink -f $0))
 cd "${dir}/../packages/ethereum"
 
 # deploy smart contracts
-yarn deploy --network "${NETWORK}"
+yarn hardhat deploy --network "${NETWORK}"
