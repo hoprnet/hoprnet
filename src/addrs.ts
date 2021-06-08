9822acc230f7279677b407b2f76f5322db84e35a
Robert Kiel <robert.kiel@hoprnet.io>
2021-06-08 15:29:07 +0200

    improve testing and ignore link-locale addresses

--- src/addrs.ts
+++ src/addrs.ts
@@ -1,40 +1,62 @@
 import { networkInterfaces, NetworkInterfaceInfo } from 'os'
-import { nodeToMultiaddr } from './utils'
+import { isLinkLocaleAddress, nodeToMultiaddr } from './utils'
 import { Multiaddr } from 'multiaddr'
 import Debug from 'debug'
 const log = Debug('hopr-connect')
 
 import { isLocalhost, ipToU8aAddress, isPrivateAddress } from './utils'
 
+type AddrOptions = {
+  interface?: string
+  useIPv4?: boolean
+  useIPv6?: boolean
+  includePrivateIPv4?: boolean
+  includeLocalhostIPv4?: boolean
+  includeLocalhostIPv6?: boolean
+}
+
+function validateOptions(opts?: AddrOptions) {
+  if (opts == undefined) {
+    return
+  }
+  if (opts.useIPv4 == false && (opts.includePrivateIPv4 || opts.includeLocalhostIPv4)) {
+    throw Error(`Contradiction in opts. Cannot add private or local IPv4 address if IPv4 is disabled.`)
+  }
+
+  if (opts.useIPv6 == false && opts.includeLocalhostIPv6) {
+    throw Error(`Contradiction in opts. Cannot add private or local IPv4 address if IPv4 is disabled.`)
+  }
+}
+
+function getAddrsOfInterface(iface: string) {
+  let ifaceAddrs = networkInterfaces()[iface]
+
+  if (ifaceAddrs == undefined) {
+    log(
+      `Interface <${iface}> does not exist on this machine. Available are <${Object.keys(networkInterfaces()).join(
+        ', '
+      )}>`
+    )
+    return []
+  }
+
+  return ifaceAddrs
+}
+
 export function getAddrs(
   port: number,
   peerId: string,
-  options?: {
-    interface?: string
-    useIPv4?: boolean
-    useIPv6?: boolean
-    includeLocalIPv4?: boolean
-    includeLocalIPv6?: boolean
-    includeLocalhostIPv4?: boolean
-    includeLocalhostIPv6?: boolean
-  }
+  options?: AddrOptions,
+  __fakeInterfaces?: ReturnType<typeof networkInterfaces>
 ) {
+  validateOptions(options)
+
   let interfaces: (NetworkInterfaceInfo[] | undefined)[]
 
-  if (options?.interface != undefined) {
-    let _tmp = networkInterfaces()[options.interface]
-
-    if (_tmp == undefined) {
-      log(
-        `Interface <${options.interface}> does not exist on this machine. Available are <${Object.keys(
-          networkInterfaces()
-        ).join(', ')}>`
-      )
-      return []
-    }
-    interfaces = [_tmp]
+  if (options?.interface) {
+    interfaces = [getAddrsOfInterface(options.interface)]
   } else {
-    interfaces = Object.values(networkInterfaces())
+    interfaces = Object.values(__fakeInterfaces ?? networkInterfaces())
   }
 
   const multiaddrs: Multiaddr[] = []
@@ -46,11 +68,13 @@ export function getAddrs(
 
     for (const address of iface) {
       const u8aAddr = ipToU8aAddress(address.address, address.family)
+
+      if (isLinkLocaleAddress(u8aAddr, address.family)) {
+        continue
+      }
+
       if (isPrivateAddress(u8aAddr, address.family)) {
-        if (address.family === 'IPv4' && (options == undefined || options.includeLocalIPv4 != true)) {
-          continue
-        }
-        if (address.family === 'IPv6' && (options == undefined || options.includeLocalIPv6 != true)) {
+        if (address.family === 'IPv4' && (options == undefined || options.includePrivateIPv4 != true)) {
           continue
         }
       }
@@ -64,11 +88,11 @@ export function getAddrs(
         }
       }
 
-      if (address.family === 'IPv4' && options != undefined && options.useIPv4 == false) {
+      if (address.family === 'IPv4' && options != undefined && options.useIPv4 != true) {
         continue
       }
 
-      if (address.family === 'IPv6' && options != undefined && options.useIPv6 == false) {
+      if (address.family === 'IPv6' && options != undefined && options.useIPv6 != true) {
         continue
       }
 
