92a0966482eb22a480aeb4aabd8047d6d948ab0d
Robert Kiel <robert.kiel@hoprnet.io>
2021-06-11 18:10:40 +0200

    add password to e2e tests

--- scripts/run-integration-tests-source.sh
+++ scripts/run-integration-tests-source.sh
@@ -94,6 +94,7 @@ function setup_node() {
     --testAnnounceLocalAddresses --identity="${id}" \
     --host="0.0.0.0:${host_port}" \
     --data="${dir}" --rest --restPort "${port}" --announce > \
+    --password="e2e-test" \
     "${log}" 2>&1 &
 
   wait_for_http_port "${port}" "${wait_delay}" "${wait_max_wait}" "${log}"
