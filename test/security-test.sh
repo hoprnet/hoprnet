945fa3c14f693e4cb8b94b31b8193489e8cd1608
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-06-21 13:15:49 +0300

    changed to new logging system

--- test/security-test.sh
+++ test/security-test.sh
@@ -1,58 +1,68 @@
 #!/bin/bash
 
-set -e 
+set -Eeuo pipefail
 set -x
+
+# set log id and use shared log function for readable logs
+declare mydir
+mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+declare HOPR_LOG_ID="e2e-test"
+source "${mydir}/../scripts/utils.sh"
+
+usage() {
+  msg
+  msg "Usage: $0 <rest_ip:port> <admin_ip:port>"
+  msg
+}
 
 declare rest_host_port="${1}"
 declare admin_host_port="${2}"
 
-echo " - Security tests started"
-echo " - Rest API @ ${rest_host_port}"
-echo " - WS API @ ${admin_host_port}"
+log " - Security tests started"
+log " - Rest API @ ${rest_host_port}"
+log " - WS API @ ${admin_host_port}"
 
 # should fail authentication without proper token
-echo " - Testing REST rejecting null token"
+log " - Testing REST rejecting null token"
 STATUS_CODE=$(curl -H "X-Auth-Token: bad-token" --output /dev/null --write-out "%{http_code}" --silent --max-time 360 -X POST --data "fake cmd" "${rest_host_port}/api/v1/command")
 if [ ${STATUS_CODE} -ne 403 ]; then
-  echo " - Didn't get 403 with bad token"
+  log " - Didn't get 403 with bad token"
   exit 1
 fi
 
 # should fail authentication without a token
-echo " - Testing REST rejecting bad token"
+log " - Testing REST rejecting bad token"
 STATUS_CODE=$(curl --output /dev/null --write-out "%{http_code}" --silent --max-time 360 -X POST --data "fake cmd" "${rest_host_port}/api/v1/command")
 if [ ${STATUS_CODE} -ne 403 ]; then
-  echo " - Didn't get 403 with no token"
+  log " - Didn't get 403 with no token"
   exit 1
 fi
 
 # should reject admin panel commands with no tocken
-echo " - Testing WS rejecting null token"
+log " - Testing WS rejecting null token"
 WS_RESPONSE=$(npx wscat --connect ws://${admin_host_port} --execute info)
 if [ "${WS_RESPONSE}" != "authentication failed" ]; then
-  echo " - Didn't fail ws authentication with no token"
-  echo " - Expected response: 'authentication failed' "
-  echo " - Actual response:"
-  echo "${WS_RESPONSE}"
+  log " - Didn't fail ws authentication with no token"
+  log " - Expected response: 'authentication failed' "
+  log " - Actual response:"
+  log "${WS_RESPONSE}"
   exit 1
 fi
 
 # should reject admin panel commands with bad token
-echo " - Testing WS rejecting bad token"
+log " - Testing WS rejecting bad token"
 WS_RESPONSE=$(npx wscat --connect ws://${admin_host_port} --execute info --header "Cookie:X-Auth-Token=bad-token")
 if [ "${WS_RESPONSE}" != "authentication failed" ]; then
-  echo " - Didn't fail ws authentication with bad token"
-  echo " - Expected response: 'authentication failed' "
-  echo " - Actual response:"
-  echo "${WS_RESPONSE}"
+  log " - Didn't fail ws authentication with bad token"
+  log " - Expected response: 'authentication failed' "
+  log " - Actual response:"
+  log "${WS_RESPONSE}"
   exit 1
 fi
 
 # should execute commands with right token
-echo " - Testing WS executing commands with right token"
+log " - Testing WS executing commands with right token"
 WS_RESPONSE=$(npx wscat --connect ws://${admin_host_port} --execute info --header "Cookie:X-Auth-Token=e2e-api-token")
-echo " - Actual response:"
-echo "${WS_RESPONSE}"
-echo "${WS_RESPONSE}" | grep -q "ws connection authenticated with token"
+log "${WS_RESPONSE}" | grep -q "ws connection authenticated with token"
 
-echo " - Security tests finished successfully"
+log " - Security tests finished successfully"
