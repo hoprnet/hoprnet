a9daf0ef8fcbba90f7d0cb8d5e989d0ef91109ba
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-06-22 12:11:32 +0300

    improved websocat install script

--- scripts/install-websocat.sh
+++ scripts/install-websocat.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-set -e
+# prevent sourcing of this script, only allow execution
+$(return >/dev/null 2>&1)
+test "$?" -eq "0" && { echo "This script should only be executed." >&2; exit 1; }
 
-which websocat > /dev/null || \
-(
-    sudo apt-get update && \
-    sudo apt-get install curl -y && \
-    curl -sLO https://github.com/vi/websocat/releases/download/v1.8.0/websocat_1.8.0_newer_amd64.deb && \
+# set log id and use shared log function for readable logs
+declare mydir
+mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+declare HOPR_LOG_ID="e2e-setup"
+source "${mydir}/../scripts/utils.sh"
+
+declare exit_code
+
+exit_code=$(which websocat > /dev/null)
+if [ ${exit_code} -ne 0 ]; then
+    log "websocat binary not found, trying to install Linux version"
+    sudo apt-get update 
+    sudo apt-get install curl -y 
+    curl -sLO https://github.com/vi/websocat/releases/download/v1.8.0/websocat_1.8.0_newer_amd64.deb 
     sudo dpkg -i websocat_1.8.0_newer_amd64.deb
-)
+fi
+
