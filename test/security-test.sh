d0278b050c76b3610a64f2bb8c617a883340feb0
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-06-22 11:46:51 +0300

    removed explicit -x

--- test/security-test.sh
+++ test/security-test.sh
@@ -4,8 +4,6 @@
 $(return >/dev/null 2>&1)
 test "$?" -eq "0" && { echo "This script should only be executed." >&2; exit 1; }
 
-set -x
-
 # set log id and use shared log function for readable logs
 declare mydir
 mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
