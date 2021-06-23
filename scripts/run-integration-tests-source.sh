5e93771bdc9dd083efa137262969f3acc4de433e
nionis <stivennoni77@gmail.com>
2021-06-23 16:49:09 +0300

    fix E2E test

--- scripts/run-integration-tests-source.sh
+++ scripts/run-integration-tests-source.sh
@@ -216,7 +216,7 @@ wait_for_port 19095 "${node5_log}"
 
 # --- Run security tests --- {{{
 ${mydir}/../test/security-test.sh \
-  127.0.0.1 13301 19501
+  127.0.0.1 13301 19091
 #}}}
 
 # --- Run protocol test --- {{{
