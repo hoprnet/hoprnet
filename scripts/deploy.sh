93dab93ab5773408159ce3ae471d2f4c331737d3
Tino Breddin <t@tolbrino.com>
2021-06-29 10:30:05 +0200

    Ensure release branches use xdai when deploying nodes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,11 +15,17 @@ source scripts/cleanup.sh
 # ENV Variables:
 # - GITHUB_REF: ie. `/refs/heads/mybranch`
 # - RPC: provider address, ie `https://rpc-mainnet.matic.network`
+# - RPC_NETWORK: provider network id, e.g. xdai
 # - FUNDING_PRIV_KEY: funding private key, raw
 # - BS_PASSWORD: database password
 
-if [ -z "${RPC:-}" ]; then
+if [ -z "${RPC:-}" ] && [ "${RPC_NETWORK:-}" = "goerli" ]; then
   RPC=https://eth-goerli.gateway.pokt.network/v1/6021a2b6928ff9002e6c7f2f
+elif [ -z "${RPC:-}" ] && [ "${RPC_NETWORK:-}" = "xdai" ]; then
+  RPC=wss://still-patient-forest.xdai.quiknode.pro/f0cdbd6455c0b3aea8512fc9e7d161c1c0abf66a/
+elif [ "${RPC_NETWORK:-}" != "xdai" ] && [ "${RPC_NETWORK:-}" != "goerli" ]; then
+  echo "Missing supported RPC_NETWORK"
+  exit 1
 fi
 
 # Get version from package.json
