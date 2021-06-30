299fd1fa3de7cbe3e1f4630ef499d823d467fecc
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-06-30 14:41:13 +0200

    wip

--- packages/cover-traffic/lib/index.js
+++ packages/cover-traffic/lib/index.js
@@ -1,1 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const hopr_core_1 = __importDefault(require("@hoprnet/hopr-core"));
+async function main() {
+    const peerId = null;
+    const options = {};
+    const node = new hopr_core_1.default(peerId, options);
+    await node.waitForFunds();
+    await node.start();
+}
+main();
 //# sourceMappingURL=index.js.map
---
+++ packages/cover-traffic/src/index.ts
@@ -0,0 +1,12 @@
+import Hopr from '@hoprnet/hopr-core'
+import type { HoprOptions } from '@hoprnet/hopr-core'
+
+async function main(){
+  const peerId = null
+  const options: HoprOptions = {} as any
+  const node = new Hopr(peerId, options)
+  await node.waitForFunds()
+  await node.start()
+}
+
+main()
