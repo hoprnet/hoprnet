fe321203e2ca9f4d3cf11114c401532b15a4f953
Tino Breddin <tino@hoprnet.org>
2021-07-23 15:29:40 +0200

    use yarn instead of lerna for npm packages

--- scripts/publish-pre-release.sh
+++ scripts/publish-pre-release.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
+# prevent souring of this script, only allow execution
+$(return >/dev/null 2>&1)
+test "$?" -eq "0" && { echo "This script should only be executed." >&2; exit 1; }
+
+# exit on errors, undefined variables, ensure errors in pipes are not hidden
+set -Eeuo pipefail
 
 declare branch
 declare mydir
@@ -24,15 +27,22 @@ test -n "${npm_package}" && version_type="preminor" || version_type="prerelease"
 yarn
 yarn build
 
-# create new version in each package, and tag in Git
-npx lerna version "${version_type}" --yes --exact --no-push --no-changelog \
-  --preid next -m "chore(release): publish %s"
+# create new version in each package
+yarn workspaces foreach -piv --no-private --topological-dev version prerelease
+declare new_version
+new_version=$(HOPR_PACKAGE=hoprd ${mydir}/get-package-version.sh)
+
+# commit changes and create Git tag
+git add packages/*/package.json
+git commit -m "chore(release): publish ${new_version}"
+git tag v${new_version}
 
 # only make remote changes if running in CI
 if [ "${CI:-}" = "true" ] && [ -z "${ACT:-}" ]; then
   # push changes back onto origin including new tag
   git push origin "${branch}" --tags
 
-  # publish version to npm
-  npx lerna publish from-package --yes
+  # publish each workspace package to npm
+  yarn workspaces foreach -piv --no-private --topological-dev \
+    npm publish --access public
 fi
