bf6c50210e8541ff4f3d254efffcd867b1e66324
Robert Kiel <robert.kiel@hoprnet.org>
2021-07-23 09:59:34 +0200

    Less verbose connect errors (#2143)
    * chore(deps-dev): bump @types/secp256k1 from 4.0.2 to 4.0.3
    
    Bumps [@types/secp256k1](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/secp256k1) from 4.0.2 to 4.0.3.
    - [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)
    - [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/secp256k1)
    
    ---
    updated-dependencies:
    - dependency-name: "@types/secp256k1"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * upgrade libp2p packages
    
    * fix libp2p api change
    
    * relay management api
    
    * typo
    
    * small API change
    
    * bump hopr-connect
    
    * remove comment
    
    * type fixes
    
    * package upgrade
    
    * chore(deps-dev): bump typedoc-plugin-markdown from 3.10.3 to 3.10.4
    
    Bumps [typedoc-plugin-markdown](https://github.com/tgreyuk/typedoc-plugin-markdown/tree/HEAD/packages/typedoc-plugin-markdown) from 3.10.3 to 3.10.4.
    - [Release notes](https://github.com/tgreyuk/typedoc-plugin-markdown/releases)
    - [Changelog](https://github.com/tgreyuk/typedoc-plugin-markdown/blob/master/packages/typedoc-plugin-markdown/CHANGELOG.md)
    - [Commits](https://github.com/tgreyuk/typedoc-plugin-markdown/commits/typedoc-plugin-markdown@3.10.4/packages/typedoc-plugin-markdown)
    
    ---
    updated-dependencies:
    - dependency-name: typedoc-plugin-markdown
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * bump hopr-connect
    
    * streamline bn.js
    
    * package upgrades
    
    * remove unnecessary packets
    
    * remove unused constant
    
    * make dial errors less verbose
    
    * restore dht property
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Robert Kiel <robert.kiel@hoprnet.io>

--- packages/utils/src/libp2p/index.ts
+++ packages/utils/src/libp2p/index.ts
@@ -95,8 +95,8 @@ export type DialResponse =
     }
   | {
       status: 'E_DIAL'
-      error: Error
-      dht: boolean
+      error: string
+      dhtContacted: boolean
     }
   | {
       status: 'E_DHT_QUERY'
@@ -155,7 +155,7 @@ export async function dial(
   if ((err != null || struct == null) && libp2p._dht == undefined) {
     logError(`Could not dial ${destination.toB58String()} directly and libp2p was started without a DHT.`)
     clearTimeout(timeout)
-    return { status: 'E_DIAL', error: err, dht: false }
+    return { status: 'E_DIAL', error: err.message, dhtContacted: false }
   }
 
   // Try to get some fresh addresses from the DHT
@@ -176,7 +176,7 @@ export async function dial(
   // Only start a dial attempt if we have received new addresses
   if (newAddresses.length == 0) {
     clearTimeout(timeout)
-    return { status: 'E_DIAL', error: new Error('No new addresses'), dht: true }
+    return { status: 'E_DIAL', error: 'No new addresses after contacting the DHT', dhtContacted: true }
   }
 
   try {
@@ -185,7 +185,7 @@ export async function dial(
   } catch (err) {
     logError(`Using new addresses after querying the DHT did not lead to a connection. Cannot connect. ${err.message}`)
     clearTimeout(timeout)
-    return { status: 'E_DIAL', error: err, dht: true }
+    return { status: 'E_DIAL', error: err.message, dhtContacted: true }
   }
 
   if (signal.aborted) {
