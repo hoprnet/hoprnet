2544052023df03e6046d3257a07373c7f5289f6d
Tino Breddin <t@tolbrino.com>
2021-08-06 16:44:32 +0200

    get nix shell to work

--- flake.nix
+++ flake.nix
@@ -2,16 +2,13 @@
   description = "hoprnet monorepo";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix {
-            inherit
-              pkgs;
-          };
-        }
-      );
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "hoprnet";
+      shell = ./shell.nix;
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+    };
 }
