33f4c571a8781d400d8ff40f3d3e0804a8db3d17
Robert Kiel <robert.kiel@hoprnet.io>
2021-08-10 15:48:43 +0200

    fix cleanup script

--- packages/ethereum/tasks/slimDeployments.ts
+++ packages/ethereum/tasks/slimDeployments.ts
@@ -3,31 +3,31 @@ import type { DeployFunction } from 'hardhat-deploy/types'
 import { join } from 'path'
 import { readdir, writeFile } from 'fs/promises'
 
-const DEPLOYMENTS_PATH = join(__dirname, '..', 'deployments')
-
 // deployments are done by `hardhat-deploy`
 // after a deployment, `hardhat-deploy` populates `deployments`
 // folder with various artifacts, this task loops through `deployments`
 // folder and removes data that are optional & will end up being commited
 const main: DeployFunction = async function (_hre: HardhatRuntimeEnvironment) {
-  const networks = await readdir(DEPLOYMENTS_PATH)
+  const basePath = join(
+    __dirname,
+    '..',
+    'deployments',
+    process.env['DEPLOY_LABEL'] ?? 'default',
+    _hre.network.name == 'hardhat' ? 'localhost' : _hre.network.name
+  )
+  const contracts = (await readdir(basePath)).filter((file) => file.endsWith('.json'))
 
-  for (const network of networks) {
-    const contracts = await readdir(join(DEPLOYMENTS_PATH, network)).then((files) =>
-      files.filter((file) => file.endsWith('.json'))
-    )
-
-    for (const contract of contracts) {
-      const filePath = join(DEPLOYMENTS_PATH, network, contract)
-      const data = require(filePath)
-      const slimmed = {
-        address: data.address,
-        transactionHash: data.transactionHash,
-        abi: data.abi
-      }
-
-      await writeFile(filePath, JSON.stringify(slimmed, null, 2))
+  for (const contract of contracts) {
+    const filePath = join(basePath, contract)
+    const data = require(filePath)
+    const slimmed = {
+      address: data.address,
+      transactionHash: data.transactionHash,
+      blockNumber: data.receipt.blockNumber,
+      abi: data.abi
     }
+
+    await writeFile(filePath, JSON.stringify(slimmed, null, 2))
   }
 }
 
