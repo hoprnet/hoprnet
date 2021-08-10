eb153757d4e78ab377716d1de409266670e9399c
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-08-10 10:25:31 +0200

    refactored test

--- testenv.ts
+++ testenv.ts
@@ -1,12 +1,16 @@
 import { validate } from 'jsonschema'
 import fs from 'fs'
 
+function load_json(file_path: string): any {
+    const content = fs.readFileSync(file_path, 'utf-8')
+    return JSON.parse(content)
+}
+
 function validate_file(file_path: string, schema_path: string) {
     console.log(`testing ${file_path} against ${schema_path}`)
-    const schema_content = fs.readFileSync(schema_path, 'utf-8')
-    const schema = JSON.parse(schema_content)
-    const data_content = fs.readFileSync(file_path, 'utf-8')
-    const data = JSON.parse(data_content)
+
+    const data = load_json(file_path)
+    const schema = load_json(schema_path)
 
     const res = validate(data, schema)
     for (const err of res.errors) {
@@ -15,6 +19,7 @@ function validate_file(file_path: string, schema_path: string) {
     if (res.errors.length > 0) {
         throw new Error(`validation failed`)
     }
+    console.log(`schema test ok`)
 }
 
 validate_file('./environments.json', './environments_schema.json')
