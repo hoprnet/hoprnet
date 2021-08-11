15c22173acdad8ddd632ce79141475de16a4f73a
Robert Kiel <robert.kiel@hoprnet.io>
2021-08-11 17:48:25 +0200

    refactoring

--- packages/ethereum/type-extension.ts
+++ packages/ethereum/type-extension.ts
@@ -1,33 +1,9 @@
-import { EthereumProvider } from 'hardhat/types'
-import { Address, DeploymentsExtension } from 'hardhat-deploy/types'
+import 'hardhat-deploy'
 
+// Extend Hardhat's runtime environment type to include
+// environment property
 declare module 'hardhat/types/runtime' {
   interface HardhatRuntimeEnvironment {
     environment: string
-    deployments: DeploymentsExtension
-    getNamedAccounts: () => Promise<{
-      [name: string]: Address
-    }>
-    getUnnamedAccounts: () => Promise<string[]>
-    getChainId(): Promise<string>
-    companionNetworks: {
-      [name: string]: {
-        deployments: DeploymentsExtension
-        getNamedAccounts: () => Promise<{
-          [name: string]: Address
-        }>
-        getUnnamedAccounts: () => Promise<string[]>
-        getChainId(): Promise<string>
-        provider: EthereumProvider
-      }
-    }
-  }
-
-  interface Network {
-    live: boolean
-    saveDeployments: boolean
-    tags: Record<string, boolean>
-    deploy: string[]
-    companionNetworks: { [name: string]: string }
   }
 }
