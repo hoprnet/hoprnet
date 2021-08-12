2bc553695f2fd29f65e3bd3ce4ea5c3f09df170b
HOPR Linting robot <noreply@hoprnet.org>
2021-08-12 15:53:59 +0000

    Fix linting

--- packages/hoprd/src/admin.ts
+++ packages/hoprd/src/admin.ts
@@ -26,7 +26,7 @@ export class AdminServer {
   private wsServer: any
   private cmds: Commands
 
-  constructor(private logs: LogStream, private host: string, private port: number, private apiToken?: string) { }
+  constructor(private logs: LogStream, private host: string, private port: number, private apiToken?: string) {}
 
   authenticate(req): boolean {
     if (!this.apiToken) {
@@ -47,7 +47,7 @@ export class AdminServer {
       try {
         // NB: We use a placeholder domain since req.url only passes query params
         const url = new URL(`https://hoprnet.org${req.url}`)
-        const apiToken = url.searchParams && url.searchParams.get('apiToken') || ''
+        const apiToken = (url.searchParams && url.searchParams.get('apiToken')) || ''
         if (encodeURI(apiToken) == this.apiToken) {
           return true
         }
@@ -155,16 +155,16 @@ export class AdminServer {
     this.node.on('hopr:warning:unfunded', (addr) => {
       this.logs.log(
         `- The account associated with this node has no ${Balance.SYMBOL},\n` +
-        `  in order to send messages, or open channels, you will need to send` +
-        `  at least ${MIN_BALANCE} to ${addr}`
+          `  in order to send messages, or open channels, you will need to send` +
+          `  at least ${MIN_BALANCE} to ${addr}`
       )
     })
 
     this.node.on('hopr:warning:unfundedNative', (addr) => {
       this.logs.log(
         `- The account associated with this node has no ${NativeBalance.SYMBOL},\n` +
-        `  in order to fund gas for protocol overhead you will need to send\n` +
-        `  ${MIN_NATIVE_BALANCE} to ${addr}`
+          `  in order to fund gas for protocol overhead you will need to send\n` +
+          `  ${MIN_NATIVE_BALANCE} to ${addr}`
       )
     })
 
@@ -176,7 +176,7 @@ export class AdminServer {
 
     process.env.NODE_ENV == 'production' && showDisclaimer(this.logs)
 
-    this.cmds.execute(() => { }, `alias ${node.getId().toB58String()} me`)
+    this.cmds.execute(() => {}, `alias ${node.getId().toB58String()} me`)
   }
 }
 
