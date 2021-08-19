3f2a3cf9c29a35fa4e129f8acca1d9803daddfb5
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-08-19 14:34:45 +0200

    throw on var expansion error

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -7,8 +7,16 @@ export function timer(fn: () => void): number {
   return end
 }
 
+/**
+ * 
+ * @param input a string containing templated references to environment variables e.g. 'foo ${bar}'
+ * @returns a string with variables resolved to the actual values
+ */
 export function expand_env_vars(input: string) {
   return input.replace(/\$\{(.*)\}/g, (_, var_name) => {
+    if (!(var_name in process.env)) {
+      throw new Error(`failed to expand env vars in string '${input}', env var ${var_name} not defined`)
+    }
     return process.env[var_name]
   })
 }
