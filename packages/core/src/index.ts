a2041b958c70e14de5a8ec0a26a1ba72147466a6
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-08-19 15:38:24 +0200

    added type comments

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -75,21 +75,21 @@ type PeerStoreAddress = {
 export type Network = {
   id: string
   description: string
-  chain_id: number
+  chain_id: number // >= 0
   live: boolean
-  default_provider: string
-  gas: string
-  gas_multiplier: number
+  default_provider: string // a valid HTTP url pointing at a RPC endpoint
+  gas: string // e.g. '1 gwei'
+  gas_multiplier: number // e.g. 1.1
   native_token_name: string
   hopr_token_name: string
 }
 
 export type Environment = {
   id: string
-  network_id: string
-  channel_contract_deploy_block: number
-  token_contract_address: string
-  channels_contract_address: string
+  network_id: string // must match one of the Network.id
+  channel_contract_deploy_block: number // >= 0
+  token_contract_address: string // an Ethereum address
+  channels_contract_address: string // an Ethereum address
 }
 
 export type ProtocolConfig = {
@@ -101,8 +101,8 @@ export type ResolvedEnvironment = {
   id: string
   network: Network
   channel_contract_deploy_block: number
-  token_contract_address: string
-  channels_contract_address: string
+  token_contract_address: string // an Ethereum address
+  channels_contract_address: string // an Ethereum address
 }
 
 export type HoprOptions = {
