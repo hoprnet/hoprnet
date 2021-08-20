b132086c3280f95c045d50b44f54f1805c5695aa
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-08-20 11:19:29 +0200

    fixed snake_case

--- packages/hoprd/test/protocol-config.spec.ts
+++ packages/hoprd/test/protocol-config.spec.ts
@@ -2,12 +2,12 @@ import { validate } from 'jsonschema'
 import fs from 'fs'
 import type { Network, ProtocolConfig } from '@hoprnet/hopr-core'
 
-function load_json(file_path: string): any {
+function loadJson(file_path: string): any {
     const content = fs.readFileSync(file_path, 'utf-8')
     return JSON.parse(content)
 }
 
-function validate_data(data: ProtocolConfig, schema: any) {
+function validateData(data: ProtocolConfig, schema: any) {
     const res = validate(data, schema)
     for (const err of res.errors) {
         console.log(err.stack)
@@ -19,14 +19,14 @@ function validate_data(data: ProtocolConfig, schema: any) {
 
 describe('protocol config', async function () {
     it('should conform to schema', async function () {
-        const env_data = load_json('./protocol-config.json') as ProtocolConfig
-        const env_schema = load_json('./protocol-config-schema.json')
+        const env_data = loadJson('./protocol-config.json') as ProtocolConfig
+        const env_schema = loadJson('./protocol-config-schema.json')
 
-        validate_data(env_data, env_schema)
+        validateData(env_data, env_schema)
     })
 
     it('should be internally consistent', async function () {
-        function get_network(id: string): Network | null {
+        function getNetwork(id: string): Network | null {
             for (const network of env_data.networks) {
                 if (network.id === id) {
                     return network
@@ -35,10 +35,10 @@ describe('protocol config', async function () {
             return null
         }
 
-        const env_data = load_json('./protocol-config.json') as ProtocolConfig
+        const env_data = loadJson('./protocol-config.json') as ProtocolConfig
 
         for (const env of env_data.environments) {
-            if (get_network(env.network_id) == null) {
+            if (getNetwork(env.network_id) == null) {
                 throw new Error(`no such network: ${env.network_id}`)
             }
         }
