6210add5e8595df05e0c3e75af48b53fe0421e4f
jjperezaguinaga <me@jjperezaguinaga.com>
2021-08-26 07:28:20 +0000

    hotfix: Increasing confirmation blocks

--- packages/core-ethereum/src/constants.ts
+++ packages/core-ethereum/src/constants.ts
@@ -4,5 +4,5 @@
  */
 export const PROVIDER_CACHE_TTL = 30e3 // 30 seconds
 export const PROVIDER_DEFAULT_URI = 'http://127.0.0.1:8545/'
-export const CONFIRMATIONS = 8
+export const CONFIRMATIONS = 40
 export const INDEXER_BLOCK_RANGE = 2000
