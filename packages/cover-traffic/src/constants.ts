3422d2390ee7bed5290916691ec088787632ddcd
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2021-09-08 17:46:36 +0200

    Add code comments

--- packages/cover-traffic/src/constants.ts
+++ packages/cover-traffic/src/constants.ts
@@ -1,9 +1,10 @@
 import BN from 'bn.js'
 
 export const CHANNELS_PER_COVER_TRAFFIC_NODE = 10
-export const CHANNEL_STAKE = new BN('1000')
+export const CHANNEL_STAKE = new BN('1000') // Minimum HOPR token balance of the cover traffic node to remain working.
 export const MINIMUM_STAKE_BEFORE_CLOSURE = new BN('0')
 export const CT_INTERMEDIATE_HOPS = 2 // 3  // NB. min is 2
 export const MESSAGE_FAIL_THRESHOLD = 1000 // Failed sends to channel before we autoclose
 export const CT_PATH_RANDOMNESS = 0.2
-export const CT_NETWORK_QUALITY_THRESHOLD = 0.15
+export const CT_NETWORK_QUALITY_THRESHOLD = 0.15 // Minimum channel quality to keep a channel open.
+export const CT_OPEN_CHANNEL_QUALITY_THRESHOLD = 0.6 // Minimum channel quality to open a new channel.
