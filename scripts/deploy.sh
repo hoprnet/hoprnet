b0aab7897edb7a29d5b9f64dbf7bc5b134c06ddf
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-09-14 12:08:29 +0300

    working loop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,6 @@ set -e #u
 shopt -s expand_aliases
 #set -o xtrace
 
-set -x
-
 source scripts/environments.sh
 source scripts/testnet.sh
 source scripts/cleanup.sh
@@ -24,16 +22,40 @@ if [ -z "${RELEASE:-}" ]; then
   RELEASE=$(node -p -e "require('./packages/hoprd/package.json').version")
 fi
 
-# Get RELEASE_NAME, from environment
-get_environment
+_jq() {
+  echo "$1" | base64 --decode | jq -r "$2"
+}
 
-TESTNET_NAME="$RELEASE_NAME-$(echo "$VERSION_MAJ_MIN" | sed 's/\./-/g')"
-TESTNET_SIZE=3
+echo "Looking for releases to deploy (GITHUB_REF == ${GITHUB_REF})"
 
-echo "Cleaning up before deploy"
-#cleanup
+# iterate through releases with git_ref == $GITHUB_REF
+for row in $(cat packages/hoprd/releases.json | ./node_modules/.bin/strip-json-comments | jq -r ".[] | select(.git_ref==\"${GITHUB_REF}\") | @base64"); do
+  declare RELEASE_ID=$(_jq "${row}" ".id")
+  declare DEPRECATED=$(_jq "${row}" ".deprecated")
+  declare ENVIRONMENT_ID=$(_jq "${row}" ".environment_id")
+  declare VERSION_MAJOR=$(_jq "${row}" ".version_major")
+  declare VERSION_MINOR=$(_jq "${row}" ".version_minor")
 
-cleanup_instance "${TESTNET_NAME}"
+  echo "Matching release id: ${RELEASE_ID}"
+  
+  if [ "${DEPRECATED}" == "true" ]; then
+    echo "deprecated, skipping"
+    continue
+  fi
 
-echo "Starting testnet '$TESTNET_NAME' with $TESTNET_SIZE nodes and image hoprd:$RELEASE, environment id: $ENVIRONMENT_ID"
-start_testnet $TESTNET_NAME $TESTNET_SIZE "gcr.io/hoprassociation/hoprd:$RELEASE" $ENVIRONMENT_ID
+  declare VERSION_MAJ_MIN
+  if [ "${VERSION_MAJOR}" != "null" ] && [ "${VERSION_MINOR}" != "null" ]; then
+    VERSION_MAJ_MIN="${VERSION_MAJOR}.${VERSION_MINOR}"
+  else
+    VERSION_MAJ_MIN="unversioned"
+  fi
+
+  echo "Deploying release ${RELEASE_ID} ${VERSION_MAJ_MIN} to environment ${ENVIRONMENT_ID}"
+  declare TESTNET_NAME="$RELEASE_ID-$(echo "$VERSION_MAJ_MIN" | sed 's/\./-/g')"
+  declare TESTNET_SIZE=3
+
+  echo "Testnet name: ${TESTNET_NAME}"
+  cleanup_instance "${TESTNET_NAME}"
+  echo "Starting testnet '$TESTNET_NAME' with $TESTNET_SIZE nodes and image hoprd:$RELEASE, environment id: $ENVIRONMENT_ID"
+  start_testnet $TESTNET_NAME $TESTNET_SIZE "gcr.io/hoprassociation/hoprd:$RELEASE" $ENVIRONMENT_ID
+done
