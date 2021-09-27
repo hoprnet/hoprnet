32e77bea7004827ed01b7ab3429a1008faecc49e
Robert Kiel <robert.kiel@hoprnet.io>
2021-09-27 13:09:59 +0200

    cherry-pick util-upgrades

--- packages/utils/src/libp2p/index.spec.ts
+++ packages/utils/src/libp2p/index.spec.ts
@@ -18,13 +18,13 @@ import { Multiaddr } from 'multiaddr'
 
 describe(`test convertPubKeyFromPeerId`, function () {
   it(`should equal to a newly created pubkey from PeerId`, async function () {
-    const id = await PeerId.create({ keyType: 'secp256k1', bits: 256 })
-    const pubKey = await convertPubKeyFromPeerId(id)
+    const id = await PeerId.create({ keyType: 'secp256k1' })
+    const pubKey = convertPubKeyFromPeerId(id)
     assert(id.pubKey.toString() === pubKey.toString())
   })
   it(`should equal to pubkey from a PeerId CID`, async function () {
     const testIdB58String = '16Uiu2HAmCPgzWWQWNAn2E3UXx1G3CMzxbPfLr1SFzKqnFjDcbdwg'
-    const pubKey = await convertPubKeyFromB58String(testIdB58String)
+    const pubKey = convertPubKeyFromB58String(testIdB58String)
     const id = PeerId.createFromB58String(testIdB58String)
     assert(id.pubKey.toString() === pubKey.toString())
   })
