304e6b8f585e73b2e0e3c013a3accec137ba4130
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2021-09-27 12:34:15 +0200

    swap get pending tx and get confirmed tx

--- packages/core-ethereum/src/ethereum.ts
+++ packages/core-ethereum/src/ethereum.ts
@@ -62,8 +62,8 @@ export async function createChainWrapper(providerURI: string, privateKey: Uint8A
     {
       getLatestBlockNumber: async () => provider.getBlockNumber(),
       getTransactionCount: (address, blockNumber) => provider.getTransactionCount(address.toHex(), blockNumber),
-      getConfirmedTransactions: (_addr) => Array.from(transactions.confirmed.values()),
-      getPendingTransactions: (_addr) => Array.from(transactions.pending.values())
+      getPendingTransactions: (_addr) => Array.from(transactions.pending.values()),
+      getConfirmedTransactions: (_addr) => Array.from(transactions.confirmed.values())
     },
     durations.minutes(15)
   )
