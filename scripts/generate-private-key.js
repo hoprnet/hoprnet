dbef50fbf412563d56e3a4e709c41af0149aab17
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-10-06 16:02:03 +0200

    better script

--- scripts/generate-private-key.js
+++ scripts/generate-private-key.js
@@ -1,5 +1,8 @@
 var PeerId = require('peer-id')
+var PublicKey = require('../packages/utils').PublicKey
 
 PeerId.create({ keyType: 'secp256k1' }).then((pid) => {
-  console.log(pid.privKey.marshal().toString('hex'))
+  console.log("PEERID: ", pid.toB58String())
+  console.log("ADDRESS: ", PublicKey.fromPeerId(pid).toAddress().toHex())
+  console.log('PRIVATE KEY:', pid.privKey.marshal().toString('hex'))
 })
