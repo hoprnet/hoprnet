f109a0d6009a8fb3060ea6f0a24b91abb974fa28
nionis <stivennoni77@gmail.com>
2021-10-06 15:02:15 +0300

    deploy script fix

--- packages/ethereum/deploy/02_HoprToken.ts
+++ packages/ethereum/deploy/02_HoprToken.ts
@@ -15,16 +15,20 @@ const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   if (network.tags.testing || network.tags.development) {
     const hoprToken = new HoprToken__factory(deployer).attach(result.address)
     const MINTER_ROLE = await hoprToken.MINTER_ROLE()
+    // on all transaction we wait for 10 blocks, this is because
+    // hardhat doesn't track their nonces, causing failing deployments
     if (!(await hoprToken.hasRole(MINTER_ROLE, deployer.address))) {
       console.log('Granting MINTER role to', deployer.address)
       await (await hoprToken.grantRole(MINTER_ROLE, deployer.address)).wait(10)
       console.log('Minting tokens to', deployer.address)
-      await hoprToken.mint(
-        '0x2402da10A6172ED018AEEa22CA60EDe1F766655C',
-        utils.parseEther('130000000'),
-        constants.HashZero,
-        constants.HashZero
-      )
+      await (
+        await hoprToken.mint(
+          '0x2402da10A6172ED018AEEa22CA60EDe1F766655C',
+          utils.parseEther('130000000'),
+          constants.HashZero,
+          constants.HashZero
+        )
+      ).wait(10)
     }
   }
 }
