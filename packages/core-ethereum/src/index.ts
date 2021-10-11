06ece27800bc157aca5dc70958edd9fa0c6d43bd
HOPR CI robot <noreply@hoprnet.org>
2021-10-11 11:10:46 +0000

    chore(build): apply linter changes

--- packages/core-ethereum/src/index.ts
+++ packages/core-ethereum/src/index.ts
@@ -165,8 +165,7 @@ export default class HoprEthereum extends EventEmitter {
   }
 
   public async commitToChannel(c: ChannelEntry): Promise<void> {
-    const setCommitment = async (commitment: Hash) =>
-      this.chain.setCommitment(c.source.toAddress(), commitment)
+    const setCommitment = async (commitment: Hash) => this.chain.setCommitment(c.source.toAddress(), commitment)
     const getCommitment = async () => (await this.indexer.getChannel(c.getId())).commitment
     initializeCommitment(this.db, c.getId(), getCommitment, setCommitment)
   }
