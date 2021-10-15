ba54085f1e6353f64ac93fee27f5d4bd08099523
Robert Kiel <robert.kiel@hoprnet.io>
2021-10-15 14:37:20 +0200

    properly export types

--- packages/ethereum/index.ts
+++ packages/ethereum/index.ts
@@ -3,7 +3,7 @@ import { join } from 'path'
 
 export * from './constants'
 export * from './types'
-export * from './types/common'
+export type { TypedEventFilter, TypedEvent } from './types/common'
 
 export type ContractNames = 'HoprToken' | 'HoprChannels' | 'HoprDistributor'
 
