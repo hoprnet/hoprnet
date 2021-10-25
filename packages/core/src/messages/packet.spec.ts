d52404475cc79bae4df30059214611d51552187e
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-10-25 10:30:22 +0200

    simpler

--- packages/core/src/messages/packet.spec.ts
+++ packages/core/src/messages/packet.spec.ts
@@ -1,48 +1,10 @@
 import { Packet, INTERMEDIATE_HOPS } from './packet'
-import {
-  HoprDB,
-  UINT256,
-  u8aEquals
-} from '@hoprnet/hopr-utils'
+import { HoprDB, UINT256, u8aEquals } from '@hoprnet/hopr-utils'
 import PeerId from 'peer-id'
 import assert from 'assert'
 import BN from 'bn.js'
 
-function createMockTickets(_privKey: Uint8Array) {
-  /*
-  const acknowledge = () => {}
-
-  
-  const getChannel = (_self: PublicKey, counterparty: PublicKey) => ({
-    acknowledge,
-    createTicket: async (pathLength: number, challenge: Challenge) => {
-      return Promise.resolve(
-        Ticket.create(
-          counterparty.toAddress(),
-          challenge,
-          new UINT256(new BN(0)),
-          new UINT256(new BN(0)),
-          new Balance(PRICE_PER_PACKET.muln(pathLength)),
-          UINT256.fromInverseProbability(new BN(1)),
-          new UINT256(new BN(0)),
-          privKey
-        )
-      )
-    },
-    createDummyTicket: (challenge: Challenge) =>
-      Ticket.create(
-        counterparty.toAddress(),
-        challenge,
-        new UINT256(new BN(0)),
-        new UINT256(new BN(0)),
-        new Balance(new BN(0)),
-        UINT256.DUMMY_INVERSE_PROBABILITY,
-        new UINT256(new BN(0)),
-        privKey
-      )
-  })
-  */
-
+function createMockTickets() {
   const db = {
     getChannelTo: () => ({
       getId: () => ({ toHex: () => '0xdeadbeef'}),
@@ -64,26 +26,21 @@ describe('packet creation and transformation', function () {
     const [self, ...path] = await Promise.all(
       Array.from({ length: AMOUNT }).map((_) => PeerId.create({ keyType: 'secp256k1' }))
     )
-
-    const { db } = createMockTickets(self.privKey.marshal())
-
+    const { db } = createMockTickets()
     const testMsg = new TextEncoder().encode('test')
     let packet = await Packet.create(testMsg, path, self, db)
     assert(packet.ackChallenge != null, `ack challenge must be set to track if message was sent`)
 
     for (const [index, node] of path.entries()) {
       packet = Packet.deserialize(packet.serialize(), node, index == 0 ? self : path[index - 1])
-
-      const { db } = createMockTickets(self.privKey.marshal())
+      const { db } = createMockTickets()
       await packet.checkPacketTag(db)
 
       if (packet.isReceiver) {
         assert(index == path.length - 1)
-
         assert(u8aEquals(packet.plaintext, testMsg))
       } else {
         await packet.storeUnacknowledgedTicket(db)
-
         await packet.forwardTransform(node, db)
       }
     }
@@ -94,25 +51,18 @@ describe('packet creation and transformation', function () {
     const [self, ...path] = await Promise.all(
       Array.from({ length: AMOUNT }).map((_) => PeerId.create({ keyType: 'secp256k1' }))
     )
-
-    const { db } = createMockTickets(self.privKey.marshal())
-
+    const { db } = createMockTickets()
     const testMsg = new TextEncoder().encode('test')
-
     let packet = await Packet.create(testMsg, path, self, db)
-
     assert(packet.ackChallenge != null, `ack challenge must be set to track if message was sent`)
 
     for (const [index, node] of path.entries()) {
       packet = Packet.deserialize(packet.serialize(), node, index == 0 ? self : path[index - 1])
-
-      const { db } = createMockTickets(self.privKey.marshal())
-
+      const { db } = createMockTickets()
       await packet.checkPacketTag(db)
 
       if (packet.isReceiver) {
         assert(index == path.length - 1)
-
         assert(u8aEquals(packet.plaintext, testMsg))
       } else {
         await packet.storeUnacknowledgedTicket(db)
@@ -127,27 +77,21 @@ describe('packet creation and transformation', function () {
       Array.from({ length: AMOUNT }).map((_) => PeerId.create({ keyType: 'secp256k1' }))
     )
 
-    const { db }= createMockTickets(self.privKey.marshal())
-
+    const { db }= createMockTickets()
     const testMsg = new TextEncoder().encode('test')
-
     let packet = await Packet.create(testMsg, path, self, db)
-
     assert(packet.ackChallenge != null, `ack challenge must be set to track if message was sent`)
 
     for (const [index, node] of path.entries()) {
       packet = Packet.deserialize(packet.serialize(), node, index == 0 ? self : path[index - 1])
-
-      const { db } = createMockTickets(self.privKey.marshal())
+      const { db } = createMockTickets()
       await packet.checkPacketTag(db)
 
       if (packet.isReceiver) {
         assert(index == path.length - 1)
-
         assert(u8aEquals(packet.plaintext, testMsg))
       } else {
         await packet.storeUnacknowledgedTicket(db)
-
         await packet.forwardTransform(node, db)
       }
     }
@@ -158,17 +102,11 @@ describe('packet creation and transformation', function () {
     const [self, ...path] = await Promise.all(
       Array.from({ length: AMOUNT }).map((_) => PeerId.create({ keyType: 'secp256k1' }))
     )
-
-    const { db } = createMockTickets(self.privKey.marshal())
-
+    const { db } = createMockTickets()
     const testMsg = new TextEncoder().encode('test')
-
     const packet = await Packet.create(testMsg, path, self, db)
-
     const transformedPacket = Packet.deserialize(packet.serialize(), path[0], self)
-
     await transformedPacket.forwardTransform(path[0],  db)
-
     assert.throws(() => Packet.deserialize(transformedPacket.serialize(), path[0], self))
   })
 })
