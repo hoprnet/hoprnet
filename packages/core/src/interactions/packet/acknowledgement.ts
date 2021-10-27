448f99d7a0e33109420629d873e26160b15a5be0
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-10-27 11:16:18 +0200

    Simplifying acknowledgement

--- packages/core/src/interactions/packet/acknowledgement.ts
+++ packages/core/src/interactions/packet/acknowledgement.ts
@@ -1,5 +1,5 @@
 import { durations, oneAtATime, debug, AcknowledgedTicket } from '@hoprnet/hopr-utils'
-import type { HalfKey, UnacknowledgedTicket } from '@hoprnet/hopr-utils'
+import type { UnacknowledgedTicket } from '@hoprnet/hopr-utils'
 import { findCommitmentPreImage, bumpCommitment } from '@hoprnet/hopr-core-ethereum'
 
 import type { SendMessage, Subscribe } from '../../index'
@@ -12,21 +12,17 @@ const error = debug('hopr-core:acknowledgement:error')
 
 const ACKNOWLEDGEMENT_TIMEOUT = durations.seconds(2)
 
-/**
- * Reserve a preImage for the given ticket if it is a winning ticket.
- * @param ticket the acknowledged ticket
- */
 async function acknowledge(
   unacknowledgedTicket: UnacknowledgedTicket,
-  acknowledgement: HalfKey,
+  acknowledgement: Acknowledgement,
   db: HoprDB
 ): Promise<AcknowledgedTicket | null> {
-  if (!unacknowledgedTicket.verifyChallenge(acknowledgement)) {
+  if (!unacknowledgedTicket.verifyChallenge(acknowledgement.ackKeyShare)) {
     throw Error(`The acknowledgement is not sufficient to solve the embedded challenge.`)
   }
 
   const channelId = (await db.getChannelFrom(unacknowledgedTicket.signer)).getId()
-  const response = unacknowledgedTicket.getResponse(acknowledgement)
+  const response = unacknowledgedTicket.getResponse(acknowledgement.ackKeyShare)
 
   const ticket = unacknowledgedTicket.ticket
   const opening = await findCommitmentPreImage(db, channelId)
@@ -38,6 +34,8 @@ async function acknowledge(
 
     try {
       await bumpCommitment(db, channelId)
+      await db.replaceUnAckWithAck(acknowledgement.ackChallenge, ack)
+      log(`Stored winning ticket`)
       return ack
     } catch (e) {
       log(`ERROR: commitment could not be bumped ${e}, thus dropping ticket`)
@@ -50,28 +48,33 @@ async function acknowledge(
   }
 }
 
+/**
+ * Reserve a preImage for the given ticket if it is a winning ticket.
+ */
+async function handleAcknowledgement(
+  msg: Uint8Array,
+  remotePeer: PeerId,
+  pubKey: PeerId,
+  db: HoprDB,
+  onMessage: (ackMessage: Acknowledgement) => void
+) {
+  const acknowledgement = Acknowledgement.deserialize(msg, pubKey, remotePeer)
+  const unacknowledgedTicket = await db.getUnacknowledgedTicket(acknowledgement.ackChallenge)
+  await acknowledge(unacknowledgedTicket, acknowledgement, db)
+  onMessage(acknowledgement)
+}
+
 export function subscribeToAcknowledgements(
   subscribe: Subscribe,
   db: HoprDB,
   pubKey: PeerId,
   onMessage: (ackMessage: Acknowledgement) => void
 ) {
-  async function handleAcknowledgement(msg: Uint8Array, remotePeer: PeerId) {
-    const ackMsg = Acknowledgement.deserialize(msg, pubKey, remotePeer)
-
-    let unacknowledgedTicket = await db.getUnacknowledgedTicket(ackMsg.ackChallenge)
-    const ackedTicket = await acknowledge(unacknowledgedTicket, ackMsg.ackKeyShare, db)
-    if (ackedTicket) {
-      log(`Storing winning ticket`)
-      await db.replaceUnAckWithAck(ackMsg.ackChallenge, ackedTicket)
-    }
-    onMessage(ackMsg)
-  }
 
   const limitConcurrency = oneAtATime()
   subscribe(
     PROTOCOL_ACKNOWLEDGEMENT,
-    (msg: Uint8Array, remotePeer: PeerId) => limitConcurrency(() => handleAcknowledgement(msg, remotePeer)),
+    (msg: Uint8Array, remotePeer: PeerId) => limitConcurrency(() => handleAcknowledgement(msg, remotePeer, pubKey, db, onMessage)),
     false,
     (err: any) => {
       error(`Error while receiving acknowledgement`, err)
