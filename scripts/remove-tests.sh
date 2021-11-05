13337f4e1ff7fa00a10fac6bcd9e1ea9b4bea19d
Robert Kiel <robert.kiel@hoprnet.io>
2021-11-05 16:52:39 +0100

    better script

--- scripts/remove-tests.sh
+++ scripts/remove-tests.sh
@@ -7,13 +7,26 @@ test "$?" -eq "0" && { echo "This script should only be executed." >&2; exit 1; 
 # exit on errors, undefined variables, ensure errors in pipes are not hidden
 set -Eeuo pipefail
 
-declare packagedir
-packagedir=$1
-
 declare mydir
 mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-# Remove built tests and source maps
+declare HOPR_LOG_ID="remove tests"
+source "${mydir}/utils.sh"
+
+usage() {
+  msg
+  msg "Usage: $0 [<pkg_dir>]"
+  msg
+  msg "\t<pkg_dir> package directory, e.g. 'packages/utils'"
+  msg
+}
+
+([ -z ${1:-} ] || [ "${1:-}" = "-h" ] || [ "${1:-}" = "--help" ]) && { usage; exit 0; }
+
+declare packagedir
+packagedir=$1
+
+# Remove built unit tests and source maps from given directory
 rm -Rf ${mydir}/../${packagedir}/lib/**/*.spec.js ${mydir}/../${packagedir}/lib/*.spec.js
 rm -Rf ${mydir}/../${packagedir}/lib/**/*.spec.js.map ${mydir}/../${packagedir}/lib/*.spec.js.map
 rm -Rf ${mydir}/../${packagedir}/lib/**/*.spec.d.ts ${mydir}/../${packagedir}/lib/*.spec.d.ts
