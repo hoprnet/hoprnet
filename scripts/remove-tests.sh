20637486157455f51d0365be05fa200d59a40f2e
Robert Kiel <robert.kiel@hoprnet.io>
2021-11-05 16:38:14 +0100

    add feedback

--- scripts/remove-tests.sh
+++ scripts/remove-tests.sh
@@ -7,13 +7,14 @@ test "$?" -eq "0" && { echo "This script should only be executed." >&2; exit 1; 
 # exit on errors, undefined variables, ensure errors in pipes are not hidden
 set -Eeuo pipefail
 
-declare mydir
-mydir=$(pwd -P)
+declare packagedir
+packagedir=$1
 
-echo ${mydir}
+declare mydir
+mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 # Remove built tests and source maps
-rm -Rf ${mydir}/lib/**/*.spec.js ${mydir}/lib/*.spec.js
-rm -Rf ${mydir}/lib/**/*.spec.js.map ${mydir}/lib/*.spec.js.map
-rm -Rf ${mydir}/lib/**/*.spec.d.ts ${mydir}/lib/*.spec.d.ts
-rm -Rf ${mydir}/lib/**/*.spec.d.ts.map ${mydir}/lib/*.spec.d.ts.map
+rm -Rf ${mydir}/../${packagedir}/lib/**/*.spec.js ${mydir}/../${packagedir}/lib/*.spec.js
+rm -Rf ${mydir}/../${packagedir}/lib/**/*.spec.js.map ${mydir}/../${packagedir}/lib/*.spec.js.map
+rm -Rf ${mydir}/../${packagedir}/lib/**/*.spec.d.ts ${mydir}/../${packagedir}/lib/*.spec.d.ts
+rm -Rf ${mydir}/../${packagedir}/lib/**/*.spec.d.ts.map ${mydir}/../${packagedir}/lib/*.spec.d.ts.map
