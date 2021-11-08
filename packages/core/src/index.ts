33959b0d0f7c752b914b4d9ed7b360d58f492d5d
Tino Breddin <tino@hoprnet.org>
2021-11-08 11:22:50 +0000

    fix goerli SC deployments

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -162,7 +162,6 @@ class Hopr extends EventEmitter {
       options.forceCreateDB
     )
     this.environment = options.environment
-    console.log(options.environment)
 
     const provider = expandVars(this.environment.network.default_provider, process.env)
     log(`using environment: ${this.environment.id}`)
@@ -189,7 +188,6 @@ class Hopr extends EventEmitter {
     }
     this.indexer = this.paymentChannels.indexer // TODO temporary
 
-    log(`using environment: ${this.environment.id}`)
   }
 
   private async startedPaymentChannels(): Promise<HoprCoreEthereum> {
