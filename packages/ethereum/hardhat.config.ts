7438e6d227ae9516dc183c54403a8c6f28e3301c
Tino Breddin <tino@hoprnet.org>
2021-11-09 15:16:40 +0000

    use untyped assignment

--- packages/ethereum/hardhat.config.ts
+++ packages/ethereum/hardhat.config.ts
@@ -86,7 +86,7 @@ const environment = PROTOCOL_CONFIG.environments[HOPR_ENVIRONMENT_ID] || {}
 
 for (const [networkId, network] of Object.entries(PROTOCOL_CONFIG.networks)) {
   // environment could be undefined at this point
-  network.tags = environment.tags || []
+  network["tags"] = environment.tags || []
   const hardhatNetwork = networkToHardhatNetwork(networkId, network)
   networks[networkId] = hardhatNetwork
 }
