b7fb9c8194ad8cf12af825a8dff02e108623c700
Tino Breddin <tino@hoprnet.org>
2021-11-11 15:56:10 +0000

    fix receiver account for minted tokens in CI/CD

--- packages/ethereum/deploy/02_HoprToken.ts
+++ packages/ethereum/deploy/02_HoprToken.ts
@@ -3,8 +3,14 @@ import type { DeployFunction } from 'hardhat-deploy/types'
 import { HoprToken__factory } from '../src/types'
 import { utils, constants } from 'ethers'
 
+const PROTOCOL_CONFIG = require('../../core/protocol-config.json')
+
 const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { ethers, deployments, network, getNamedAccounts } = hre
+  const { ethers, deployments, network, getNamedAccounts, environment } = hre
+
+  const environmentConfig = PROTOCOL_CONFIG.environments[environment]
+  const mintedTokenReceiver = environmentConfig['minted_token_receiver_address']
+
   const deployer = await getNamedAccounts().then((o) => ethers.getSigner(o.deployer))
 
   const result = await deployments.deploy('HoprToken', {
@@ -22,18 +28,27 @@ const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     if (!isDeployerMinter) {
       console.log('Granting MINTER role to', deployer.address)
       const grantRoleTx = hoprToken.grantRole(MINTER_ROLE, deployer.address)
-      if (network.tags.development) await (await grantRoleTx).wait(10)
-      else await grantRoleTx
+      if (network.tags.development) {
+        await (await grantRoleTx).wait(10)
+      } else {
+        await grantRoleTx
+      }
 
-      console.log('Minting tokens to', deployer.address)
-      const mintTx = hoprToken.mint(
-        '0x2402da10A6172ED018AEEa22CA60EDe1F766655C',
-        utils.parseEther('130000000'),
-        constants.HashZero,
-        constants.HashZero
-      )
-      if (network.tags.development) await (await mintTx).wait(10)
-      else await mintTx
+      if (mintedTokenReceiver) {
+        console.log('Minting tokens to', mintedTokenReceiver)
+        const mintTx = hoprToken.mint(
+          mintedTokenReceiver,
+          utils.parseEther('130000000'),
+          constants.HashZero,
+          constants.HashZero
+        )
+
+        if (network.tags.development) {
+          await (await mintTx).wait(10)
+        } else {
+          await mintTx
+        }
+      }
     }
   }
 }
