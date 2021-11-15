5f74a05f5306e4d04548627a60319d8c34debb2d
jjperezaguinaga <me@jjperezaguinaga.com>
2021-11-15 13:50:38 +0000

    test: Mocking classes to avoid breaking on refactor

--- packages/core/src/index.spec.ts
+++ packages/core/src/index.spec.ts
@@ -1,9 +1,9 @@
-import { debug, expandVars, HoprDB, PublicKey } from '@hoprnet/hopr-utils'
+import { debug, HoprDB } from '@hoprnet/hopr-utils'
 import HoprCoreEthereum from '@hoprnet/hopr-core-ethereum'
 import { expect } from 'chai'
 import PeerId from 'peer-id'
 import sinon from 'sinon'
-import Hopr, { HoprOptions, resolveEnvironment, VERSION } from '.'
+import Hopr, { HoprOptions } from '.'
 
 const log = debug('hopr-core:test:index')
 
@@ -11,21 +11,9 @@ describe('hopr core (instance)', async function () {
   let peerId: PeerId, db: HoprDB, chain: HoprCoreEthereum, options: HoprOptions
   beforeEach(async function () {
     peerId = await PeerId.create({ keyType: 'secp256k1', bits: 256 })
-    options = new HoprOptions(resolveEnvironment('master-xdai'))
-    db = new HoprDB(
-      PublicKey.fromPrivKey(peerId.privKey.marshal()),
-      true,
-      VERSION,
-      options.dbPath,
-      options.forceCreateDB
-    )
-    chain = new HoprCoreEthereum(db, PublicKey.fromPeerId(peerId), peerId.privKey.marshal(), {
-      chainId: options.environment.network.chain_id,
-      environment: options.environment.id,
-      gasPrice: options.environment.network.gasPrice,
-      network: options.environment.network.id,
-      provider: expandVars(options.environment.network.default_provider, process.env)
-    })
+    options = { environment: { id: '1' } } as unknown as HoprOptions
+    db = sinon.createStubInstance(HoprDB)
+    chain = sinon.createStubInstance(HoprCoreEthereum)
   })
 
   afterEach(function () {
