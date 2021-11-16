467076c09bf0371b38dffc26948536b75792694f
Andrey Kiryushkin <andrey.kiryushkin@hoprnet.org>
2021-11-16 16:42:23 +0100

    removing ct_node1_dir

--- scripts/run-integration-tests-source.sh
+++ scripts/run-integration-tests-source.sh
@@ -103,7 +103,7 @@ function cleanup {
 
   # Cleaning up everything
   log "Wiping databases"
-  rm -rf "${node1_dir}" "${node2_dir}" "${node3_dir}" "${node4_dir}" "${node5_dir}" "${node6_dir}" "${node7_dir}"
+  rm -rf "${node1_dir}" "${node2_dir}" "${node3_dir}" "${node4_dir}" "${node5_dir}" "${node6_dir}" "${node7_dir}" "${ct_node1_dir}"
 
   log "Cleaning up processes"
   for port in 8545 13301 13302 13303 13304 13305 13306 13307 19091 19092 19093 19094 19095 19096 19097; do
