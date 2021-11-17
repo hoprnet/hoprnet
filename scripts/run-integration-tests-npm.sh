1b6314437fc7b72839f1d529b1d24f5878bb4810
Robert Kiel <robert.kiel@hoprnet.io>
2021-11-17 13:33:14 +0100

    fixes

--- scripts/run-integration-tests-npm.sh
+++ scripts/run-integration-tests-npm.sh
@@ -297,7 +297,7 @@ fi
 
 # --- Running Mock Blockchain --- {{{
 log "Running hardhat local node"
-HOPR_ENVIRONMENT_ID="hardhat-localhost" TS_NODE_PROJECT=${mydir}/../packages/ethereum/tsconfig.hardhat.json yarn workspace @hoprnet/hopr-ethereum run network \
+HOPR_ENVIRONMENT_ID="hardhat-localhost" TS_NODE_PROJECT=${mydir}/../packages/ethereum/tsconfig.hardhat.json yarn workspace @hoprnet/hopr-ethereum exec "hardhat node" \
   --show-stack-traces > \
   "${hardhat_rpc_log}" 2>&1 &
 
@@ -328,7 +328,7 @@ wait_for_regex ${node7_log} "using blockchain address"
 # }}}
 
 #  --- Fund nodes --- {{{
-HOPR_ENVIRONMENT_ID=hardhat-localhost yarn workspace @hoprnet/hopr-ethereum run faucet \
+HOPR_ENVIRONMENT_ID=hardhat-localhost TS_NODE_PROJECT=${mydir}/../packages/ethereum/tsconfig.hardhat.json yarn workspace @hoprnet/hopr-ethereum exec "hardhat faucet" \
   --identity-prefix "${node_prefix}" \
   --identity-directory "${tmp}" \
   --use-local-identities \
