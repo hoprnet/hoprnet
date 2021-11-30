4ff731716356af1472eb264236bd7744a3a04c18
jjperezaguinaga <me@jjperezaguinaga.com>
2021-11-30 15:43:28 +0000

    test: SEnsuring we are not missing use cases during refactor

---
+++ packages/core-ethereum/src/index.spec.ts
@@ -0,0 +1,16 @@
+import { dbMock, debug, privKeyToPeerId, PublicKey } from '@hoprnet/hopr-utils'
+import HoprEthereum from '.'
+
+const namespace = 'hopr:test:hopr-ethereum'
+const log = debug(namespace)
+
+const privateKey = '0xcb1e5d91d46eb54a477a7eefec9c87a1575e3e5384d38f990f19c09aa8ddd332'
+const mockPeerId = privKeyToPeerId(privateKey)
+
+describe(`HoprEthereum`, function () {
+  it('should instantiate a new class w/o any issues', function () {
+    log('starting new instance of HoprEthereum.')
+    new HoprEthereum(dbMock, PublicKey.fromPeerId(mockPeerId), mockPeerId.privKey.marshal())
+    log('successfully created the HoprEthereum instance.')
+  })
+})
