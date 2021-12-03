a5e25481814fcf18ef77c0b364ca4081383e2b14
NumberFour8 <lukas.pohanka@inina.net>
2021-12-03 11:01:05 +0100

    Add custom logger which terminates the process as well

--- packages/hoprd/src/index.ts
+++ packages/hoprd/src/index.ts
@@ -16,6 +16,7 @@ import { LogStream } from './logs'
 import { getIdentity } from './identity'
 
 import type { HoprOptions, ResolvedEnvironment } from '@hoprnet/hopr-core'
+import { setLogger } from 'trace-unhandled'
 
 const DEFAULT_ID_PATH = path.join(process.env.HOME, '.hopr-identity')
 
@@ -195,6 +196,10 @@ async function generateNodeOptions(environment: ResolvedEnvironment): Promise<Ho
 
 function addUnhandledPromiseRejectionHandler() {
     require('trace-unhandled/register')
+    setLogger(msg => {
+        console.error( msg );
+        process.exit(1);
+    })
 }
 
 async function main() {
