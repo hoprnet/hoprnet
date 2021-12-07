febfe4ec8ecf1079c892119352cff7dcf269130b
Robert Kiel <robert.kiel@hoprnet.org>
2021-12-07 22:56:33 +0100

    Cleanups (#3051)
    * cleanups
    
    * remove unnecessary package
    
    * use correct solidity version
    
    * fix build issue
    
    * remove obsolete script
    
    Co-authored-by: Robert Kiel <robert.kiel@hoprnet.io>

--- scripts/run-integration-tests-npm.sh
+++ scripts/run-integration-tests-npm.sh
@@ -304,8 +304,11 @@ fi
 
 # --- Running Mock Blockchain --- {{{
 log "Running hardhat local node"
-HOPR_ENVIRONMENT_ID="hardhat-localhost" yarn workspace @hoprnet/hopr-ethereum hardhat node \
-  --network hardhat --show-stack-traces > \
+HOPR_ENVIRONMENT_ID="hardhat-localhost" \
+TS_NODE_PROJECT=${mydir}/../packages/ethereum/tsconfig.hardhat.json \
+yarn workspace @hoprnet/hopr-ethereum hardhat node \
+  --network hardhat \
+  --show-stack-traces > \
   "${hardhat_rpc_log}" 2>&1 &
 
 wait_for_regex ${hardhat_rpc_log} "Started HTTP and WebSocket JSON-RPC server"
@@ -335,7 +338,9 @@ wait_for_regex ${node7_log} "using blockchain address"
 # }}}
 
 #  --- Fund nodes --- {{{
-HOPR_ENVIRONMENT_ID=hardhat-localhost yarn workspace @hoprnet/hopr-ethereum hardhat faucet \
+HOPR_ENVIRONMENT_ID=hardhat-localhost \
+TS_NODE_PROJECT=${mydir}/../packages/ethereum/tsconfig.hardhat.json \
+yarn workspace @hoprnet/hopr-ethereum hardhat faucet \
   --identity-prefix "${node_prefix}" \
   --identity-directory "${tmp}" \
   --use-local-identities \
