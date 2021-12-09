84c9018d886e3a93afee448860fed02f673e6796
Jose Aguinaga <me@jjperezaguinaga.com>
2021-12-09 08:53:12 -0600

    chore: Importing w/type ensures instantiating mistakes can not be made

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -37,7 +37,8 @@ import type {
   Hash,
   HalfKeyChallenge
 } from '@hoprnet/hopr-utils'
-import HoprCoreEthereum, { ChainStatus } from '@hoprnet/hopr-core-ethereum'
+import HoprCoreEthereum from '@hoprnet/hopr-core-ethereum'
+import type { ChainStatus } from '@hoprnet/hopr-core-ethereum'
 import type { Indexer } from '@hoprnet/hopr-core-ethereum'
 import BN from 'bn.js'
 
@@ -93,7 +94,7 @@ export class HoprOptions {
     // when true, addresses will be sorted local first
     // when false, addresses will be sorted public first
     public preferLocalAddresses?: boolean
-  ) {}
+  ) { }
 }
 
 export type NodeStatus = 'UNINITIALIZED' | 'INITIALIZING' | 'RUNNING' | 'DESTROYED'
@@ -266,7 +267,7 @@ class Hopr extends EventEmitter {
       },
       (ack: AcknowledgedTicket) => this.connector.emit('ticket:win', ack),
       // TODO: automatically reinitialize commitments
-      () => {},
+      () => { },
       protocolAck
     )
 
