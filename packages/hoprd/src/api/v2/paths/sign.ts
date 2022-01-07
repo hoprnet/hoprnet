e63c263585021fe2db9b7e03b38f2e1006026ee9
Jose Aguinaga <me@jjperezaguinaga.com>
2022-01-07 06:59:11 -0600

    feat(api-v2): Fixes #3236 - API endpoint for signing (see hoprnet/myne-chat#5)

---
+++ packages/hoprd/src/api/v2/paths/sign.ts
@@ -0,0 +1,46 @@
+import { Operation } from 'express-openapi'
+import { u8aToHex } from '@hoprnet/hopr-utils'
+import { encodeMessage } from '../../../commands/utils'
+
+export const parameters = []
+
+export const POST: Operation = [
+    async (req, res, _next) => {
+        const message = encodeMessage(req.body.body)
+
+        try {
+            const signedMessage = await req.context.node.signMessage(message)
+            res.status(200).send({ signedMessage: u8aToHex(signedMessage) })
+        } catch (err) {
+            res.status(422).json({ error: err.message })
+        }
+    }
+]
+
+POST.apiDoc = {
+    description: 'Signs a message given using the node’s private key. Prefixes messsage with “HOPR Signed Message: ” before signing',
+    tags: ['sign'],
+    operationId: 'signMessage',
+    parameters: [],
+    requestBody: {
+        content: {
+            'application/json': {
+                schema: {
+                    type: 'object',
+                    properties: {
+                        body: {
+                            description: 'The message body which should be sent.',
+                            type: 'string'
+                        },
+                    },
+                    required: ['body']
+                }
+            }
+        }
+    },
+    responses: {
+        '200': {
+            description: 'The message was signed successfully.'
+        }
+    }
+}
