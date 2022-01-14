48bcc43b6f4cd1dde86336e4a085ff1f92f6b1c5
Lukas P <lukas.pohanka@inina.net>
2022-01-14 14:07:57 +0100

    Update scripts/vm-nat-testing/faucet.sh
    Co-authored-by: Robert Kiel <robert.kiel@hoprnet.org>

--- scripts/vm-nat-testing/faucet.sh
+++ scripts/vm-nat-testing/faucet.sh
@@ -3,5 +3,5 @@
 cd /opt/hopr || exit 1
 
 HOPR_ENVIRONMENT_ID=hardhat-localhost yarn workspace @hoprnet/hopr-ethereum \
-	hardhat faucet --use-local-identities --identity-directory /var/hopr/identities/ --identity-prefix local \
+	hardhat faucet --use-local-identities --identity-directory /var/hopr/identities/ --identity-prefix "local" \
 	--password switzerland --network hardhat
