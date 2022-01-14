385fb0fe7e7910e5ffd1a97218cdf1f54b8c3ded
Lukas P <lukas.pohanka@inina.net>
2022-01-14 14:32:36 +0100

    Add scripts to spin Docker compose setup for a node behind NAT (#3212)
    * Add script to sping Docker compose setup for a node behind NAT
    
    * Added usage to NAT node script
    
    * Fix error function
    
    * Fix getopts formatting argument
    
    * Update script with feedback
    
    * Fix bugs in run script
    
    * Updated README.md with instructions to start the NAT'd node
    
    * chore(build): apply linter changes
    
    * Add Vagrant setup for NAT tests
    
    * Update file permissions
    
    * Fixed redirection
    
    * chore(build): apply linter changes
    
    * Fix node startup script
    
    * Refactor startup scripts
    
    * Improvements and fixes to scripts
    
    * Add --testWeakCrypto flag for testing
    
    * chore(build): apply linter changes
    
    * Add faucet.sh script for funding on HardHat testnet
    
    * Update announce address sorting
    
    * Separated hardhat node, public node also uses docker-compose
    
    * chore(build): apply linter changes
    
    * Improved provisioning, vagrant setup and docker subnets
    
    * Added README.md for the Vagrant setup
    
    * chore(build): apply linter changes
    
    * Typo
    
    * Update announce address sorting
    
    * chore(build): apply linter changes
    
    * Add admin panel reachability info
    
    * Add token info
    
    * Update wording
    
    * Use regex match for class B
    
    * Fix unnecessary throw
    
    * Update in README
    
    * chore(build): apply linter changes
    
    * Move run-nat-node script to a different branch
    
    * Remove README.md section about run-nat-node.sh
    
    * Change node base image to slim
    
    * minor vagrant configuration improvements for nat testing (#3254)
    
    * Add arm64 support to the Vagrant NAT testing setup (#3258)
    
    This is needed to run the setup scripts on macOS M1 machines.
    
    * Update scripts/vm-nat-testing/faucet.sh
    
    Co-authored-by: Robert Kiel <robert.kiel@hoprnet.org>
    
    Co-authored-by: HOPR CI robot <noreply@hoprnet.org>
    Co-authored-by: Tino Breddin <tolbrino@users.noreply.github.com>
    Co-authored-by: Robert Kiel <robert.kiel@hoprnet.org>

---
+++ scripts/vm-nat-testing/startup-network.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+cd /opt/hopr/ || exit 1
+
+declare log_file="/tmp/hardhat.logs"
+
+if [ "$(curl -s -o /dev/null -w ''%{http_code}'' $provider_ip:8545)" != "200" ]; then
+	# make sure other instances are killed
+	sudo pkill node || :
+	# Start the HardHat network on localhost
+	echo "Starting HardHat network..."
+	HOPR_ENVIRONMENT_ID=hardhat-localhost yarn run:network > ${log_file} 2>&1 &
+fi
+
+while [[
+	"$(curl -s -o /dev/null -w ''%{http_code}'' 127.0.0.1:8545)" != "200" ||
+	! -f "${log_file}" ||
+  -z "$(grep "Started HTTP and WebSocket JSON-RPC server" "${log_file}" || echo "")"
+	]] ; do
+	echo "Waiting for hardhat network to come up..."
+	sleep 5;
+done
+
+echo "HardHat provider started up!"
+cp -R packages/ethereum/deployments/hardhat-localhost/localhost/* packages/ethereum/deployments/hardhat-localhost/hardhat
+
+if [ "$(curl -s -o /dev/null -w ''%{http_code}'' $provider_ip:8546)" != "200" ]; then
+	# Mirror localhost:8545 -> outboud 8546
+	socat TCP-LISTEN:8546,fork TCP:127.0.0.1:8545 &
+fi
+
+echo "Done."
