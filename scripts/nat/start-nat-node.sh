4ef84dd4b0632b38eb4e3e271206157e4446dafd
NumberFour8 <lukas.pohanka@inina.net>
2022-01-24 12:12:55 +0100

    Improve start-nat-node.sh - make sure container is stopped

--- scripts/nat/start-nat-node.sh
+++ scripts/nat/start-nat-node.sh
@@ -25,7 +25,9 @@ fi
 declare admin_port=${HOPRD_ADMIN_PORT:-3000}
 declare rest_port=${HOPRD_REST_PORT:-3001}
 declare healthcheck_port=${HOPRD_HEALTHCHECK_PORT:-8080}
+
 declare network_name="hopr-nat"
+declare container_name="hoprd-behind-nat"
 
 # Create an isolated network to force NAT
 if [ "$(docker network ls | grep -c "${network_name}" )" = "0" ] ; then
@@ -35,8 +37,12 @@ if [ "$(docker network ls | grep -c "${network_name}" )" = "0" ] ; then
   fi
 fi
 
+# Stop the Docker container if it was running already
+docker stop ${container_name} 2> /dev/null || true
+
 # Fork here and pass all the environment variables down into the forked image
 docker run --pull always -v /var/hoprd/:/app/db -p ${admin_port}:3000 -p ${rest_port}:3001 -p ${healthcheck_port}:8080 \
+ --name=${container_name} --rm \
  --network=${network_name} \
  --env-file <(env) \
  "gcr.io/hoprassociation/hoprd:$HOPRD_RELEASE" "$@"
