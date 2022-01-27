98175ea9668c2e2b09225a9bd6be75852be02a83
HOPR CI robot <noreply@hoprnet.org>
2022-01-27 18:11:48 +0000

    chore(build): apply linter changes

--- packages/connect/src/types.ts
+++ packages/connect/src/types.ts
@@ -59,7 +59,7 @@ export type StreamResult = IteratorResult<StreamType>
 
 export type HoprConnectOptions = {
   publicNodes?: PublicNodesEmitter
-  allowLocalConnections?: boolean,
+  allowLocalConnections?: boolean
   initialNodes?: PeerStoreType[]
   interface?: string
   maxRelayedConnections?: number
