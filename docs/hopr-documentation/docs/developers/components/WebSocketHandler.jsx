7b2b2d513dce2417988442ea118c4098ced42e88
HOPR CI robot <noreply@hoprnet.org>
2022-02-07 17:54:09 +0000

    chore(build): apply linter changes

--- docs/hopr-documentation/docs/developers/components/WebSocketHandler.jsx
+++ docs/hopr-documentation/docs/developers/components/WebSocketHandler.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from "react";
-import useWebsocket from "../hooks/useWebSockets";
+import React, { useEffect, useState } from 'react'
+import useWebsocket from '../hooks/useWebSockets'
 
 export const WebSocketHandler = ({ wsEndpoint, securityToken }) => {
-  const [message, setMessage] = useState("");
-  const websocket = useWebsocket({ wsEndpoint, securityToken });
-  const { socketRef } = websocket;
+  const [message, setMessage] = useState('')
+  const websocket = useWebsocket({ wsEndpoint, securityToken })
+  const { socketRef } = websocket
   const handleReceivedMessage = async (ev) => {
     try {
       // we are only interested in messages, not all the other events coming in on the socket
-      const data = JSON.parse(ev.data);
-      console.log("WebSocket Data", data);
-      if (data.type === "message") {
-        setMessage(data.msg);
+      const data = JSON.parse(ev.data)
+      console.log('WebSocket Data', data)
+      if (data.type === 'message') {
+        setMessage(data.msg)
       }
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
   useEffect(() => {
-    if (!socketRef.current) return;
-    socketRef.current.addEventListener("message", handleReceivedMessage);
+    if (!socketRef.current) return
+    socketRef.current.addEventListener('message', handleReceivedMessage)
 
     return () => {
-      if (!socketRef.current) return;
-      socketRef.current.removeEventListener("message", handleReceivedMessage);
-    };
-  }, [socketRef.current]);
+      if (!socketRef.current) return
+      socketRef.current.removeEventListener('message', handleReceivedMessage)
+    }
+  }, [socketRef.current])
 
-  return <span>{message ? message : "You have no messages."}</span>;
-};
+  return <span>{message ? message : 'You have no messages.'}</span>
+}
 
-export default WebSocketHandler;
+export default WebSocketHandler
