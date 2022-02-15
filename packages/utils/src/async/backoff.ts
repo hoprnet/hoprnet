fa07c9b03b37be0996e3a0bd3dacb7e2a6f3dc34
Robert Kiel <robert.kiel@hoprnet.io>
2022-02-15 10:20:42 +0100

    simplify retryWithBackoff

--- packages/utils/src/async/backoff.ts
+++ packages/utils/src/async/backoff.ts
@@ -1,6 +1,6 @@
 import { durations } from '../time'
 import { debug } from '../process'
-import { setTimeout } from 'timers/promises'
+import { setTimeout, setImmediate } from 'timers/promises'
 
 const log = debug('hopr:utils:retry')
 
@@ -15,7 +15,7 @@ export async function wait(milliseconds: number): Promise<void> {
  * @param options.maxDelay maximum delay, we reject once we reach this
  * @param options.delayMultiple multiplier to apply to increase running delay
  */
-export function retryWithBackoff<T>(
+export async function retryWithBackoff<T>(
   fn: () => Promise<T>,
   options: {
     minDelay?: number
@@ -24,33 +24,30 @@ export function retryWithBackoff<T>(
   } = {}
 ): Promise<T> {
   const { minDelay = durations.seconds(1), maxDelay = durations.minutes(10), delayMultiple = 2 } = options
+  let delay = minDelay
 
   if (minDelay >= maxDelay) {
-    return Promise.reject(Error('minDelay should be smaller than maxDelay'))
+    throw Error('minDelay should be smaller than maxDelay')
   } else if (delayMultiple <= 1) {
-    return Promise.reject(Error('delayMultiple should be larger than 1'))
+    throw Error('delayMultiple should be larger than 1')
   }
 
-  return new Promise<T>((resolve, reject) => {
-    // Use call-by-reference
-    const state = {
-      delay: minDelay
+  while (true) {
+    try {
+      return await fn()
+    } catch (err) {
+      if (delay >= maxDelay) {
+        throw err
+      }
+      log(`failed, attempting again in ${delay} (${err})`)
     }
 
-    const fetch = () => {
-      fn().then(resolve, (err: any) => {
-        if (state.delay >= maxDelay) {
-          reject(err)
-        }
-        log(`failed, attempting again in ${state.delay} (${err})`)
+    await setTimeout(delay)
 
-        setImmediate(() => {
-          wait(state.delay).then(fetch)
-          state.delay = state.delay * delayMultiple
-        })
-      })
-    }
+    // Give other tasks CPU time to happen
+    // Push next loop iteration to end of next event loop iteration
+    await setImmediate()
 
-    fetch()
-  })
+    delay = delay * delayMultiple
+  }
 }
