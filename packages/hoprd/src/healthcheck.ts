add8b2ea3621224143e0ad4df8ea762468253158
Mr. H <anon.hopr@gmail.com>
2022-02-20 21:54:45 +0000

    cors

--- packages/hoprd/src/healthcheck.ts
+++ packages/hoprd/src/healthcheck.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import http from 'http'
-
+import cors from 'cors'
 import type Hopr from '@hoprnet/hopr-core'
 
 import type { LogStream } from './logs'
 
 export default function setupHealthcheck(node: Hopr, logs: LogStream, host: string, port: number) {
   const service = express()
-
+  service.use(cors());
   service.get('/healthcheck/v1/version', (_, res) => res.send(node.getVersion()))
   service.get('/healthcheck/v1/environment', (_, res) => res.send(node.environment.id))
 
