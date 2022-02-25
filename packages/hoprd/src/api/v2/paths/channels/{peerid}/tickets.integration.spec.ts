829da2897730f42f5690b18a36b29f8959130502
Mariusz Fudali <szczebel1995@gmail.com>
2022-02-25 15:57:46 +0100

    cleaned up

--- packages/hoprd/src/api/v2/paths/channels/{peerid}/tickets.integration.spec.ts
+++ packages/hoprd/src/api/v2/paths/channels/{peerid}/tickets.integration.spec.ts
@@ -4,22 +4,6 @@ import chaiResponseValidator from 'chai-openapi-response-validator'
 import chai, { expect } from 'chai'
 import { createTestApiInstance, invalidTestPeerId, testPeerId, testTicket } from '../../../fixtures'
 import { STATUS_CODES } from '../../../utils'
-// import { rewiremock } from '../../../rewiremock'
-import rewiremock from 'rewiremock'
-rewiremock.overrideEntryPoint(module)
-// import * as formatTicketFile from '../../tickets'
-// rewiremock.around(
-//   () => import('./tickets'),
-//   (mock) => {
-//     mock(() => import('../../tickets')).with({ formatTicket: () => 'ticket' as any })
-//   }
-// )
-// rewiremock.proxy('./tickets', (r) => ({
-//   '../../tickets': r.with({ formatTicket: () => 'ticket' as any })
-// }))
-
-// rewiremock('./tickets').with({ formatTicket: () => 'ticket' as any })
-// rewiremock.enable()
 
 let node = sinon.fake() as any
 node.getTickets = sinon.fake.returns([testTicket])
@@ -29,13 +13,8 @@ chai.use(chaiResponseValidator(api.apiDoc))
 
 describe('GET /channels/{peerId}/tickets', () => {
   it('should get tickets successfully', async () => {
-    // const stub = sinon.stub(formatTicketFile, 'formatTicket')
-    // stub.returns('ticket' as any)
     const res = await request(service).get(`/api/v2/channels/${testPeerId}/tickets`)
-    // expect(res.status).to.equal(204)
     expect(res).to.satisfyApiSpec
-    // expect(res.body).to.be.empty
-    // expect(res.body).to.deep.equal({ muje: 'huje' })
   })
 
   it('should fail when no tickets to get', async () => {
