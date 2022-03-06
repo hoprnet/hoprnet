6bacc9e969c95eea680b3c74f3c713ad48d085ac
nioni <stivennoni77@gmail.com>
2022-03-06 14:04:34 +0200

    WIP

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -489,7 +489,7 @@ class Hopr extends EventEmitter {
     for (const destination of closeChannelDestinations) {
       verbose(`closing ${destination}`)
       try {
-        await this.closeChannel(destination.toPeerId())
+        await this.closeChannel(destination.toPeerId(), 'outgoing')
         verbose(`closed channel to ${destination.toString()}`)
         this.emit('hopr:channel:closed', destination.toPeerId())
       } catch (e) {
@@ -941,9 +941,15 @@ class Hopr extends EventEmitter {
     }
   }
 
-  public async closeChannel(counterparty: PeerId): Promise<{ receipt: string; status: ChannelStatus }> {
+  public async closeChannel(
+    counterparty: PeerId,
+    direction: 'incoming' | 'outgoing'
+  ): Promise<{ receipt: string; status: ChannelStatus }> {
     const counterpartyPubKey = PublicKey.fromPeerId(counterparty)
-    const channel = await this.db.getChannelX(this.pubKey, counterpartyPubKey)
+    const channel =
+      direction === 'outgoing'
+        ? await this.db.getChannelX(this.pubKey, counterpartyPubKey)
+        : await this.db.getChannelX(counterpartyPubKey, this.pubKey)
 
     // TODO: should we wait for confirmation?
     if (channel.status === ChannelStatus.Closed) {
@@ -1030,6 +1036,16 @@ class Hopr extends EventEmitter {
     const counterpartyPubKey = new PublicKey(peerId.pubKey.marshal())
     const channel = await this.db.getChannelX(selfPubKey, counterpartyPubKey)
     await this.connector.redeemTicketsInChannel(channel)
+  }
+
+  /**
+   * Get the channel entry between source and destination node.
+   * @param src PeerId
+   * @param dest PeerId
+   * @returns the channel entry of those two nodes
+   */
+  public async getChannel(src: PeerId, dest: PeerId): Promise<ChannelEntry> {
+    return await this.db.getChannelX(new PublicKey(src.pubKey.marshal()), new PublicKey(dest.pubKey.marshal()))
   }
 
   public async getChannelsFrom(addr: Address): Promise<ChannelEntry[]> {
--- packages/hoprd/src/api/v2/paths/channels/peerid.spec.ts
+++ packages/hoprd/src/api/v2/paths/channels/peerid.spec.ts
@@ -1,17 +1,66 @@
 import assert from 'assert'
 import sinon from 'sinon'
+import { ChannelEntry } from '@hoprnet/hopr-utils'
 import { STATUS_CODES } from '../../utils'
-import { invalidTestPeerId, testPeerId } from '../../fixtures'
-import { closeChannel } from './{peerid}'
+import { invalidTestPeerId, testPeerId, testPeerIdInstance, ALICE_PEER_ID as COUNTERPARTY } from '../../fixtures'
+import { getChannel, closeChannel } from './{peerid}'
+import { formatIncomingChannel, formatOutgoingChannel } from '.'
 
 let node = sinon.fake() as any
+
+describe('getChannel', function () {
+  const outgoingMock = ChannelEntry.createMock()
+  const incomingMock = ChannelEntry.createMock()
+  node.getId = sinon.fake.returns(testPeerIdInstance)
+
+  it('should return no channels', async function () {
+    assert.rejects(
+      () => {
+        return getChannel(node, COUNTERPARTY.toB58String(), 'incoming')
+      },
+      (err: Error) => {
+        return err.message.includes(STATUS_CODES.CHANNEL_NOT_FOUND)
+      }
+    )
+    assert.rejects(
+      () => {
+        return getChannel(node, COUNTERPARTY.toB58String(), 'outgoing')
+      },
+      (err: Error) => {
+        return err.message.includes(STATUS_CODES.CHANNEL_NOT_FOUND)
+      }
+    )
+  })
+
+  it('should return outgoing channel', async function () {
+    node.getChannel = sinon.stub()
+    node.getChannel.withArgs(testPeerIdInstance, COUNTERPARTY).resolves(outgoingMock)
+
+    const outgoing = await getChannel(node, COUNTERPARTY.toB58String(), 'outgoing')
+    assert.notEqual(outgoing, undefined)
+    assert.deepEqual(outgoing, formatOutgoingChannel(outgoingMock))
+  })
+
+  it('should return outgoing and incoming channels', async function () {
+    node.getChannel = sinon.stub()
+    node.getChannel.withArgs(testPeerIdInstance, COUNTERPARTY).resolves(outgoingMock)
+    node.getChannel.withArgs(COUNTERPARTY, testPeerIdInstance).resolves(incomingMock)
+
+    const outgoing = await getChannel(node, COUNTERPARTY.toB58String(), 'outgoing')
+    assert.notEqual(outgoing, undefined)
+    assert.deepEqual(outgoing, formatOutgoingChannel(outgoingMock))
+    const incoming = await getChannel(node, COUNTERPARTY.toB58String(), 'incoming')
+    assert.notEqual(incoming, undefined)
+    assert.deepEqual(incoming, formatIncomingChannel(incomingMock))
+  })
+})
 
 describe('closeChannel', () => {
   it('should close channel', async () => {
     const expectedStatus = { channelStatus: 2, receipt: 'receipt' }
     node.closeChannel = sinon.fake.returns({ status: expectedStatus.channelStatus, receipt: expectedStatus.receipt })
 
-    const closureStatus = await closeChannel(node, testPeerId)
+    const closureStatus = await closeChannel(node, testPeerId, 'outgoing')
     assert.deepEqual(closureStatus, expectedStatus)
   })
 
@@ -21,7 +70,7 @@ describe('closeChannel', () => {
 
     assert.rejects(
       () => {
-        return closeChannel(node, invalidTestPeerId)
+        return closeChannel(node, invalidTestPeerId, 'outgoing')
       },
       (err: Error) => {
         return err.message.includes(STATUS_CODES.INVALID_PEERID)
@@ -34,7 +83,7 @@ describe('closeChannel', () => {
 
     assert.rejects(
       () => {
-        return closeChannel(node, testPeerId)
+        return closeChannel(node, testPeerId, 'outgoing')
       },
       // we only care if it throws
       (_err: Error) => {
