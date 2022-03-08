a721c32ca86ce13ca2932f83e759b3c6e49ca39e
Tino Breddin <t@tolbrino.com>
2022-03-08 16:19:35 +0100

    remove obsolete type casts

--- packages/connect/src/relay/index.spec.ts
+++ packages/connect/src/relay/index.spec.ts
@@ -66,7 +66,7 @@ function getPeer(peerId: PeerId, network: EventEmitter) {
       handle,
       upgrader: {
         upgradeInbound: (async (conn: RelayConnection) => {
-          const shaker = handshake<StreamType>(conn as any)
+          const shaker = handshake(conn)
 
           const message = new TextDecoder().decode((await shaker.read()).slice())
 
@@ -119,7 +119,7 @@ describe('test relay', function () {
       const conn = await Alice.connect(Bob.libp2p.peerId, Charly.libp2p.peerId)
 
       assert(conn != undefined, `Should be able to connect`)
-      const shaker = handshake<StreamType>(conn as any)
+      const shaker = handshake(conn)
 
       const msg = '<Hello>, that should be sent and echoed through relayed connection'
       shaker.write(new TextEncoder().encode(msg))
@@ -152,7 +152,7 @@ describe('test relay', function () {
       const conn = await Alice.connect(Bob.libp2p.peerId, Charly.libp2p.peerId)
 
       assert(conn != undefined, `Should be able to connect`)
-      const shaker = handshake<StreamType>(conn as any)
+      const shaker = handshake(conn)
 
       const msg = '<Hello>, that should be sent and echoed through relayed connection'
       shaker.write(new TextEncoder().encode(msg))
