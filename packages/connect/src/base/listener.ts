00d1c42f3785f11fc07d49d6d9384541a1af6cea
Robert Kiel <robert.kiel@hoprnet.org>
2022-04-05 19:46:20 +0200

    integrate address sorting

--- packages/connect/src/base/listener.ts
+++ packages/connect/src/base/listener.ts
@@ -27,7 +27,13 @@ import { getAddrs } from './addrs'
 import { isAnyAddress, u8aEquals, defer } from '@hoprnet/hopr-utils'
 import { TCPConnection } from './tcp'
 import { EntryNodes, RELAY_CHANGED_EVENT } from './entry'
-import { bindToPort, attemptClose, nodeToMultiaddr } from '../utils'
+import {
+  bindToPort,
+  attemptClose,
+  nodeToMultiaddr,
+  compareAddressesLocalMode,
+  compareAddressesPublicMode
+} from '../utils'
 import type HoprConnect from '..'
 import { UpnpManager } from './upnp'
 import type { Filter } from '../filter'
@@ -348,9 +354,9 @@ class Listener extends EventEmitter implements InterfaceListener {
       ]
     }
 
-    this.addrs.interface = internalInterfaces.map((internalInterface) =>
-      nodeToMultiaddr(internalInterface, this.peerId)
-    )
+    this.addrs.interface = internalInterfaces
+      .map((internalInterface) => nodeToMultiaddr(internalInterface, this.peerId))
+      .sort(this.testingOptions.__preferLocalAddresses ? compareAddressesLocalMode : compareAddressesPublicMode)
 
     this.attachSocketHandlers()
 
