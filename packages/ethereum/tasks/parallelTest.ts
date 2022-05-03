21232c4c2acd9f64977a7a173df5adca210e6912
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2022-05-03 17:41:36 +0200

    improve tests and setup hardhat for parallel testing

---
+++ packages/ethereum/tasks/parallelTest.ts
@@ -0,0 +1,45 @@
+import type { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { TASK_TEST, TASK_TEST_GET_TEST_FILES } from 'hardhat/builtin-tasks/task-names'
+import { resolve } from 'path'
+
+
+export type ParallelTestCLIOpts = {
+    config: Array<{
+        date: string
+        testFiles: string[]
+    }>
+}
+
+/**
+ * Create config files for tests in "parallel" ganache instances
+ */
+async function main(
+  opts: ParallelTestCLIOpts,
+  { run, config }: HardhatRuntimeEnvironment
+): Promise<void> {
+    const parallelTestConfigs = []; // an array of hardhat config files. parallelTestConfigs[i] is for groupedTestFiles[i]
+    const groupedTestFiles = [];    // an array of arrays of relative paths of test files. Test files of one array shares the same config file.
+    // Test files without specifying their initialDate are saved in this variable. They use the default initialDate (now). This variable is populated with all the test files under the default test folder
+    let remainingTestFiles: string[] = await run(TASK_TEST_GET_TEST_FILES);
+
+    // put test files into groups with specified ganach starting dates
+    if (opts.config.length > 0) {
+        // put test files into groups
+        opts.config.forEach(parallelGroup => {
+            const testFiles = parallelGroup.testFiles.map(testFile => resolve(config.paths.tests, testFile))
+            remainingTestFiles = remainingTestFiles.filter(remainingTestFile => !testFiles.includes(remainingTestFile))
+            const newConfig = {...config}
+            newConfig.networks.hardhat.initialDate = parallelGroup.date;
+            parallelTestConfigs.push(newConfig)
+            groupedTestFiles.push(testFiles)
+        })
+    }
+    // leave the remaining test files with the default hardhat config file.
+    parallelTestConfigs.push({...config})
+    groupedTestFiles.push([...remainingTestFiles])
+
+    // run tests with their config files
+    await run(TASK_TEST, {testFiles: groupedTestFiles[0]})
+}
+
+export default main
---
+++ packages/ethereum/test/utils/index.ts
@@ -0,0 +1,36 @@
+import { ethers } from 'ethers'
+
+export * from './time'
+export * from './deploy'
+export * from './interface'
+
+export const getAccount = (privateKey: string) => {
+  const wallet = new ethers.Wallet(privateKey)
+  const uncompressedPublicKey = ethers.utils.computePublicKey(wallet.publicKey, false)
+
+  return {
+    wallet,
+    privateKey: wallet.privateKey,
+    uncompressedPublicKey: ethers.utils.hexDataSlice(uncompressedPublicKey, 1), // remove identifier
+    publicKey: ethers.utils.hexDataSlice(wallet.publicKey, 1), // remove identifier
+    address: wallet.address
+  }
+}
+
+/**
+ * Sign message using private key provided
+ * @param message
+ * @param privKey
+ * @returns signature properties
+ */
+export const signMessage = async (message: string, privKey: string) => {
+  const wallet = new ethers.Wallet(privKey)
+  return {
+    wallet,
+    signature: await wallet.signMessage(ethers.utils.arrayify(message))
+  }
+}
+
+export const toSolPercent = (multiplier: number, percent: number): string => {
+  return String(Math.floor(percent * multiplier))
+}
