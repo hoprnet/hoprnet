02c2aa14c05c250d9319f2b2d0be38659dd8ec36
HOPR CI robot <noreply@hoprnet.org>
2022-05-04 12:54:10 +0000

    chore(build): apply linter changes

--- packages/ethereum/test/utils/accounts.ts
+++ packages/ethereum/test/utils/accounts.ts
@@ -1,18 +1,18 @@
 import { ethers } from 'ethers'
 
 export const getAccount = (privateKey: string) => {
-    const wallet = new ethers.Wallet(privateKey)
-    const uncompressedPublicKey = ethers.utils.computePublicKey(wallet.publicKey, false)
-  
-    return {
-      wallet,
-      privateKey: wallet.privateKey,
-      uncompressedPublicKey: ethers.utils.hexDataSlice(uncompressedPublicKey, 1), // remove identifier
-      publicKey: ethers.utils.hexDataSlice(wallet.publicKey, 1), // remove identifier
-      address: wallet.address
-    }
+  const wallet = new ethers.Wallet(privateKey)
+  const uncompressedPublicKey = ethers.utils.computePublicKey(wallet.publicKey, false)
+
+  return {
+    wallet,
+    privateKey: wallet.privateKey,
+    uncompressedPublicKey: ethers.utils.hexDataSlice(uncompressedPublicKey, 1), // remove identifier
+    publicKey: ethers.utils.hexDataSlice(wallet.publicKey, 1), // remove identifier
+    address: wallet.address
   }
-  
+}
+
 /**
  * Sign message using private key provided
  * @param message
@@ -20,11 +20,11 @@ export const getAccount = (privateKey: string) => {
  * @returns signature properties
  */
 export const signMessage = async (message: string, privKey: string) => {
-    const wallet = new ethers.Wallet(privKey)
-    return {
-        wallet,
-        signature: await wallet.signMessage(ethers.utils.arrayify(message))
-    }
+  const wallet = new ethers.Wallet(privKey)
+  return {
+    wallet,
+    signature: await wallet.signMessage(ethers.utils.arrayify(message))
+  }
 }
 
 export const ACCOUNT_DEPLOYER = getAccount('0xf54bd518dd7e3e42710e9a96c92b1b244727df5a5afae34611089bee344d6bd4')
