3021a1d8f4ff70b5297473e5c33d1483a8ee92a2
Steve <stivennoni77@gmail.com>
2022-05-17 10:49:09 +0300

    disable NR in development networks (#3812)
    * disable NR in development networks
    
    * cleaner code
    
    * check status before tx call
    
    * small test improvement
    
    * Revert "small test improvement"
    
    This reverts commit e11429e8120ec33beaf4c290014c78921b772c76.
    
    * increase CI timeouts by 5 min
    
    * increase CI timeout

--- packages/ethereum/deploy/07_HoprNetworkRegistry.ts
+++ packages/ethereum/deploy/07_HoprNetworkRegistry.ts
@@ -1,4 +1,5 @@
 import type { HardhatRuntimeEnvironment } from 'hardhat/types'
+import type { HoprNetworkRegistry } from '../src/types'
 
 const PROTOCOL_CONFIG = require('../../core/protocol-config.json')
 
@@ -7,21 +8,29 @@ const main = async function (hre: HardhatRuntimeEnvironment) {
 
   const environmentConfig = PROTOCOL_CONFIG.environments[environment]
   const deployer = await getNamedAccounts().then((o) => ethers.getSigner(o.deployer))
+  const inProd = environmentConfig['network_id'] == 'xdai'
 
   const adminAddress =
     network.name == 'hardhat' ? deployer.address : environmentConfig['network_registry_admin_address']
-  const registryProxy =
-    environmentConfig['network_id'] == 'xdai'
-      ? await deployments.get('HoprStakingProxyForNetworkRegistry')
-      : await deployments.get('HoprDummyProxyForNetworkRegistry')
+  const registryProxy = inProd
+    ? await deployments.get('HoprStakingProxyForNetworkRegistry')
+    : await deployments.get('HoprDummyProxyForNetworkRegistry')
 
-  const networkRegistry = await deployments.deploy('HoprNetworkRegistry', {
+  const networkRegistryContract = await deployments.deploy('HoprNetworkRegistry', {
     from: deployer.address,
     log: true,
     args: [registryProxy.address, adminAddress]
   })
 
-  console.log(`"HoprNetworkRegistry" deployed at ${networkRegistry.address}`)
+  console.log(`"HoprNetworkRegistry" deployed at ${networkRegistryContract.address}`)
+
+  if (!inProd) {
+    const networkRegistry = (await ethers.getContractFactory('HoprNetworkRegistry')).attach(
+      networkRegistryContract.address
+    ) as HoprNetworkRegistry
+    await networkRegistry.disableRegistry()
+    console.log(`Disabled "HoprNetworkRegistry"`)
+  }
 }
 
 main.dependencies = ['preDeploy', 'HoprNetworkRegistryProxy']
