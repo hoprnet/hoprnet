88004f04c3f63d59ccc8c370be2bad85f8f0bd8d
HOPR CI robot <noreply@hoprnet.org>
2022-05-23 18:45:43 +0000

    chore(build): apply linter changes

--- packages/ethereum/deploy/16_HoprNetworkRegistryProxy.ts
+++ packages/ethereum/deploy/16_HoprNetworkRegistryProxy.ts
@@ -2,8 +2,8 @@ import type { HardhatRuntimeEnvironment } from 'hardhat/types'
 
 const PROTOCOL_CONFIG = require('../../core/protocol-config.json')
 const MIN_STAKE = 0
-const DUMMY_PROXY = "HoprDummyProxyForNetworkRegistry"
-const STAKING_PROXY = "HoprStakingProxyForNetworkRegistry"
+const DUMMY_PROXY = 'HoprDummyProxyForNetworkRegistry'
+const STAKING_PROXY = 'HoprStakingProxyForNetworkRegistry'
 
 // Deploy directly a HoprNetworkRegistry contract, using hardcoded staking contract.
 const main = async function ({ deployments, getNamedAccounts, network, environment }: HardhatRuntimeEnvironment) {
@@ -16,7 +16,7 @@ const main = async function ({ deployments, getNamedAccounts, network, environme
       : environmentConfig['stake_contract_address']
 
   // Local development environment uses HoprDummyProxyForNetworkRegistry. All the other network uses HoprStakingProxyForNetworkRegistry
-  const registryProxyName = network.name == 'hardhat' ? DUMMY_PROXY : STAKING_PROXY;
+  const registryProxyName = network.name == 'hardhat' ? DUMMY_PROXY : STAKING_PROXY
 
   const registryProxy = await deployments.deploy('HoprNetworkRegistryProxy', {
     contract: registryProxyName,
