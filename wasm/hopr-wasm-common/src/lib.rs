013eb11f31af4331e6c92fe5665f1af41547d847
Lukas <lukas.pohanka@inina.net>
2022-05-26 13:30:36 +0200

    Created Makefile for WASM modules

--- wasm/hopr-wasm-common/src/lib.rs
+++ wasm/hopr-wasm-common/src/lib.rs
@@ -4,7 +4,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    fn read_file_to_string(s: &str) -> Option<String>;
+    fn read_file(s: &str) -> Box<[u8]>;
 }
 
 #[derive(Deserialize)]
@@ -15,10 +15,9 @@ struct PackageJsonFile {
 #[wasm_bindgen]
 pub fn get_hoprd_version() -> Result<String, JsValue> {
 
-    if let Some(file_contents) = read_file_to_string("packages/hoprd/package.json") {
-        if let Ok(v) = serde_json::from_str::<PackageJsonFile>(file_contents.as_str()) {
-            return Ok(v.version);
-        }
+    let file_contents = read_file("packages/hoprd/package.json");
+    if let Ok(v) = serde_json::from_slice::<PackageJsonFile>(&*file_contents) {
+        return Ok(v.version);
     }
 
     Err(JsValue::from("Failed to parse package.json"))
---
+++ wasm/src/io.ts
@@ -0,0 +1,5 @@
+import fs from 'fs'
+
+export function read_file(file: string) : Uint8Array {
+  return new Uint8Array(fs.readFileSync(file))
+}
