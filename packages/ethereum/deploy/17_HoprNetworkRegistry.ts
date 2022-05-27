d18d307cbf557d99f0f604ae8ee4001dec7147ac
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2022-05-27 07:22:04 +0200

    Network Registry is enabled in staging

--- packages/ethereum/deploy/17_HoprNetworkRegistry.ts
+++ packages/ethereum/deploy/17_HoprNetworkRegistry.ts
@@ -4,8 +4,6 @@ import type { HoprNetworkRegistry } from '../src/types'
 const main = async function ({ ethers, deployments, getNamedAccounts, network }: HardhatRuntimeEnvironment) {
   const { deployer } = await getNamedAccounts()
 
-  const inProd = network.name === 'xdai'
-
   // Local development environment uses HoprDummyProxyForNetworkRegistry. All the other network uses HoprStakingProxyForNetworkRegistry
   const registryProxy = await deployments.get('HoprNetworkRegistryProxy')
 
@@ -20,13 +18,28 @@ const main = async function ({ ethers, deployments, getNamedAccounts, network }:
     networkRegistryContract.address
   ) as HoprNetworkRegistry
 
-  if (!inProd) {
-    const isEnabled = await networkRegistry.enabled()
+  const isEnabled = await networkRegistry.enabled()
+
+  // when in production, network registry should be disabled // FIXME: for the moment, until NR is fully tested in staging
+  // when in staging, network registry should be enabled.
+  // for other environment, network registry should be disabled
+  if (network.tags.production) {
+    if (isEnabled) {
+      await networkRegistry.disableRegistry()
+      console.log(`Disabled "HoprNetworkRegistry" in production.`)
+    }
+  } else if (network.tags.staging) {
+    if (!isEnabled) {
+      await networkRegistry.enableRegistry()
+      console.log(`Enabled "HoprNetworkRegistry" in staging.`)
+    }
+  } else {
     if (isEnabled) {
       await networkRegistry.disableRegistry()
       console.log(`Disabled "HoprNetworkRegistry" in production.`)
     }
   }
+  console.log(`NetworkRegistry on ${network.name} network with tags ${Object.keys(network.tags)} is ${(await networkRegistry.enabled())? "enabled": "disabled"}.`)
 }
 
 main.dependencies = ['preDeploy', 'HoprNetworkRegistryProxy']
