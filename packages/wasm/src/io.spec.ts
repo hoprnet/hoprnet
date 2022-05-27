492909c6a314d726d98f129da2bbbe01003360c1
HOPR CI robot <noreply@hoprnet.org>
2022-05-27 15:29:32 +0000

    chore(build): apply linter changes

--- packages/wasm/src/io.spec.ts
+++ packages/wasm/src/io.spec.ts
@@ -4,7 +4,7 @@ import * as assert from 'assert'
 
 describe('test io abstraction for wasm', async function () {
   it('test reading files', async function () {
-      let file = "package.json"
-      assert.deepEqual(read_file(file), new Uint8Array(fs.readFileSync(file)))
+    let file = 'package.json'
+    assert.deepEqual(read_file(file), new Uint8Array(fs.readFileSync(file)))
   })
 })
