54ed6297836ac8a343b22d345bab709cb4fc5d5a
Robert Kiel <robert.kiel@hoprnet.org>
2022-05-30 18:54:24 +0200

    enhance shim

--- packages/hoprd/src/main.ts
+++ packages/hoprd/src/main.ts
@@ -3,9 +3,9 @@
 // Replace default process name (`node`) by `hoprd`
 process.title = 'hoprd'
 
-const majorNodeVersion = parseInt(process.version.match(/(?<=^v)[0-9]{1,}/)?.[0] ?? '-1')
+const majorNodeVersion = process.version.match(/(?<=^v)[0-9]{1,}/)
 
-if (majorNodeVersion == -1 || majorNodeVersion < 16) {
+if (!majorNodeVersion || majorNodeVersion.length == 0 || parseInt(majorNodeVersion[0]) < 16) {
   throw Error(`Incompatible Node.js version. Please use Node.js 15+`)
 }
 
