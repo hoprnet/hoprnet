15b1d8ad23635a224aa422c9ac7f784706ad19f3
Robert Kiel <robert.kiel@hoprnet.org>
2022-06-01 09:27:28 +0200

    Update packages/hoprd/src/main.ts
    Co-authored-by: Tino Breddin <tolbrino@users.noreply.github.com>

--- packages/hoprd/src/main.ts
+++ packages/hoprd/src/main.ts
@@ -6,7 +6,7 @@ process.title = 'hoprd'
 const majorNodeVersion = process.version.match(/(?<=^v)[0-9]{1,}/)
 
 if (!majorNodeVersion || majorNodeVersion.length == 0 || parseInt(majorNodeVersion[0]) < 16) {
-  throw Error(`Incompatible Node.js version. Please use Node.js 15+`)
+  throw Error(`Incompatible Node.js major version. Please use at least Node.js 16`)
 }
 
 // Start bootstrap process *after* Node.js version has been checked
