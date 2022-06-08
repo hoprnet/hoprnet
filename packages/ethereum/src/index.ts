8cede765dc7a5b787c8a11167559165c54e966e2
Steve <stivennoni77@gmail.com>
2022-06-08 14:10:32 +0300

    export all contract types (#3848)

--- packages/ethereum/src/index.ts
+++ packages/ethereum/src/index.ts
@@ -1,7 +1,18 @@
 import { join } from 'path'
 
 export * from './constants'
-export type { HoprChannels, HoprToken, HoprNetworkRegistry } from './types'
+export type {
+  HoprToken,
+  HoprChannels,
+  HoprDistributor,
+  HoprNetworkRegistry,
+  HoprBoost,
+  HoprStake,
+  HoprStake2,
+  HoprStakeSeason3,
+  HoprStakeSeason4,
+  HoprWhitehat
+} from './types'
 export type { TypedEventFilter, TypedEvent } from './types/common'
 
 export type ContractNames =
