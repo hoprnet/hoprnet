7521f1308698a8261676236ba6247547e4fcc695
NumberFour8 <lukas.pohanka@inina.net>
2022-06-30 08:48:52 +0200

    Improve backups

--- scripts/build_avado.sh
+++ scripts/build_avado.sh
@@ -58,6 +58,11 @@ if [[ -z $(grep -E "${default_development_environment}" "./build/Dockerfile") ]]
   exit 1
 fi
 
+# Create backups
+cp ./docker-compose.yml ./docker-compose.bak
+cp ./dappnode_package.json ./dappnode_package.bak
+cp ./build/Dockerfile ./build/Dockerfile.bak
+
 # Write AVADO docker build version
 sed -e "s/image:[ ]'hopr\.avado\.dnp\.dappnode\.eth:[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/image: 'hopr.avado.dnp.dappnode.eth:${AVADO_VERSION}/" ./docker-compose.yml \
   > ./docker-compose.yml.tmp && mv ./docker-compose.yml.tmp ./docker-compose.yml
@@ -83,5 +88,7 @@ sudo avadosdk build --provider http://80.208.229.228:5001
 
 # http://go.ava.do/install/<IPFS HASH>
 
-# Undo changes to Avado Dockerfile
-sed -e "s/${environment_id}/master-goerli/" ./build/Dockerfile > ./build/Dockerfile.tmp && mv ./build/Dockerfile.tmp ./build/Dockerfile 
+# Undo changes restoring backups
+mv ./docker-compose.bak ./docker-compose.yml
+mv ./dappnode_package.bak ./dappnode_package.json
+mv ./build/Dockerfile.bak ./build/Dockerfile
