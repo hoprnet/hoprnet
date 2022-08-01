16d16401003e2713ed10daa6e397df94cb06b165
HOPR CI robot <noreply@hoprnet.org>
2022-08-01 14:58:50 +0000

    chore(build): apply linter changes

--- packages/hoprd/src/commands/utils/message.spec.ts
+++ packages/hoprd/src/commands/utils/message.spec.ts
@@ -3,10 +3,10 @@ import assert from 'assert'
 
 describe('Message encoding & decoding', () => {
   it('check if message can be encoded and then decoded', async () => {
-    let msg = "some test message!"
+    let msg = 'some test message!'
     let encodedMsg = encodeMessage(msg)
 
-    await new Promise(r => setTimeout(r, 2000));
+    await new Promise((r) => setTimeout(r, 2000))
 
     let decodedMsg = decodeMessage(encodedMsg)
     assert.deepEqual(decodedMsg.msg, msg)
