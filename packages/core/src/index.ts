a753f70fef9ff2d4b983a13f3a54e1f4d2e4f8b6
Robert Kiel <robert.kiel@hoprnet.org>
2022-08-25 10:39:28 +0200

    fix typo

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -449,7 +449,7 @@ class Hopr extends EventEmitter {
       } catch (err) {
         // @TODO what to do here? E.g. delete channel from db?
         error(
-          `Could set commitment in channel to ${c.destination.toPeerId().toString()} (channelId ${c.getId().toHex()})`
+          `Couldn't set commitment in channel to ${c.destination.toPeerId().toString()} (channelId ${c.getId().toHex()})`
         )
       }
     }
