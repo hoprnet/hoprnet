79a028d7cae5112f9ef9638f7330e0ec2f92bbb4
adharasi <58601773+adharasi@users.noreply.github.com>
2022-08-26 15:19:10 +0100

    Top-level Sections Pruned to 5

--- docs/hopr-documentation/sidebars.js
+++ docs/hopr-documentation/sidebars.js
@@ -21,6 +21,75 @@ const sidebars = {
   tutorialSidebar: [
     {
       type: 'category',
+      label: 'What is HOPR?',
+      items: [
+        'core/what-is-hopr',
+        'core/what-is-metadata',
+        'core/anonymous-routing',
+        'core/mixnets',
+        'core/incentives',
+        'core/proof-of-relay',
+        'core/tickets-and-payment-channels',
+        'core/probabilistic-payments',
+        'core/cover-traffic',
+        'core/cover-traffic-nodes',
+        'core/balancing-cover-traffic',
+        'faq'
+      ]
+    },
+
+    {
+      type: 'category',
+      label: 'Run a HOPR node',
+      items: ['node/start-here',
+        'node/using-avado',
+        'node/using-docker',
+        'node/guide-using-a-hoprd-node',
+        'node/hoprd-commands']
+    },
+
+    {
+      type: 'category',
+      label: 'Developers',
+      link: { type: 'doc', id: 'developers/intro' },
+      items: [
+        'developers/starting-local-cluster',
+        'developers/connecting-node',
+        'developers/network-registry',
+        'developers/tutorial-hello-world',
+        'developers/demo-boomerang-chat',
+        'developers/demo-rps-game',
+        'developers/rest-api',
+        'developers/smart-contract',
+        'developers/snippets',
+        'developers/visualising-hopr-network-topology'
+      ]
+    },
+
+    {
+      type: 'category',
+      label: 'HOPR dApps',
+      items: ['dapps/myne-chat']
+    },
+
+    {
+      type: 'category',
+      label: 'Clutter',
+      items: [
+        'about-hopr',
+        'ecosystem/hopr-token',
+        'ecosystem/gnosis-hopr-tokens',
+        'ecosystem/staking',
+        'ecosystem/hoprd',
+        'staking/how-to-get-hopr',
+        'staking/how-to-stake',
+        'staking/convert-hopr'
+      ]
+    }
+
+    /*
+    {
+      type: 'category',
       label: 'About HOPR',
       items: ['about-hopr']
     },
@@ -88,7 +157,7 @@ const sidebars = {
       type: 'doc',
       id: 'faq',
       label: 'FAQ'
-    }
+    }*/
   ]
 }
 
