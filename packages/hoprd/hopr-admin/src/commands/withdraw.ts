10a143da79c850b9d30c33e853004e6df21129d4
Steve <stivennoni77@gmail.com>
2022-09-05 18:26:40 +0300

    fix withdraw command & add tests (#4079)

--- packages/hoprd/hopr-admin/src/commands/withdraw.ts
+++ packages/hoprd/hopr-admin/src/commands/withdraw.ts
@@ -6,7 +6,7 @@ export default class Withdraw extends Command {
   constructor(api: API, cache: CacheFunctions) {
     super(
       {
-        default: [[['number', 'amount'], ['hoprOrNative'], ['nativeAddress']], 'withdraw']
+        default: [[['number', 'amount'], ['hoprOrNative'], ['nativeAddress']], `withdraw 'hopr' or 'native' tokens`]
       },
       api,
       cache
@@ -18,11 +18,11 @@ export default class Withdraw extends Command {
   }
 
   public description(): string {
-    return 'Withdraw native or hopr to a specified recipient'
+    return 'Withdraw hopr or native tokens to a given native address'
   }
 
   /**
-   * Withdraws native or hopr balance.
+   * Withdraws hopr or native tokens.
    */
   public async execute(log: (msg: string) => void, query: string): Promise<void> {
     const [error, , amount, currency, recipient] = this.assertUsage(query) as [
@@ -35,18 +35,20 @@ export default class Withdraw extends Command {
     if (error) return log(error)
 
     const amountWei = ethersUtils.parseEther(String(amount))
+
+    log(`Withdrawing ${amount} ${currency} to "${recipient}"..`)
     const response = await this.api.withdraw(amountWei.toString(), currency, recipient)
 
     if (!response.ok) {
       return log(
-        await this.failedApiCall(response, 'withdraw', {
+        await this.failedApiCall(response, `withdraw ${amount} ${currency}`, {
           400: (v) => `one or more invalid inputs ${v.status}`,
           422: (v) => v.status
         })
       )
     }
 
-    const receipt = response.json().then((res) => res.receipt)
-    return log(`Withdrawing ${amount} ${currency} to ${recipient}, receipt ${receipt}.`)
+    const receipt = await response.json().then((res) => res.receipt)
+    return log(`Withdrawed ${amount} ${currency} to "${recipient}", receipt ${receipt}.`)
   }
 }
