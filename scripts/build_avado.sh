d2ceece5376460c08f7dc1e6f07898187b7a0199
NumberFour8 <lukas.pohanka@inina.net>
2022-09-13 12:55:47 +0200

    Improvements to Avado build

--- scripts/build_avado.sh
+++ scripts/build_avado.sh
@@ -15,10 +15,10 @@ source "${mydir}/utils.sh"
 
 usage() {
   msg
-  msg "Usage: $0 [-h|--help] version [environment] [release]"
+  msg "Usage: $0 [-h|--help] version [environment] [release] [api token]"
   msg
   msg "Sets the version of the AVADO build and builds the image. Version must be semver"
-  msg "Optional: set default environment and release name"
+  msg "Optional: set default environment, release name and API token"
   msg
 }
 
@@ -28,45 +28,38 @@ if [ -z "${1:-}" ]; then
   exit 1
 fi
 
-declare environment_id="${2:-"$(${mydir}/get-default-environment.sh)"}"
-declare release_id="${3:-"$(${mydir}/get-default-environment.sh --release)"}"
-
-if [ -z "${environment_id}" ]; then
-  msg "Could not determine default environment"
-  exit 1
-fi
-
-# Retrieve the provider URL for the given environment ID from protocol-config.json
-provider_url=$(jq -r ".environments.\"${environment_id}\".network_id as \$envid | .networks[\$envid].default_provider // \"\"" "${mydir}/../packages/core/protocol-config.json")
-if [ -z "${provider_url}" ]; then
-  msg "Environment ${environment_id} has invalid network_id"
-  exit 1
-fi
-
 if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then
   usage
   exit 0
 fi
 
-msg "Building Avado for release ${release_id} in environment ${environment_id} with default provider ${provider_url}"
+# Get/set default values of parameters
+declare avado_version="${1}"
+declare environment_id="${2:-"$(${mydir}/get-default-environment.sh)"}"
+declare release_id="${3:-"$(${mydir}/get-default-environment.sh --release)"}"
+declare api_token="${4:-"!5qxc9Lp1BE7IFQ-nrtttU"}"
 
-declare AVADO_VERSION="${1}"
-
-if ! [[ $AVADO_VERSION =~ [0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}$ ]]; then
-  # AVADO build tool requires proper semver versions
-  msg "Version '${AVADO_VERSION}' is not semver"
+# Validate environment and release ids
+if [[ -z "${environment_id}" ]] || [[ -z "${release_id}" ]]; then
+  msg "Could not determine default environment or release id"
   exit 1
 fi
+
+# AVADO build tool requires proper semver versions
+if ! [[ $avado_version =~ [0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}$ ]]; then
+  msg "Version '${avado_version}' is not semver"
+  exit 1
+fi
+
+# Retrieve the provider URL for the given environment ID from protocol-config.json
+declare provider_url=$(jq -r ".environments.\"${environment_id}\".network_id as \$envid | .networks[\$envid].default_provider // \"\"" "${mydir}/../packages/core/protocol-config.json")
+if [ -z "${provider_url}" ]; then
+  msg "Environment ${environment_id} has invalid network_id"
+  exit 1
+fi
+
 
 cd "${mydir}/../packages/avado"
-
-declare default_development_release_id="master-goerli"
-
-if [[ -z $(grep -E "${default_development_release_id}" "./build/Dockerfile") ]]; then
-  # Fail if default environment is no longer `master-goerli`
-  msg "Avado Dockerfile differs. Could not find \"${default_development_release_id}\" release id in it"
-  exit 1
-fi
 
 function cleanup {
   local EC=$?
@@ -81,6 +74,8 @@ function cleanup {
   exit $EC
 }
 
+msg "Building Avado v. ${avado_version} for release ${release_id} using environment ${environment_id} with default provider ${provider_url}"
+
 # Create backups
 cp ./docker-compose.yml ./docker-compose.bak
 cp ./dappnode_package.json ./dappnode_package.bak
@@ -88,34 +83,35 @@ cp ./build/Dockerfile ./build/Dockerfile.bak
 
 trap cleanup SIGINT SIGTERM ERR EXIT
 
-# Write AVADO docker build version & used provider
-sed -E "s/image:[ ]'hopr\.avado\.dnp\.dappnode\.eth:[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/image: 'hopr.avado.dnp.dappnode.eth:${AVADO_VERSION}/ ;\
- s/%ENV_ID%/${environment_id}/g ; s|(.+HOPRD_PROVIDER=).+'(,?)|\1${provider_url}'\2|" ./docker-compose.yml \
+### Update docker-compose.yaml
+sed -E "s/%AVADO_VERSION%/${avado_version}/g ; s/%TOKEN%/${api_token}/g ; \
+ s/%ENV_ID%/${environment_id}/g ; s|%PROVIDER_URL%|${provider_url}|g" ./docker-compose.yml \
   > ./docker-compose.yml.tmp && mv ./docker-compose.yml.tmp ./docker-compose.yml
+
+###
+### Update dappnode_package.json
 
 # Copy sections between *_JSON_EXPORT of docker-compose.yaml to dappnode_package.json
 # and also set dappnode version
 sed -n '/BEGIN_JSON_EXPORT/,/END_JSON_EXPORT/{//!p}' ./docker-compose.yml \
   | sed -E "s/]/],/ ; s/'/\"/g ; s/#([{}])/\1/" \
-  | jq -s ".[0].image += .[1] | .[0] | .version = \"${AVADO_VERSION}\"" ./dappnode_package.json /dev/stdin \
+  | jq -s ".[0].image += .[1] | .[0] | .version = \"${avado_version}\"" ./dappnode_package.json /dev/stdin \
   > ./dappnode_package.json.tmp && mv ./dappnode_package.json.tmp ./dappnode_package.json
 
-# Replace API token in the json manifest
-declare api_token=$(sed -rn "s/.+HOPRD_API_TOKEN=(.+)',/\1/p" ./docker-compose.yml)
-sed -E "s/%TOKEN%/${api_token}/" ./dappnode_package.json  > ./dappnode_package.json.tmp && mv ./dappnode_package.json.tmp ./dappnode_package.json
+sed -E "s/%TOKEN%/${api_token}/g" ./dappnode_package.json \
+  > ./dappnode_package.json.tmp && mv ./dappnode_package.json.tmp ./dappnode_package.json
 
-# Overwrite default environment in Dockerfile with the one currently used
-sed -e "s/${default_development_release_id}/${release_id}/" ./build/Dockerfile \
-  > ./build/Dockerfile.tmp && mv ./build/Dockerfile.tmp ./build/Dockerfile
+###
 
 # AVADO SDK does not do proper releases, therefore using GitHub + git commit hashes
-declare AVADO_SDK_COMMIT="7b035be"
+declare avado_sdk_commit="7b035be"
 
 # Must be installed globally due to bad directory calls
-npm install -g git+https://github.com/AvadoDServer/AVADOSDK.git#${AVADO_SDK_COMMIT}
+#npm install -g git+https://github.com/AvadoDServer/AVADOSDK.git#${avado_sdk_commit}
 
 # Must run as sudo due to underlying call to docker-compose
-sudo avadosdk build --provider http://80.208.229.228:5001
+#sudo avadosdk build --provider http://80.208.229.228:5001
 
 # http://go.ava.do/install/<IPFS HASH>
 
+sleep 300s
