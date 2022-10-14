1867d961de1b4baf5b9a948ce201f72af37d9c1f
HOPR CI robot <noreply@hoprnet.org>
2022-10-14 06:35:41 +0000

    chore(build): apply linter changes

--- packages/ethereum/test/stake/HoprStakeSeason5.test.ts
+++ packages/ethereum/test/stake/HoprStakeSeason5.test.ts
@@ -5,17 +5,14 @@ import { deployContractFromFactory } from '../utils'
 
 const blockedIndexes = [2, 3, 4, 7, 8, 9, 10, 11, 12, 13]
 // known indices that are not blocked and not exceeding 16, with a few more random uint256 indices
-const otherIndices = [1, 5, 6, 14, 15, 16].reduce(
-  (acc, cur) => {
-    const randomUint256 = BigNumber.from(utils.randomBytes(32))
-    // random uint256 is not the blocked indice
-    if (blockedIndexes.findIndex(blocked => BigNumber.from(blocked).eq(randomUint256)) > -1) {
-      acc.concat(cur.toString()).concat(randomUint256.toString())
-    }
-    return acc
-  },
-  [] as string[]
-)
+const otherIndices = [1, 5, 6, 14, 15, 16].reduce((acc, cur) => {
+  const randomUint256 = BigNumber.from(utils.randomBytes(32))
+  // random uint256 is not the blocked indice
+  if (blockedIndexes.findIndex((blocked) => BigNumber.from(blocked).eq(randomUint256)) > -1) {
+    acc.concat(cur.toString()).concat(randomUint256.toString())
+  }
+  return acc
+}, [] as string[])
 
 describe('HoprStakeSeason5', function () {
   let deployer: Signer
