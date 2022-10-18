ef95dbf780bb87928f88df3c2f2799d3180b7cce
NumberFour8 <lukas.pohanka@inina.net>
2022-10-18 16:08:31 +0200

    Use `--experimental-wasm-modules` when running integration tests

--- scripts/run-integration-tests-source.sh
+++ scripts/run-integration-tests-source.sh
@@ -195,7 +195,7 @@ function setup_node() {
     HOPRD_HEARTBEAT_VARIANCE=1000 \
     HOPRD_NETWORK_QUALITY_THRESHOLD="0.3" \
     HOPRD_ON_CHAIN_CONFIRMATIONS=2 \
-    node packages/hoprd/lib/main.cjs \
+    node --experimental-wasm-modules packages/hoprd/lib/main.cjs \
       --admin \
       --adminHost "127.0.0.1" \
       --adminPort ${admin_port} \
