cbff59ac54bc7e932c987866403bdd5942b6f588
Robert Kiel <robert.kiel@hoprnet.org>
2022-10-20 13:08:56 +0200

    remove wildcard imports

--- packages/real/src/io.spec.ts
+++ packages/real/src/io.spec.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import { read_file } from './io.js'
-import * as assert from 'assert'
+import assert from 'assert'
 
 describe('test io abstraction for real', async function () {
   it('test reading files', async function () {
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -15,6 +15,6 @@ export * from './ethereum/index.js'
 export * from './utils.js'
 
 // Load `utils-misc` crate
-import * as utils_misc from '../lib/utils_misc.js'
-utils_misc.set_panic_hook()
+import { set_panic_hook } from '../lib/utils_misc.js'
+set_panic_hook()
 export { get_package_version } from '../lib/utils_misc.js'
