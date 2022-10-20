bcc44808bafb3792f809a4fd894b91089da9dd3a
Lukas <lukas.pohanka@inina.net>
2022-10-20 14:09:00 +0200

    Update with latest improvements

--- packages/core/crates/core-crypto/src/lib.rs
+++ packages/core/crates/core-crypto/src/lib.rs
@@ -8,6 +8,19 @@ pub mod shared_keys;
 pub mod primitives;
 pub mod prp;
 
+#[allow(dead_code)]
+#[wasm_bindgen]
+pub fn set_panic_hook() {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function at least once during initialization, and then
+    // we will get better error messages if our code ever panics.
+    //
+    // For more details see
+    // https://github.com/rustwasm/console_error_panic_hook#readme
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -18,8 +31,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 extern crate static_assertions;
 extern crate core;
 
-// Static assertions on cryptographic parameters
+use wasm_bindgen::prelude::wasm_bindgen;
 
+// Static assertions on cryptographic parameters
 
 const_assert!(parameters::SECRET_KEY_LENGTH >= 32);
 //const_assert_eq!(constants::SECRET_KEY_LENGTH, keys::KeyBytes::)
--- packages/core/crates/core-crypto/src/utils.rs
+++ packages/core/crates/core-crypto/src/utils.rs
@@ -1,17 +1,6 @@
 use std::fmt::Display;
 use wasm_bindgen::JsValue;
 
-pub fn set_panic_hook() {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
-    // `set_panic_hook` function at least once during initialization, and then
-    // we will get better error messages if our code ever panics.
-    //
-    // For more details see
-    // https://github.com/rustwasm/console_error_panic_hook#readme
-    #[cfg(feature = "console_error_panic_hook")]
-    console_error_panic_hook::set_once();
-}
-
 pub fn as_jsvalue<T>(v: T) -> JsValue where T: Display {
     JsValue::from(v.to_string())
 }
