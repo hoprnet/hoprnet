7de8fdf1123114b80afa441f1586c6a69c60c31d
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2022-10-21 14:13:49 +0200

    extend test cases

--- packages/ethereum/crates/README.md
+++ packages/ethereum/crates/README.md
@@ -41,3 +41,18 @@ openzeppelin-contracts-v3-0-1=OpenZeppelin/openzeppelin-contracts@v3.0.1 \
 3. If `forge coverage` is not found in as a command, update `foundryup` to the [latest nightly release](https://github.com/foundry-rs/foundry/releases) may solve the issue.
 
 4. `forge coverage` may run into `Error: Function has no kind` when compiler has multiple versions. Opened an issue https://github.com/foundry-rs/foundry/issues/3519
+
+<!-- 5. To move faster on the migration of toolchain and to avoid constantly running into foundry's error during the compilation `Error: Discovered incompatible solidity versions in following`, the compiler version of the following contracts have be moved to `pragma solidity >=0.6.0 <0.9.0;`
+- src/HoprToken.sol (^0.6.0)
+- src/ERC777/ERC777Snapshot.sol (^0.6.0)
+- src/openzeppelin-contracts/ERC777.sol (>=0.6.0 <0.8.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/access/AccessControl.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/GSN/Context.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/introspection/IERC1820Registry.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/math/SafeMath.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/token/ERC20/IERC20.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/token/ERC777/IERC777.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/token/ERC777/IERC777Recipient.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/token/ERC777/IERC777Sender.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/utils/EnumerableSet.sol (^0.6.0)
+- lib/openzeppelin-contracts-v3-0-1/contracts/utils/Address.sol (^0.6.2) -->
--- packages/ethereum/crates/contracts/test/HoprChannels.t.sol
+++ packages/ethereum/crates/contracts/test/HoprChannels.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.6.0 <0.9.0;
 
 import "./utils/Deploy.sol";
 import "./utils/Accounts.sol";
@@ -7,33 +7,55 @@ import "../src/HoprChannels.sol";
 import "forge-std/Test.sol";
 
 contract HoprChannelsTest is Test, ERC1820RegistryFixture, AccountsFixture {
-    HoprChannels public hoprChannel;
+    HoprChannels public hoprChannels;
 
+    /**
+     * Emitted on every channel state change.
+     */
+    event ChannelUpdated(
+        address indexed source,
+        address indexed destination,
+        HoprChannels.Channel newState
+    );
+
+    /**
+     * Emitted once an account announces.
+     */
     event Announcement(
         address indexed account,
         bytes publicKey,
         bytes multiaddr
     );
 
+    /**
+     * Emitted once a channel if funded.
+     */
+    event ChannelFunded(
+        address indexed funder,
+        address indexed source,
+        address indexed destination,
+        uint256 amount
+    );
+
     function setUp() public virtual override {
         super.setUp();
-        // FIXME: set a fixture of HOPR token
-        hoprChannel = new HoprChannels(vm.addr(1), 15);
+        // make vm.addr(1) HoprToken contract
+        hoprChannels = new HoprChannels(vm.addr(1), 15);
     }
 
     function testAnnounceAddressFromPublicKey() public {
         bytes memory multiAddress = hex"1234";
         vm.prank(accountA.accountAddr);
-        vm.expectEmit(true, false, false, true, address(hoprChannel));
+        vm.expectEmit(true, false, false, true, address(hoprChannels));
         emit Announcement(accountA.accountAddr, accountA.publicKey, multiAddress);
-        hoprChannel.announce(accountA.publicKey, multiAddress);
+        hoprChannels.announce(accountA.publicKey, multiAddress);
     }
 
     function testRevert_AnnouceWrongPublicKey() public {
         bytes memory multiAddress = hex"1234";
         vm.prank(accountB.accountAddr);
         vm.expectRevert("publicKey's address does not match senders");
-        hoprChannel.announce(accountA.publicKey, multiAddress);
+        hoprChannels.announce(accountA.publicKey, multiAddress);
     }
 
     function testRevert_AnnouceRandomPublicKey(bytes calldata randomPublicKey) public {
@@ -41,8 +63,61 @@ contract HoprChannelsTest is Test, ERC1820RegistryFixture, AccountsFixture {
         bytes memory multiAddress = hex"1234";
         vm.prank(accountB.accountAddr);
         vm.expectRevert("publicKey's address does not match senders");
-        hoprChannel.announce(randomPublicKey, multiAddress);
+        hoprChannels.announce(randomPublicKey, multiAddress);
     }
 
-    // function testFundChannelMulti()
+    // // it should fail to fund without accountA announcement
+    function testRevert_FundChannelMultiWithoutAccountAAnnoucement(uint256 amount1) public {
+        amount1 = bound(amount1, 1, 1e36);
+        // accountA is not annouced and only accountB is announced
+        bytes memory multiAddress = hex"1234";
+        vm.prank(accountB.accountAddr);
+        hoprChannels.announce(accountB.publicKey, multiAddress);
+        vm.prank(address(1));
+
+        vm.expectRevert("source has not announced");
+        hoprChannels.fundChannelMulti(accountA.accountAddr, accountB.accountAddr, amount1, 0);
+    }
+
+    // it should fail to fund without accountB announcement
+    function testRevert_FundChannelMultiWithoutAccountBAnnoucement(uint256 amount1) public {
+        amount1 = bound(amount1, 1, 1e36);
+        // accountB is not annouced and only accountA is announced
+        bytes memory multiAddress = hex"1234";
+        vm.prank(accountA.accountAddr);
+        hoprChannels.announce(accountA.publicKey, multiAddress);
+        vm.prank(address(1));
+
+        vm.expectRevert("destination has not announced");
+        hoprChannels.fundChannelMulti(accountA.accountAddr, accountB.accountAddr, amount1, 0);
+    }
+
+    function testFundChannelMulti(uint256 amount1, uint256 amount2) public {
+        amount1 = bound(amount1, 1, 1e36);
+        amount2 = bound(amount2, 1, 1e36);
+        // accountA and accountB are both announced
+        bytes memory multiAddress = hex"1234";
+        vm.prank(accountA.accountAddr);
+        hoprChannels.announce(accountA.publicKey, multiAddress);
+        vm.prank(accountB.accountAddr);
+        hoprChannels.announce(accountB.publicKey, multiAddress);
+        vm.prank(address(1));
+
+        // fund channel for two parties triggers 
+        vm.expectEmit(true, true, false, false, address(hoprChannels));
+        emit ChannelUpdated(accountA.accountAddr, accountB.accountAddr, HoprChannels.Channel(amount1, bytes32(0), 0, 0, HoprChannels.ChannelStatus.WAITING_FOR_COMMITMENT, 1, 15));
+        vm.expectEmit(true, true, true, true, address(hoprChannels));
+        emit ChannelFunded(address(1), accountA.accountAddr, accountB.accountAddr, amount1);
+        vm.expectEmit(true, true, false, false, address(hoprChannels));
+        emit ChannelUpdated(accountB.accountAddr, accountA.accountAddr, HoprChannels.Channel(amount2, bytes32(0), 0, 0, HoprChannels.ChannelStatus.WAITING_FOR_COMMITMENT, 1, 15));
+        vm.expectEmit(true, true, true, true, address(hoprChannels));
+        emit ChannelFunded(address(1), accountB.accountAddr, accountA.accountAddr, amount2);
+        // fund channel multi calls token transfer under the hood
+        vm.mockCall(
+            vm.addr(1),
+            abi.encodeWithSelector(bytes4(keccak256("transferFrom(address,address,uint256)"))),
+            abi.encode(address(1), address(hoprChannels), amount1 + amount2)
+        );
+        hoprChannels.fundChannelMulti(accountA.accountAddr, accountB.accountAddr, amount1, amount2);
+    }
 }
