c4346dbf5fd2a3a20ab935795e47055e3fcd67c6
NumberFour8 <lukas.pohanka@inina.net>
2022-11-02 12:35:44 +0100

    Add PRG correspondence tests

---
+++ packages/core/src/cryptography.spec.ts
@@ -0,0 +1,36 @@
+
+import { PRG as Rust_PRG } from './cryptography.js'
+import { PRG as TS_PRG } from '@hoprnet/hopr-utils'
+import assert from 'assert'
+
+describe('cryptographic correspondence tests', async function () {
+  it('PRG correspondence', async function () {
+
+    let key = new Uint8Array(16);
+    let iv = new Uint8Array(12);
+
+    {
+      let rs_output = new Rust_PRG(key, iv).digest(5, 10);
+      let ts_output = TS_PRG.createPRG({ key, iv }).digest(5, 10);
+      assert.deepEqual(rs_output, ts_output)
+    }
+
+    {
+      let rs_output = new Rust_PRG(key, iv).digest(0, 100);
+      let ts_output = TS_PRG.createPRG({ key, iv }).digest(0, 100);
+      assert.deepEqual(rs_output, ts_output)
+    }
+
+    {
+      let rs_output = new Rust_PRG(key, iv).digest(10, 100);
+      let ts_output = TS_PRG.createPRG({ key, iv }).digest(10, 100);
+      assert.deepEqual(rs_output, ts_output)
+    }
+
+    {
+      let rs_output = new Rust_PRG(key, iv).digest(16, 22);
+      let ts_output = TS_PRG.createPRG({ key, iv }).digest(16, 22);
+      assert.deepEqual(rs_output, ts_output)
+    }
+  })
+})
