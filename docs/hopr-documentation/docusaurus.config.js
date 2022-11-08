995ee542e835f771b0769bbaa6000e0eb9b298df
adharasi <58601773+adharasi@users.noreply.github.com>
2022-11-08 09:20:20 +0000

    revert to current

--- docs/hopr-documentation/docusaurus.config.js
+++ docs/hopr-documentation/docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           editUrl: 'https://github.com/hoprnet/hoprnet/edit/master/docs/hopr-documentation',
-          lastVersion: './versioned_docs/version-v1.90'
+          lastVersion: 'current'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
