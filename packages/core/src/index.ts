a3190b569191db3c21e4445ca67933aebaeea652
Robert Kiel <robert.kiel@hoprnet.org>
2022-12-22 15:43:13 +0100

    fix typo

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -632,7 +632,7 @@ class Hopr extends EventEmitter {
 
       out += `${channel.toString()}\n`
       currentChannels.push(channel)
-      if (!(await this.isAllowedAccessToNetwork(channel.destination.toPeerId()))) {
+      if (await this.isAllowedAccessToNetwork(channel.destination.toPeerId())) {
         this.networkPeers.register(channel.destination.toPeerId(), NetworkPeersOrigin.STRATEGY_EXISTING_CHANNEL) // Make sure current channels are 'interesting'
       } else {
         error(`Protocol error: Strategy is monitoring non-registered peer ${channel.destination.toString()}`)
@@ -699,7 +699,7 @@ class Hopr extends EventEmitter {
 
     for (let i = 0; i < tickResult.toOpen.length; i++) {
       const channel = tickResult.toOpen[i]
-      if (!(await this.isAllowedAccessToNetwork(channel.destination.toPeerId()))) {
+      if (await this.isAllowedAccessToNetwork(channel.destination.toPeerId())) {
         this.networkPeers.register(channel.destination.toPeerId(), NetworkPeersOrigin.STRATEGY_NEW_CHANNEL)
       } else {
         error(`Protocol error: strategy wants to open channel to non-registered peer ${channel.destination.toString()}`)
