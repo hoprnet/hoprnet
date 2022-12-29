e2cf372416da2bad7b370edc78c665448deaf8d2
Robert Kiel <robert.kiel@hoprnet.org>
2022-12-29 16:21:45 +0100

    compile-time args

--- packages/hoprd/crates/hoprd-misc/src/cli.rs
+++ packages/hoprd/crates/hoprd-misc/src/cli.rs
@@ -6,15 +6,7 @@ use clap::{Arg, ArgAction, ArgMatches, Args, Command, FromArgMatches as _};
 use real_base::real;
 use serde::{Deserialize, Serialize};
 use serde_json;
-impl From<ArgMatches> for CliArgs {
-    fn from(m: ArgMatches) -> Self {
-        CliArgs {
-            enviromment: m.get_one::<String>("name").cloned().unwrap(),
-            api_port: m.get_one::<u16>("apiPort").cloned().unwrap(),
-            api_host: m.get_one("apiHost").cloned().unwrap(),
-        }
-    }
-}
+
 use serde_json::{Map, Value};
 use wasm_bindgen::JsValue;
 
@@ -28,6 +20,12 @@ const CONFIRMATIONS: u32 = 8;
 
 const NETWORK_QUALITY_THRESHOLD: f32 = 0.5;
 
+const DEFAULT_API_HOST: &str = "localhost";
+const DEFAULT_API_PORT: u16 = 3001;
+
+const DEFAULT_HEALTH_CHECK_HOST: &str = "localhost";
+const DEFAULT_HEALTH_CHECK_PORT: u16 = 8080;
+
 #[derive(serde::Deserialize)]
 pub struct ProtocolConfigFile {
     environments: Map<String, Value>,
@@ -35,9 +33,237 @@ pub struct ProtocolConfigFile {
 
 #[derive(Serialize, Args)]
 struct CliArgs {
-    enviromment: String,
-    api_port: u16,
+    #[arg(
+        long, 
+        default_value_t = false, 
+        env = "HOPRD_API", 
+        help = format!("Expose the API on {}:{}", DEFAULT_API_HOST, DEFAULT_API_PORT), 
+        action = ArgAction::SetTrue
+    )]
+    api: bool,
+
+    #[arg(
+        long = "apiHost",
+        default_value_t = DEFAULT_API_HOST.to_string(),
+        value_name = "HOST",
+        help = "Set host IP to which the API server will bind",
+        env = "HOPRD_API_HOST"
+    )]
     api_host: String,
+    #[arg(
+        long = "apiPort",
+        default_value_t = DEFAULT_API_PORT,
+        value_parser = clap::value_parser!(u16),
+        value_name = "PORT",
+        help = "Set port to which the API server will bind",
+        env = "HOPRD_API_HOST"
+    )]
+    api_port: u16,
+
+    #[arg(
+        long = "apiToken",
+        help = "A REST API token and for user authentication",
+        value_name = "TOKEN",
+        env = "HOPRD_API_TOKEN"
+    )]
+    api_token: Option<String>,
+
+    #[arg(
+        long = "healthCheck",
+        default_value_t = false,
+        env = "HOPRD_HEALTH_CHECK",
+        help = format!("Run a health check end point on {}:{}", DEFAULT_HEALTH_CHECK_HOST, DEFAULT_HEALTH_CHECK_PORT)
+    )]
+    health_check: bool,
+
+    #[arg(
+        long = "healthCheckHost",
+        default_value_t = DEFAULT_HEALTH_CHECK_HOST.to_string(),
+        value_name = "HOST",
+        help = "Updates the host for the healthcheck server",
+        env = "HOPRD_HEALTH_CHECK_HOST",
+    )]
+    health_check_host: String,
+
+    #[arg(
+        long = "healthCheckPort",
+        default_value_t = DEFAULT_HEALTH_CHECK_PORT,
+        value_name = "PORT",
+        value_parser = clap::value_parser!(u16),
+        help = "Updates the port for the healthcheck server",
+        env = "HOPRD_HEALTH_CHECK_PORT"
+    )]
+    health_check_port: u16,
+
+    #[arg(
+        long,
+        env = "HOPRD_PASSWORD",
+        help = "A password to encrypt your keys"
+    )]
+    password: Option<String>,
+
+    #[arg(
+        long,
+        help = "A custom RPC provider to be used for the node to connect to blockchain",
+        env = "HOPRD_PROVIDER",
+        value_name = "PROVIDER"
+    )]
+    provider: Option<String>,
+
+    #[arg(
+        long = "dryRun",
+        help = "List all the options used to run the HOPR node, but quit instead of starting",
+        env = "HOPRD_DRY_RUN",
+        default_value_t = false,
+        action = ArgAction:: SetTrue
+    )]
+    dry_run: bool,
+
+    #[arg(
+        long,
+        help = "initialize a database if it doesn't already exist",
+        action = ArgAction::SetTrue,
+        env = "HOPRD_INIT",
+        default_value_t = false
+    )]
+    init: bool,
+
+    #[arg(
+        long = "privateKey",
+        hide = true,
+        help = "A private key to be used for the node",
+        env = "HOPRD_PRIVATE_KEY",
+        value_name = "PRIVATE_KEY"
+    )]
+    private_key: Option<String>,
+
+    #[arg(
+        long = "allowLocalNodeConnections",
+        env = "HOPRD_ALLOW_LOCAL_NODE_CONNECTIONS",
+        action = ArgAction::SetTrue,
+        help = "Allow connections to other nodes running on localhost",
+        default_value_t = false
+    )]
+    allow_local_node_connections: bool,
+
+    #[arg(
+        long = "allowPrivateNodeConnections",
+        env = "HOPRD_ALLOW_PRIVATE_NODE_CONNECTIONS",
+        action = ArgAction::SetTrue,
+        default_value_t = false,
+        help = "Allow connections to other nodes running on private addresses",
+    )]
+    allow_private_node_connections: bool,
+
+    #[arg(
+        long = "testAnnounceLocalAddresses",
+        env = "HOPRD_TEST_ANNOUNCE_LOCAL_ADDRESSES",
+        help = "For testing local testnets. Announce local addresses",
+        action = ArgAction::SetTrue,
+        default_value_t = false
+    )]
+    test_announce_local_addresses:bool,
+
+    #[arg(
+        long = "testPreferLocalAddresses",
+        env = "HOPRD_TEST_PREFER_LOCAL_ADDRESSES",
+        action = ArgAction::SetTrue,
+        help = "For testing local testnets. Prefer local peers to remote",
+        default_value_t = true,
+        hide = true
+    )]
+    test_prefer_local_addresses: bool,
+
+    #[arg(
+        long = "testUseWeakCrypto",
+        env = "HOPRD_TEST_USE_WEAK_CRYPTO",
+        action = ArgAction::SetTrue,
+        help = "weaker crypto for faster node startup",
+        hide = true,
+        default_value_t = false
+    )]
+    test_use_weak_crypto: bool,
+
+    #[arg(
+        long = "testNoAuthentication",
+        help = "no remote authentication for easier testing",
+        action = ArgAction::SetTrue,
+        env = "HOPRD_TEST_NO_AUTHENTICATION",
+        default_value_t = false,
+        hide = true
+    )]
+    test_no_authentication: bool,
+
+    #[arg(
+        long = "testNoDirectConnections",
+        help = "NAT traversal testing: prevent nodes from establishing direct TCP connections",
+        env = "HOPRD_TEST_NO_DIRECT_CONNECTIONS",
+        default_value_t = false,
+        action = ArgAction::SetTrue,
+        hide = true
+    )]
+    test_no_direct_connections: bool,
+
+    #[arg(
+        long = "testNoWebRTCUpgrade",
+        help = "NAT traversal testing: prevent nodes from establishing direct TCP connections",
+        env = "HOPRD_TEST_NO_WEBRTC_UPGRADE",
+        default_value_t = false,
+        action = ArgAction::SetTrue,
+        hide = true
+    )]
+    test_no_webrtc_upgrade: bool,
+
+    #[arg(
+        long = "heartbeatInterval",
+        help = "Interval in milliseconds in which the availability of other nodes get measured",
+        value_name = "MILLISECONDS",
+        value_parser = clap::value_parser!(u32),
+        default_value_t = HEARTBEAT_INTERVAL,
+        env = "HOPRD_HEARTBEAT_INTERVAL",
+    )]
+    heartbeat_interval: u32,
+
+    #[arg(
+        long = "heartbeatThreshold",
+        help = "Timeframe in milliseconds after which a heartbeat to another peer is performed, if it hasn't been seen since",
+        value_name = "MILLISECONDS",
+        value_parser = clap::value_parser!(u32),
+        default_value_t = HEARTBEAT_THRESHOLD,
+        env = "HOPRD_HEARTBEAT_THRESHOLD",
+    )]
+    heartbeat_threshold: u32,
+
+    #[arg(
+        long = "heartbeatVariance",
+        help = "Upper bound for variance applied to heartbeat interval in milliseconds",
+        value_name = "MILLISECONDS",
+        value_parser = clap::value_parser!(u32),
+        default_value_t = HEARTBEAT_INTERVAL_VARIANCE,
+        env = "HOPRD_HEARTBEAT_VARIANCE"
+    )]
+    heartbeat_variance: u32,
+
+    #[arg(
+        long = "onChainConfirmations",
+        help = "Number of confirmations required for on-chain transactions",
+        value_name = "CONFIRMATIONS",
+        value_parser = clap::value_parser!(u32),
+        default_value_t = CONFIRMATIONS,
+        env = "HOPRD_ON_CHAIN_CONFIRMATIONS",
+
+    )]
+    on_chain_confirmations: u32,
+
+    #[arg(
+        long = "networkQualityThreshold",
+        help = "Miniumum quality of a peer connection to be considered usable",
+        value_name = "THRESHOLD",
+        value_parser = clap::value_parser!(f32),
+        default_value_t = NETWORK_QUALITY_THRESHOLD,
+        env = "HOPRD_NETWORK_QUALITY_THRESHOLD"
+    )]
+    network_quality_threshold: f32,
 }
 
 #[derive(serde::Deserialize)]
@@ -94,20 +320,7 @@ pub fn parse_cli_arguments(cli_args: Vec<&str>) -> Result<JsValue, JsValue> {
     let cmd = Command::new("hoprd")
     .after_help("All CLI options can be configured through environment variables as well. CLI parameters have precedence over environment variables.")
         .version(&version)
-        .arg(
-            Arg::new("apiHost")
-                .long("apiHost")
-                .default_value("localhost")
-                .env("HOPRD_API_HOST")
-                .value_name("HOST")
-                .help("Set host IP to which the API server will bind"))
-        .arg(
-            Arg::new("apiPort")
-                .long("apiPort")
-                .default_value("3001")
-                .value_parser(clap::value_parser!(u16))
-                .env("HOPRD_API_PORT")
-                .help("Set host port to which the API server will bind."))
+
         .arg(
             Arg::new("environment")
                 .long("environment")
@@ -116,160 +329,31 @@ pub fn parse_cli_arguments(cli_args: Vec<&str>) -> Result<JsValue, JsValue> {
                 .value_name("ENVIRONMENT")
                 .help("Environment id which the node shall run on")
                 .value_parser(PossibleValuesParser::new(envs)))
-        .arg(
-            Arg::new("api")
-                .long("api")
-                .env("HOPRD_API")
-                .action(ArgAction::SetTrue)
-                .help("Expose the API on localhost:3001")
-                .default_value("false"))
-        .arg(Arg::new("apiToken")
-                .long("apiToken")
-                .env("HOPRD_API_TOKEN")
-                .help("A REST API token and for user authentication"))
-        .arg(Arg::new("healthCheck")
-                .long("healthCheck")
-                .env("HOPRD_HEALTH_CHECK")
-                .help("Run a health check end point on localhost:8080")
-                .action(ArgAction::SetTrue)
-            .default_value("false"))
-        .arg(Arg::new("healthCheckHost")
-                .long("healthCheckHost")
-                .env("HOPRD_HEALTH_CHECK_HOST")
-                .help("Updates the host for the healthcheck server")
-                .default_value("localhost"))
-        .arg(Arg::new("healthCheckPort")
-                .long("healthCheckPort")
-                .env("HOPRD_HEALTH_CHECK_PORT")
-                .help("Updates the port for the healthcheck server")
-                .value_parser(clap::value_parser!(u16))
-                .default_value("8080"))
-        .arg(Arg::new("password")
-                .long("password")
-                .help("A password to encrypt your keys")
-                .env("HOPRD_PASSWORD")
-                .default_value(""))
-        .arg(Arg::new("provider")
-                .long("provider")
-                .help("A custom RPC provider to be used for the node to connect to blockchain")
-                .env("HOPRD_PROVIDER"))
+        
         .arg(Arg::new("identity")
                 .long("identity")
                 .help("The path to the identity file")
                 .env("HOPRD_IDENTITY")
                 .default_value(DEFAULT_ID_PATH))
-        .arg(Arg::new("dryRun")
-                .long("dryRun")
-                .help("List all the options used to run the HOPR node, but quit instead of starting")
-                .env("HOPRD_DRY_RUN")
-                .default_value("false")
-                .action(ArgAction::SetTrue))
         .arg(Arg::new("data")
                 .long("data")
                 .help("manually specify the data directory to use")
                 .env("HOPRD_DATA")
-                .default_value(&default_data_path))
-        .arg(Arg::new("init")
-                .long("init")
-                .help("initialize a database if it doesn't already exist")
-                .action(ArgAction::SetTrue)
-                .env("HOPRD_INIT")
-                .default_value("false"))
-        .arg(Arg::new("privateKey")
-                .long("privateKey")
-                .hide(true)
-                .help("A private key to be used for the node")
-                .env("HOPRD_PRIVATE_KEY"))
-        .arg(Arg::new("allowLocalNodeConnections")
-                .long("allowLocalNodeConnections")
-                .env("HOPRD_ALLOW_LOCAL_NODE_CONNECTIONS")
-                .action(ArgAction::SetTrue)
-                .help("Allow connections to other nodes running on localhost")
-                .default_value("false"))
-        .arg(Arg::new("allowPrivateNodeConnections")
-                .long("allowPrivateNodeConnections")
-                .env("HOPRD_ALLOW_PRIVATE_NODE_CONNECTIONS")
-                .action(ArgAction::SetTrue)
-                .help("Allow connections to other nodes running on private addresses")
-                .default_value("false"))
-        .arg(Arg::new("testAnnounceLocalAddresses")
-                .long("testAnnounceLocalAddresses")
-                .env("HOPRD_TEST_ANNOUNCE_LOCAL_ADDRESSES")
-                .help("For testing local testnets. Announce local addresses")
-                .action(ArgAction::SetTrue)
-                .default_value("false"))
-                .hide(true)
-        .arg(Arg::new("testPreferLocalAddresses")
-                .long("testPreferLocalAddresses")
-                .env("HOPRD_TEST_PREFER_LOCAL_ADDRESSES")
-                .action(ArgAction::SetTrue)
-                .help("For testing local testnets. Prefer local peers to remote")
-                .default_value("false")
-                .hide(true))
-        .arg(Arg::new("testUseWeakCrypto")
-                .long("testUseWeakCrypto")
-                .env("HOPRD_TEST_USE_WEAK_CRYPTO")
-                .action(ArgAction::SetTrue)
-                .help("weaker crypto for faster node startup")
-                .default_value("false")
-                .hide(true))
-        .arg(Arg::new("testNoAuthentication")
-                .long("testNoAuthentication")
-                .help("no remote authentication for easier testing")
-                .action(ArgAction::SetTrue)
-                .env("HOPRD_TEST_NO_AUTHENTICATION")
-                .default_value("false")
-                .hide(true))
-        .arg(Arg::new("testNoDirectConnections")
-                .long("testNoDirectConnections")
-                .help("NAT traversal testing: prevent nodes from establishing direct TCP connections")
-                .env("HOPRD_TEST_NO_DIRECT_CONNECTIONS")
-                .default_value("false")
-                .action(ArgAction::SetTrue)
-                .hide(true))
-        .arg(Arg::new("testNoWebRTCUpgrade")
-                .long("testNoWebRTCUpgrade")
-                .help("NAT traversal testing: prevent nodes from establishing direct TCP connections")
-                .env("HOPRD_TEST_NO_WEB_RTC_UPGRADE")
-                .default_value("false")
-                .hide(true))
-        .arg(Arg::new("heartbeatInterval")
-                .long("heartbeatInterval")
-                .help("Interval in milliseconds in which the availability of other nodes get measured")
-                .env("HOPRD_HEARTBEAT_INTERVAL")
-                .default_value(HEARTBEAT_INTERVAL.to_string()))
-        .arg(Arg::new("heartbeatThreshold")
-                .long("heartbeatThreshold")
-                .help("Timeframe in milliseconds after which a heartbeat to another peer is performed, if it hasn't been seen since")
-                .env("HOPRD_HEARTBEAT_THRESHOLD")
-                .default_value(HEARTBEAT_THRESHOLD.to_string()))
-        .arg(Arg::new("heartbeatVariance")
-                .long("heartbeatVariance")
-                .help("Upper bound for variance applied to heartbeat interval in milliseconds")
-                .env("HOPRD_HEARTBEAT_VARIANCE")
-                .default_value(HEARTBEAT_INTERVAL_VARIANCE.to_string()))
-        .arg(Arg::new("networkQualityThreshold")
-                .long("networkQualityThreshold")
-                .help("Miniumum quality of a peer connection to be considered usable")
-                .env("HOPRD_NETWORK_QUALITY_THRESHOLD")
-                .default_value(NETWORK_QUALITY_THRESHOLD.to_string()))
-        .arg(Arg::new("onChainConfirmations")
-                .long("onChainConfirmations")
-                .help("Number of confirmations required for on-chain transactions")
-                .env("HOPRD_ON_CHAIN_CONFIRMATIONS")
-                .default_value(CONFIRMATIONS.to_string()));
+                .default_value(&default_data_path));
+
+        
 
     // CliArgs::from_arg_matches(&cmd.try_get_matches_from(cli_args).unwrap());
 
-    let args = match cmd.try_get_matches_from(cli_args) {
-        Ok(matches) => CliArgs::from(matches),
-        Err(e) => return Err(JsValue::from(e.to_string())),
-    };
+    // let args = match cmd.try_get_matches_from(cli_args) {
+    //     Ok(matches) => CliArgs::from(matches),
+    //     Err(e) => return Err(JsValue::from(e.to_string())),
+    // };
 
-    match serde_wasm_bindgen::to_value(&args) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(JsValue::from(e.to_string())),
-    }
+    // match serde_wasm_bindgen::to_value(&args) {
+    //     Ok(s) => Ok(s),
+    //     Err(e) => Err(JsValue::from(e.to_string())),
+    // }
     // Args::try_update_from(
     //     ,
     //     cli_args,
@@ -284,6 +368,7 @@ pub fn parse_cli_arguments(cli_args: Vec<&str>) -> Result<JsValue, JsValue> {
     // .map_err(|e| JsValue::from(e))
 
     // serde_json::from_slice(&data)
+    Ok(JsValue::from("foo"))
 }
 
 pub mod wasm {
