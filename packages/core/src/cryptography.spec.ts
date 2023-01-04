18981876ed805305ffe68fbf23d572347d703447
Lukas <lukas.pohanka@inina.net>
2023-01-04 10:39:39 +0100

    Additional PRP tests

--- packages/core/src/cryptography.spec.ts
+++ packages/core/src/cryptography.spec.ts
@@ -1,4 +1,4 @@
-import { PRG as Rust_PRG } from './cryptography.js'
+import { PRG as Rust_PRG, PRP as Rust_PRP } from './cryptography.js'
 import { PRG as TS_PRG, PRP as TS_PRP } from '@hoprnet/hopr-utils'
 import assert from 'assert'
 
@@ -35,9 +35,32 @@ describe('cryptographic correspondence tests', async function () {
   it('PRP correspondence', async function () {
     let key = new Uint8Array(16)
     let iv = new Uint8Array(12)
-    let pt = new Uint8Array(400)
 
     let ts_prp = TS_PRP.createPRP({key, iv})
-    ts_prp.permutate(pt)
+    let rs_prp = new Rust_PRP(key, iv)
+
+    {
+      let pt = new Uint8Array(400)
+      let ct_1 = rs_prp.forward(pt)
+      let ct_2 = ts_prp.permutate(pt)
+
+      assert.deepEqual(ct_1, ct_2)
+    }
+
+    {
+      let pt_1 = new Uint8Array(400)
+      let ct = ts_prp.permutate(pt_1)
+      let pt_2 = rs_prp.inverse(ct)
+
+      assert.deepEqual(pt_1, pt_2)
+    }
+
+    {
+      let pt_1 = new Uint8Array(400)
+      let ct = rs_prp.forward(pt_1)
+      let pt_2 = ts_prp.inverse(ct)
+
+      assert.deepEqual(pt_1, pt_2)
+    }
   })
 })
