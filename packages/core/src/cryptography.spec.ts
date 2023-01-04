13f671b477364cb2de5e67fe632e43508512a40e
Lukas <lukas.pohanka@inina.net>
2023-01-04 09:21:18 +0100

    WIP on PRP testing

--- packages/core/src/cryptography.spec.ts
+++ packages/core/src/cryptography.spec.ts
@@ -1,5 +1,5 @@
 import { PRG as Rust_PRG } from './cryptography.js'
-import { PRG as TS_PRG } from '@hoprnet/hopr-utils'
+import { PRG as TS_PRG, PRP as TS_PRP } from '@hoprnet/hopr-utils'
 import assert from 'assert'
 
 describe('cryptographic correspondence tests', async function () {
@@ -30,5 +30,14 @@ describe('cryptographic correspondence tests', async function () {
       let ts_output = TS_PRG.createPRG({ key, iv }).digest(16, 22)
       assert.deepEqual(rs_output, ts_output)
     }
+  })
+
+  it('PRP correspondence', async function () {
+    let key = new Uint8Array(16)
+    let iv = new Uint8Array(12)
+    let pt = new Uint8Array(400)
+
+    let ts_prp = TS_PRP.createPRP({key, iv})
+    ts_prp.permutate(pt)
   })
 })
