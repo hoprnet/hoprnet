fd4e8f572008cf17f7440468b64001285a2279a5
Robert Kiel <robert.kiel@hoprnet.org>
2023-01-05 17:56:41 +0100

    enhance environments

--- packages/core/crates/core-misc/src/environment.rs
+++ packages/core/crates/core-misc/src/environment.rs
@@ -11,10 +11,10 @@ pub struct SemverResult {
 #[wasm_bindgen(module = "semver")]
 extern "C" {
     // Reads the given file and returns it as array of bytes.
-    #[wasm_bindgen(catch)]
+    #[wasm_bindgen(catch, js_namespace = semver)]
     pub fn coerce(version: String) -> Result<SemverResult, JsValue>;
 
-    #[wasm_bindgen(catch)]
+    #[wasm_bindgen(catch, js_namespace = semver)]
     pub fn satisfies(version: String, range: String) -> Result<bool, JsValue>;
 }
 
@@ -36,7 +36,7 @@ impl ToString for EnvironmentType {
     }
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct NetworkOptions {
     id: String,
     description: String,
@@ -58,6 +58,7 @@ pub struct NetworkOptions {
 
 #[derive(Deserialize, Serialize, Clone)]
 pub struct Environment {
+    #[serde(skip)]
     id: String,
     /// must match one of the Network.id
     network_id: String,
@@ -80,14 +81,39 @@ pub struct Environment {
     network_registry_contract_address: String,
 }
 
+#[derive(Serialize, Clone)]
+pub struct ResolvedEnvironment {
+    /// the environment identifier, e.g. monte_rosa
+    id: String,
+    network: NetworkOptions,
+    environment_type: EnvironmentType,
+    /// an Ethereum address
+    channels_contract_address: String,
+    channel_contract_deploy_block: u32,
+    /// an Ethereum address
+    token_contract_address: String,
+    /// an Ethereum address
+    xhopr_contract_address: String,
+    /// an Ethereum address
+    boost_contract_address: String,
+    /// an Ethereum address
+    stake_contract_address: String,
+    /// an Ethereum address
+    network_registry_proxy_contract_address: String,
+    /// an Ethereum address
+    network_registry_contract_address: String,
+}
+
 #[derive(Deserialize)]
 pub struct ProtocolConfig {
     environments: std::collections::HashMap<String, Environment>,
     networks: std::collections::HashMap<String, NetworkOptions>,
 }
 
-pub fn get_protocol_config(path: &str) -> Result<ProtocolConfig, JsValue> {
-    let data = real::read_file(&path)?;
+pub fn get_protocol_config(mono_repo_path: &str) -> Result<ProtocolConfig, JsValue> {
+    let protocol_config_path = format!("{}/packages/core/protocol-config.json", mono_repo_path);
+
+    let data = real::read_file(protocol_config_path.as_str())?;
 
     serde_json::from_slice::<ProtocolConfig>(&data).map_err(|e| JsValue::from(e.to_string()))
 }
@@ -97,32 +123,85 @@ struct PackageJsonFile {
     version: String,
 }
 
-fn get_package_version(path: &str) -> Result<String, JsValue> {
-    let data = real::read_file(&path)?;
+fn get_package_version(mono_repo_path: &str) -> Result<String, JsValue> {
+    let package_json_path = format!("{}/packages/hoprd/package.json", mono_repo_path);
+
+    let data = real::read_file(package_json_path.as_str())?;
 
     match serde_json::from_slice::<PackageJsonFile>(&data) {
         Ok(json) => Ok(coerce(json.version).unwrap().version),
         Err(e) => Err(JsValue::from(e.to_string())),
     }
 }
-pub fn supported_environments(
-    package_json_path: &str,
-    protocol_config_path: &str,
-) -> Result<Vec<Environment>, JsValue> {
-    let protocol_config = get_protocol_config(&protocol_config_path)?;
-    let version = get_package_version(package_json_path)?;
+
+/// Returns a list of environments which the node is able to work with
+pub fn supported_environments(mono_repo_path: &str) -> Result<Vec<Environment>, JsValue> {
+    let mut protocol_config_path = get_protocol_config(&mono_repo_path)?;
+    let version = get_package_version(mono_repo_path)?;
 
     let mut allowed: Vec<Environment> = vec![];
 
-    for (_id, env) in protocol_config.environments.iter() {
+    for (id, env) in protocol_config_path.environments.iter_mut() {
         let range = env.version_range.to_owned();
 
         if satisfies(version.to_owned(), range).unwrap() {
+            env.id = id.to_owned();
             allowed.push(env.to_owned())
         }
     }
 
     Ok(allowed)
+}
+/// Returns the environment details, returns an error if environment is not supported
+pub fn resolve_environment(
+    mono_repo_path: &str,
+    environment_id: &str,
+    maybe_custom_provider: Option<&str>,
+) -> Result<ResolvedEnvironment, JsValue> {
+    let mut protocol_config = get_protocol_config(mono_repo_path)?;
+    let version = get_package_version(mono_repo_path)?;
+
+    let environment = protocol_config
+        .environments
+        .get_mut(environment_id)
+        .ok_or(JsValue::from(format!(
+            "Could not find environment {} in protocol config",
+            environment_id
+        )))?;
+
+    let network = protocol_config
+        .networks
+        .get_mut(&environment.network_id)
+        .ok_or(JsValue::from(format!(
+            "Invalid network_id {} for environment {}",
+            environment.network_id, environment_id
+        )))?;
+
+    if let Some(custom_provider) = maybe_custom_provider {
+        network.default_provider = String::from(custom_provider);
+    }
+
+    match satisfies(version, environment.version_range.to_owned()) {
+        Ok(true) => Ok(ResolvedEnvironment {
+            id: String::from(environment_id),
+            network: network.to_owned(),
+            environment_type: environment.environment_type,
+            channel_contract_deploy_block: environment.channel_contract_deploy_block,
+            token_contract_address: environment.token_contract_address.to_owned(),
+            channels_contract_address: environment.channels_contract_address.to_owned(),
+            xhopr_contract_address: environment.xhopr_contract_address.to_owned(),
+            boost_contract_address: environment.boost_contract_address.to_owned(),
+            stake_contract_address: environment.stake_contract_address.to_owned(),
+            network_registry_contract_address: environment
+                .network_registry_contract_address
+                .to_owned(),
+            network_registry_proxy_contract_address: environment
+                .network_registry_proxy_contract_address
+                .to_owned(),
+        }),
+        Ok(false) => Err(JsValue::from("Environment does not satisfy version range")),
+        Err(e) => Err(e),
+    }
 }
 
 pub mod wasm {
@@ -130,33 +209,25 @@ pub mod wasm {
     use wasm_bindgen::JsValue;
 
     #[wasm_bindgen]
-    pub fn supported_environments(
-        package_json_path: &str,
-        protocol_config_path: &str,
-    ) -> Result<JsValue, JsValue> {
-        let supported_envs =
-            super::supported_environments(package_json_path, protocol_config_path)?;
+    pub fn supported_environments(mono_repo_path: &str) -> Result<JsValue, JsValue> {
+        let supported_envs = super::supported_environments(mono_repo_path)?;
 
-        match serde_wasm_bindgen::to_value(&supported_envs) {
-            Ok(string) => Ok(string),
-            Err(e) => Err(JsValue::from(e.to_string())),
-        }
+        serde_wasm_bindgen::to_value(&supported_envs).map_err(|e| JsValue::from(e.to_string()))
+    }
+
+    #[wasm_bindgen]
+    pub fn resolve_environment(
+        mono_repo_path: &str,
+        environment_id: &str,
+        maybe_custom_provider: Option<String>,
+    ) -> Result<JsValue, JsValue> {
+        let resolved_environment = super::resolve_environment(
+            mono_repo_path,
+            environment_id,
+            maybe_custom_provider.as_ref().map(|c| c.as_str()),
+        )?;
+
+        serde_wasm_bindgen::to_value(&resolved_environment)
+            .map_err(|e| JsValue::from(e.to_string()))
     }
 }
-
-// /**
-//  * @param version HOPR version
-//  * @returns environments that the given HOPR version should be able to use
-//  */
-// export function supportedEnvironments(): Environment[] {
-//     const environments = Object.entries((protocolConfig as ProtocolConfig).environments)
-
-//     return environments
-//       .filter(([_, env]) => {
-//         return semver.satisfies(FULL_VERSION_COERCED, env.version_range)
-//       })
-//       .map(([id, env]) => ({
-//         id,
-//         ...env
-//       }))
-//   }
