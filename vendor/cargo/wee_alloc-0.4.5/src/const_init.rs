fd97d8356f49e3ed87ec545a8eb248ed57ab70ce
Robert Kiel <robert.kiel@hoprnet.org>
2023-01-10 08:33:01 +0100

    update cargo

--- vendor/cargo/wee_alloc-0.4.5/src/const_init.rs
+++
@@ -1,9 +0,0 @@
-/// Anything that can be initialized with a `const` value.
-pub(crate) trait ConstInit {
-    /// The `const` default initializer value for `Self`.
-    const INIT: Self;
-}
-
-impl<T> ConstInit for *const T {
-    const INIT: Self = 0 as *mut _;
-}
