5d569b9e6c998996a13c607416d4b7a762abf6c1
NumberFour8 <lukas.pohanka@inina.net>
2023-01-12 22:16:03 +0100

    Smaller improvements, ready for UTs

--- packages/core/crates/core-strategy/src/promiscuous.rs
+++ packages/core/crates/core-strategy/src/promiscuous.rs
@@ -3,9 +3,21 @@ use utils_types::primitives::Balance;
 
 use crate::generic::{ChannelOpenRequest, ChannelStrategy, StrategyTickResult};
 
-pub struct PromiscuousStrategy ;
+pub struct PromiscuousStrategy {
+    network_quality_threshold: f64,
+    minimum_channel_stake: Balance,
+    minimum_node_balance: Balance
+}
 
-pub const NETWORK_QUALITY_THRESHOLD: f64 = 0.1f64;
+impl Default for PromiscuousStrategy {
+    fn default() -> Self {
+        PromiscuousStrategy {
+            network_quality_threshold: 0.5,
+            minimum_channel_stake: Balance::from_str("100000000000000000", "txHOPR").unwrap(),
+            minimum_node_balance: Balance::from_str("100000000000000000", "txHOPR").unwrap()
+        }
+    }
+}
 
 impl ChannelStrategy for PromiscuousStrategy {
     fn name(&self) -> &str {
@@ -14,10 +26,6 @@ impl ChannelStrategy for PromiscuousStrategy {
 
     fn tick<Q>(&self, balance: Balance, network_size: u32, outgoing_channel_peer_ids: &[&str], quality_of: Q, peer_ids: &[&str]) -> StrategyTickResult
         where Q: Fn(&str) -> Option<f64> {
-
-        let min_stake   = Balance::from_str("100000000000000000", "txHOPR").unwrap(); // 0.1 txHOPR
-        let min_balance = Balance::from_str("100000000000000000", "txHOPR").unwrap();
-
         // We compute the upper bound for channels as a square-root of the perceived network size
         let max_channels = (network_size as f64).sqrt().ceil() as usize;
 
@@ -31,37 +39,40 @@ impl ChannelStrategy for PromiscuousStrategy {
         // the order of those who have the same quality.
         all_peers_qualities.sort_unstable_by(|&(_, q1), &(_, q2)| q1.partial_cmp(&q2).unwrap());
 
-        // Find all outgoing channels which dropped below the quality threshold
-        // those we will be closing.
+        // Find all outgoing channels which dropped below the quality threshold - we're going to close those.
         let to_close: Vec<String> = all_peers_qualities
             .iter()
-            .filter(|(p, q)| *q < NETWORK_QUALITY_THRESHOLD && outgoing_channel_peer_ids.contains(p))
+            .filter(|(p, q)| *q <= self.network_quality_threshold && outgoing_channel_peer_ids.contains(p))
             .copied()
             .map(|(peer_id,_)| peer_id.to_string())
             .collect();
 
         // Maximum number of channels we can open
         let max_to_open = max_channels - outgoing_channel_peer_ids.len() + to_close.len();
-        let mut remaining_balance = balance.clone();
 
+        // Find potential new candidates for opening channels to them
         let new_channel_candidates = all_peers_qualities
             .into_iter()
-            .filter(|(p, q)| *q > NETWORK_QUALITY_THRESHOLD && !outgoing_channel_peer_ids.contains(p))
+            .filter(|(p, q)| *q > self.network_quality_threshold && !outgoing_channel_peer_ids.contains(p))
             .map(|(peer_id,_)| peer_id)
             .take(max_to_open);
 
         let mut to_open: Vec<ChannelOpenRequest> = vec![];
+        let mut remaining_balance = balance.clone();
+
         for peer_id in new_channel_candidates  {
-            if remaining_balance.lte(&min_balance) {
+            // Stop if we ran out of balance
+            if remaining_balance.lte(&self.minimum_node_balance) {
                 break;
             }
 
+            // If we haven't added this peer id yet, add it to the list for channel opening
             if to_open.iter().find(|&p| p.peer_id.eq(peer_id.into())).is_none() {
                 to_open.push(ChannelOpenRequest{
                     peer_id: peer_id.to_string(),
-                    stake: min_stake.clone()
+                    stake: self.minimum_channel_stake.clone()
                 });
-                remaining_balance = balance.sub(&min_stake);
+                remaining_balance = balance.sub(&self.minimum_channel_stake);
             }
         }
 
@@ -98,6 +109,23 @@ pub mod wasm {
 
     #[wasm_bindgen]
     impl PromiscuousStrategy {
+
+        #[wasm_bindgen(constructor)]
+        pub fn new(network_quality_threshold: f64, minimum_node_balance: Balance, minimum_channel_stake: Balance) -> Self {
+            PromiscuousStrategy {
+                w: super::PromiscuousStrategy {
+                    network_quality_threshold,
+                    minimum_node_balance: minimum_node_balance.w,
+                    minimum_channel_stake: minimum_channel_stake.w
+                }
+            }
+        }
+
+        pub fn default() -> Self {
+            PromiscuousStrategy {
+                w: super::PromiscuousStrategy::default()
+            }
+        }
 
         #[wasm_bindgen(getter)]
         pub fn name(&self) -> String {
