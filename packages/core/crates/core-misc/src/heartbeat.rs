d2f71559a0267cb227697fc801e5b901923161f2
Robert Kiel <robert.kiel@hoprnet.org>
2023-01-26 10:46:08 +0100

    incorporate feedback

--- packages/core/crates/core-misc/src/heartbeat.rs
+++ packages/core/crates/core-misc/src/heartbeat.rs
@@ -1,26 +1,27 @@
 use blake2::{Blake2s256, Digest};
 
 /// Takes a ping request message and returns a ping response message
-pub fn generate_ping_response(req: Vec<u8>) -> Vec<u8> {
+pub fn generate_ping_response(req: Box<[u8]>) -> Box<[u8]> {
     let mut hasher = Blake2s256::new();
 
-    hasher.update(req.as_slice());
+    hasher.update(req);
 
-    hasher.finalize().to_vec()
+    Box::from_iter(hasher.finalize().to_vec())
 }
 
+#[cfg(feature = "wasm")]
 pub mod wasm {
     // use super::reply_to_ping;
     use futures::stream::{Stream, StreamExt};
-    use js_sys::{AsyncIterator, Uint8Array};
-    use utils_misc::async_iterable::wasm::{to_jsvalue_stream, to_vec_u8_stream};
+    use js_sys::AsyncIterator;
+    use utils_misc::async_iterable::wasm::{to_box_u8_stream, to_jsvalue_stream};
     use wasm_bindgen::prelude::*;
     use wasm_bindgen_futures::stream::JsStream;
 
     // Copied because wasm_bindgen does not support lifetimes yet
     #[wasm_bindgen]
     pub struct AsyncIterableHelperCoreHeartbeat {
-        stream: Box<dyn Stream<Item = Result<Vec<u8>, String>> + Unpin>,
+        stream: Box<dyn Stream<Item = Result<Box<[u8]>, String>> + Unpin>,
     }
 
     #[wasm_bindgen]
@@ -35,7 +36,7 @@ pub mod wasm {
         stream: AsyncIterator,
     ) -> Result<AsyncIterableHelperCoreHeartbeat, JsValue> {
         let stream = JsStream::from(stream)
-            .map(to_vec_u8_stream)
+            .map(to_box_u8_stream)
             .map(|req| req.map(super::generate_ping_response));
 
         Ok(AsyncIterableHelperCoreHeartbeat {
@@ -45,12 +46,7 @@ pub mod wasm {
 
     /// Used to pre-compute ping responses
     #[wasm_bindgen]
-    pub fn generate_ping_response(u8a: Uint8Array) -> Uint8Array {
-        let ping_reponse = super::generate_ping_response(u8a.to_vec());
-
-        let res = Uint8Array::new_with_length(32);
-        res.copy_from(ping_reponse.as_slice());
-
-        res
+    pub fn generate_ping_response(u8a: &[u8]) -> Box<[u8]> {
+        super::generate_ping_response(u8a.into())
     }
 }
--- packages/utils/crates/utils-misc/src/async_iterable.rs
+++ packages/utils/crates/utils-misc/src/async_iterable.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "wasm")]
 pub mod wasm {
     use futures::stream::{Stream, StreamExt};
     use js_sys::Uint8Array;
@@ -10,24 +11,24 @@ pub mod wasm {
     pub struct IteratorResult {
         done: bool,
         #[serde(with = "serde_bytes")]
-        value: Option<Vec<u8>>,
+        value: Option<Box<[u8]>>,
     }
 
-    /// Turns a JsValue stream into a Vec<u8> stream
+    /// Turns a JsValue stream into a Box<[u8]> stream
     ///
     /// ```
     /// use futures::stream::{Stream,StreamExt};
     /// use js_sys::Uint8Array;
-    /// use utils_misc::async_iterable::wasm::to_vec_u8_stream;
+    /// use utils_misc::async_iterable::wasm::to_box_u8_stream;
     /// use wasm_bindgen::prelude::*;
     ///
-    /// pub async fn take_stream(stream: impl Stream<Item = Result<JsValue, JsValue>>) -> Vec<Result<Vec<u8>,String>> {
-    ///   stream.map(to_vec_u8_stream).collect::<Vec<Result<Vec<u8>,String>>>().await
+    /// pub async fn take_stream(stream: impl Stream<Item = Result<JsValue,JsValue>>) -> Vec<Result<Box<[u8]>,String>> {
+    ///   stream.map(to_box_u8_stream).collect::<Vec<Result<Box<[u8]>,String>>>().await
     /// }
     /// ```
-    pub fn to_vec_u8_stream(item: Result<JsValue, JsValue>) -> Result<Vec<u8>, String> {
+    pub fn to_box_u8_stream(item: Result<JsValue, JsValue>) -> Result<Box<[u8]>, String> {
         match item {
-            Ok(x) => Ok(Uint8Array::new(&x).to_vec()),
+            Ok(x) => Ok(Box::from_iter(Uint8Array::new(&x).to_vec())),
             Err(e) => Err(format!("{:?}", e)),
         }
     }
@@ -39,11 +40,11 @@ pub mod wasm {
     /// use utils_misc::async_iterable::wasm::to_jsvalue_stream;
     /// use wasm_bindgen::prelude::*;
     ///
-    /// pub async fn take_stream(mut stream: impl Stream<Item = Result<Vec<u8>,String>> + Unpin) -> Result<JsValue, JsValue> {
+    /// pub async fn take_stream(mut stream: impl Stream<Item = Result<Box<[u8]>,String>> + Unpin) -> Result<JsValue, JsValue> {
     ///    to_jsvalue_stream(stream.next().await)
     /// }
     /// ```
-    pub fn to_jsvalue_stream(item: Option<Result<Vec<u8>, String>>) -> Result<JsValue, JsValue> {
+    pub fn to_jsvalue_stream(item: Option<Result<Box<[u8]>, String>>) -> Result<JsValue, JsValue> {
         match item {
             Some(Ok(m)) => Ok(serde_wasm_bindgen::to_value(&IteratorResult {
                 done: false,
@@ -66,17 +67,17 @@ pub mod wasm {
     /// use wasm_bindgen::prelude::*;
     /// use wasm_bindgen_futures::stream::JsStream;
     /// use js_sys::AsyncIterator;
-    /// use utils_misc::async_iterable::wasm::to_vec_u8_stream;
+    /// use utils_misc::async_iterable::wasm::to_box_u8_stream;
     ///
     /// #[wasm_bindgen]
     /// pub struct AsyncIterableHelper {
-    ///    stream: Box<dyn Stream<Item = Result<Vec<u8>, String>> + Unpin>, // must not be pub
+    ///    stream: Box<dyn Stream<Item = Result<Box<[u8]>, String>> + Unpin>, // must not be pub
     /// }
     /// #[wasm_bindgen]
     /// pub fn async_test(some_async_iterable: AsyncIterator) -> Result<AsyncIterableHelper, JsValue> {
     ///     let stream = JsStream::from(some_async_iterable);
     ///
-    ///     let stream = stream.map(to_vec_u8_stream);
+    ///     let stream = stream.map(to_box_u8_stream);
     ///
     ///     Ok(AsyncIterableHelper {
     ///         stream: Box::new(stream),
@@ -85,7 +86,7 @@ pub mod wasm {
     /// ```
     #[wasm_bindgen]
     pub struct AsyncIterableHelper {
-        stream: Box<dyn Stream<Item = Result<Vec<u8>, String>> + Unpin>,
+        stream: Box<dyn Stream<Item = Result<Box<[u8]>, String>> + Unpin>,
     }
 
     #[wasm_bindgen]
