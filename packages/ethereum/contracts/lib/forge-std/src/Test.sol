31aab5ca803a785e5a31e0acf0932a168bae09bc
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-02-02 09:38:57 +0100

    bump forge-std version to 1.2.0

--- packages/ethereum/contracts/lib/forge-std/src/Test.sol
+++ packages/ethereum/contracts/lib/forge-std/src/Test.sol
@@ -1,11 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.9.0;
 
-import {CommonBase} from "./Common.sol";
-import "ds-test/test.sol";
-// forgefmt: disable-next-line
-import {console, console2, StdAssertions, StdCheats, stdError, stdJson, stdMath, StdStorage, stdStorage, StdUtils, Vm} from "./Components.sol";
+// 💬 ABOUT
+// Standard Library's default Test
 
-abstract contract TestBase is CommonBase {}
+// 🧩 MODULES
+import {console} from "./console.sol";
+import {console2} from "./console2.sol";
+import {StdAssertions} from "./StdAssertions.sol";
+import {StdChains} from "./StdChains.sol";
+import {StdCheats} from "./StdCheats.sol";
+import {stdError} from "./StdError.sol";
+import {stdJson} from "./StdJson.sol";
+import {stdMath} from "./StdMath.sol";
+import {StdStorage, stdStorage} from "./StdStorage.sol";
+import {StdUtils} from "./StdUtils.sol";
+import {Vm} from "./Vm.sol";
 
-abstract contract Test is TestBase, DSTest, StdAssertions, StdCheats, StdUtils {}
+// 📦 BOILERPLATE
+import {TestBase} from "./Base.sol";
+import {DSTest} from "ds-test/test.sol";
+
+// ⭐️ TEST
+abstract contract Test is DSTest, StdAssertions, StdChains, StdCheats, StdUtils, TestBase {
+// Note: IS_TEST() must return true.
+// Note: Must have failure system, https://github.com/dapphub/ds-test/blob/cd98eff28324bfac652e63a239a60632a761790b/src/test.sol#L39-L76.
+}
