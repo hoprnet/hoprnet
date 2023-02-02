61c41f6a1eb7e3f3ad8f99bd371b30f8c48ca093
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-02-02 09:38:57 +0100

    bump forge-std version to 1.2.0

--- packages/ethereum/contracts/lib/forge-std/src/Script.sol
+++ packages/ethereum/contracts/lib/forge-std/src/Script.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.9.0;
 
-import {CommonBase} from "./Common.sol";
-// forgefmt: disable-next-line
-import {console, console2, StdCheatsSafe, stdJson, stdMath, StdStorage, stdStorageSafe, StdUtils, VmSafe} from "./Components.sol";
+// 💬 ABOUT
+// Standard Library's default Script.
 
-abstract contract ScriptBase is CommonBase {
-    VmSafe internal constant vmSafe = VmSafe(VM_ADDRESS);
-}
+// 🧩 MODULES
+import {ScriptBase} from "./Base.sol";
+import {console} from "./console.sol";
+import {console2} from "./console2.sol";
+import {StdChains} from "./StdChains.sol";
+import {StdCheatsSafe} from "./StdCheats.sol";
+import {stdJson} from "./StdJson.sol";
+import {stdMath} from "./StdMath.sol";
+import {StdStorage, stdStorageSafe} from "./StdStorage.sol";
+import {StdUtils} from "./StdUtils.sol";
+import {VmSafe} from "./Vm.sol";
 
-abstract contract Script is ScriptBase, StdCheatsSafe, StdUtils {
+// 📦 BOILERPLATE
+import {ScriptBase} from "./Base.sol";
+
+// ⭐️ SCRIPT
+abstract contract Script is StdChains, StdCheatsSafe, StdUtils, ScriptBase {
+    // Note: IS_SCRIPT() must return true.
     bool public IS_SCRIPT = true;
 }
