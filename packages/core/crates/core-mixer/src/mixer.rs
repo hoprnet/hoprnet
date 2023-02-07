9b3033a0a934d57553643cbc45bcfbedaaa060b6
Tibor Csóka <csokat@gmail.com>
2023-02-07 22:47:56 +0100

    Extend behavior to support the specified scenario

--- packages/core/crates/core-mixer/src/mixer.rs
+++ packages/core/crates/core-mixer/src/mixer.rs
@@ -4,20 +4,13 @@ use priority_queue::PriorityQueue;
 use rand::Rng;
 
 
-/// Mixer
-/// API definition
-/// - gets a random int to set a threshold for packet release
-/// - uses a heap to establish an internal data structure
-///
-/// - push(packet) - push a packet into the mixer queue
-/// - next() // create an iterable that returns a packet promise
-/// - end() // clear mixer timeouts
 
-/// mixer value comparator function
 
 type Packet = u8;      // [u8; MAX_PACKET_SIZE];
 
 const MIXER_BUFFER_CAPACITY: usize = 1024;
+const DELAY_MIN_VALUE: u16 = 50;    /// minimum delay in milliseconds
+const DELAY_MAX_VALUE: u16 = 1000;  /// maximum delay in milliseconds
 
 #[derive(Eq, PartialEq, Debug, Copy, Clone)]
 #[cfg_attr(feature = "wasm", wasm_bindgen::prelude::wasm_bindgen)]
@@ -46,18 +39,34 @@ impl PartialOrd for MixerTimestamp {
 }
 
 
+fn random_value_from_range(min: f32, max: f32) -> f32
+{
+    let random = rand::random::<f32>();
+    ((max - min) * random) + min
+}
 
+
+/// Mixer
+/// API definition
+/// - gets a random int to set a threshold for packet release
+/// - uses a heap to establish an internal data structure
+///
+/// - push(packet) - push a packet into the mixer queue
+/// - next() // create an iterable that returns a packet promise
+/// - end() // clear mixer timeouts
+///
+/// mixer value comparator function
 #[cfg_attr(feature = "wasm", wasm_bindgen::prelude::wasm_bindgen)]
-pub struct Mixer {
+pub struct StochasticUniformMixer {
    queue: PriorityQueue<Packet, MixerTimestamp>,
    min_packet_count: usize,
 }
 
 
 #[cfg_attr(feature = "wasm", wasm_bindgen::prelude::wasm_bindgen)]
-impl Mixer {
-    pub fn new(packet_release_threshold: usize) -> Mixer  {
-        Mixer {
+impl StochasticUniformMixer {
+    pub fn new(packet_release_threshold: usize) -> StochasticUniformMixer {
+        StochasticUniformMixer {
             queue: PriorityQueue::with_capacity_and_default_hasher(MIXER_BUFFER_CAPACITY),
             min_packet_count: packet_release_threshold
         }
@@ -65,7 +74,7 @@ impl Mixer {
 }
 
 
-impl Mixer {
+impl StochasticUniformMixer {
     pub fn pop(&mut self) -> Option<Packet> {
         if self.queue.len() > self.min_packet_count  {
             if let Some(top) = self.queue.peek() {
@@ -77,15 +86,39 @@ impl Mixer {
         None
     }
 
-    pub fn push(&mut self, packet: Packet) {
-        let random_priority = 5;
-        if let Some(_) = self.queue.push(packet, MixerTimestamp{value: random_priority}) {
+    /// Ingests a single packet along with the current timestamp
+    ///
+    /// # Arguments
+    ///
+    /// * `packet` - A string slice that holds the name of the person
+    /// * `timestamp` - The current timestamp (note: necessary due to WASM interface)
+    pub fn push(&mut self, packet: Packet, timestamp: u32) {    //
+        let random_delay = random_value_from_range(DELAY_MIN_VALUE as f32, DELAY_MAX_VALUE as f32) as u32 ;
+        if let Some(_) = self.queue.push(packet, MixerTimestamp{value: timestamp + random_delay}) {
             panic!("Duplicate packet detected")
         }
     }
 
     pub fn length(&self) -> usize {
         self.queue.len()
+    }
+}
+
+/// Module for WASM-specific Rust code
+#[cfg(feature = "wasm")]
+pub mod wasm {
+    // TODO: add JS API here
+    // Use this module to specify everything that is WASM-specific (e.g. uses wasm-bindgen types, js_sys, ...etc.)
+
+    use super::*;
+    use wasm_bindgen::prelude::*;
+    // use wasm_bindgen::JsValue;
+
+    #[wasm_bindgen]
+    impl StochasticUniformMixer {
+        pub fn push_data(&mut self, packet: Packet) {
+            self.push(packet, 0);   // TODO: add javascript time
+        }
     }
 }
 
@@ -106,10 +139,11 @@ mod tests {
 
     #[test]
     fn test_mixer_timestamp_derives_reverse_instead_of_direct_ordering() {
-        let left = MixerTimestamp{ value: 1 };
-        let right = MixerTimestamp{ value: 2 };
+        let smaller = MixerTimestamp{ value: 1 };
+        let greater = MixerTimestamp{ value: 2 };
 
-        assert!(left > right)
+        assert!(smaller > greater);
+        assert!(greater < smaller);
     }
 
     fn random_packet() -> Packet {
@@ -121,16 +155,16 @@ mod tests {
 
     #[test]
     fn test_mixer_without_any_packets_should_declare_none() {
-        let mixer = Mixer::new(NO_PACKET_THRESHOLD);
+        let mixer = StochasticUniformMixer::new(NO_PACKET_THRESHOLD);
 
         assert_eq!(0, mixer.length())
     }
 
     #[test]
     fn test_mixer_should_register_packets() {
-        let mut mixer = Mixer::new(NO_PACKET_THRESHOLD);
+        let mut mixer = StochasticUniformMixer::new(NO_PACKET_THRESHOLD);
 
-        mixer.push(random_packet());
+        mixer.push(random_packet(), 0);
         assert_eq!(1, mixer.length());
     }
 
@@ -139,16 +173,16 @@ mod tests {
     fn test_mixer_should_panic_on_adding_the_same_packet() {
         std::panic::set_hook(Box::new(|_| {}));
 
-        let mut mixer = Mixer::new(NO_PACKET_THRESHOLD);
+        let mut mixer = StochasticUniformMixer::new(NO_PACKET_THRESHOLD);
 
         let random_generated_packet = random_packet();
-        mixer.push(random_generated_packet);
-        mixer.push(random_generated_packet);
+        mixer.push(random_generated_packet, 0);
+        mixer.push(random_generated_packet, 1);
     }
 
     #[test]
     fn test_mixer_should_return_no_value_if_it_is_empty() {
-        let mut mixer = Mixer::new(NO_PACKET_THRESHOLD);
+        let mut mixer = StochasticUniformMixer::new(NO_PACKET_THRESHOLD);
 
         let actual = mixer.pop();
         assert!(actual.is_none())
@@ -157,9 +191,9 @@ mod tests {
     #[test]
     fn test_mixer_should_return_no_value_unless_it_contains_at_least_the_threshold_amount_of_values() {
         let packet_threshold = 1;
-        let mut mixer = Mixer::new(packet_threshold);
+        let mut mixer = StochasticUniformMixer::new(packet_threshold);
 
-        mixer.push(random_packet());
+        mixer.push(random_packet(), 0);
         let actual = mixer.pop();
 
         assert!(actual.is_none())
@@ -167,12 +201,29 @@ mod tests {
 
     #[test]
     fn test_mixer_should_return_value_if_more_than_threshold_values_are_present() {
-        let mut mixer = Mixer::new(NO_PACKET_THRESHOLD);
+        let mut mixer = StochasticUniformMixer::new(NO_PACKET_THRESHOLD);
 
-        mixer.push(random_packet());
+        mixer.push(random_packet(), 0);
         let actual = mixer.pop();
 
         assert!(actual.is_some())
+    }
+
+    #[test]
+    fn test_mixer_should_the_value_with_an_earlier_timestamp_first() {
+        let at_least_two_packets = 1;
+        let mut mixer = StochasticUniformMixer::new(at_least_two_packets);
+
+        let (packet_with_earlier_timestamp, earlier_timestamp) = (random_packet(), DELAY_MIN_VALUE as u32);
+        let (packet_with_latter_timestamp, latter_timestamp) = (random_packet(), (DELAY_MAX_VALUE * 2) as u32);
+
+        mixer.push(packet_with_latter_timestamp, latter_timestamp);
+        mixer.push(packet_with_earlier_timestamp, earlier_timestamp);
+
+        let first = mixer.pop();
+
+        assert!(first.is_some());
+        assert_eq!(first.unwrap(), packet_with_earlier_timestamp);
     }
 }
 
@@ -200,26 +251,4 @@ mod tests {
 // }
 
 
-
-/// Module for WASM-specific Rust code
-#[cfg(feature = "wasm")]
-pub mod wasm {
-
-     // Use this module to specify everything that is WASM-specific (e.g. uses wasm-bindgen types, js_sys, ...etc.)
-
-    // use super::*;
-    // use wasm_bindgen::prelude::*;
-    // use wasm_bindgen::JsValue;
-
-    // #[wasm_bindgen]
-    // pub fn foo(_val: JsValue) -> u32 {
-    //     super::foo()
-    // }
-    //
-    // #[wasm_bindgen]
-    // impl MyStruct {
-    //     // Specify methods of MyStruct which use WASM-specific
-    // }
-
-}
 
