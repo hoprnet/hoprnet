bc5a7f3e12d178d9e48e860f189fef38329e38d9
Lukas <lukas.pohanka@inina.net>
2023-02-20 12:18:33 +0100

    Update packages/utils/crates/utils-types/src/errors.rs
    Co-authored-by: Tibor Csoka <9529609+Teebor-Choka@users.noreply.github.com>

--- packages/utils/crates/utils-types/src/errors.rs
+++ packages/utils/crates/utils-types/src/errors.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 /// General error thrown from when operating on types.
 #[derive(Error, Debug)]
 pub enum GeneralError {
-    #[error("error while parsing or deserializing data")]
+    #[error("failed to parse/deserialize the data")]
     ParseError,
     #[error("error computing the result, possibly cause by invalid input")]
     MathError
