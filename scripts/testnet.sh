d59f3906328c0fd1dcb3ef7a94f561f6602359e8
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-02-28 12:34:23 +0100

    update faucet command

--- scripts/testnet.sh
+++ scripts/testnet.sh
@@ -210,9 +210,9 @@ fund_nodes() {
   local addr_arg=""
   [[ -n "${4:-}" ]] && addr_arg="--address ${4}"
 
-  ${mydir}/../.cargo/bin/foundry-tool \
+  ${mydir}/../.cargo/bin/foundry-tool faucet \
     --environment-name anvil-localhost --environment-type development \
-    faucet --password "${password}" --use-local-identities \
+    --password "${password}" --use-local-identities \
     --identity-prefix "${node_prefix}" --identity-directory "${tmp}" \
     --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
     --make-root "${mydir}/../packages/ethereum/contracts" \
