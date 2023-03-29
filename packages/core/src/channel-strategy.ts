90118d9184dd739046fce6c2ec658409b23f10c1
Tino Breddin <t@tolbrino.com>
2023-03-29 21:01:22 +0200

    Use wrtc fork

--- packages/core/src/channel-strategy.ts
+++ packages/core/src/channel-strategy.ts
@@ -26,7 +26,7 @@ export { StrategyTickResult } from '../lib/core_strategy.js'
 import { ChannelStatus } from '@hoprnet/hopr-utils'
 
 const STRATEGIES = ['passive', 'promiscuous', 'random']
-export type Strategy = (typeof STRATEGIES)[number]
+export type Strategy = typeof STRATEGIES[number]
 
 export function isStrategy(str: string): str is Strategy {
   return STRATEGIES.includes(str)
