22f8854b30063b024f617badbc83885d1fcdc86f
Robert Kiel <robert.kiel@hoprnet.org>
2023-04-12 17:32:53 +0200

    finish logic

--- packages/connect/crates/connect-relay/src/connection_manager.rs
+++ packages/connect/crates/connect-relay/src/connection_manager.rs
@@ -186,7 +186,7 @@ impl TryFrom<(PeerId, PeerId)> for RelayConnectionIdentifier {
     }
 }
 
-fn get_key_and_value<'a, St>(
+fn get_key_and_value<St>(
     id_a: PeerId,
     conn_a: Server<St>,
     id_b: PeerId,
@@ -224,8 +224,8 @@ impl<St> ToString for RelayConnections<St> {
     }
 }
 
-impl<St: DuplexStream + 'static> RelayConnections<St> {
-    fn create_new(&'static mut self, id_a: PeerId, stream_a: St, id_b: PeerId, stream_b: St) -> Result<(), String> {
+impl<St: DuplexStream> RelayConnections<St> {
+    pub fn create_new(&'static mut self, id_a: PeerId, stream_a: St, id_b: PeerId, stream_b: St) -> Result<(), String> {
         let id1: RelayConnectionIdentifier = (id_a, id_b).try_into().unwrap();
 
         let bar = self.conns.as_ref();
@@ -270,9 +270,7 @@ impl<St: DuplexStream + 'static> RelayConnections<St> {
         Ok(())
     }
 
-    // async isActive(source: PeerId, destination: PeerId, timeout?: number): Promise<boolean> {
-
-    async fn is_active(&mut self, source: PeerId, destination: PeerId, maybe_timeout: Option<u64>) -> bool {
+    pub async fn is_active(&mut self, source: PeerId, destination: PeerId, maybe_timeout: Option<u64>) -> bool {
         let id: RelayConnectionIdentifier = match (source, destination).try_into() {
             Ok(x) => x,
             Err(e) => {
@@ -314,7 +312,7 @@ impl<St: DuplexStream + 'static> RelayConnections<St> {
         }
     }
 
-    fn update_existing(&mut self, source: PeerId, destination: PeerId, to_source: St) -> bool {
+    pub fn update_existing(&mut self, source: PeerId, destination: PeerId, to_source: St) -> bool {
         let id: RelayConnectionIdentifier = match (source, destination).try_into() {
             Ok(x) => x,
             Err(e) => {
@@ -357,7 +355,92 @@ impl<St: DuplexStream + 'static> RelayConnections<St> {
     }
 }
 
-// #[cfg(feature = "wasm")]
-// pub mod wasm {
-//     struct
-// }
+#[cfg(feature = "wasm")]
+pub mod wasm {
+    use std::str::FromStr;
+
+    use crate::streaming_iterable::{JsStreamingIterable, StreamingIterable};
+    use js_sys::Number;
+    use libp2p::PeerId;
+    use utils_misc::ok_or_jserr;
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen]
+    struct RelayState {
+        w: super::RelayConnections<StreamingIterable>,
+    }
+
+    #[wasm_bindgen]
+    extern "C" {
+        pub type JsPeerId;
+
+        #[wasm_bindgen(structural, method, js_name = "toString")]
+        pub fn to_string(this: &JsPeerId) -> String;
+    }
+
+    #[wasm_bindgen]
+    impl RelayState {
+        #[wasm_bindgen(js_name = "createNew")]
+        pub fn create_new(
+            &mut self,
+            source: JsPeerId,
+            destination: JsPeerId,
+            to_source: JsStreamingIterable,
+            to_destination: JsStreamingIterable,
+        ) -> Result<JsValue, JsValue> {
+            let source = ok_or_jserr!(PeerId::from_str(source.to_string().as_str()))?;
+            let destination = ok_or_jserr!(PeerId::from_str(destination.to_string().as_str()))?;
+
+            let this = unsafe {
+                std::mem::transmute::<
+                    &mut super::RelayConnections<StreamingIterable>,
+                    &'static mut super::RelayConnections<StreamingIterable>,
+                >(&mut self.w)
+            };
+
+            match this.create_new(
+                source,
+                StreamingIterable::from(to_source),
+                destination,
+                StreamingIterable::from(to_destination),
+            ) {
+                Ok(()) => Ok(JsValue::undefined()),
+                Err(e) => Err(JsValue::from(e)),
+            }
+        }
+
+        #[wasm_bindgen(js_name = "isActive")]
+        pub async fn is_active(
+            &mut self,
+            source: JsPeerId,
+            destination: JsPeerId,
+            timeout: Option<Number>,
+        ) -> Result<JsValue, JsValue> {
+            let source = ok_or_jserr!(PeerId::from_str(source.to_string().as_str()))?;
+            let destination = ok_or_jserr!(PeerId::from_str(destination.to_string().as_str()))?;
+
+            let res = self
+                .w
+                .is_active(source, destination, timeout.map(|n| n.value_of() as u64))
+                .await;
+
+            Ok(JsValue::from(res))
+        }
+
+        #[wasm_bindgen(js_name = "updateExisting")]
+        pub fn update_existing(
+            &mut self,
+            source: JsPeerId,
+            destination: JsPeerId,
+            to_source: JsStreamingIterable,
+        ) -> Result<JsValue, JsValue> {
+            let source = ok_or_jserr!(PeerId::from_str(source.to_string().as_str()))?;
+            let destination = ok_or_jserr!(PeerId::from_str(destination.to_string().as_str()))?;
+
+            self.w
+                .update_existing(source, destination, StreamingIterable::from(to_source));
+
+            Ok(JsValue::undefined())
+        }
+    }
+}
