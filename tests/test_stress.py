649e50fb7d52ad24bd86709817f33294434f84bf
HOPR CI robot <noreply@hoprnet.org>
2023-04-14 12:45:51 +0000

    Re-generate API docs for packages

--- tests/test_stress.py
+++ tests/test_stress.py
@@ -11,26 +11,28 @@ HTTP_STATUS_CODE_SEND_MESSAGE_OK = 202
 
 
 @pytest.mark.asyncio
-@pytest.mark.skipif(os.getenv('CI','false') == 'true', reason="stress tests fail randomly on CI due to resources")
+@pytest.mark.skipif(os.getenv("CI", "false") == "true", reason="stress tests fail randomly on CI due to resources")
 async def test_stress_hoprd_send_message_should_send_sequential_messages_without_errors(request, cmd_line_args):
-    if '127.0.0.1' in cmd_line_args['stress_tested_api'] or "localhost" in cmd_line_args['stress_tested_api']:
-        request.getfixturevalue('setup_7_nodes')
+    if "127.0.0.1" in cmd_line_args["stress_tested_api"] or "localhost" in cmd_line_args["stress_tested_api"]:
+        request.getfixturevalue("setup_7_nodes")
 
     STRESS_SEQUENTIAL_MESSAGE_COUNT = cmd_line_args["stress_seq_request_count"]
 
-    alice = HoprdAPI(cmd_line_args['stress_tested_api'], cmd_line_args['stress_tested_api_token'])
+    alice = HoprdAPI(cmd_line_args["stress_tested_api"], cmd_line_args["stress_tested_api_token"])
     hops = []  # zero hop
 
     # wait for peers to be connected
     for _ in range(20):
-        if len([i["peerId"] for i in (await alice.peers()).json()["connected"]]) > cmd_line_args[
-            'stress_minimum_peer_count']:
+        if (
+            len([i["peerId"] for i in (await alice.peers()).json()["connected"]])
+            > cmd_line_args["stress_minimum_peer_count"]
+        ):
             break
         else:
             await asyncio.sleep(1)
 
     connected_peers = [i["peerId"] for i in (await alice.peers()).json()["connected"]]
-    assert len(connected_peers) > cmd_line_args['stress_minimum_peer_count']
+    assert len(connected_peers) > cmd_line_args["stress_minimum_peer_count"]
 
     expected = [HTTP_STATUS_CODE_SEND_MESSAGE_OK] * STRESS_SEQUENTIAL_MESSAGE_COUNT
     actual = [
@@ -41,10 +43,10 @@ async def test_stress_hoprd_send_message_should_send_sequential_messages_without
 
 
 @pytest.mark.asyncio
-@pytest.mark.skipif(os.getenv('CI','false') == 'true', reason="stress tests fail randomly on CI due to resources")
+@pytest.mark.skipif(os.getenv("CI", "false") == "true", reason="stress tests fail randomly on CI due to resources")
 async def test_stress_hoprd_send_message_should_send_parallel_messages_without_errors(request, cmd_line_args):
-    if '127.0.0.1' in cmd_line_args['stress_tested_api'] or "localhost" in cmd_line_args['stress_tested_api']:
-        request.getfixturevalue('setup_7_nodes')
+    if "127.0.0.1" in cmd_line_args["stress_tested_api"] or "localhost" in cmd_line_args["stress_tested_api"]:
+        request.getfixturevalue("setup_7_nodes")
 
     STRESS_PARALLEL_MESSAGE_COUNT = cmd_line_args["stress_par_request_count"]
 
