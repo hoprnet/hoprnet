6496fbc9675f9dc83cb3e36cceb316bb5c1e2b65
Tibor Csóka <csokat@gmail.com>
2023-04-26 15:35:30 +0200

    Fix core-db business logic API trait

--- packages/core/crates/core-db/src/traits.rs
+++ packages/core/crates/core-db/src/traits.rs
@@ -1,32 +1,27 @@
 use async_trait::async_trait;
 
 use core_crypto::types::{HalfKeyChallenge, Hash, PublicKey};
-use utils_types::primitives::{Address, Balance};
+use core_types::acknowledgment::UnacknowledgedTicket;
+use core_types::channels::{ChannelEntry, Ticket};
+use utils_types::primitives::{Address, Balance, U256};
 
 pub type Result<T> = std::result::Result<T, utils_db::errors::DbError>;
 
-// TODO: placeholder for the non-existent types
-type Ticket = u128;
-type UnacknowledgedTicket = u128;
-type ChannelEntry = u128;
-
-// TODO: packet interaction only
-#[async_trait]
+#[async_trait(? Send)] // not placing the `Send` trait limitations on the trait
 pub trait HoprCoreDbActions {
-    // TODO: update u128 to u256
-    async fn get_current_ticket_index(&self, channel_id: &Hash) -> Result<Option<u128>>;
-    async fn set_current_ticket_index(&self, channel_id: &Hash, commitment: &Hash) -> Result<()>;
+    async fn get_current_ticket_index(&self, channel_id: &Hash) -> Result<Option<U256>>;
+    async fn set_current_ticket_index(&self, channel_id: &Hash, commitment: U256) -> Result<()>;
 
     // TODO: trait with generic argument rather than allocated Box
     async fn get_tickets(&self, predicate: Box<dyn Fn() -> bool>) -> Result<Vec<Ticket>>;
-    async fn get_channel_to(&self, dest: PublicKey) -> ChannelEntry;
-    async fn get_channel_from(&self, origin: PublicKey) -> ChannelEntry;
-
-    async fn get_pending_balance_to(&self, counterparty: &Address) -> Balance;
     async fn mark_pending(&self, ticket: &Ticket);
     async fn mark_rejected(&self, ticket: &Ticket);
 
-    async fn check_and_set_packet_tag(&self, tag: Box<[u8]>);
+    async fn get_channel_to(&self, dest: PublicKey) -> ChannelEntry;
+    async fn get_channel_from(&self, origin: PublicKey) -> ChannelEntry;
+    async fn get_pending_balance_to(&self, counterparty: &Address) -> Balance;
+
+    async fn check_and_set_packet_tag(&self, tag: Box<[u8]>) -> bool;
 
     async fn store_pending_acknowledgment(
         &self,
