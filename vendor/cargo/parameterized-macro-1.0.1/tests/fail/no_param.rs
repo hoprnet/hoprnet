e8594d1de8ba2be62d8d779fb895663ff97b4002
Robert Kiel <robert.kiel@hoprnet.org>
2023-05-12 11:17:34 +0200

    remove vendor

--- vendor/cargo/parameterized-macro-1.0.1/tests/fail/no_param.rs
+++
@@ -1,6 +0,0 @@
-use parameterized_macro::parameterized;
-
-#[parameterized(y = { 1, 2, 3 })]
-fn my_test(x: i32) {}
-
-fn main() {}
--- vendor/cargo/parity-scale-codec-3.4.0/tests/max_encoded_len_ui/missing_crate_specifier.rs
+++
@@ -1,9 +0,0 @@
-use parity_scale_codec::{Encode, MaxEncodedLen};
-
-#[derive(Encode, MaxEncodedLen)]
-#[codec(parity_scale_codec)]
-struct Example;
-
-fn main() {
-	let _ = Example::max_encoded_len();
-}
