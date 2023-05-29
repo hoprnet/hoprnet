8424b90d27e7c74ee7325a53b2f1a6c3caf47492
NumberFour8 <lukas.pohanka@inina.net>
2023-05-29 11:30:33 +0200

    Update default max log level

--- packages/utils/crates/utils-log/src/logger.rs
+++ packages/utils/crates/utils-log/src/logger.rs
@@ -12,11 +12,10 @@ extern "C" {
 
 impl JsLogger {
     /// Install this logger as a backend with optional maximum level.
+    /// Maximum level defaults to DEBUG if not set (note: ERROR is the lowest, TRACE is the highest)
     pub fn install(logger: &'static JsLogger, max_level: Option<Level>) -> Result<(), String> {
         log::set_logger(logger).map_err(|e| e.to_string())?;
-        if let Some(lvl) = max_level {
-            log::set_max_level(lvl.to_level_filter());
-        }
+        log::set_max_level(max_level.unwrap_or(Level::Debug).to_level_filter());
         Ok(())
     }
 }
