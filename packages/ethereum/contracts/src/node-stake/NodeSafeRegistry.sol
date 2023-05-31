12860c1bcb01183d6d21888e84c63459a7e822ad
Robert Kiel <robert.kiel@hoprnet.org>
2023-05-31 12:47:15 +0200

    eip712 signing structs (#5119)
    * fix eip 712 encoding
    
    * switch to abi.encode
    
    * more structs

--- packages/ethereum/contracts/src/node-stake/NodeSafeRegistry.sol
+++ packages/ethereum/contracts/src/node-stake/NodeSafeRegistry.sol
@@ -25,38 +25,29 @@ contract HoprNodeSafeRegistry {
     address nodeChainKeyAddress;
   }
 
-  bytes32 public domainSeparator;
+  // Currently deployed version, starting with 1.0.0
+  string private constant version = '1.0.0';
+
+  bytes32 private immutable domainSeparator;
   mapping(address => address) public nodeToSafe;
-  // NodeSafe type hash. keccak256("NodeSafe(address safeAddress,address nodeChainKeyAddress)");
+  // NodeSafe struct type hash.
+  // keccak256("NodeSafe(address safeAddress,address nodeChainKeyAddress)");
   bytes32 private constant NODE_SAFE_TYPEHASH = hex'6e9a9ee91e0fce141f0eeaf47e1bfe3af5b5f40e5baf2a86acc37a075199c16d';
 
   event RegisteredNodeSafe(address indexed safeAddress, address indexed nodeAddress);
   event DergisteredNodeSafe(address indexed safeAddress, address indexed nodeAddress);
 
-  /**
-   * @param networkName string, e.g. 'monte_rosa_2_0'
-   */
-  constructor(string memory networkName) {
+  constructor() {
+    // following encoding guidelines of EIP712
     domainSeparator = keccak256(
       abi.encode(
         keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-        keccak256(bytes('NodeStakeStorage')),
-        keccak256(bytes(networkName)),
+        keccak256(bytes('NodeStakeRegistry')),
+        keccak256(bytes(version)),
         block.chainid,
         address(this)
       )
     );
-  }
-
-  /**
-   * @dev returns the hashed NodeSafe typed data, which can be signed by the nodeChainKeyAddress
-   * @param nodeSafe NodeSafe struct which contains node address and safe address
-   */
-  function getNodeSafeDigest(NodeSafe memory nodeSafe) public view returns (bytes32) {
-    bytes32 nodeSafeHash = keccak256(
-      abi.encode(NODE_SAFE_TYPEHASH, nodeSafe.safeAddress, nodeSafe.nodeChainKeyAddress)
-    );
-    return keccak256(abi.encodePacked(bytes1(0x19), bytes1(0x01), domainSeparator, nodeSafeHash));
   }
 
   /**
@@ -66,19 +57,20 @@ contract HoprNodeSafeRegistry {
   function registerSafeWithNodeSig(NodeSafe memory nodeSafe, bytes calldata sig) external {
     // check adminKeyAddress has added HOPR tokens to the staking contract.
 
-    // build digest
-    bytes32 onStartHash = keccak256(
-      abi.encodePacked(bytes1(0x19), bytes1(0x01), domainSeparator, getNodeSafeDigest(nodeSafe))
-    );
+    // following encoding guidelines of EIP712
+    bytes32 hashStruct = keccak256(abi.encode(NODE_SAFE_TYPEHASH, nodeSafe));
+
+    // build typed digest
+    bytes32 registerHash = keccak256(abi.encode(bytes1(0x19), bytes1(0x01), domainSeparator, hashStruct));
 
     // verify that signatures is from nodeChainKeyAddress. This signature can only be
-    (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(onStartHash, sig);
+    (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(registerHash, sig);
     if (error != ECDSA.RecoverError.NoError || recovered != nodeSafe.nodeChainKeyAddress) {
       revert NotValidSignatureFromNode();
     }
 
     // store those state, emit events etc.
-    addNodeSafe(nodeSafe.safeAddress, nodeSafe.nodeChainKeyAddress);
+    addNodeSafe(nodeSafe);
   }
 
   /**
@@ -92,7 +84,7 @@ contract HoprNodeSafeRegistry {
     }
 
     // ensure that node is an owner
-    ensureNodeIsOwnerAndHasRole(msg.sender, nodeAddr);
+    ensureNodeIsOwnerAndHasRole(NodeSafe({safeAddress: msg.sender, nodeChainKeyAddress: nodeAddr}));
 
     // update and emit event
     nodeToSafe[nodeAddr] = address(0);
@@ -103,50 +95,49 @@ contract HoprNodeSafeRegistry {
    * @dev register the Safe by the node, directly with call made by the node
    */
   function registerSafeByNode(address safeAddr) external {
-    addNodeSafe(safeAddr, msg.sender);
+    addNodeSafe(NodeSafe({safeAddress: safeAddr, nodeChainKeyAddress: msg.sender}));
   }
 
   /**
    * @dev internal funciton to store safe-node pair and emit events
    */
-  function addNodeSafe(address safeAddr, address nodeAddr) internal {
+  function addNodeSafe(NodeSafe memory nodeSafe) internal {
     // check this node hasn't been registered ower
-    if (nodeToSafe[nodeAddr] != address(0)) {
+    if (nodeToSafe[nodeSafe.nodeChainKeyAddress] != address(0)) {
       revert NodeHasSafe();
     }
     // Safe address cannot be zero
-    if (safeAddr == address(0)) {
+    if (nodeSafe.safeAddress == address(0)) {
       revert InvalidSafeAddress();
     }
 
     // ensure that node is an owner
-    ensureNodeIsOwnerAndHasRole(safeAddr, nodeAddr);
+    ensureNodeIsOwnerAndHasRole(nodeSafe);
 
     // update and emit event
-    nodeToSafe[nodeAddr] = safeAddr;
-    emit RegisteredNodeSafe(safeAddr, nodeAddr);
+    nodeToSafe[nodeSafe.nodeChainKeyAddress] = nodeSafe.safeAddress;
+    emit RegisteredNodeSafe(nodeSafe.safeAddress, nodeSafe.nodeChainKeyAddress);
   }
 
   /**
    * @dev Ensure that the node address is an owner of safe address
    * Ensure that the node address has the NODE_ROLE on the guard of the safe
-   * @param safeAddr address of the Safe
-   * @param nodeAddr address of the node
+   * @param nodeSafe struct to check
    */
-  function ensureNodeIsOwnerAndHasRole(address safeAddr, address nodeAddr) internal {
+  function ensureNodeIsOwnerAndHasRole(NodeSafe memory nodeSafe) internal view {
     // check safeAddress has nodeChainKeyAddress as owner
-    address[] memory owners = ISafe(safeAddr).getOwners();
+    address[] memory owners = ISafe(nodeSafe.safeAddress).getOwners();
     uint256 index = 0;
     for (index; index < owners.length; index++) {
-      if (owners[index] == nodeAddr) break;
+      if (owners[index] == nodeSafe.nodeChainKeyAddress) break;
     }
     if (index >= owners.length) {
       revert NotValidSafeOwner();
     }
 
     // check nodeChainKeyAddress has NODE_ROLE in safeAddress guard
-    IDualRoleAccess safeGuard = IDualRoleAccess(ISafe(safeAddr).getGuard());
-    if (safeGuard.hasRole(safeGuard.NODE_ROLE(), nodeAddr)) {
+    IDualRoleAccess safeGuard = IDualRoleAccess(ISafe(nodeSafe.safeAddress).getGuard());
+    if (safeGuard.hasRole(safeGuard.NODE_ROLE(), nodeSafe.nodeChainKeyAddress)) {
       revert NotValidGuardRole();
     }
   }
