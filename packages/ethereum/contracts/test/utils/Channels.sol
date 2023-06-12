4198c47e429107687917ae2d86d23ef9e489fd5a
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-06-12 19:00:50 +0800

    add changes to Channels

--- packages/ethereum/contracts/test/utils/Channels.sol
+++ packages/ethereum/contracts/test/utils/Channels.sol
@@ -2,72 +2,39 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 import 'forge-std/Test.sol';
-import '../../src/HoprChannels.sol';
+import '../../src/Channels.sol';
 
-contract ChannelsUtilsTest is Test {
+abstract contract ChannelsUtilsTest is Test {
   bytes32 public SECRET_0 = keccak256(abi.encodePacked('secret'));
   bytes32 public SECRET_1 = keccak256(abi.encodePacked(SECRET_0));
   bytes32 public SECRET_2 = keccak256(abi.encodePacked(SECRET_1));
 
   /**
-   * Emitted on every channel state change.
-   */
-  event ChannelUpdated(address indexed source, address indexed destination, HoprChannels.Channel newState);
+   * @dev copy all the events
+   */ 
+  event ChannelOpened(address indexed source, address indexed destination, HoprChannels.Balance amount);
+  event ChannelBalanceIncreased(bytes32 indexed channelId, HoprChannels.Balance newBalance);
+  event ChannelBalanceDecreased(bytes32 indexed channelId, HoprChannels.Balance newBalance);
+  event CommitmentSet(bytes32 channelId, HoprChannels.ChannelEpoch epoch);
+  event OutgoingChannelClosureInitiated(bytes32 channelId, HoprChannels.Timestamp closureInitiationTime);
+  event ChannelClosed(bytes32 channelId);
+  event TicketRedeemed(bytes32 channelId, HoprChannels.TicketIndex newTicketIndex);
 
-  /**
-   * Emitted once an account announces.
-   */
-  event Announcement(address indexed account, bytes publicKey, bytes multiaddr);
+  function assertEqChannels(HoprChannels.Channel memory channel1, HoprChannels.Channel memory channel2) public {
+    bytes32 channel1Hash = getChannelHash(channel1);
+    bytes32 channel2Hash = getChannelHash(channel2);
+    assertEq(channel1Hash, channel2Hash);
+  }
 
-  /**
-   * Emitted once a channel if funded.
-   */
-  event ChannelFunded(address indexed funder, address indexed source, address indexed destination, uint256 amount);
-
-  /**
-   * Emitted once bumpChannel is called.
-   */
-  event ChannelBumped(
-    address indexed source,
-    address indexed destination,
-    bytes32 newCommitment,
-    uint256 ticketEpoch,
-    uint256 channelBalance
-  );
-
-  /**
-   * Emitted once a channel is opened.
-   */
-  event ChannelOpened(address indexed source, address indexed destination);
-
-  /**
-   * Emitted once a channel closure is initialized.
-   */
-  event ChannelClosureInitiated(address indexed source, address indexed destination, uint32 closureInitiationTime);
-
-  /**
-   * Emitted once a channel closure is finalized.
-   */
-  event ChannelClosureFinalized(
-    address indexed source,
-    address indexed destination,
-    uint32 closureFinalizationTime,
-    uint256 channelBalance
-  );
-  /**
-   * Emitted once a ticket is redeemed.
-   */
-  event TicketRedeemed(
-    address indexed source,
-    address indexed destination,
-    bytes32 nextCommitment,
-    uint256 ticketEpoch,
-    uint256 ticketIndex,
-    bytes32 proofOfRelaySecret,
-    uint256 amount,
-    uint256 winProb,
-    bytes signature
-  );
+  function assertEqChannelsWithId(
+    HoprChannels hoprChannels, 
+    bytes32 channelId, 
+    HoprChannels.Channel memory channel2
+  ) public {
+    bytes32 channel1Hash = getChannelHash(getChannelFromTuple(hoprChannels, channelId));
+    bytes32 channel2Hash = getChannelHash(channel2);
+    assertEq(channel1Hash, channel2Hash);
+  }
 
   /**
    * @param source the address of source
@@ -78,28 +45,42 @@ contract ChannelsUtilsTest is Test {
     return keccak256(abi.encodePacked(source, destination));
   }
 
-  function assertEqChannels(HoprChannels.Channel memory channel1, HoprChannels.Channel memory channel2) public {
-    bytes32 channel1Hash = getChannelHash(channel1);
-    bytes32 channel2Hash = getChannelHash(channel2);
-    assertEq(channel1Hash, channel2Hash);
-  }
-
-  function getChannelFromTuple(HoprChannels hoprChannels, bytes32 channelId)
+  function wrapChannel(
+    bytes32 _commitment, 
+    uint256 _balance, 
+    uint256 _ticketIndex, 
+    HoprChannels.ChannelStatus _status,
+    uint256 _epoch, 
+    uint256 _closureTime
+  )
     public
     view
     returns (HoprChannels.Channel memory)
   {
-    (uint256 a, bytes32 b, uint256 c, uint256 d, HoprChannels.ChannelStatus e, uint256 f, uint32 g) = hoprChannels
-      .channels(channelId);
     return
       HoprChannels.Channel({
-        balance: a,
-        commitment: b,
-        ticketEpoch: c,
-        ticketIndex: d,
-        status: e,
-        channelEpoch: f,
-        closureTime: g
+        commitment: _commitment,
+        balance: HoprChannels.Balance.wrap(uint96(_balance)),
+        ticketIndex: HoprChannels.TicketIndex.wrap(uint64(_ticketIndex)),
+        status: _status,
+        epoch: HoprChannels.ChannelEpoch.wrap(uint24(_epoch)),
+        closureTime: HoprChannels.Timestamp.wrap(uint32(_closureTime))
+      });
+  }
+
+  function getChannelFromTuple(
+    HoprChannels hoprChannels, 
+    bytes32 channelId
+  ) public view returns (HoprChannels.Channel memory) {
+    (bytes32 a,HoprChannels.Balance b,HoprChannels.TicketIndex c, HoprChannels.ChannelStatus d,HoprChannels.ChannelEpoch e,HoprChannels.Timestamp f) = hoprChannels.channels(channelId);
+    return
+      HoprChannels.Channel({
+        commitment: a,
+        balance: b,
+        ticketIndex: c,
+        status: d,
+        epoch: e,
+        closureTime: f
       });
   }
 
@@ -107,14 +88,36 @@ contract ChannelsUtilsTest is Test {
     return
       keccak256(
         abi.encodePacked(
-          channel.balance,
           channel.commitment,
-          channel.ticketEpoch,
+          channel.balance,
           channel.ticketIndex,
           channel.status,
-          channel.channelEpoch,
+          channel.epoch,
           channel.closureTime
         )
       );
-  }
+  }  
+
+  // function getTicketLuckInternal(
+  //     bytes32 ticketHash,
+  //     bytes32 secretPreImage,
+  //     bytes32 proofOfRelaySecret
+  // ) external pure returns (WinProb) {
+  //     return _getTicketLuck(ticketHash, secretPreImage, proofOfRelaySecret);
+  // }
+
+  // function getTicketHashInternal(
+  //     address recipient,
+  //     uint256 recipientCounter,
+  //     bytes32 proofOfRelaySecret,
+  //     uint256 channelIteration,
+  //     uint256 amount,
+  //     uint256 ticketIndex,
+  //     uint256 winProb
+  // ) external pure returns (bytes32) {
+  //     return ECDSA.toEthSignedMessageHash(
+  //         keccak256(_getEncodedTicket(recipient, recipientCounter, proofOfRelaySecret, channelIteration, amount, ticketIndex, winProb))
+  //     );
+  // }
 }
+
