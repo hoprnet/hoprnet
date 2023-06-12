2fc6c196b92456c13036614b61786e917c612454
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-06-12 19:21:53 +0800

    adapt to latest Channels struct

--- packages/ethereum/contracts/test/utils/Tickets.sol
+++ packages/ethereum/contracts/test/utils/Tickets.sol
@@ -12,7 +12,7 @@ abstract contract TicketsUtilsTest is Test, AccountsFixtureTest, ChannelsUtilsTe
   bytes32 public CHANNEL_BA_ID = getChannelId(accountB.accountAddr, accountA.accountAddr);
   HoprChannels.WinProb public WIN_PROB_100 = HoprChannels.WinProb.wrap(type(uint56).max);
   HoprChannels.WinProb public WIN_PROB_0 = HoprChannels.WinProb.wrap(type(uint56).min);
-  uint16 public RESERVED = 0;
+  HoprChannels.TicketReserved public RESERVED = HoprChannels.TicketReserved.wrap(uint16(0));
 
   /**
      *@dev a winning ticket issued by account A:
@@ -29,19 +29,26 @@ abstract contract TicketsUtilsTest is Test, AccountsFixtureTest, ChannelsUtilsTe
             nextCommitment: SECRET_1
         )
      */
-  HoprChannels.Ticket public TICKET_AB_WIN =
-    HoprChannels.Ticket({
+  HoprChannels.TicketData public TICKET_AB_WIN =
+    HoprChannels.TicketData({
       channelId: CHANNEL_AB_ID,
       amount: HoprChannels.Balance.wrap(uint96(10)),
-      ticketIndex: HoprChannels.TicketIndex.wrap(uint64(1)),
+      index: HoprChannels.TicketIndex.wrap(uint64(1)),
       epoch: HoprChannels.ChannelEpoch.wrap(uint24(0)),
       winProb: WIN_PROB_100,
-      resered: RESERVED
+      reserved: RESERVED
     });
 
   HoprChannels.CompactSignature public TICKET_AB_WIN_SIG = HoprChannels.CompactSignature({
     r: hex'e28514db6bf62eab85e382e77a551639f616a51e527480dc922004a197b44600',
     vs: hex'6e396a671f35c69bbe966fbf26ccdd31da7722ea566fd34ba2724f6a10d7231b'
+  });
+
+  HoprChannels.RedeemableTicket public REDEEMABLE_TICKET_AB_WIN = HoprChannels.RedeemableTicket({
+    signature: TICKET_AB_WIN_SIG,
+    data: TICKET_AB_WIN,
+    opening: SECRET_1,
+    porSecret:PROOF_OF_RELAY_SECRET_0
   });
 
   /**
@@ -59,19 +66,26 @@ abstract contract TicketsUtilsTest is Test, AccountsFixtureTest, ChannelsUtilsTe
             nextCommitment: SECRET_1
         )
     */
-    HoprChannels.Ticket public TICKET_BA_WIN =
-    HoprChannels.Ticket({
+    HoprChannels.TicketData public TICKET_BA_WIN =
+    HoprChannels.TicketData({
       channelId: CHANNEL_BA_ID,
       amount: HoprChannels.Balance.wrap(uint96(10)),
-      ticketIndex: HoprChannels.TicketIndex.wrap(uint64(1)),
+      index: HoprChannels.TicketIndex.wrap(uint64(1)),
       epoch: HoprChannels.ChannelEpoch.wrap(uint24(0)),
       winProb: WIN_PROB_100,
-      resered: RESERVED
+      reserved: RESERVED
     });
 
   HoprChannels.CompactSignature public TICKET_BA_WIN_SIG = HoprChannels.CompactSignature({
     r: hex'40e302cb0b8b18dbdd08ca1bfc93f1f2c40d5b93e8366dbd97f323aabb26e05f',
     vs: hex'91b2e8828a3a15ffb39d10c55e3fbc2fca72c4d3a3083f09fe07797a6a9ecc54'
+  });
+
+  HoprChannels.RedeemableTicket public REDEEMABLE_TICKET_BA_WIN = HoprChannels.RedeemableTicket({
+    signature: TICKET_BA_WIN_SIG,
+    data: TICKET_BA_WIN,
+    opening: SECRET_1,
+    porSecret:PROOF_OF_RELAY_SECRET_0
   });
 
   /**
@@ -89,19 +103,26 @@ abstract contract TicketsUtilsTest is Test, AccountsFixtureTest, ChannelsUtilsTe
             nextCommitment: SECRET_1
         )
     */
-  HoprChannels.Ticket public TICKET_AB_LOSS =
-    HoprChannels.Ticket({
+  HoprChannels.TicketData public TICKET_AB_LOSS =
+    HoprChannels.TicketData({
       channelId: CHANNEL_AB_ID,
       amount: HoprChannels.Balance.wrap(uint96(10)),
-      ticketIndex: HoprChannels.TicketIndex.wrap(uint64(1)),
+      index: HoprChannels.TicketIndex.wrap(uint64(1)),
       epoch: HoprChannels.ChannelEpoch.wrap(uint24(0)),
       winProb: WIN_PROB_0,
-      resered: RESERVED
+      reserved: RESERVED
     });
 
   HoprChannels.CompactSignature public TICKET_BA_LOSS_SIG = HoprChannels.CompactSignature({
     r:  hex'c81d8a3fe9d2dfbbf916bad5c3ff2acfb557c4972eb172f6441b85058e8cbd26',
     vs: hex'b67afed7d5e72eae4e5bbf0b4ed9d949c0b06b0755b81b80742e4898f36fcc33'
+  });
+
+  HoprChannels.RedeemableTicket public REDEEMABLE_TICKET_AB_LOSS = HoprChannels.RedeemableTicket({
+    signature: TICKET_BA_LOSS_SIG,
+    data: TICKET_AB_LOSS,
+    opening: SECRET_1,
+    porSecret:PROOF_OF_RELAY_SECRET_0
   });
 
   /**
@@ -119,18 +140,25 @@ abstract contract TicketsUtilsTest is Test, AccountsFixtureTest, ChannelsUtilsTe
             nextCommitment: SECRET_1
         )
     */
-  HoprChannels.Ticket public TICKET_AB_WIN_RECYCLED =
-    HoprChannels.Ticket({
+  HoprChannels.TicketData public TICKET_AB_WIN_RECYCLED =
+    HoprChannels.TicketData({
       channelId: CHANNEL_AB_ID,
       amount: HoprChannels.Balance.wrap(uint96(10)),
-      ticketIndex: HoprChannels.TicketIndex.wrap(uint64(1)),
+      index: HoprChannels.TicketIndex.wrap(uint64(1)),
       epoch: HoprChannels.ChannelEpoch.wrap(uint24(0)),
       winProb: WIN_PROB_100,
-      resered: RESERVED
+      reserved: RESERVED
     });
 
   HoprChannels.CompactSignature public TICKET_BA_WIN_RECYCLED_SIG = HoprChannels.CompactSignature({
     r: hex'43bbf7f4a28786e47be61b6e3c40f4ff95f214e0ac3b43b10d9d962a076e7e0f',
     vs: hex'0a35e4a487ba460af46f9b061e3474c1af399a50033a3f6a48f84a279acdc981'
+  });
+
+  HoprChannels.RedeemableTicket public REDEEMABLE_TICKET_AB_RECYCLED = HoprChannels.RedeemableTicket({
+    signature: TICKET_BA_WIN_RECYCLED_SIG,
+    data: TICKET_AB_WIN_RECYCLED,
+    opening: SECRET_1,
+    porSecret:PROOF_OF_RELAY_SECRET_0
   });
 }
