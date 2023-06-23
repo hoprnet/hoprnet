76435a787fe88266baef572d6e2fd985a74d17b9
ausias-armesto <ausiasarmesto@gmail.com>
2023-06-23 17:05:40 +0200

    Fixing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,7 +23,7 @@ declare branch cluster_size docker_image
 : ${FAUCET_SECRET_API_KEY:?"env var missing"}
 
 # docker_image and cluster_size are configurable through script arguments
-docker_image="${1:-gcr.io/hoprassociation/hoprd}"
+docker_image="${1:-europe-west3-docker.pkg.dev/hoprassociation/docker-images/hoprd:latest}"
 cluster_size=${2:-3}
 branch=${3:-master} # bratislava , providence
 cluster_tag=${4:-} # optional cluster tag
@@ -43,24 +43,14 @@ echo "Looking for releases to deploy from branch ${branch}"
 for git_ref in $(cat "${mydir}/../packages/hoprd/releases.json" | jq -r "to_entries[] | .value.git_ref" | uniq); do
   if [[ "${branch}" =~ ${git_ref} ]]; then
     for row in $(cat "${mydir}/../packages/hoprd/releases.json" | jq -r "to_entries[] | select(.value.git_ref==\"${git_ref}\") | @base64"); do
-      declare release_id deprecated network version_major version_minor docker_image_full
+      declare release_id deprecated network version_major version_minor
 
       release_id=$(_jq "${row}" ".key")
       deprecated=$(_jq "${row}" ".value.deprecated")
       network=$(_jq "${row}" ".value.network")
       version_major=$(_jq "${row}" ".value.version_major")
       version_minor=$(_jq "${row}" ".value.version_minor")
-      docker_image_version=$(_jq "${row}" ".value.docker_image_version")
 
-      if [ "${docker_image_version}" != "null" ]; then
-        docker_image_full="${docker_image}:${docker_image_version}"
-      elif [[ "${branch}" =~ staging/.* ]]; then
-        # Prepend "staging-" tag prefix, if this is a staging branch
-        docker_image_full="${docker_image}:staging-${release_id}"
-        cluster_tag="-staging${cluster_tag}"
-      else
-        docker_image_full="${docker_image}:${release_id}"
-      fi
 
       if [ "${deprecated}" == "true" ]; then
         log "${release_id} deprecated, skipping deployment"
@@ -81,7 +71,7 @@ for git_ref in $(cat "${mydir}/../packages/hoprd/releases.json" | jq -r "to_entr
       log "deploying release ${release_id}"
       log "\tversion: ${version_maj_min}"
       log "\tnetwork id: ${network}"
-      log "\tdocker image: ${docker_image_full}"
+      log "\tdocker image: ${docker_image}"
       log "\tcluster name: ${cluster_name}"
       log "\tcluster template name: ${cluster_template_name}"
 
@@ -92,7 +82,7 @@ for git_ref in $(cat "${mydir}/../packages/hoprd/releases.json" | jq -r "to_entr
           "${network}" \
           "" \
           "${cluster_name}" \
-          "${docker_image_full}" \
+          "${docker_image}" \
           "${cluster_size}" \
           "${cluster_template_name}" \
           ""
@@ -102,7 +92,7 @@ for git_ref in $(cat "${mydir}/../packages/hoprd/releases.json" | jq -r "to_entr
           "${network}" \
           "" \
           "${cluster_name}" \
-          "${docker_image_full}" \
+          "${docker_image}" \
           "${cluster_size}" \
           "${cluster_template_name}" \
           "true"
