7f793d9ed6e984d882311e11ce72a66db3161cb1
Tino Breddin <tino@hoprnet.org>
2023-06-27 08:58:33 +0200

    tools: Update wasm-pack to v0.12.0

--- shell.nix
+++ shell.nix
@@ -1,16 +1,5 @@
-{
-  system ? builtins.currentSystem,
-  pkgs ? import <nixpkgs> { inherit system; }
-, pkgs-dev ? let
-    lock = (builtins.fromJSON (builtins.readFile
-      ./flake.lock)).nodes.nixpkgs.locked;
-    nixpkgs = fetchTarball {
-      url =
-        "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
-      sha256 = lock.narHash;
-    };
-  in
-  import nixpkgs { inherit system; }
+{ pkgs ? import <nixpkgs> { }
+, pkgs-dev
 , ...
 }:
 let
