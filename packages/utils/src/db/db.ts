3ddd8b7a56286962768e6187db491f9a4fc248cd
Lukas <lukas.pohanka@inina.net>
2023-07-11 17:15:03 +0200

    Fixed typo

--- packages/utils/src/db/db.ts
+++ packages/utils/src/db/db.ts
@@ -294,9 +294,9 @@ export class LevelDb {
           } else {
             key = (key as Buffer).subarray(nextDelimiter + 1)
           }
-        dumpFile.write(out + ' ' + value.toString('hex') + '\n')
-      })
-      .on('end', function () {
+          dumpFile.write(out + ' ' + value.toString('hex') + '\n')
+        }
+      }).on('end', function () {
         dumpFile.close()
       })
   }
