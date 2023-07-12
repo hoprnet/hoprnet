b4ce2cc413b83f57976148a85386c41309128de9
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-07-12 22:01:22 +0800

    update proxy interface

--- packages/ethereum/contracts/src/IHoprNetworkRegistryRequirement.sol
+++ packages/ethereum/contracts/src/IHoprNetworkRegistryRequirement.sol
@@ -9,11 +9,12 @@ pragma solidity ^0.8.0;
  */
 interface IHoprNetworkRegistryRequirement {
   /**
-   * @dev Get the maximum number of nodes' peer ids that an account can register.
-   * This check is only performed when registering new nodes, i.e. if the number gets
+   * @dev Get the maximum number of nodes that a staking account can register, and 
+   * the number of nodes that can be registered by the staking account
+   * @notice This check is only performed when registering new nodes, i.e. if the number gets
    * reduced later, it does not affect registered nodes.
-   *
+   * Not all the nodes can be registered by the given staking account.
    * @param account Address that can register other nodes
    */
-  function maxAllowedRegistrations(address account) external view returns (uint256);
+  function maxAllowedRegistrations(address stakingAccount, address[] memory nodeAddresses) external view returns (uint256 allowancePerAccount, uint256 eligibleNodeCount);
 }
