9e36ebd6c604c636f31dd7ece7f8f2e3e38eec01
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-07-16 11:30:13 +0800

    Improve general coverage attempt: add test for ERC777Mock

---
+++ packages/ethereum/contracts/src/interfaces/INodeSafeRegistry.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Minimum interface for NodeSafeRegistry contract
+ */
+contract IHoprNodeSafeRegistry {
+  function nodeToSafe(address node) public view returns (address) {}
+}
