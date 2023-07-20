e01bf54bca68946c1f115ce0dd7def46fe6c4fdb
ausias-armesto <ausiasarmesto@gmail.com>
2023-07-20 10:43:45 +0200

    Testing building

--- scripts/check-pr.sh
+++ scripts/check-pr.sh
@@ -52,12 +52,13 @@ done
 # Check wether the pushed commits to the PR involve building docker images
 function check_push() {
   rm -rf ${results_file}
+  touch ${results_file}
   if [ -z "${base_branch}" ] || [  -z "${head_branch:-}" ]; then
     log "Parameter 'base_branch' and 'head_branch' are required"
     usage
     exit 1
   fi
-
+  
   echo "Checking pushed changeset from ${head_branch} against ${base_branch}"
   git diff --name-only --diff-filter=ACMRT "${base_branch}" "${head_branch}" > changes.txt
   if grep -e ^scripts/ -e ^.github/workflows/build.yaml -e ^.github/workflows/build-toolchain.yaml -e ^Makefile$ -e ^package.json$ -e ^.yarnrc.yml$ -e ^rust-toolchain.toml$ -e ^.nvmrc -e ^yarn.lock$ -e ^Cargo.toml$ changes.txt 1> /dev/null; then
