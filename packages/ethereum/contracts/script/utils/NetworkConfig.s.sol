c8bb1b007fe0bd31388ac70b832a719361d5bb4c
Robert Kiel <robert.kiel@hoprnet.org>
2023-08-04 16:06:56 +0200

    Reduce Solidity compiler warnings (#5264)

--- packages/ethereum/contracts/script/utils/NetworkConfig.s.sol
+++ packages/ethereum/contracts/script/utils/NetworkConfig.s.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/Script.sol";
--- packages/ethereum/contracts/test/mocks/EnumerableTargetSetMock.sol
+++ packages/ethereum/contracts/test/mocks/EnumerableTargetSetMock.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
 pragma solidity ^0.8.0;
 
 import "../../src/utils/EnumerableTargetSet.sol";
