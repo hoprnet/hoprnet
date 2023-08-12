3ce7d152766fb91093d6fbdbdff807779d5994d1
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-08-12 23:24:30 +0800

    early return false in isNodeSafeRegistered

--- packages/ethereum/contracts/src/node-stake/NodeSafeRegistry.sol
+++ packages/ethereum/contracts/src/node-stake/NodeSafeRegistry.sol
@@ -91,9 +91,14 @@ contract HoprNodeSafeRegistry is HoprNodeSafeRegistryEvents {
     }
 
     /**
-     * @dev checks whether a NodeSafe combination has been registered before.
+     * @dev checks whether a NodeSafe combination is registered
      */
     function isNodeSafeRegistered(NodeSafe memory nodeSafe) external view returns (bool) {
+        // node is not registered to any safe
+        if (nodeToSafe[nodeSafe.nodeChainKeyAddress] == address(0)) {
+            return false;
+        }
+
         return nodeToSafe[nodeSafe.nodeChainKeyAddress] == nodeSafe.safeAddress;
     }
 
