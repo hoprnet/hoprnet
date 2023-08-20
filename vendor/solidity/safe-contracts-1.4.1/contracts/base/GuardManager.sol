1da36a957738b0bb15615efebd1e867cd4159782
Tino Breddin <tino@hoprnet.org>
2023-08-20 09:32:10 +0200

    sc: Update safe-contracts to v1.4.1

---
+++ vendor/solidity/safe-contracts-1.4.1/contracts/base/GuardManager.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+
+import "../common/Enum.sol";
+import "../common/SelfAuthorized.sol";
+import "../interfaces/IERC165.sol";
+
+interface Guard is IERC165 {
+    function checkTransaction(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver,
+        bytes memory signatures,
+        address msgSender
+    ) external;
+
+    function checkAfterExecution(bytes32 txHash, bool success) external;
+}
+
+abstract contract BaseGuard is Guard {
+    function supportsInterface(bytes4 interfaceId) external view virtual override returns (bool) {
+        return
+            interfaceId == type(Guard).interfaceId || // 0xe6d7a83a
+            interfaceId == type(IERC165).interfaceId; // 0x01ffc9a7
+    }
+}
+
+/**
+ * @title Guard Manager - A contract managing transaction guards which perform pre and post-checks on Safe transactions.
+ * @author Richard Meissner - @rmeissner
+ */
+abstract contract GuardManager is SelfAuthorized {
+    event ChangedGuard(address indexed guard);
+
+    // keccak256("guard_manager.guard.address")
+    bytes32 internal constant GUARD_STORAGE_SLOT = 0x4a204f620c8c5ccdca3fd54d003badd85ba500436a431f0cbda4f558c93c34c8;
+
+    /**
+     * @dev Set a guard that checks transactions before execution
+     *      This can only be done via a Safe transaction.
+     *      ⚠️ IMPORTANT: Since a guard has full power to block Safe transaction execution,
+     *        a broken guard can cause a denial of service for the Safe. Make sure to carefully
+     *        audit the guard code and design recovery mechanisms.
+     * @notice Set Transaction Guard `guard` for the Safe. Make sure you trust the guard.
+     * @param guard The address of the guard to be used or the 0 address to disable the guard
+     */
+    function setGuard(address guard) external authorized {
+        if (guard != address(0)) {
+            require(Guard(guard).supportsInterface(type(Guard).interfaceId), "GS300");
+        }
+        bytes32 slot = GUARD_STORAGE_SLOT;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            sstore(slot, guard)
+        }
+        emit ChangedGuard(guard);
+    }
+
+    /**
+     * @dev Internal method to retrieve the current guard
+     *      We do not have a public method because we're short on bytecode size limit,
+     *      to retrieve the guard address, one can use `getStorageAt` from `StorageAccessible` contract
+     *      with the slot `GUARD_STORAGE_SLOT`
+     * @return guard The address of the guard
+     */
+    function getGuard() internal view returns (address guard) {
+        bytes32 slot = GUARD_STORAGE_SLOT;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            guard := sload(slot)
+        }
+    }
+}
