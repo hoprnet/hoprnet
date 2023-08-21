6a4e0ee05fec38df414a3fbb1385baa0d49b67d9
Robert Kiel <robert.kiel@hoprnet.org>
2023-08-21 15:44:37 +0200

    remove unused import

--- packages/ethereum/contracts/test/Channels.t.sol
+++ packages/ethereum/contracts/test/Channels.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.6.0 <0.9.0;
 
-import { Test, console2 } from "forge-std/Test.sol";
+import { Test } from "forge-std/Test.sol";
 import { ERC1820RegistryFixtureTest } from "./utils/ERC1820Registry.sol";
 import { HoprChannels, HoprChannelsEvents } from "../src/Channels.sol";
 import { CryptoUtils } from "./utils/Crypto.sol";
