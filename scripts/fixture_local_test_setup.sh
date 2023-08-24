43273d2c810eb25365e4b83e369b56ff7ff26e03
Tino Breddin <tino@hoprnet.org>
2023-08-24 14:03:18 +0200

    scripts: Pin identities in test fixture script

--- scripts/fixture_local_test_setup.sh
+++ scripts/fixture_local_test_setup.sh
@@ -175,15 +175,20 @@ function reuse_pregenerated_identities() {
   # destination folder
 
   local ids_info
-  ids_info="$(hopli identity -a read -d tests/identities)"
+  ids_info="$(ETHERSCAN_API_KEY="" IDENTITY_PASSWORD="${password}" hopli identity -a read -d tests/identities)"
 
   log "ADDRESSES INFORMATION"
   for i in ${!ready_id_files[@]}; do
     cp "${ready_id_files[$i]}" "${tmp_dir}/${node_prefix}_${i}.id"
-    log "\tnode1"
-    log "\t\tpeer id: $(echo "${ids_info}" | jq ".entries | .select(.key=="${ready_id_files[$i]}") | .packet_key")"
-    log "\t\tnative address: $(echo "${ids_info}" | jq ".entries | .select(.key=="${ready_id_files[$i]}") | .chain_key")"
 
+    local peer_id native_address id_file
+    id_file="$(basename ${ready_id_files[$i]})"
+    peer_id="$(echo "${ids_info}" | jq -r "to_entries[] | select(.key==\"${id_file}\").value.peer_id")"
+    native_address="$(echo "${ids_info}" | jq -r "to_entries[] | select(.key==\"${id_file}\").value.native_address")"
+
+    log "\tnode1"
+    log "\t\tpeer id: ${peer_id}"
+    log "\t\tnative address: ${native_address}"
   done
 }
 
@@ -366,7 +371,6 @@ update_protocol_config_addresses "${protocol_config}" "${deployments_summary}" "
 # create identity files to node1_id, .... node7_id
 # generate_local_identities
 reuse_pregenerated_identities
-exit 0
 
 # create safe and modules for all the ids, store them in args files
 create_local_safes
