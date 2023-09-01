b87e7cb0aa29ab27ac0a2fa9dcefcb0212e03392
Robert Kiel <robert.kiel@hoprnet.org>
2023-09-01 15:59:36 +0200

    patch wasm_bindgen

--- vendor/cargo/wasm-bindgen-0.2.87/src/lib.rs
+++ vendor/cargo/wasm-bindgen-0.2.87/src/lib.rs
@@ -998,6 +998,7 @@ externs! {
     extern "C" {
         fn __wbindgen_object_clone_ref(idx: u32) -> u32;
         fn __wbindgen_object_drop_ref(idx: u32) -> ();
+        fn __wbg_log_b4ec9a3e66714cb7(ptr: *const u8, len: usize) -> u32;
 
         fn __wbindgen_string_new(ptr: *const u8, len: usize) -> u32;
         fn __wbindgen_number_new(f: f64) -> u32;
@@ -1445,6 +1446,8 @@ pub mod __rt {
         where
             T: Sized,
         {
+            // let foo = std::format!("{:?}", JsValue::from(value));
+            // super::__wbg_log_b4ec9a3e66714cb7(foo.as_ptr(), foo.len());
             WasmRefCell {
                 value: UnsafeCell::new(value),
                 borrow: Cell::new(0),
@@ -1559,6 +1562,7 @@ pub mod __rt {
     }
 
     fn borrow_fail(str: &str) -> ! {
+        panic!("wasm_bindgen borrow checker");
         super::throw_str(
             std::format!("recursive use of an object detected which would lead to \
              unsafe aliasing in rust {}", str).as_str(),
