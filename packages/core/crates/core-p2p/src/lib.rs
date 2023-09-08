d5c7cb2eae64eaab2d5008a8e543c2f0147f4063
Tibor Csóka <csokat@gmail.com>
2023-09-08 11:06:55 +0200

    Bump 'hopr/msg' protocol to v 0.2.0

--- packages/core/crates/core-p2p/src/lib.rs
+++ packages/core/crates/core-p2p/src/lib.rs
@@ -29,7 +29,7 @@ pub struct Ping(pub ControlMessage);
 pub struct Pong(pub ControlMessage, pub String);
 
 pub const HOPR_HEARTBEAT_PROTOCOL_V_0_1_0: &str = "/hopr/heartbeat/0.1.0";
-pub const HOPR_MESSAGE_PROTOCOL_V_0_1_0: &str = "/hopr/msg/0.1.0";
+pub const HOPR_MESSAGE_PROTOCOL_V_0_1_0: &str = "/hopr/msg/0.2.0";
 pub const HOPR_ACKNOWLEDGE_PROTOCOL_V_0_1_0: &str = "/hopr/ack/0.1.0";
 
 const HOPR_HEARTBEAT_CONNECTION_KEEPALIVE: std::time::Duration = std::time::Duration::from_secs(3600); // 1 hour
