c667a0de5b5b2adbf94cdc309df544a049a741b2
Lukas <lukas.pohanka@inina.net>
2023-09-19 09:12:54 +0200

    Update packages/core/crates/core-p2p/src/lib.rs
    Co-authored-by: Tino Breddin <tolbrino@users.noreply.github.com>

--- packages/core/crates/core-p2p/src/lib.rs
+++ packages/core/crates/core-p2p/src/lib.rs
@@ -26,7 +26,7 @@ pub struct Pong(pub ControlMessage, pub String);
 pub const HOPR_HEARTBEAT_PROTOCOL_V_0_1_0: &str = "/hopr/heartbeat/0.1.0";
 pub const HOPR_MESSAGE_PROTOCOL_V_0_1_0: &str = "/hopr/msg/0.1.0";
 pub const HOPR_ACKNOWLEDGE_PROTOCOL_V_0_1_0: &str = "/hopr/ack/0.1.0";
-pub const HOPR_TICKET_AGGREGATION_PROTOCOL_V_0_1_0: &str = "/hopr/aggregate_ticket/0.1.0";
+pub const HOPR_TICKET_AGGREGATION_PROTOCOL_V_0_1_0: &str = "/hopr/ticket-aggregation/0.1.0";
 
 const HOPR_HEARTBEAT_CONNECTION_KEEPALIVE: std::time::Duration = std::time::Duration::from_secs(3600); // 1 hour
 const HOPR_HEARTBEAT_REQUEST_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(30);
