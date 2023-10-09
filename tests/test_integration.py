6c3efd86deac899104e3625eec2be1418df0d8ab
NumberFour8 <lukas.pohanka@inina.net>
2023-10-09 17:06:00 +0200

    Fix incorrect expectation

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -106,7 +106,7 @@ async def check_all_tickets_redeemed(src):
 
 
 async def check_unredeemed_tickets_value(src, value):
-    while int((await src["api"].get_tickets_statistics()).unredeemed_value) >= value:
+    while int((await src["api"].get_tickets_statistics()).unredeemed_value) < value:
         await asyncio.sleep(CHECK_RETRY_INTERVAL)
 
 
