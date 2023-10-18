05c50f521c16faf99e8d1b6cc45ee6ade401c504
Lukas <lukas.pohanka@inina.net>
2023-10-18 14:03:37 +0200

    Update aggregating.rs

--- packages/core/crates/core-strategy/src/aggregating.rs
+++ packages/core/crates/core-strategy/src/aggregating.rs
@@ -116,7 +116,7 @@ impl<Db: HoprCoreEthereumDbActions + Clone, T, U> AggregatingStrategy<Db, T, U> 
 impl<Db: HoprCoreEthereumDbActions + 'static + Clone, T, U> AggregatingStrategy<Db, T, U> {
     async fn start_aggregation(&self, channel: ChannelEntry, redeem_if_failed: bool) -> crate::errors::Result<()> {
         debug!("{self} strategy: starting aggregation in {channel}");
-        // Perform marking as redeem ahead, to avoid concurrent aggregation race here
+        // Perform marking as aggregated ahead, to avoid concurrent aggregation races in spawn_local
         let tickets_to_agg = self
             .db
             .write()
