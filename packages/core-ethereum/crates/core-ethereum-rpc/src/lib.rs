c96aee95fe14db0a40cd54b7cfaf3e6437ad759c
Lukas <lukas.pohanka@inina.net>
2023-11-10 20:10:46 +0100

    Update RPC API
    Updates the API based on recent discussions and RPC provider capabilities.

--- packages/core-ethereum/crates/core-ethereum-rpc/src/lib.rs
+++ packages/core-ethereum/crates/core-ethereum-rpc/src/lib.rs
@@ -1,15 +1,19 @@
-use crate::errors::Result;
 use async_trait::async_trait;
 use core_crypto::types::Hash;
-use futures::Stream;
 use primitive_types::H256;
+use std::fmt::{Display, Formatter};
 use utils_types::primitives::{Address, Balance, BalanceType, U256};
+use utils_types::traits::BinarySerializable;
+
+use crate::errors::Result;
 
 pub use ethers::types::transaction::eip2718::TypedTransaction;
 pub use ethers::types::TxHash;
+pub use futures::channel::mpsc::UnboundedReceiver;
 
 pub mod errors;
 //pub mod rpc;
+//pub mod indexer;
 
 //#[cfg(all(target_arch = "wasm32", feature = "wasm"))]
 //pub mod nodejs;
@@ -18,14 +22,28 @@ pub mod errors;
 /// calls.
 #[derive(Debug, Clone)]
 pub struct Block {
-    /// Block number or `None` if pending.
+    /// Block number
     pub number: Option<u64>,
-    /// Block hash or `None` if pending.
+    /// Block hash if any.
     pub hash: Option<Hash>,
     /// Block timestamp
     pub timestamp: U256,
     /// Transaction hashes within this block
     pub transactions: Vec<Hash>,
+}
+
+impl Display for Block {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} (@ {}) with {} txs",
+            self.number
+                .map(|i| format!("block #{i}"))
+                .unwrap_or("pending block".into()),
+            self.timestamp.as_u64(),
+            self.transactions.len()
+        )
+    }
 }
 
 impl From<ethers::types::Block<H256>> for Block {
@@ -42,11 +60,17 @@ impl From<ethers::types::Block<H256>> for Block {
 /// A type containing selected fields from  the `eth_getLogs` RPC calls.
 #[derive(Debug, Clone)]
 pub struct Log {
+    /// Contract address
     pub address: Address,
+    /// Topics
     pub topics: Vec<Hash>,
+    /// Raw log data
     pub data: Box<[u8]>,
+    /// Transaction index
     pub tx_index: Option<u64>,
+    /// Corresponding block number
     pub block_number: Option<u64>,
+    /// Log index
     pub log_index: Option<U256>,
 }
 
@@ -63,45 +87,96 @@ impl From<ethers::types::Log> for Log {
     }
 }
 
+impl From<Log> for ethers::abi::RawLog {
+    fn from(value: Log) -> Self {
+        ethers::abi::RawLog {
+            topics: value.topics.iter().map(|h| H256::from_slice(&h.to_bytes())).collect(),
+            data: value.data.into(),
+        }
+    }
+}
+
+impl Display for Log {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "log of {} with {} topics", self.address, self.topics.len())
+    }
+}
+
+/// Represents a mined block with also corresponding logs fetched (according to some `EventsQuery` filter).
+#[derive(Debug, Clone)]
+pub struct BlockWithLogs {
+    /// Block with TX hashes.
+    pub block: Block,
+    /// Logs of interest corresponding to the block.
+    pub logs: Vec<Log>,
+}
+
+impl Display for BlockWithLogs {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} and {} logs", self.block, self.logs.len())
+    }
+}
+
 /// Represents a query to extract logs containing specific contract events.
 #[derive(Debug, Clone)]
 pub struct EventsQuery {
     /// Contract address
     pub address: Address,
-    /// Event topics
+    /// Event topics (maximum 4)
     pub topics: Vec<TxHash>,
-    /// Start block number
-    pub from: u64,
-    /// End block number
-    pub to: u64,
 }
 
+impl From<EventsQuery> for ethers::types::Filter {
+    fn from(value: EventsQuery) -> Self {
+        let addr: ethers::types::H160 = value.address.into();
+        let mut ret = ethers::types::Filter::new().address::<ethers::types::H160>(addr.into());
+
+        for i in 0..4.min(value.topics.len()) {
+            ret.topics[i] = Some(value.topics[i].into())
+        }
+
+        ret
+    }
+}
+
+/// Trait defining general set of operations an RPC provider
+/// must provide to the HOPR node.
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 pub trait HoprRpcOperations {
-    type BlockStream: Stream<Item = Block>;
-
-    type LogStream: Stream<Item = Log>;
-
-    async fn genesis_block(&self) -> Result<u64>;
-
-    async fn block_number(&self) -> Result<u64>;
-
+    /// Retrieves the timestamp from the given block number.
     async fn get_timestamp(&self, block_number: u64) -> Result<Option<u64>>;
 
+    /// Retrieves the node's account balance of the given type.
     async fn get_balance(&self, balance_type: BalanceType) -> Result<Balance>;
 
-    async fn get_transactions_in_block(&self, block_number: u64) -> Result<Vec<Hash>>;
-
+    /// Retrieves info of the given node module's target.
     async fn get_node_management_module_target_info(&self, target: Address) -> Result<Option<U256>>;
 
+    /// Retrieves safe address of the given node address from the registry.
     async fn get_safe_from_node_safe_registry(&self, node: Address) -> Result<Address>;
 
+    /// Retrieves target address of the node module.
     async fn get_module_target_address(&self) -> Result<Address>;
 
+    /// Sends transaction to the RPC provider.
     async fn send_transaction(&self, tx: TypedTransaction) -> Result<Hash>;
+}
 
-    async fn subscribe_blocks(&self) -> Result<Self::BlockStream>;
+/// Extension of `HoprRpcOperations` trait with functionality required by the Indexer.
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+pub trait HoprIndexerRpcOperations: HoprRpcOperations {
+    /// Retrieves the latest block number.
+    async fn block_number(&self) -> Result<u64>;
 
-    async fn subscribe_logs(&self, query: EventsQuery) -> Result<Self::LogStream>;
+    /// Starts streaming the blocks with logs from the given `start_block_number`.
+    /// If no `start_block_number` is given, the stream starts from the latest block.
+    /// The given `filter` is applied to retrieve the logs for each retrieved block.
+    /// The streaming stops only when the corresponding channel is closed by the returned receiver.
+    async fn poll_blocks_with_logs(
+        &self,
+        start_block_number: Option<u64>,
+        filter: EventsQuery,
+    ) -> Result<UnboundedReceiver<BlockWithLogs>>;
 }
