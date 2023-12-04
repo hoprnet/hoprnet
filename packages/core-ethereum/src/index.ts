a0cb56e4e168a78f5ac8c63ea7d999b7f350fcf2
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2023-12-04 16:38:23 +0800

    One safe multi nodes support (#5715)

--- packages/core-ethereum/src/index.ts
+++ packages/core-ethereum/src/index.ts
@@ -354,7 +354,10 @@ export default class HoprCoreEthereum extends EventEmitter {
       log('Node is not associated with a Safe in NodeSafeRegistry yet')
       return true
     } else if (!registeredAddress.eq(safeAddress)) {
-      throw Error('Node is associated with a different Safe in NodeSafeRegistry')
+      log(
+        'Node is associated with a different Safe in NodeSafeRegistry. Attempt to re-register. If re-registration fails, the node will node start'
+      )
+      return true
     } else {
       log('Node is associated with correct Safe in NodeSafeRegistry')
     }
