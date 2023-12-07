97a83058fbca4cca26d1036930564c0e03d08b75
Tino Breddin <tino@hoprnet.org>
2023-12-07 16:20:02 +0100

    scripts: Some minor scripts improvements

--- scripts/merge-back.sh
+++ scripts/merge-back.sh
@@ -23,50 +23,52 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-release_name=${1} # Can be any of these values Build | ReleaseCandidate | Patch | Minor | Major
+release_name="${1:-}"
 
+if [ "$(jq '.["'${release_name}'"]' "${mydir}/../packages/hoprd/releases.json")" = "null" ]; then
+  echo "Error: Parameter <release_name> contains unsupported value"
+  exit 1
+fi
 
-if [  -z "$(git status --porcelain)" ]; then 
-  git checkout release/${release_name}
+exit 1
+if [ -z "$(git status --porcelain)" ]; then
+  git checkout "release/${release_name}"
   git pull
   git checkout master
   git pull
-  git checkout -b merge-back-release-${release_name}
-  git merge release/${release_name} > /tmp/merge.log || true
+  git checkout -b "merge-back-release-${release_name}"
+  git merge "release/${release_name}" > /tmp/merge.log || true
   cat /tmp/merge.log
 
   # Resolve conflicts for package.json
   packages=(core-ethereum core hoprd real utils)
   for package in "${packages[@]}"; do
-    if [ `git diff packages/$package/package.json | wc -l` -gt "0" ]; then 
-      if [ `git diff packages/$package/package.json  | grep @@ | wc -l` -eq "1" ]; then
-        git checkout --theirs packages/$package/package.json
-        git add packages/$package/package.json
+    if [ `git diff packages/${package}/package.json | wc -l` -gt "0" ]; then
+      if [ `git diff packages/${package}/package.json  | grep @@ | wc -l` -eq "1" ]; then
+        git checkout --theirs packages/${package}/package.json
+        git add packages/${package}/package.json
       else
-        echo "[ERROR] Review changes manully for ./packages/$package/package.json"
+        echo "[ERROR] Review changes manually for ./packages/${package}/package.json"
         exit 1
       fi
     fi
   done
 
-
   # Check if there are conflicts
-  conflicts=$(grep "CONFLICT" /tmp/merge.log | grep -v package.json | wc -l)
-  if [[ "$conflicts" -gt "0" ]];
-  then
+  conflicts="$(grep "CONFLICT" /tmp/merge.log | grep -v package.json | wc -l)"
+  if [ "${conflicts}" -gt "0" ]; then
     echo "[ERROR] There are conflicts in source code files"
     echo "[ERROR] You will need to continue manually on branch 'merge-back-release-${release_name}' or abort 'git merge --abort'"
     exit 1
   fi
 
-  
   git commit -am "Merge branch 'master' into merge-back-release-${release_name}"
-  git push --set-upstream origin merge-back-release-${release_name}
+  git push --set-upstream origin "merge-back-release-${release_name}"
   echo "[INFO] Created remote branch merge-back-release-${release_name}"
   today=`date +%Y-%m-%d`
   echo "Creating github pull request using github cli"
-  gh pr create --title "Merge back from ${release_name} - ${today}" --base master --label merge-back --reviewer hoprnet/hopr-development --body "The scope of this PR is to merge back to master all the bug fixing found in release ${release_name}"
-else 
+  gh pr create --title "Merge back from ${release_name} - ${today}" --base master --label merge-back --reviewer hoprnet/hopr-development --body "The scope of this PR is to merge back to master all the bug fixing found in release ${release_type}"
+else
   echo "[ERROR] Clean your workspace before"
   exit 1
 fi
