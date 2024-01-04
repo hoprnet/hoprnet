1a35bbd9c7290ebd6b560a5494dcdfb7f7a8548a
Tibor Csóka <csokat@gmail.com>
2024-01-05 00:09:01 +0100

    ci: service script cleanup

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -20,19 +20,6 @@ mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 new_version="${1}"
 
-# define packages for versioning
-# does not include ethereum, which isn't a real package anymore, just a folder
-declare -a versioned_packages=( utils real hoprd )
-
-# create new version in each package
-for package in "${versioned_packages[@]}"; do
-  echo "Updating package ${package} to version ${new_version}"
-  cd "${mydir}/../packages/${package}"
-  jq ".version = \"${new_version}\"" package.json > package.json.new
-  mv package.json.new package.json
-  cd "${mydir}/.."
-done
-
 # Update hopr-lib Rust manifest
 sed -i'.original' 's/^version = ".*"$/version = "'${new_version}'"/' ${mydir}/../packages/hoprd/crates/hopr-lib/Cargo.toml
 rm ${mydir}/../packages/hoprd/crates/hopr-lib/Cargo.toml.original
