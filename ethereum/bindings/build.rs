26c297239625e38b2f592f8eba8673a3d088d242
Tibor Csóka <csokat@gmail.com>
2024-01-17 22:37:32 +0100

    Use build.rs script instead of Makefile

--- ethereum/bindings/build.rs
+++ ethereum/bindings/build.rs
@@ -1,19 +1,21 @@
-use std::process::Command;
+//! Creates a build specification for the smart contract codegen.
+
 use std::env;
 use std::path::Path;
-
+use std::process::Command;
 
 fn main() {
-    let contracts_package_path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
-        .parent()
-        .unwrap()
-        .join("contracts");
+    let cargo_manifest_dir = &env::var("CARGO_MANIFEST_DIR").expect("Points to a valid manifest dir");
 
-    Command::new("make")
-        .args(&["generate-bindings"])
-        .current_dir(contracts_package_path.clone())
-        .status()
-        .unwrap();
+    let contracts_package_path = Path::new(&cargo_manifest_dir).parent().unwrap().join("contracts");
 
     println!("cargo:rerun-if-changed={}", contracts_package_path.to_str().unwrap());
+
+    // Ideally this would run a `forge bind` command directly
+    assert!(Command::new("make")
+        .args(["generate-bindings"])
+        .current_dir(contracts_package_path.clone())
+        .status()
+        .unwrap()
+        .success());
 }
