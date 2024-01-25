32a9f9850737b7d921c5d6431502cc2147721fc0
Tibor <9529609+Teebor-Choka@users.noreply.github.com>
2024-01-25 15:27:04 +0100

    Apply suggestions from code review
    Co-authored-by: Tino Breddin <tolbrino@users.noreply.github.com>

--- scripts/generate-identity.sh
+++ scripts/generate-identity.sh
@@ -21,14 +21,14 @@ fi
 mkdir -p gen
 
 if [ ! -f ./gen/node_0.id ]; then
-  ./.cargo/bin/hopli identity \
+  hopli identity \
     --action create \
     --identity-directory "./gen" \
     --identity-prefix node_ \
     --number 1
 fi
 
-env DEPLOYER_PRIVATE_KEY="${DEPLOYER_PRIVATE_KEY}" ./.cargo/bin/hopli \
+env DEPLOYER_PRIVATE_KEY="${DEPLOYER_PRIVATE_KEY}" hopli \
   create-safe-module --network $HOPR_NETWORK \
   --identity-directory "./gen" \
   --hopr-amount 10 --native-amount 0.1 \
