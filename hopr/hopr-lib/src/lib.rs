6eb4a556b0f4ad11e034f1e80c2c6c3db54effaf
NumberFour8 <lukas.pohanka@inina.net>
2024-01-31 16:40:14 +0100

    Remove unused import

--- hopr/hopr-lib/src/lib.rs
+++ hopr/hopr-lib/src/lib.rs
@@ -59,8 +59,7 @@ use core_transport::{
 use core_transport::{ChainKeypair, Hash, HoprTransport, OffchainKeypair};
 use core_transport::{ExternalNetworkInteractions, IndexerToProcess, Network, PeerEligibility, PeerOrigin};
 use hopr_platform::file::native::{join, read_file, remove_dir_all, write};
-use log::{debug, warn};
-use log::{error, info};
+use log::{debug, error, info};
 use utils_db::db::DB;
 use utils_db::CurrentDbShim;
 
