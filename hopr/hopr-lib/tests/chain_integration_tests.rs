57b24ac6967203487f271640f82e2a61b17fa0dc
Tibor Csóka <csokat@gmail.com>
2024-02-15 14:48:35 +0100

    Remove the env logger from the chain integration test

--- hopr/hopr-lib/tests/chain_integration_tests.rs
+++ hopr/hopr-lib/tests/chain_integration_tests.rs
@@ -237,8 +237,6 @@ async fn start_node_chain_logic(
 
 #[async_std::test]
 async fn integration_test_indexer() {
-    let _ = env_logger::builder().is_test(true).try_init();
-
     let block_time = Duration::from_secs(1);
     let anvil = create_anvil(Some(block_time));
     let contract_deployer = ChainKeypair::from_secret(anvil.keys()[0].to_bytes().as_ref()).unwrap();
