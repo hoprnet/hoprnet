c67058392c4abbe3ac5603a3ebecf095f5ed681f
Tibor Csóka <csokat@gmail.com>
2024-02-21 13:05:11 +0100

    Decrease the read lock scope in channel closing strategy

--- logic/strategy/src/strategy.rs
+++ logic/strategy/src/strategy.rs
@@ -104,10 +104,10 @@ impl<Db: HoprCoreEthereumDbActions + Clone + Send + Sync> SingularStrategy for C
         // Do not attempt to finalize closure of channels that have been overdue for closure for more than an hour
         let ts_limit = current_time().sub(Duration::from_secs(3600));
 
-        let out_channels = self.db.read().await.get_outgoing_channels().await?;
+        let outgoing_channels = self.db.read().await.get_outgoing_channels().await?;
 
-        let to_close = out_channels
-            .into_iter()
+        let to_close = outgoing_channels
+            .iter()
             .filter(|channel| {
                 matches!(channel.status, ChannelStatus::PendingToClose(ct) if ct > ts_limit)
                     && channel.closure_time_passed(current_time())
