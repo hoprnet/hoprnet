0e17311de7f2aafd9f3784bee9afdd4bd36c34c1
Tibor <9529609+Teebor-Choka@users.noreply.github.com>
2024-04-03 22:56:07 +0200

    Move the peers database purge immediately into the DB creation (#6146)

--- db/api/src/tickets.rs
+++ db/api/src/tickets.rs
@@ -389,7 +389,10 @@ impl Default for ChannelTicketStatistics {
     }
 }
 
-async fn find_stats_for_channel(tx: &OpenTransaction, channel_id: &Hash) -> Result<ticket_statistics::Model> {
+pub(crate) async fn find_stats_for_channel(
+    tx: &OpenTransaction,
+    channel_id: &Hash,
+) -> Result<ticket_statistics::Model> {
     if let Some(model) = ticket_statistics::Entity::find()
         .filter(ticket_statistics::Column::ChannelId.eq(channel_id.to_hex()))
         .one(tx.as_ref())
@@ -686,6 +689,7 @@ impl HoprDbTicketOperations for HoprDb {
                                         acc.redeemed_value + BalanceType::HOPR.balance_bytes(stats.redeemed_value);
                                     acc.rejected_value =
                                         acc.rejected_value + BalanceType::HOPR.balance_bytes(stats.rejected_value);
+                                    acc.winning_tickets += stats.winning_tickets as u128;
                                     acc
                                 });
 
