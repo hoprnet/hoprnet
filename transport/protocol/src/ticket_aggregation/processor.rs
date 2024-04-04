49fcb7882e9fd065a69b220c5636b3dafaa15afd
Tibor Csóka <csokat@gmail.com>
2024-04-04 15:27:01 +0200

    Fix aggregating on empty ticket list

--- transport/protocol/src/ticket_aggregation/processor.rs
+++ transport/protocol/src/ticket_aggregation/processor.rs
@@ -334,7 +334,7 @@ where
                     }
                     TicketAggregationToProcess::ToSend(channel, prerequsites, finalizer) => {
                         match db.prepare_aggregation_in_channel(&channel, prerequsites).await {
-                            Ok(Some((source, tickets))) => {
+                            Ok(Some((source, tickets))) if !tickets.is_empty() => {
                                 #[cfg(all(feature = "prometheus", not(test)))]
                                 {
                                     METRIC_AGGREGATED_TICKETS.increment_by(tickets.len() as u64);
@@ -342,12 +342,14 @@ where
                                 }
 
                                 Some(TicketAggregationProcessed::Send(source.into(), tickets, finalizer))
-                            }
-                            Ok(None) => { finalizer.finalize(); None },
+                            },
                             Err(e) => {
                                 error!("An error occured when preparing the channel aggregation: {e}");
                                 None
                             },
+                            _ => {
+                                finalizer.finalize(); None
+                            }
                         }
                     }
                 };
