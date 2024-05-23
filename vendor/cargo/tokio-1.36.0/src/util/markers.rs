b4800213a0b4155bede72716ed5195001904a1a6
Tibor Csóka <csokat@gmail.com>
2024-05-23 12:48:48 +0200

    Update tokio in the vendor

--- vendor/cargo/tokio-1.36.0/src/util/markers.rs
+++
@@ -1,8 +0,0 @@
-/// Marker for types that are `Sync` but not `Send`
-pub(crate) struct SyncNotSend(*mut ());
-
-unsafe impl Sync for SyncNotSend {}
-
-cfg_rt! {
-    pub(crate) struct NotSendOrSync(*mut ());
-}
