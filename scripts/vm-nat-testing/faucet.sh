6be8293eb46f353925b78ab5fdee114358a32f2f
Tibor <9529609+Teebor-Choka@users.noreply.github.com>
2024-05-31 12:26:21 +0200

    Update the docker compose based deployment (#6307)

--- scripts/vm-nat-testing/faucet.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /opt/hopr || exit 1
-
-IDENTITY_PASSWORD=switzerland PRIVATE_KEY=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
-	hopli faucet \
-	--environment-name anvil-localhost \
-	--identity-prefix "local" --identity-directory "/var/hopr/identities/" \
-	--contracts-root "./ethereum/contracts"
