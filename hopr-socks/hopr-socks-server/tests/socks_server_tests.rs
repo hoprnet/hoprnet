c7f29f7afeeb3d3d3eb227f689f24f01f79b5a00
Jean Demeusy <jdemeusy@me.com>
2024-06-21 15:12:02 +0200

    test checks back in

--- hopr-socks/hopr-socks-server/tests/socks_server_tests.rs
+++ hopr-socks/hopr-socks-server/tests/socks_server_tests.rs
@@ -1,26 +1,27 @@
 use hopr_socks_server::cli::AuthMode;
 use hopr_socks_server::SocksServer;
-use reqwest::{Client, Proxy};
+use reqwest::{Client, Error, Proxy};
 use tokio::time::{sleep, Duration};
 
-static SERVER_DELAY: Duration = Duration::from_millis(100);
+static SERVER_DELAY: Duration = Duration::from_millis(50);
 
-async fn socks_client(bind_address: String, auth: AuthMode) -> Client {
+async fn socks_client(bind_address: String, auth: AuthMode) -> Result<Client, Error> {
     let proxy = Proxy::all(match auth {
         AuthMode::NoAuth => format!("socks5://{}", bind_address),
         AuthMode::Password { username, password } => format!("socks5://{}:{}@{}", username, password, bind_address),
     })
     .unwrap();
 
-    let client = Client::builder().proxy(proxy).build().unwrap();
-    return client;
+    return Client::builder().proxy(proxy).build();
 }
 
-async fn get_request(client: &Client, url: String) -> Vec<u8> {
-    let response = client.get(&url).send().await.unwrap();
-    let body = response.bytes().await.unwrap();
+async fn get_request(client: &Client, url: String) -> Result<Vec<u8>, Error> {
+    let response = client.get(&url).timeout(Duration::from_secs(5)).send().await?;
 
-    return body.to_vec();
+    match response.bytes().await {
+        Ok(body) => Ok(body.to_vec()),
+        Err(e) => Err(e),
+    }
 }
 
 #[tokio::test]
@@ -36,10 +37,14 @@ async fn test_connect_client_incorrect_bind_address() {
             .expect("SOCKS5 server could not be run");
     });
 
-    sleep(SERVER_DELAY).await;
-
     // Open TCP connection through proxy (incorrect bind address)
-    let _client = socks_client(String::from("127.0.0.2:1332"), AuthMode::NoAuth).await;
+    let client = socks_client(String::from("127.0.0.2:1332"), AuthMode::NoAuth)
+        .await
+        .expect("Failed to create a SOCKS5 client");
+
+    assert!(get_request(&client, String::from("http://www.example.com"))
+        .await
+        .is_err());
 
     server_proc.abort();
     sleep(SERVER_DELAY).await;
@@ -61,7 +66,12 @@ async fn test_connect_client_correct_bind_address() {
     sleep(SERVER_DELAY).await;
 
     // Open TCP connection through proxy (correct bind address)
-    let _client = socks_client(String::from(bind_address), AuthMode::NoAuth).await;
+    let client = socks_client(String::from(bind_address), AuthMode::NoAuth)
+        .await
+        .expect("Failed to create a SOCKS5 client");
+    let _response: Vec<u8> = get_request(&client, String::from("http://www.example.com"))
+        .await
+        .expect("Failed to send HTTP request");
 
     server_proc.abort();
     sleep(SERVER_DELAY).await;
@@ -84,8 +94,12 @@ async fn test_http_request_through_socks_proxy() {
     sleep(SERVER_DELAY).await;
 
     // Open TCP connection to existing endpoint
-    let client = socks_client(String::from(bind_address), AuthMode::NoAuth).await;
-    let _response: Vec<u8> = get_request(&client, String::from(host_address)).await;
+    let client = socks_client(String::from(bind_address), AuthMode::NoAuth)
+        .await
+        .expect("Failed to create a SOCKS5 client");
+    let _response: Vec<u8> = get_request(&client, String::from(host_address))
+        .await
+        .expect("Failed to send HTTP request");
 
     server_proc.abort();
     sleep(SERVER_DELAY).await;
@@ -109,13 +123,18 @@ async fn test_multiple_clients() {
 
     // Create a vector of Socks5Stream<TcpStream> clients
     let mut clients = Vec::new();
-    for _ in 0..20 {
-        clients.push(socks_client(String::from(bind_address), AuthMode::NoAuth).await);
+    for _ in 0..5 {
+        let client = socks_client(String::from(bind_address), AuthMode::NoAuth)
+            .await
+            .expect("Failed to create a SOCKS5 client");
+        clients.push(client);
     }
 
     // Send HTTP request to each client
     for client in clients {
-        let _results = get_request(&client, String::from(host_address)).await;
+        get_request(&client, String::from(host_address))
+            .await
+            .expect("Failed to send HTTP request");
     }
 
     server_proc.abort();
@@ -146,14 +165,19 @@ async fn test_connect_unauthenticated_client_with_auth() {
 
     sleep(SERVER_DELAY).await;
 
-    let _client = socks_client(
+    let client = socks_client(
         String::from(bind_address),
         AuthMode::Password {
             username: String::from(auth_username),
             password: String::from("wrong_password"),
         },
     )
-    .await;
+    .await
+    .expect("Failed to create a SOCKS5 client");
+
+    assert!(get_request(&client, String::from("http://www.example.com"))
+        .await
+        .is_err());
 
     server_proc.abort();
     sleep(SERVER_DELAY).await;
@@ -183,14 +207,19 @@ async fn test_connect_authenticated_client_with_auth() {
 
     sleep(SERVER_DELAY).await;
 
-    let _client = socks_client(
+    let client = socks_client(
         String::from(bind_address),
         AuthMode::Password {
             username: String::from(auth_username),
             password: String::from(auth_password),
         },
     )
-    .await;
+    .await
+    .expect("Failed to create a SOCKS5 client");
+
+    let _response = get_request(&client, String::from("http://www.example.com"))
+        .await
+        .expect("Failed to send HTTP request");
 
     server_proc.abort();
     sleep(SERVER_DELAY).await;
@@ -199,7 +228,7 @@ async fn test_connect_authenticated_client_with_auth() {
 #[tokio::test]
 async fn test_https_request_through_socks_proxy() {
     let bind_address = "127.0.0.1:1337";
-    let host_address = "https://www.hoprnet.org";
+    let host_address = "https://www.example.com";
 
     let server_proc = tokio::spawn(async move {
         SocksServer::new(String::from(bind_address), 10, AuthMode::NoAuth)
@@ -212,10 +241,12 @@ async fn test_https_request_through_socks_proxy() {
 
     sleep(SERVER_DELAY).await;
 
-    let client = socks_client(String::from(bind_address), AuthMode::NoAuth).await;
-    let _response = get_request(&client, String::from(host_address)).await;
-
-    // eprintln!("{}", String::from_utf8(_response).unwrap());
+    let client = socks_client(String::from(bind_address), AuthMode::NoAuth)
+        .await
+        .expect("Failed to create a SOCKS5 client");
+    let _response = get_request(&client, String::from(host_address))
+        .await
+        .expect("Failed to send HTTP request");
 
     server_proc.abort();
     sleep(SERVER_DELAY).await;
