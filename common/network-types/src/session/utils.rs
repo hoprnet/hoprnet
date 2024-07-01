e440407340949adaafa3d5bc45ff258d761b7ecb
Lukas <lukas.pohanka@inina.net>
2024-07-01 21:16:21 +0200

    Use AsyncRead + AsyncWrite as the underlying downstream transport (#6348)

--- common/network-types/src/session/utils.rs
+++ common/network-types/src/session/utils.rs
@@ -1,3 +1,6 @@
+use futures::{AsyncRead, AsyncWrite, Stream};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 use std::time::{Duration, Instant};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -42,5 +45,127 @@ impl RetryToken {
                 backoff_base: self.backoff_base,
             }),
         }
+    }
+}
+
+pub(crate) struct AsyncReadStreamer<R: AsyncRead + Unpin, const S: usize>(R);
+
+impl<R: AsyncRead + Unpin, const S: usize> AsyncReadStreamer<R, S> {
+    pub fn new(reader: R) -> Self {
+        Self(reader)
+    }
+}
+
+impl<R: AsyncRead + Unpin, const S: usize> Stream for AsyncReadStreamer<R, S> {
+    type Item = std::io::Result<Box<[u8]>>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut buffer = vec![0u8; S];
+
+        match Pin::new(&mut self.0).poll_read(cx, &mut buffer) {
+            Poll::Ready(result) => match result {
+                Ok(size) => {
+                    if size == 0 {
+                        Poll::Ready(None)
+                    } else {
+                        buffer.truncate(size);
+                        Poll::Ready(Some(Ok(buffer.into_boxed_slice())))
+                    }
+                }
+                Err(err) => Poll::Ready(Some(Err(err))),
+            },
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+pub(crate) struct DuplexIO<R, W>(pub R, pub W);
+
+impl<R, W> AsyncRead for DuplexIO<R, W>
+where
+    R: AsyncRead + Unpin,
+    W: AsyncWrite + Unpin,
+{
+    fn poll_read(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<std::io::Result<usize>> {
+        let this = self.get_mut();
+        Pin::new(&mut this.0).poll_read(cx, buf)
+    }
+}
+
+impl<R, W> AsyncWrite for DuplexIO<R, W>
+where
+    R: AsyncRead + Unpin,
+    W: AsyncWrite + Unpin,
+{
+    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<std::io::Result<usize>> {
+        let this = self.get_mut();
+        Pin::new(&mut this.1).poll_write(cx, buf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        let this = self.get_mut();
+        Pin::new(&mut this.1).poll_flush(cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        let this = self.get_mut();
+        Pin::new(&mut this.1).poll_close(cx)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::TryStreamExt;
+
+    #[async_std::test]
+    async fn test_async_read_streamer_complete_chunk() {
+        let data = b"Hello, World!!";
+        let mut streamer = AsyncReadStreamer::<_, 14>::new(&data[..]);
+        let mut results = Vec::new();
+
+        while let Some(res) = streamer.try_next().await.unwrap() {
+            results.push(res);
+        }
+
+        assert_eq!(results, vec![Box::from(*data)]);
+    }
+
+    #[async_std::test]
+    async fn test_async_read_streamer_complete_more_chunks() {
+        let data = b"Hello, World and do it twice";
+        let mut streamer = AsyncReadStreamer::<_, 14>::new(&data[..]);
+        let mut results = Vec::new();
+
+        while let Some(res) = streamer.try_next().await.unwrap() {
+            results.push(res);
+        }
+
+        let (data1, data2) = data.split_at(14);
+        assert_eq!(results, vec![Box::from(data1), Box::from(data2)]);
+    }
+
+    #[async_std::test]
+    async fn test_async_read_streamer_complete_more_chunks_with_incomplete() {
+        let data = b"Hello, World and do it twice, ...";
+        let streamer = AsyncReadStreamer::<_, 14>::new(&data[..]);
+
+        let results = streamer.try_collect::<Vec<_>>().await.expect("should get chunks");
+
+        let (data1, rest) = data.split_at(14);
+        let (data2, data3) = rest.split_at(14);
+        assert_eq!(results, vec![Box::from(data1), Box::from(data2), Box::from(data3)]);
+    }
+
+    #[async_std::test]
+    async fn test_async_read_streamer_incomplete_chunk() {
+        let data = b"Hello, World!!";
+        let reader = &data[0..8]; // An incomplete chunk
+        let mut streamer = AsyncReadStreamer::<_, 14>::new(reader);
+
+        assert_eq!(
+            Some(Box::from(reader)),
+            streamer.try_next().await.expect("should get chunk")
+        );
     }
 }
