b323794f38decd62c91c7dcba0ecaf40b51597a0
Tibor Csóka <csokat@gmail.com>
2024-09-03 14:50:30 +0200

    Fix minor issues in the fixture to tear down anvil

--- tests/conftest.py
+++ tests/conftest.py
@@ -514,8 +514,9 @@ async def swarm7(request):
     yield nodes
 
     # POST TEST CLEANUP
-    logging.debug(f"Tearing down the {len(nodes)} nodes cluster")
+    logging.info(f"Tearing down the {len(nodes)} nodes cluster")
     [node.clean_up() for node in nodes.values()]
+    run(["make", "kill-anvil", f"port={anvil_port}"], cwd=PWD.parent, check=True)
 
 
 def to_ws_url(host, port):
