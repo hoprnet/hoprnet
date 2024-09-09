7c32585ead90e07b59f1d0017b98bdf62c84c12c
Tibor Csóka <csokat@gmail.com>
2024-09-09 12:39:43 +0200

    Update the packages

--- hoprd/keypair/src/key_pair.rs
+++ hoprd/keypair/src/key_pair.rs
@@ -16,7 +16,7 @@ use serde::{ser::SerializeStruct, Serialize, Serializer};
 use serde_json::{from_str as from_json_string, to_string as to_json_string};
 use sha3::{digest::Update, Digest, Keccak256};
 use std::fmt::Debug;
-use tracing::{error, info, warn};
+use tracing::{info, warn};
 use typenum::Unsigned;
 use uuid::Uuid;
 
