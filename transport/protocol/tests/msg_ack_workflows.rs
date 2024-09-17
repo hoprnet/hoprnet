c96693db42f6e243c56ffd2d239b5a9cd34fcc3c
NumberFour8 <lukas.pohanka@inina.net>
2024-09-17 12:39:02 +0200

    Fix unit test build

--- transport/protocol/tests/msg_ack_workflows.rs
+++ transport/protocol/tests/msg_ack_workflows.rs
@@ -210,6 +210,7 @@ async fn peer_setup_for(count: usize) -> anyhow::Result<(Vec<WireChannels>, Vec<
             packet_keypair: opk.clone(),
             chain_keypair: ock.clone(),
             mixer: MixerConfig::default(), // TODO: unnecessary, can be removed
+            outgoing_ticket_win_prob: 1.0,
         };
 
         hopr_transport_protocol::run_msg_ack_protocol(
