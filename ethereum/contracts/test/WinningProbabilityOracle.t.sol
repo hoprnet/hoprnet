61230d7fbb2dc006fde6393154094ea704acb22e
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2024-09-20 07:56:31 +0200

    add test for winning probability contract

---
+++ ethereum/contracts/test/WinningProbabilityOracle.t.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.6.0 <0.9.0;
+
+import { Test } from "forge-std/Test.sol";
+import { Ownable2Step } from "openzeppelin-contracts/access/Ownable2Step.sol";
+import { WinProb, HoprWinningProbablityOracle, HoprWinningProbablityOracleEvents } from "../src/WinningProbabilityOracle.sol";
+
+contract Ownable2StepEvents {
+    event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+}
+
+contract TicketWinningProbabilityOracleTest is Test, HoprWinningProbablityOracleEvents, Ownable2StepEvents{
+    HoprWinningProbablityOracle public oracle;
+    address public owner;
+
+    function setUp() public {
+        owner = vm.addr(101); // make address(101) new owner
+        oracle = new HoprWinningProbablityOracle(owner, WinProb.wrap(0xffffffffffffff));
+    }
+
+    function test_setUpWithZero() public {
+        HoprWinningProbablityOracle newOracle = new HoprWinningProbablityOracle(owner, WinProb.wrap(0));
+        assertEq(address(oracle).code, address(newOracle).code);
+    }
+
+    function test_setZero() public {
+        vm.prank(owner);
+        vm.expectEmit(true, false, false, false, address(oracle));
+        emit WinProbUpdated(WinProb.wrap(0xffffffffffffff), WinProb.wrap(0));
+        oracle.setWinProb(WinProb.wrap(0));
+    }
+
+    function testRevert_setSameFails() public {
+        vm.prank(owner);
+        vm.expectRevert(HoprWinningProbablityOracle.WinProbMustNotBeSame.selector);
+        oracle.setWinProb(WinProb.wrap(0xffffffffffffff));
+    }
+
+    function testRevert_setByNonOwnerFails() public {
+        vm.prank(vm.addr(102));
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        oracle.setWinProb(WinProb.wrap(0xffffffffffff00));
+    }
+
+    function testFuzz_setWinProb(WinProb newWinProb) public {
+        // vm.assume(newWinProb != oracle.currentWinProb());
+        if (newWinProb == oracle.currentWinProb()) {
+            // the new winning probability must not be the same as the current one
+            vm.expectRevert(HoprWinningProbablityOracle.WinProbMustNotBeSame.selector);
+        } else {
+            vm.expectEmit(true, false, false, false, address(oracle));
+            emit WinProbUpdated(oracle.currentWinProb(), newWinProb);
+        }
+
+        vm.prank(owner);
+        oracle.setWinProb(newWinProb);
+    }
+
+    function test_setALargeValueFails() public {
+        // use a value that is larger than the max of WinProb type
+        uint256 largeValue = type(uint256).max;
+        vm.prank(owner);
+
+        // fail to use a low-level call to set a large value
+        bytes memory payload = abi.encodePacked(oracle.setWinProb.selector, largeValue);
+        (bool success, ) = address(oracle).call(payload);
+        assertFalse(success);
+    }
+
+    function test_transferOwnership() public {
+        address newOwner = vm.addr(103);
+        // initiate the ownership transfer
+        vm.prank(owner);
+        vm.expectEmit(true, true, true, false, address(oracle));
+        emit OwnershipTransferStarted(owner, newOwner);
+        oracle.transferOwnership(newOwner);
+
+        // complete the ownership transfer
+        vm.prank(newOwner);
+        vm.expectEmit(true, true, true, false, address(oracle));
+        emit OwnershipTransferred(owner, newOwner);
+        oracle.acceptOwnership();
+    
+        assertEq(oracle.owner(), newOwner);
+    }
+}
