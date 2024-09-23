53164af2b35aa25de4b22ad19cd6c2ebddeaa783
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2024-09-23 16:01:30 +0200

    align cfg file

--- chain/types/src/lib.rs
+++ chain/types/src/lib.rs
@@ -50,7 +50,7 @@ pub struct ContractAddresses {
     pub safe_registry: Address,
     /// Price oracle contract
     pub price_oracle: Address,
-    /// Minimum ticket winning probability contract,
+    /// Minimum ticket winning probability contract
     pub win_prob_oracle: Address,
     /// Stake factory contract
     pub stake_factory: Address,
