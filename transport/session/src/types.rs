a21287b76b1a86d7b64f8206bc2826daab774630
NumberFour8 <lukas.pohanka@inina.net>
2024-09-27 09:40:19 +0200

    Optimize SessionId

--- transport/session/src/types.rs
+++ transport/session/src/types.rs
@@ -7,7 +7,8 @@ use hopr_network_types::session::state::{SessionConfig, SessionSocket};
 use hopr_primitive_types::traits::BytesRepresentable;
 use libp2p_identity::PeerId;
 use std::collections::HashSet;
-use std::fmt::Formatter;
+use std::fmt::{Debug, Formatter};
+use std::hash::{Hash, Hasher};
 use std::time::Duration;
 use std::{
     fmt::Display,
@@ -39,20 +40,27 @@ const MAX_SESSION_ID_STR_LEN: usize = 100;
 /// Simple wrapper around the maximum range of the port like session unique identifier.
 /// It is a simple combination of an application tag and a peer id that will in future be
 /// replaced by a more robust session id representation.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct SessionId {
     tag: u16,
     peer: PeerId,
-    cached: arrayvec::ArrayString<MAX_SESSION_ID_STR_LEN>, // Allows the SessionId to be Copy
+    // Since this SessionId is commonly represented as a string,
+    // we cache its string representation here.
+    // Also by using a statically allocated ArrayString, we allow the SessionId to remain Copy.
+    // This representation is possibly truncated to MAX_SESSION_ID_STR_LEN.
+    cached: arrayvec::ArrayString<MAX_SESSION_ID_STR_LEN>,
 }
 
 impl SessionId {
     pub fn new(tag: u16, peer: PeerId) -> Self {
+        let mut cached = format!("{peer}:{tag}");
+        cached.truncate(MAX_SESSION_ID_STR_LEN);
+
         Self {
             tag,
             peer,
-            cached: format!("{peer}:{tag}").parse().expect("session id too long"),
+            cached: cached.parse().expect("cannot fail due to truncation"),
         }
     }
 
@@ -77,6 +85,27 @@ impl SessionId {
 impl Display for SessionId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.cached)
+    }
+}
+
+impl Debug for SessionId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.cached)
+    }
+}
+
+impl PartialEq for SessionId {
+    fn eq(&self, other: &Self) -> bool {
+        self.tag == other.tag && self.peer == other.peer
+    }
+}
+
+impl Eq for SessionId {}
+
+impl Hash for SessionId {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.tag.hash(state);
+        self.peer.hash(state);
     }
 }
 
@@ -248,7 +277,7 @@ impl futures::AsyncWrite for Session {
                 if let Some(notifier) = self.shutdown_notifier.take() {
                     if let Err(err) = notifier.unbounded_send(self.id) {
                         error!(
-                            session_id = tracing::field::debug(self.id),
+                            session_id = tracing::field::display(self.id),
                             "failed to notify session closure: {err}"
                         );
                     }
