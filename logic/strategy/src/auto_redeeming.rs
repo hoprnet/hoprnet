4ce1262676e6a2d38c35b760653a51e967198795
NumberFour8 <lukas.pohanka@inina.net>
2024-10-01 10:07:29 +0200

    Fix the default min_redeem_hopr value
    Fixes #6490

--- logic/strategy/src/auto_redeeming.rs
+++ logic/strategy/src/auto_redeeming.rs
@@ -28,8 +28,8 @@ lazy_static::lazy_static! {
         SimpleCounter::new("hopr_strategy_auto_redeem_redeem_count", "Count of initiated automatic redemptions").unwrap();
 }
 
-fn thirty_hopr() -> Balance {
-    Balance::new_from_str("30000000000000000000", BalanceType::HOPR)
+fn min_redeem_hopr() -> Balance {
+    Balance::new_from_str("90000000000000000", BalanceType::HOPR)
 }
 
 /// Configuration object for the `AutoRedeemingStrategy`
@@ -47,10 +47,10 @@ pub struct AutoRedeemingStrategyConfig {
     /// If 0 is given, the strategy will redeem tickets regardless of their value.
     /// This is not used for cases where `on_close_redeem_single_tickets_value_min` applies.
     ///
-    /// Default is 30 HOPR.
-    #[serde(default = "thirty_hopr")]
+    /// Default is 0.09 HOPR.
+    #[serde(default = "min_redeem_hopr")]
     #[serde_as(as = "DisplayFromStr")]
-    #[default(thirty_hopr())]
+    #[default(min_redeem_hopr())]
     pub minimum_redeem_ticket_value: Balance,
 
     /// The strategy will automatically redeem if there's a single ticket left when a channel
