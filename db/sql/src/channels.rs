9b27370ca8db4bdec1bbf447f09c77792ce0c4e1
Tibor Csóka <csokat@gmail.com>
2024-10-04 11:20:32 +0200

    Remove the debugging test

--- db/sql/src/channels.rs
+++ db/sql/src/channels.rs
@@ -289,7 +289,7 @@ impl HoprDbChannelOperations for HoprDb {
 #[cfg(test)]
 mod tests {
     use crate::channels::HoprDbChannelOperations;
-    use crate::db::HoprDb;
+    use crate::db::{HoprDb, HoprDbConfig};
     use crate::HoprDbGeneralModelOperations;
     use anyhow::Context;
     use hopr_crypto_random::random_bytes;
@@ -432,54 +432,6 @@ mod tests {
 
         assert_eq!(vec![ce_2], db.get_incoming_channels(None).await?);
         assert_eq!(vec![ce_1], db.get_outgoing_channels(None).await?);
-
-        Ok(())
-    }
-
-    #[async_std::test]
-    async fn channels_should_dealocate_resources() -> anyhow::Result<()> {
-        let ckp = ChainKeypair::random();
-        let left_addrs = (0..300)
-            .map(|_| ChainKeypair::random().public().to_address())
-            .collect::<Vec<_>>();
-        let right_addrs = (0..300)
-            .map(|_| ChainKeypair::random().public().to_address())
-            .collect::<Vec<_>>();
-
-        let db = HoprDb::new_in_memory(ckp).await?;
-
-        for i in left_addrs {
-            for _ in 0..20 {
-                let db_clone = db.clone();
-                let right = right_addrs.choose(&mut rand::thread_rng()).unwrap().clone();
-                db.begin_transaction()
-                    .await?
-                    .perform(|tx| {
-                        let i = i.clone();
-                        let right = right.clone();
-                        Box::pin(async move {
-                            db_clone
-                                .upsert_channel(
-                                    Some(tx),
-                                    ChannelEntry::new(
-                                        i,
-                                        right,
-                                        BalanceType::HOPR.zero(),
-                                        1_u32.into(),
-                                        ChannelStatus::Open,
-                                        0_u32.into(),
-                                    ),
-                                )
-                                .await
-                        })
-                    })
-                    .await?;
-            }
-        }
-
-        for _ in 0..100 {
-            let _ = db.get_all_channels(None).await?;
-        }
 
         Ok(())
     }
