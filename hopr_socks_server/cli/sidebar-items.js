8874a710213061a0a0184a0e75d5a3969cc7f4e9
Teebor-Choka <Teebor-Choka@users.noreply.github.com>
2024-10-18 15:57:33 +0000

    deploy: e9393389829a641c72d47d99a068208f979d0734

---
+++ hopr_socks_server/cli/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["AuthMode"],"struct":["Opt"]};
---
+++ trait.impl/futures_sink/trait.Sink.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hopr_network_types",[["impl Sink&lt;<a class=\"struct\" href=\"hopr_network_types/session/struct.Segment.html\" title=\"struct hopr_network_types::session::Segment\">Segment</a>&gt; for <a class=\"struct\" href=\"hopr_network_types/session/struct.FrameReassembler.html\" title=\"struct hopr_network_types::session::FrameReassembler\">FrameReassembler</a>"],["impl&lt;const C: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; Sink&lt;<a class=\"enum\" href=\"hopr_network_types/session/protocol/enum.SessionMessage.html\" title=\"enum hopr_network_types::session::protocol::SessionMessage\">SessionMessage</a>&lt;C&gt;&gt; for <a class=\"struct\" href=\"hopr_network_types/session/state/struct.SessionState.html\" title=\"struct hopr_network_types::session::state::SessionState\">SessionState</a>&lt;C&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[866]}
