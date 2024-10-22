24fea0eaed82e36b9c95bf6a15637d9ed8c0afb6
Tibor Csóka <csokat@gmail.com>
2024-10-22 11:52:06 +0200

    Refactor the code to introduce the USABLE_PAYLOAD_SIZE

--- transport/session/src/types.rs
+++ transport/session/src/types.rs
@@ -1,4 +1,5 @@
 use futures::{channel::mpsc::UnboundedReceiver, pin_mut, StreamExt};
+use hopr_crypto_packet::packet::USABLE_PAYLOAD_SIZE;
 use hopr_crypto_types::types::OffchainPublicKey;
 use hopr_internal_types::protocol::{ApplicationData, PAYLOAD_SIZE};
 use hopr_network_types::prelude::state::SessionFeature;
@@ -109,13 +110,9 @@ impl Hash for SessionId {
     }
 }
 
-const PADDING_HEADER_SIZE: usize = 4;
-
 /// Inner MTU size of what the HOPR payload can take (payload - peer_id - application_tag)
-pub const SESSION_USABLE_MTU_SIZE: usize = PAYLOAD_SIZE
-    - OffchainPublicKey::SIZE
-    - std::mem::size_of::<hopr_internal_types::protocol::Tag>()
-    - PADDING_HEADER_SIZE;
+pub const SESSION_USABLE_MTU_SIZE: usize =
+    USABLE_PAYLOAD_SIZE - OffchainPublicKey::SIZE - std::mem::size_of::<hopr_internal_types::protocol::Tag>();
 
 /// Helper trait to allow Box aliasing
 trait AsyncReadWrite: futures::AsyncWrite + futures::AsyncRead + Send {}
@@ -467,7 +464,7 @@ impl futures::AsyncRead for InnerSession {
 
 // TODO: 3.0 remove once return path is implemented
 pub fn wrap_with_offchain_key(peer: &PeerId, data: Box<[u8]>) -> crate::errors::Result<Vec<u8>> {
-    if data.len() > PAYLOAD_SIZE.saturating_sub(OffchainPublicKey::SIZE + PADDING_HEADER_SIZE) {
+    if data.len() > USABLE_PAYLOAD_SIZE.saturating_sub(OffchainPublicKey::SIZE) {
         return Err(TransportSessionError::PayloadSize);
     }
 
