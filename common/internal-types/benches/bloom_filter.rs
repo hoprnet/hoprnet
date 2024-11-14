0bd718397b6eaed111a39d368aa88836a94e80cd
NumberFour8 <lukas.pohanka@inina.net>
2024-11-14 12:59:59 +0100

    Enhance Bloom Filter benchmark to bench also non-existent tags

--- common/internal-types/benches/bloom_filter.rs
+++ common/internal-types/benches/bloom_filter.rs
@@ -1,15 +1,19 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use hopr_crypto_types::types::PACKET_TAG_LENGTH;
 use hopr_internal_types::prelude::TagBloomFilter;
 
 fn tag_bloom_filter_bench(c: &mut Criterion) {
     // Fill up the Bloom filter first
     let mut bloom = TagBloomFilter::default();
     for _ in 1..bloom.capacity() {
-        bloom.set(&hopr_crypto_random::random_bytes());
+        let mut tag = hopr_crypto_random::random_bytes();
+        tag[0] = 0xaa;
+        bloom.set(&tag);
     }
 
-    let tag = hopr_crypto_random::random_bytes();
-    bloom.set(&tag);
+    let mut existing_tag = hopr_crypto_random::random_bytes();
+    existing_tag[0] = 0xaa;
+    bloom.set(&existing_tag);
 
     let mut group = c.benchmark_group("tag_bloom_filter");
     group.sample_size(100_000);
@@ -17,7 +21,14 @@ fn tag_bloom_filter_bench(c: &mut Criterion) {
 
     group.bench_function("check", |b| {
         b.iter(|| {
-            bloom.check(black_box(&tag));
+            bloom.check(black_box(&existing_tag));
+        })
+    });
+
+    let non_existent_tag = [0u8; PACKET_TAG_LENGTH];
+    group.bench_function("check_non_existent", |b| {
+        b.iter(|| {
+            bloom.check(black_box(&non_existent_tag));
         })
     });
 }
