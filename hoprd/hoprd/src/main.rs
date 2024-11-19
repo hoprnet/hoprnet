5ccfa4f0a4fe46cc32ec6a3baa2caabbfe54be31
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2024-11-19 15:43:06 +0100

    fix

--- hoprd/hoprd/src/main.rs
+++ hoprd/hoprd/src/main.rs
@@ -247,11 +247,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     // Ensures that "OWN_ALIAS" is set as alias
-    if let Err(e) = hoprd_db
-        .set_alias(node.me_peer_id().to_string(), ME_AS_ALIAS.to_string())
-        .await
-    {
-        error!(error = %e, "Failed to set the alias for the node");
+    if hoprd_db.resolve_alias(ME_AS_ALIAS.to_string()).await?.is_none() {
+        if let Err(e) = hoprd_db
+            .set_alias(node.me_peer_id().to_string(), ME_AS_ALIAS.to_string())
+            .await
+        {
+            error!(error = %e, "Failed to set the alias for the node");
+        }
     }
 
     let (mut ws_events_tx, ws_events_rx) =
