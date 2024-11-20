0babb657d15c535f323b37dd0223e0841b6e99bf
Tino Breddin <tino@hoprnet.org>
2024-11-20 09:46:35 +0100

    Include clippy improvements

--- hoprd/hoprd/build.rs
+++ hoprd/hoprd/build.rs
@@ -3,7 +3,7 @@ use vergen_gix::{Emitter, GixBuilder};
 
 pub fn main() -> Result<()> {
     // Adds a short SHA hash of the commit as `VERGEN_GIT_SHA` env variable
-    if let Err(_) = std::env::var("VERGEN_GIT_SHA") {
+    if std::env::var("VERGEN_GIT_SHA").is_err() {
         let git = GixBuilder::default().sha(true).build()?;
         Emitter::default().add_instructions(&git)?.emit()
     } else {
