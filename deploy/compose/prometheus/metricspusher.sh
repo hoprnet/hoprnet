be0a853f0203065b0a6043a57830fc32099d2029
ausias-armesto <ausiasarmesto@gmail.com>
2024-11-22 10:31:49 +0100

    Fixed

--- deploy/compose/prometheus/metricspusher.sh
+++ deploy/compose/prometheus/metricspusher.sh
@@ -6,7 +6,7 @@ METRICS_PUSH_URL=${1}
 # Run the loop
 while true; do 
     echo Publishing metrics ...
-    curl -s -H "X-Auth-Token: ${HOPRD_API_TOKEN}" http://hoprd:3001/api/v3/node/metrics | \
+    curl -s -H "X-Auth-Token: ${HOPRD_API_TOKEN}" http://hoprd:${HOPRD_API_PORT}/api/v3/node/metrics | \
     curl -s --data-binary @- "${METRICS_PUSH_URL}"
     sleep 15
 done
