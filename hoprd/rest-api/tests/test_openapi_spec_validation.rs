1d5052ede2b20b2f9f1a10cc46599aedbcd0e2ce
Tibor Csóka <csokat@gmail.com>
2024-12-17 10:28:38 +0100

    Remove temp file

--- hoprd/rest-api/tests/test_openapi_spec_validation.rs
+++ hoprd/rest-api/tests/test_openapi_spec_validation.rs
@@ -1,9 +1,4 @@
-// use anyhow::Context;
 use utoipa::OpenApi;
-
-// use std::io::Write;
-// use std::process::Command;
-// use tempfile::NamedTempFile;
 
 #[test]
 fn openapi_spec_should_validate_basic() -> anyhow::Result<()> {
@@ -11,24 +6,3 @@ fn openapi_spec_should_validate_basic() -> anyhow::Result<()> {
 
     Ok(())
 }
-
-///// The test cannot be run in CI as the temporary file creation leads to a non-existing file error
-// #[test]
-// fn openapi_spec_should_validate_advanced() -> anyhow::Result<()> {
-//     if std::env::var("CI").map_or(true, |v| v != "true") {
-//         let mut spec = NamedTempFile::new()?;
-//         spec.write_all(hoprd_api::ApiDoc::openapi().to_pretty_json()?.as_bytes())?;
-
-//         let command = Command::new("vacuum")
-//             .args([
-//                 "lint",
-//                 "-b",
-//                 spec.path().to_str().context("should be a valid tmp path")?,
-//             ])
-//             .output()?;
-
-//         assert!(command.status.success());
-//     }
-
-//     Ok(())
-// }
