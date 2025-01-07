93b091c2727773144e9ba686900df4610f3a0b1b
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-01-07 16:07:23 +0100

    Extract localcluster from `conftest.py` and restore snapshoting (#6748)

---
+++ sdk/python/api/protocol.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+
+class ProcolTemplate:
+    def __init__(self, retransmit: bool, segment: bool):
+        self.retransmit = retransmit
+        self.segment = segment
+
+
+class TCPProtocol(ProcolTemplate):
+    def __init__(self):
+        super().__init__(retransmit=True, segment=True)
+
+
+class UDPProtocol(ProcolTemplate):
+    def __init__(self):
+        super().__init__(retransmit=False, segment=False)
+
+
+class Protocol(Enum):
+    TCP = TCPProtocol()
+    UDP = UDPProtocol()
+
+    @classmethod
+    def fromString(cls, protocol: str):
+        try:
+            return getattr(cls, protocol.upper())
+        except AttributeError:
+            raise ValueError(f"Invalid protocol: {protocol}. Valid values are: {[p.name for p in cls]}")
+
+    @property
+    def segment(self):
+        return self.value.segment
+
+    @property
+    def retransmit(self):
+        return self.value.retransmit
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return other == self.name
+
+        return self.name == other.name
