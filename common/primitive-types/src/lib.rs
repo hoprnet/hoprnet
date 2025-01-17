9e26c63453876b3859d8395254b839e299062929
Tibor Csóka <csokat@gmail.com>
2025-01-17 12:04:59 +0100

    Remove RLP from the top layer

--- common/primitive-types/src/lib.rs
+++ common/primitive-types/src/lib.rs
@@ -12,47 +12,6 @@ pub mod sma;
 /// Defines commonly used traits across the entire code base.
 pub mod traits;
 
-// TODO: remove in 3.0
-// #[deprecated(note = "RLP encoding will be removed in 3.0")]
-/// Obsolete RLP encoding module to be removed in future versions.
-pub mod rlp {
-    use crate::errors::GeneralError;
-    use std::time::Duration;
-
-    pub fn encode(data: &[u8], timestamp: Duration) -> Box<[u8]> {
-        // For compatibility with JS, strip the leading 2 bytes if the timestamp byte array is longer than 6 bytes
-        let ts = (timestamp.as_millis() as u64).to_be_bytes();
-        let ts_encoded = if ts.len() > 6 { &ts[2..] } else { &ts };
-
-        rlp::encode_list::<&[u8], &[u8]>(&[data, ts_encoded])
-            .to_vec()
-            .into_boxed_slice()
-    }
-
-    pub fn decode(data: &[u8]) -> crate::errors::Result<(Box<[u8]>, Duration)> {
-        let rlp_data = rlp::Rlp::new(data);
-        let mut list = rlp_data
-            .as_list::<Vec<u8>>()
-            .map_err(|_| GeneralError::ParseError("invalid rlp structure".into()))?;
-
-        if list.len() != 2 {
-            return Err(GeneralError::ParseError("invalid rlp length".into()));
-        }
-
-        let enc_ts = list.remove(1);
-        let ts_len = enc_ts.len();
-        if ts_len > 8 {
-            return Err(GeneralError::ParseError("invalid rlp list length".into()));
-        }
-
-        let mut ts = [0u8; 8];
-        ts[8 - ts_len..].copy_from_slice(&enc_ts);
-
-        let ts = u64::from_be_bytes(ts);
-        Ok((list.remove(0).into_boxed_slice(), Duration::from_millis(ts)))
-    }
-}
-
 /// Approximately compares two double-precision floats.
 ///
 /// This function first tests if the two values relatively differ by at least `epsilon`.
@@ -71,71 +30,4 @@ pub mod prelude {
     pub use super::traits::*;
 
     pub use chrono::{DateTime, Utc};
-}
-
-#[allow(deprecated)]
-#[cfg(test)]
-mod tests {
-    use hex_literal::hex;
-    use std::time::{Duration, SystemTime};
-
-    use crate::traits::AsUnixTimestamp;
-
-    #[test]
-    fn test_rlp() -> anyhow::Result<()> {
-        let mut b_1 = [0u8; 100];
-        let ts_1 = SystemTime::now().as_unix_timestamp();
-
-        hopr_crypto_random::random_fill(&mut b_1);
-
-        let (b_2, ts_2) = crate::rlp::decode(crate::rlp::encode(&b_1, ts_1).as_ref())?;
-
-        assert_eq!(&b_1, b_2.as_ref(), "data must be equal");
-        assert_eq!(
-            ts_1.as_millis(),
-            ts_2.as_millis(),
-            "timestamps must be equal up to milliseconds"
-        );
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_rlp_fixed() -> anyhow::Result<()> {
-        let b_1 = b"hello";
-        let ts_1 = Duration::from_millis(1703086927316);
-
-        let data = hex!("cd8568656c6c6f86018c87e42dd4");
-        assert_eq!(
-            &data,
-            crate::rlp::encode(b_1, ts_1).as_ref(),
-            "encoded data must be equal"
-        );
-
-        let (b_2, ts_2) = crate::rlp::decode(&data)?;
-        assert_eq!(b_1, b_2.as_ref(), "decoded data must be equal");
-        assert_eq!(
-            ts_1.as_millis(),
-            ts_2.as_millis(),
-            "timestamps must be equal up to milliseconds"
-        );
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_rlp_zero() -> anyhow::Result<()> {
-        let b_1 = [0u8; 0];
-        let ts_1 = SystemTime::now().as_unix_timestamp();
-        let (b_2, ts_2) = crate::rlp::decode(crate::rlp::encode(&b_1, ts_1).as_ref())?;
-
-        assert_eq!(&b_1, b_2.as_ref(), "data must be equal");
-        assert_eq!(
-            ts_1.as_millis(),
-            ts_2.as_millis(),
-            "timestamps must be equal up to milliseconds"
-        );
-
-        Ok(())
-    }
 }
