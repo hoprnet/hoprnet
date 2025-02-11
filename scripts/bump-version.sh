c3c054e17562ccf8869333dac42f8052d31b2969
Tino Breddin <tino@hoprnet.org>
2025-02-11 12:31:38 +0100

    nix: Update flake deps

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -26,3 +26,8 @@ new_version="${1}"
 # Update hopr-lib Rust manifest
 sed -i'.original' 's/^version = ".*"$/version = "'${new_version}'"/' ${mydir}/../hopr/hopr-lib/Cargo.toml
 rm ${mydir}/../hopr/hopr-lib/Cargo.toml.original
+
+# Update hoprd Rust manifest
+sed -i'.original' 's/^version = ".*"$/version = "'${new_version}'"/' ${mydir}/../hoprd/hoprd/Cargo.toml
+rm ${mydir}/../hoprd/hoprd/Cargo.toml.original
+
