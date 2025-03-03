6ddb483c890010710182162c6a5671a091ae2f4d
NumberFour8 <lukas.pohanka@inina.net>
2025-03-03 15:39:01 +0100

    Add more traces

--- common/network-types/src/session/frame.rs
+++ common/network-types/src/session/frame.rs
@@ -618,6 +618,7 @@ impl FrameReassembler {
                 count += 1;
             } else {
                 // Break on the first incomplete and non-expired frame
+                tracing::trace!(incomplete = self.sequences.len(), "incomplete frames in reassembler");
                 break;
             }
         }
