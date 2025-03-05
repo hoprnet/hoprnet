967610fefec66cf95433ca8a496e024be5161fbf
Tino Breddin <tino@hoprnet.org>
2025-03-05 08:59:47 +0100

    swarm: Use default quic config

--- transport/p2p/src/swarm.rs
+++ transport/p2p/src/swarm.rs
@@ -77,10 +77,7 @@ async fn build_p2p_network(
             move || tcp_upgrade,
         )
         .map_err(|e| crate::errors::P2PError::Libp2p(e.to_string()))?
-        .with_quic_config(|mut cfg| {
-            cfg.max_concurrent_stream_limit = 5120; // from 256
-            cfg
-        })
+        .with_quic()
         .with_dns();
 
     // Both features could be enabled during testing, therefore we only use tokio when its
@@ -94,10 +91,7 @@ async fn build_p2p_network(
             || tcp_upgrade,
         )
         .map_err(|e| crate::errors::P2PError::Libp2p(e.to_string()))?
-        .with_quic_config(|mut cfg| {
-            cfg.max_concurrent_stream_limit = 5120; // from 256
-            cfg
-        })
+        .with_quic()
         .with_dns();
 
     Ok(swarm
