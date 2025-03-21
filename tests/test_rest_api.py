927231c733bda0d61034dc8ca7766e2bbea429b4
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-03-21 17:00:25 +0100

    test

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -35,3 +35,19 @@ async def test_hoprd_rest_api_should_accept_connection_with_valid_token(peer: st
 
     async with aiohttp.ClientSession(headers=headers) as s:
         assert (await s.get(url)).status == 200
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("peer", random.sample(nodes_with_auth(), 1))
+async def test_metric_endpoint_accepts_plain_text_header(peer: str, swarm7: dict[str, Node]):
+    url = f"http://{swarm7[peer].host_addr}:{swarm7[peer].api_port}/metrics"
+    headers = {"X-Auth-Token": swarm7[peer].api_token}
+
+    async with aiohttp.ClientSession(headers=headers) as s:
+        assert (await s.get(url)).status == 200
+
+    async with aiohttp.ClientSession(headers=headers+{"accept": "text/plain"}) as s:
+        assert (await s.get(url)).status == 200
+
+    async with aiohttp.ClientSession(headers=headers+{"accept": "application/json"}) as s:
+        assert (await s.get(url)).status == 406
+    
