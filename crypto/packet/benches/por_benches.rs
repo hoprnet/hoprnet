2f183d6d60082be47b8115f8585f835807b1220f
NumberFour8 <lukas.pohanka@inina.net>
2025-04-04 11:39:42 +0200

    Add POR pre_verify bench

--- crypto/packet/benches/por_benches.rs
+++ crypto/packet/benches/por_benches.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use hopr_crypto_packet::prelude::generate_proof_of_relay;
+use hopr_crypto_packet::prelude::{generate_proof_of_relay, pre_verify};
 use hopr_crypto_packet::HoprSphinxSuite;
-use hopr_crypto_sphinx::prelude::SphinxSuite;
+use hopr_crypto_sphinx::prelude::{SharedSecret, SphinxSuite};
 use hopr_crypto_types::keypairs::Keypair;
 use hopr_crypto_types::prelude::OffchainKeypair;
 
@@ -28,13 +28,19 @@ pub fn por_creation_bench(c: &mut Criterion) {
     group.finish();
 }
 
-/*pub fn por_pre_verification(c: &mut Criterion) {
+pub fn por_pre_verification(c: &mut Criterion) {
+    let secrets = (0..3).map(|_| SharedSecret::random()).collect::<Vec<_>>();
+    let (pors, porv) = generate_proof_of_relay(&secrets).unwrap();
 
-}*/
+    let mut group = c.benchmark_group("packet_receiving");
+    group.sample_size(SAMPLE_SIZE);
+    group.throughput(Throughput::Elements(1));
+    group.bench_function("pre_verify", |b| {
+        b.iter(|| {
+            pre_verify(&secrets[0], &pors[0], &porv.ticket_challenge()).unwrap();
+        })
+    });
+}
 
-criterion_group!(
-    benches,
-    por_creation_bench,
-    //por_pre_verification,
-);
+criterion_group!(benches, por_creation_bench, por_pre_verification,);
 criterion_main!(benches);
