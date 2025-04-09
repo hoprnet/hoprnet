f06d06052a8c994d39234d4d380abc5e9f5b0cec
Ronny Esterluss <esterlus@users.noreply.github.com>
2025-04-09 13:12:05 +0200

    Backport python tests and expose cluster to local network (#6979)
    Co-authored-by: Tino Breddin <tolbrino@users.noreply.github.com>
    Co-authored-by: Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>

--- scripts/run-local-cluster.sh
+++ scripts/run-local-cluster.sh
@@ -2,4 +2,4 @@ python -m venv .venv
 source .venv/bin/activate
 pip install -r tests/requirements.txt
 
-python -m sdk.python.localcluster --config ./sdk/python/localcluster.params.yml --fully_connected
+python -m sdk.python.localcluster --config ./sdk/python/localcluster.params.yml --fully_connected --exposed
