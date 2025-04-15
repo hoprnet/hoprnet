d95b2cd5f3275db4727b70b8ccae7222719d52d6
Tino Breddin <tino@hoprnet.org>
2025-04-15 09:04:56 +0200

    nix: add test shell

---
+++ nix/testShell.nix
@@ -0,0 +1,33 @@
+{ pkgs
+, extraPackages ? [ ]
+, pre-commit-check
+, solcDefault
+, ...
+}@args:
+let
+  mkShell = import ./mkShell.nix {};
+  shellHook = ''
+    if ! grep -q "solc = \"${solcDefault}/bin/solc\"" ethereum/contracts/foundry.toml; then
+      echo "solc = \"${solcDefault}/bin/solc\""
+      echo "Generating foundry.toml file!"
+      sed "s|# solc = .*|solc = \"${solcDefault}/bin/solc\"|g" \
+        ethereum/contracts/foundry.in.toml >| \
+        ethereum/contracts/foundry.toml
+    else
+      echo "foundry.toml file already exists!"
+    fi
+  '' + ''
+    uv sync
+    unset SOURCE_DATE_EPOCH
+  '' + pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+    autoPatchelf ./.venv
+  '' + ''
+    ${pre-commit-check.shellHook}
+  '';
+  packages = with pkgs; [
+    uv
+  ];
+  extraPackages = packages ++ extraPackages;
+in mkShell (args  // {
+  inherit shellHook extraPackages;
+})
