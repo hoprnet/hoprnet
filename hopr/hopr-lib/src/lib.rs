37302e542dd776ffbf76440b68cdf199114a6eb6
NumberFour8 <lukas.pohanka@inina.net>
2025-04-21 12:01:43 +0200

    Improve local cluster

--- hopr/hopr-lib/src/lib.rs
+++ hopr/hopr-lib/src/lib.rs
@@ -665,7 +665,10 @@ impl Hopr {
         let network_min_win_prob = self.hopr_chain_api.get_minimum_winning_probability().await?;
         let configured_win_prob = self.cfg.protocol.outgoing_ticket_winning_prob;
         if !std::env::var("HOPR_TEST_DISABLE_CHECKS").is_ok_and(|v| v.to_lowercase() == "true")
-            && configured_win_prob.is_some_and(|c| c < network_min_win_prob)
+            && configured_win_prob.is_some_and(|c|
+                !f64_approx_eq(c, network_min_win_prob, LOWEST_POSSIBLE_WINNING_PROB) &&
+                c < network_min_win_prob
+        )
         {
             return Err(HoprLibError::ChainApi(HoprChainError::Api(format!(
                 "configured outgoing ticket winning probability is lower than the network minimum winning probability: {configured_win_prob:?} < {network_min_win_prob}"
