d0fd5a5a7720e569ab2e0ebe53deb79476375a91
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2025-05-15 21:51:25 +0200

    make variable pub

--- common/internal-types/src/tickets.rs
+++ common/internal-types/src/tickets.rs
@@ -23,7 +23,7 @@ const ENCODED_WIN_PROB_LENGTH: usize = 7;
 /// Define the selector for the redeemTicketCall to avoid importing
 /// the entire hopr-bindings crate for one single constant.
 /// This value should be updated with the function interface changes.
-const REDEEM_CALL_SELECTOR: [u8; 4] = [252, 183, 121, 111];
+pub const REDEEM_CALL_SELECTOR: [u8; 4] = [252, 183, 121, 111];
 
 /// Winning probability encoded in 7-byte representation
 pub type EncodedWinProb = [u8; ENCODED_WIN_PROB_LENGTH];
