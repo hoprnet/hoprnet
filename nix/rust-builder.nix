e9ad491e4d5656b73fc67ff31f17d733b467d0e0
Tino Breddin <tolbrino@users.noreply.github.com>
2025-05-20 10:00:05 +0200

    Add musl-based static binary builds for supported platforms (#7158)
    Co-authored-by: Lukas <lukas.pohanka@inina.net>

--- nix/rust-builder.nix
+++ nix/rust-builder.nix
@@ -2,16 +2,15 @@
 , crossSystem ? localSystem
 , foundry
 , isCross ? false
+, isStatic ? false
 , localSystem
 , nixpkgs
 , rust-overlay
 , solc
 , useRustNightly ? false
-} @ args:
-let
-  crossSystem0 = crossSystem;
-in
-let
+}@args:
+let crossSystem0 = crossSystem;
+in let
   # the foundry overlay uses the hostPlatform, so we need to use a
   # localSystem-only pkgs to get the correct architecture
   pkgsLocal = import nixpkgs {
@@ -21,66 +20,83 @@ let
 
   localSystem = pkgsLocal.lib.systems.elaborate args.localSystem;
   crossSystem =
-    let system = pkgsLocal.lib.systems.elaborate crossSystem0; in
-    if crossSystem0 == null || pkgsLocal.lib.systems.equals system localSystem
-    then localSystem
-    else system;
+    let system = pkgsLocal.lib.systems.elaborate crossSystem0;
+    in if crossSystem0 == null
+      || pkgsLocal.lib.systems.equals system localSystem then
+      localSystem
+    else
+      system;
 
   pkgs = import nixpkgs {
     inherit localSystem crossSystem;
     overlays = [ rust-overlay.overlays.default solc.overlay ];
   };
 
-  # `hostPlatform` is the cross-compilation output platform;
-  # `buildPlatform` is the platform we are compiling on
+  # `buildPlatform` is the local host platform
+  # `hostPlatform` is the cross-compilation output platform
   buildPlatform = pkgs.stdenv.buildPlatform;
   hostPlatform = pkgs.stdenv.hostPlatform;
 
   foundryBin = pkgsLocal.foundry-bin;
 
-  envCase = triple: pkgsLocal.lib.strings.toUpper (builtins.replaceStrings [ "-" ] [ "_" ] triple);
+  envCase = triple:
+    pkgsLocal.lib.strings.toUpper
+      (builtins.replaceStrings [ "-" ] [ "_" ] triple);
 
   solcDefault = solc.mkDefault pkgs pkgs.pkgsBuildHost.solc_0_8_19;
 
   cargoTarget =
     if hostPlatform.config == "armv7l-unknown-linux-gnueabihf" then
-      "armv7-unknown-linux-gnueabihf" else hostPlatform.config;
-
-  rustToolchain =
-    if useRustNightly
-    then pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)
+      "armv7-unknown-linux-gnueabihf"
     else
-      (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile
+      hostPlatform.config;
+
+  rustToolchainFun =
+    if useRustNightly then
+      p: p.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)
+    else
+      p:
+      (p.pkgsBuildHost.rust-bin.fromRustupToolchainFile
         ../rust-toolchain.toml).override { targets = [ cargoTarget ]; };
 
-  craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+  craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchainFun;
 
   # mold is only supported on Linux, so falling back to lld on Darwin
   linker = if buildPlatform.isDarwin then "lld" else "mold";
 
-  buildEnv = {
+  buildEnvBase = {
     CARGO_BUILD_TARGET = cargoTarget;
-    "CARGO_TARGET_${envCase cargoTarget}_LINKER" = "${pkgs.stdenv.cc.targetPrefix}cc";
+    "CARGO_TARGET_${envCase cargoTarget}_LINKER" =
+      "${pkgs.stdenv.cc.targetPrefix}cc";
     HOST_CC = "${pkgs.stdenv.cc.nativePrefix}cc";
     CARGO_BUILD_RUSTFLAGS = "-C link-arg=-fuse-ld=${linker}";
   };
+  buildEnvStatic =
+    if isStatic then {
+      CARGO_BUILD_RUSTFLAGS =
+        "${buildEnvBase.CARGO_BUILD_RUSTFLAGS} -C target-feature=+crt-static";
+    } else
+      { };
+
+  buildEnv = buildEnvBase // buildEnvStatic;
+
 in
 {
-  inherit rustToolchain;
+  # inherit rustToolchain;
 
   callPackage = (package: args:
     let
-      crate = pkgs.callPackage package (args //
-        {
-          inherit foundryBin solcDefault craneLib isCross;
-        });
+      crate = pkgs.callPackage package
+        (args // { inherit foundryBin solcDefault craneLib isCross isStatic; });
+      # Override the derivation to add cross-compilation environment variables.
     in
-    # Override the derivation to add cross-compilation environment variables.
-    crate.overrideAttrs (previous: buildEnv // {
-      # We also have to override the `cargoArtifacts` derivation with the same changes.
-      cargoArtifacts =
-        if previous.cargoArtifacts != null
-        then previous.cargoArtifacts.overrideAttrs (previous: buildEnv)
-        else null;
-    }));
+    crate.overrideAttrs (previous:
+      buildEnv // {
+        # We also have to override the `cargoArtifacts` derivation with the same changes.
+        cargoArtifacts =
+          if previous.cargoArtifacts != null then
+            previous.cargoArtifacts.overrideAttrs (previous: buildEnv)
+          else
+            null;
+      }));
 }
