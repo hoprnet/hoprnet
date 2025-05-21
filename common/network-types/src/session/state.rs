45228dd93a6bfdcc31c52f9f80b780e2d7c2da3c
Tino Breddin <tolbrino@users.noreply.github.com>
2025-05-21 13:31:27 +0200

    session: Add more tracing logs (#7168)

--- common/network-types/src/session/state.rs
+++ common/network-types/src/session/state.rs
@@ -285,9 +285,10 @@ impl<const C: usize> SessionState<C> {
     fn consume_segment(&mut self, segment: Segment) -> crate::errors::Result<()> {
         let id = segment.id();
 
+        trace!(session_id = self.session_id, segment = %id, "received segment");
+
         match self.frame_reassembler.push_segment(segment) {
             Ok(_) => {
-                trace!(session_id = self.session_id, segment = %id, "RECEIVED: segment");
                 match self.incoming_frame_retries.entry(id.0) {
                     Entry::Occupied(e) => {
                         // Receiving a frame segment restarts the retry token for this frame
@@ -299,6 +300,7 @@ impl<const C: usize> SessionState<C> {
                         v.insert(RetryToken::new(Instant::now(), self.cfg.backoff_base));
                     }
                 }
+                trace!(session_id = self.session_id, segment = %id, "received segment pushed");
             }
             // The error here is intentionally not propagated
             Err(e) => warn!(session_id = self.session_id, ?id, error = %e, "segment not pushed"),
@@ -311,7 +313,7 @@ impl<const C: usize> SessionState<C> {
         trace!(
             session_id = self.session_id,
             count_of_segments = request.len(),
-            "RECEIVED: request",
+            "received request",
         );
 
         let mut count = 0;
@@ -352,7 +354,7 @@ impl<const C: usize> SessionState<C> {
         trace!(
             session_id = self.session_id,
             count = acked.len(),
-            "RECEIVED: acknowledgement frames",
+            "received acknowledgement frames",
         );
 
         for frame_id in acked {
