c3778203ced2c0bdcdc87fd1e98857d54d69d2e8
Tino Breddin <tino@hoprnet.org>
2025-05-22 10:59:16 +0200

    Fix merge conflict

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -1,34 +1,12 @@
-{
-  pkgs,
-  extraPackages ? [ ],
-  pre-commit-check,
-  solcDefault,
-  ...
-}@args:
+{ pkgs, extraPackages ? [ ], pre-commit-check, solcDefault, ... }@args:
 let
   shellHook = ''
     ${pre-commit-check.shellHook}
   '';
   packages = with pkgs; [ cargo-llvm-cov ];
   shellPackages = packages ++ extraPackages;
-  cleanArgs = removeAttrs args [
-    "pre-commit-check"
-  ];
-in
-<<<<<<< HEAD
-import ./testShell.nix (cleanArgs // {
+  cleanArgs = removeAttrs args [ "pre-commit-check" ];
+in import ./testShell.nix (cleanArgs // {
   inherit shellHook;
   extraPackages = shellPackages;
 })
-||||||| 82381cf104
-import ./testShell.nix (cleanArgs // {
-  inherit shellHook shellPackages;
-})
-=======
-import ./testShell.nix (
-  cleanArgs
-  // {
-    inherit shellHook shellPackages;
-  }
-)
->>>>>>> origin/master
--- nix/mkShell.nix
+++ nix/mkShell.nix
@@ -1,35 +1,19 @@
 { }:
 
-{
-  config,
-  pkgs,
-  crane,
-  shellHook ? "",
-  shellPackages ? [ ],
-  useRustNightly ? false,
-}:
+{ config, pkgs, crane, shellHook ? "", shellPackages ? [ ]
+, useRustNightly ? false, }:
 let
   cargoTarget = pkgs.stdenv.buildPlatform.config;
-  rustToolchain =
-    if useRustNightly then
-      pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)
-    else
-<<<<<<< HEAD
-      (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile
-        ../rust-toolchain.toml).override {
+  rustToolchain = if useRustNightly then
+    pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)
+  else
+    (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile
+      ../rust-toolchain.toml).override {
         targets = [ cargoTarget ];
         extensions = [ "llvm-tools-preview" ];
-||||||| 82381cf104
-      (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile
-        ../rust-toolchain.toml).override { targets = [ cargoTarget ]; };
-=======
-      (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ../rust-toolchain.toml).override {
-        targets = [ cargoTarget ];
->>>>>>> origin/master
       };
   craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-  minimumPackages =
-    with pkgs;
+  minimumPackages = with pkgs;
     [
       bash
       coreutils
@@ -51,22 +35,17 @@ let
 
       ## formatting
       config.treefmt.build.wrapper
-    ]
-    ++ (lib.attrValues config.treefmt.build.programs)
+    ] ++ (lib.attrValues config.treefmt.build.programs)
     ++ lib.optionals stdenv.isLinux [ autoPatchelfHook ];
   packages = minimumPackages ++ shellPackages;
 
   # mold is only supported on Linux, so falling back to lld on Darwin
   linker = if pkgs.stdenv.buildPlatform.isDarwin then "lld" else "mold";
-in
-craneLib.devShell {
+in craneLib.devShell {
   inherit shellHook packages;
-  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
-    [
-      pkgs.pkgsBuildHost.openssl
-      pkgs.pkgsBuildHost.curl
-    ]
-    ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.pkgsBuildHost.libgcc.lib ]
-  );
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath
+    ([ pkgs.pkgsBuildHost.openssl pkgs.pkgsBuildHost.curl ]
+      ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+      [ pkgs.pkgsBuildHost.libgcc.lib ]);
   CARGO_BUILD_RUSTFLAGS = "-C link-arg=-fuse-ld=${linker}";
 }
