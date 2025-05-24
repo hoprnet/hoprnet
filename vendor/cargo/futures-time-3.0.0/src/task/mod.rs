09d2681eddfb759f72de90f19222f4c8c304edc7
NumberFour8 <lukas.pohanka@inina.net>
2025-05-24 20:35:58 +0200

    Remove old vendored files

--- vendor/cargo/futures-time-3.0.0/src/task/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Types and Traits for working with asynchronous tasks.
-
-mod sleep;
-mod sleep_until;
-
-pub use sleep::{sleep, Sleep};
-pub use sleep_until::{sleep_until, SleepUntil};
--- vendor/cargo/futures-time-3.0.0/src/utils.rs
+++
@@ -1,5 +0,0 @@
-use std::io;
-
-pub(crate) fn timeout_err(msg: &'static str) -> io::Error {
-    io::Error::new(io::ErrorKind::TimedOut, msg)
-}
