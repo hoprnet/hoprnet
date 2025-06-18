52917ec9ef523e0ea0f19c2d966cc1bad7cccb15
ausias-armesto <ausiasarmesto@gmail.com>
2025-06-18 12:57:10 +0200

    fixed

--- scripts/get-current-version.sh
+++ scripts/get-current-version.sh
@@ -22,7 +22,7 @@ usage() {
 declare mydir
 mydir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-version_type=${1-semver}
+version_type=${1:-semver}
 current_version=$(grep -E '^version\s*=' ${mydir}/../hopr/hopr-lib/Cargo.toml | awk -F\" '{print $2}')
 
 if [ "${version_type}" == "docker" ]; then
