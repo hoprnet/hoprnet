ee90eda64b2a0221d3545359bbc5174588fa4f35
NumberFour8 <lukas.pohanka@inina.net>
2025-07-03 10:42:18 +0200

    feat: updated capture format

--- transport/protocol/src/capture.rs
+++ transport/protocol/src/capture.rs
@@ -33,7 +33,7 @@ impl PcapPacketWriter {
 
         writer
             .write_pcapng_block(InterfaceDescriptionBlock {
-                linktype: DataLink::ETHERNET,
+                linktype: DataLink::USER0,
                 snaplen: 0,
                 options: vec![],
             })
@@ -161,6 +161,8 @@ fn encode_incoming_packet_capture(packet: &IncomingPacket) -> Box<[u8]> {
             out.push(PacketType::Final as u8);
             out.extend_from_slice(packet_tag);
             out.extend_from_slice(previous_hop.as_ref());
+            out.extend_from_slice(previous_hop.to_peerid_str().as_bytes());
+            out.push(0); // Add null terminator to the string
             out.extend_from_slice(sender.as_ref());
             out.extend_from_slice(ack_key.as_ref());
             out.extend_from_slice((plain_text.len() as u16).to_be_bytes().as_ref());
@@ -176,7 +178,11 @@ fn encode_incoming_packet_capture(packet: &IncomingPacket) -> Box<[u8]> {
             out.push(PacketType::Forwarded as u8);
             out.extend_from_slice(packet_tag);
             out.extend_from_slice(previous_hop.as_ref());
+            out.extend_from_slice(previous_hop.to_peerid_str().as_bytes());
+            out.push(0); // Add null terminator to the string
             out.extend_from_slice(next_hop.as_ref());
+            out.extend_from_slice(next_hop.to_peerid_str().as_bytes());
+            out.push(0); // Add null terminator to the string
             out.extend_from_slice(ack.as_ref());
             out.extend_from_slice((data.len() as u16).to_be_bytes().as_ref());
             out.extend_from_slice(data.as_ref());
@@ -189,9 +195,125 @@ fn encode_outgoing_packet_capture(packet: &OutgoingPacket) -> Box<[u8]> {
     let mut out = Vec::new();
     out.push(PacketType::Outgoing as u8);
     out.extend_from_slice(packet.next_hop.as_ref());
+    out.extend_from_slice(packet.next_hop.to_peerid_str().as_bytes());
+    out.push(0); // Add null terminator to the string
     out.extend_from_slice(packet.ack_challenge.as_ref());
     out.extend_from_slice((packet.data.len() as u16).to_be_bytes().as_ref());
     out.extend_from_slice(packet.data.as_ref());
 
     out.into_boxed_slice()
+}
+
+#[cfg(test)]
+mod tests {
+    use hex_literal::hex;
+    use hopr_crypto_random::Randomizable;
+    use hopr_crypto_types::prelude::{Keypair, OffchainKeypair, SimplePseudonym};
+    use hopr_crypto_types::types::HalfKey;
+    use hopr_internal_types::prelude::Acknowledgement;
+    use hopr_network_types::prelude::protocol::{FrameAcknowledgements, SegmentRequest, SessionMessage};
+    use hopr_network_types::prelude::{FrameInfo, Segment};
+    use hopr_transport_packet::prelude::ApplicationData;
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_capture() -> anyhow::Result<()> {
+        let mut pcap = PacketCapture::new_file(File::create("test.pcap")?)?;
+
+        let packet = IncomingPacket::Final {
+            packet_tag: hopr_crypto_random::random_bytes(),
+            previous_hop: *OffchainKeypair::random().public(),
+            sender: SimplePseudonym::random(),
+            plain_text: ApplicationData::new(10u64, &hex!("deadbeef")).to_bytes(),
+            ack_key: HalfKey::random(),
+        };
+
+        pcap.capture_incoming(&packet)?;
+
+        let msg = SessionMessage::<1000>::Segment(Segment {
+            frame_id: 1,
+            seq_idx: 0,
+            seq_len: 1,
+            data: Box::new(hex!("474554202f20485454502f312e310d0a486f73743a207777772e6578616d706c652e636f6d0d0a557365722d4167656e743a206375726c2f382e372e310d0a4163636570743a202a2f2a0d0a0d0a")),
+        });
+
+        let packet = IncomingPacket::Final {
+            packet_tag: hopr_crypto_random::random_bytes(),
+            previous_hop: *OffchainKeypair::random().public(),
+            sender: SimplePseudonym::random(),
+            plain_text: ApplicationData::new_from_owned(1024_u64, msg.into_encoded()).to_bytes(),
+            ack_key: HalfKey::random(),
+        };
+
+        pcap.capture_incoming(&packet)?;
+
+        let msg = SessionMessage::<1000>::Acknowledge(FrameAcknowledgements::from(vec![1,2,100]));
+
+        let packet = IncomingPacket::Final {
+            packet_tag: hopr_crypto_random::random_bytes(),
+            previous_hop: *OffchainKeypair::random().public(),
+            sender: SimplePseudonym::random(),
+            plain_text: ApplicationData::new_from_owned(1024_u64, msg.into_encoded()).to_bytes(),
+            ack_key: HalfKey::random(),
+        };
+
+        pcap.capture_incoming(&packet)?;
+
+        let msg = SessionMessage::<1000>::Request(SegmentRequest::from_iter([
+            FrameInfo {
+                frame_id: 15,
+                missing_segments: [0b10100001].into(),
+                total_segments: 8,
+                last_update: std::time::SystemTime::now(),
+            },
+            FrameInfo {
+                frame_id: 11,
+                missing_segments: [0b00100001].into(),
+                total_segments: 8,
+                last_update: std::time::SystemTime::now(),
+            }
+        ]));
+
+        let packet = IncomingPacket::Final {
+            packet_tag: hopr_crypto_random::random_bytes(),
+            previous_hop: *OffchainKeypair::random().public(),
+            sender: SimplePseudonym::random(),
+            plain_text: ApplicationData::new_from_owned(1024_u64, msg.into_encoded()).to_bytes(),
+            ack_key: HalfKey::random(),
+        };
+
+        pcap.capture_incoming(&packet)?;
+
+        let packet = IncomingPacket::Forwarded {
+            packet_tag: hopr_crypto_random::random_bytes(),
+            previous_hop: *OffchainKeypair::random().public(),
+            next_hop: *OffchainKeypair::random().public(),
+            data: Box::new([0x08]),
+            ack: Acknowledgement::random(&OffchainKeypair::random())
+        };
+
+        pcap.capture_incoming(&packet)?;
+
+        let packet = OutgoingPacket {
+            next_hop: *OffchainKeypair::random().public(),
+            ack_challenge: HalfKey::random().to_challenge(),
+            data: ApplicationData::new(0u64, &hex!("cafebabe01")).to_bytes(),
+        };
+
+        pcap.capture_outgoing(&packet)?;
+
+        let packet = OutgoingPacket {
+            next_hop: *OffchainKeypair::random().public(),
+            ack_challenge: HalfKey::random().to_challenge(),
+            data: ApplicationData::new(1u64, &hex!("0004babe02")).to_bytes(),
+        };
+
+        pcap.capture_outgoing(&packet)?;
+
+
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        Ok(())
+    }
+
 }
