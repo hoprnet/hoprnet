960d49d3ae986ffd7c523f3fa451721402caa999
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-07-29 16:28:37 +0200

    feat(hopr-lib): add corrupted state to channels (#7296)

--- common/internal-types/src/channels.rs
+++ common/internal-types/src/channels.rs
@@ -251,6 +251,32 @@ impl ChannelChange {
     }
 }
 
+/// A wrapper around [`ChannelEntry`] representing a Channel that is corrupted.
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct CorruptedChannelEntry(ChannelEntry);
+
+impl CorruptedChannelEntry {
+    pub fn new(channel: ChannelEntry) -> Self {
+        CorruptedChannelEntry(channel)
+    }
+
+    /// Returns the inner `ChannelEntry`.
+    pub fn channel(&self) -> &ChannelEntry {
+        &self.0
+    }
+}
+
+/// A pair of source and destination addresses representing a channel.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct SrcDstPair(Address, Address);
+
+impl From<ChannelEntry> for SrcDstPair {
+    fn from(channel: ChannelEntry) -> Self {
+        SrcDstPair(channel.source, channel.destination)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::{
--- transport/api/src/helpers.rs
+++ transport/api/src/helpers.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{collections::HashSet, sync::Arc};
 
 use async_lock::RwLock;
 use futures::{StreamExt, TryStreamExt, channel::mpsc::Sender, stream::FuturesUnordered};
@@ -114,9 +114,17 @@ where
             RoutingOptions::Hops(hops) => {
                 trace!(%hops, "resolving path using hop count");
 
+                let channels_blacklist = self
+                    .db
+                    .get_corrupted_channels(None)
+                    .await?
+                    .into_iter()
+                    .map(|c| SrcDstPair::from(*c.channel()))
+                    .collect::<HashSet<_>>();
+
                 let cp = self
                     .selector
-                    .select_path(source, destination, hops.into(), hops.into())
+                    .select_path(source, destination, hops.into(), hops.into(), channels_blacklist)
                     .await?;
 
                 ValidatedPath::new(source, ChainPath::from_channel_path(cp, destination), &cg, &self.db).await?
