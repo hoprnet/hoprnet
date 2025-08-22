fd3e570738e6f0ebf91ae298b85d767b4d606773
Lukas <lukas.pohanka@inina.net>
2025-08-22 17:45:36 +0200

    fix: v1 tags should be limited to 61-bits (#7413)

--- protocols/app/src/lib.rs
+++ protocols/app/src/lib.rs
@@ -1,5 +1,6 @@
-//! This crate defines the Application layer protocol which typically runs on top of the
-//! HOPR packet protocol (as described in
+//! This crate defines the Application layer protocol as in [RFC-0014](https://github.com/hoprnet/rfc/blob/main/rfcs/RFC-0003-application-protocol/0014-application-protocol.md).
+//!
+//! This typically runs on top of the HOPR packet protocol (as described in
 //! [RFC-0003](https://github.com/hoprnet/rfc/blob/main/rfcs/RFC-0003-hopr-packet-protocol/0003-hopr-packet-protocol.md))
 
 /// Errors that can occur during the application layer protocol.
