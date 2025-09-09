a5e2c0a473dc78af7e112e467122a45e8d9c38d1
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2025-09-09 11:42:29 +0200

    feat: Allow SafeSuiteLib to be updated by the owner

--- ethereum/contracts/src/node-stake/NodeStakeFactory.sol
+++ ethereum/contracts/src/node-stake/NodeStakeFactory.sol
@@ -12,9 +12,14 @@ import { Ownable2Step } from "openzeppelin-contracts-5.4.0/access/Ownable2Step.s
 import { Ownable } from "openzeppelin-contracts-5.4.0/access/Ownable2Step.sol";
 
 abstract contract HoprNodeStakeFactoryEvents {
-    event HoprNodeStakeModuleUpdated(address moduleImplementation); // Emit when a new module implementation is set
-    event NewHoprNodeStakeModule(address instance); // Emit when a new module is created
-    event NewHoprNodeStakeSafe(address instance); // Emit when a new safe proxy is created
+    // Emit when a new module implementation is set
+    event HoprNodeStakeModuleUpdated(address moduleImplementation);
+    // Emit when a new module is created
+    event NewHoprNodeStakeModule(address instance);
+    // Emit when a new safe proxy is created
+    event NewHoprNodeStakeSafe(address instance);
+    // Emit when a new safe library address is set
+    event HoprNodeStakeSafeLibUpdated(HoprNodeStakeFactory.SafeLibAddress safeLibAddresses);
 }
 
 /**
@@ -40,6 +45,18 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
     using Address for address;
     using ClonesUpgradeable for address;
 
+    // Error indicating that there are too few owners provided
+    error TooFewOwners();
+    // Error when providing the StakeFactory contract address as an owner
+    error InvalidOwner();
+
+    struct SafeLibAddress {
+        address safeAddress;
+        address safeProxyFactoryAddress;
+        address compatibilityFallbackHandlerAddress;
+        address multiSendAddress;
+    }
+
     // A sentinel address that serves as the start pointer of the owner linked list used in the OwnerManager of
     // safe-contracts
     address internal constant SENTINEL_OWNERS = address(0x1);
@@ -53,10 +70,13 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
     // Signature of the approved hash used for EIP-1271 signature verification
     bytes internal approvalHashSig;
 
-    // Error indicating that there are too few owners provided
-    error TooFewOwners();
-    // Error when providing the StakeFactory contract address as an owner
-    error InvalidOwner();
+    // Safe library addresses. Defaults to SafeSuiteLibV141 constants, but can be updated by the owner
+    SafeLibAddress public safeLibAddresses = SafeLibAddress({
+        safeAddress: SafeSuiteLibV141.SAFE_Safe_ADDRESS,
+        safeProxyFactoryAddress: SafeSuiteLibV141.SAFE_SafeProxyFactory_ADDRESS,
+        compatibilityFallbackHandlerAddress: SafeSuiteLibV141.SAFE_CompatibilityFallbackHandler_ADDRESS,
+        multiSendAddress: SafeSuiteLibV141.SAFE_MultiSend_ADDRESS
+    });
 
     /**
      * @dev Constructor function to initialize contract state.
@@ -71,6 +91,9 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
 
         // Set the module singleton address
         _updateModuleSingletonAddress(_moduleSingletonAddress);
+
+        // Set the initial Safe library addresses
+        emit HoprNodeStakeSafeLibUpdated(safeLibAddresses);
     }
 
     /**
@@ -86,6 +109,14 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
      */
     function updateModuleSingletonAddress(address _newModuleSingletonAddress) public onlyOwner {
         _updateModuleSingletonAddress(_newModuleSingletonAddress);
+    }
+
+    /**
+     * @dev Updates the Safe library addresses. Can only be called by the contract owner.
+     * @param _newSafeLibAddresses The new Safe library addresses.
+     */
+    function updateSafeLibAddress(SafeLibAddress memory _newSafeLibAddresses) public onlyOwner {
+        _updateSafeLibAddress(_newSafeLibAddresses);
     }
 
     /**
@@ -133,32 +164,32 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
             1, // threshold
             address(0),
             hex"00",
-            SafeSuiteLibV141.SAFE_CompatibilityFallbackHandler_ADDRESS,
+            safeLibAddresses.compatibilityFallbackHandlerAddress,
             address(0),
             0,
             address(0)
         );
 
         // 2. Deploy Safe proxy
-        SafeProxy safeProxy = SafeProxyFactory(SafeSuiteLibV141.SAFE_SafeProxyFactory_ADDRESS).createProxyWithNonce(
-            SafeSuiteLibV141.SAFE_Safe_ADDRESS, safeInitializer, nonce
+        SafeProxy safeProxy = SafeProxyFactory(safeLibAddresses.safeProxyFactoryAddress).createProxyWithNonce(
+            safeLibAddresses.safeAddress, safeInitializer, nonce
         );
         address payable safeProxyAddr = payable(address(safeProxy));
 
         // Add Safe and multisend to the module, then transfer ownership to the module
         bytes memory moduleInitializer = abi.encodeWithSignature(
             "initialize(bytes)",
-            abi.encode(address(safeProxy), SafeSuiteLibV141.SAFE_MultiSend_ADDRESS, defaultTarget)
+            abi.encode(address(safeProxy), safeLibAddresses.multiSendAddress, defaultTarget)
         );
         moduleProxy.functionCall(moduleInitializer);
 
         // Enable the node management module
         bytes memory enableModuleData = abi.encodeWithSignature("enableModule(address)", moduleProxy);
-        prepareSafeTx(Safe(safeProxyAddr), 0, enableModuleData);
+        _prepareSafeTx(Safe(safeProxyAddr), enableModuleData);
         // Renonce ownership from the safe
         bytes memory swapOwnerData =
             abi.encodeWithSignature("removeOwner(address,address,uint256)", admins[admins.length - 2], address(this), 1);
-        prepareSafeTx(Safe(safeProxyAddr), 1, swapOwnerData);
+        _prepareSafeTx(Safe(safeProxyAddr), swapOwnerData);
 
         emit NewHoprNodeStakeModule(moduleProxy);
         emit NewHoprNodeStakeSafe(address(safeProxy));
@@ -175,23 +206,31 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
     }
 
     /**
+     * @dev Updates the Safe library addresses and emits an event.
+     * @param _newSafeLibAddresses The new Safe library addresses.
+     */
+    function _updateSafeLibAddress(SafeLibAddress memory _newSafeLibAddresses) private {
+        safeLibAddresses = _newSafeLibAddresses;
+        emit HoprNodeStakeSafeLibUpdated(safeLibAddresses);
+    }
+
+    /**
      * @dev Prepares and executes a transaction on the safe contract.
      * @param safe The address of the safe contract.
-     * @param nonce The nonce of the transaction.
      * @param data The data payload for the transaction.
      */
-    function prepareSafeTx(Safe safe, uint256 nonce, bytes memory data) private {
+    function _prepareSafeTx(Safe safe, bytes memory data) private {
         safe.execTransaction(
-            address(safe),
-            0,
-            data,
-            Enum.Operation.Call,
-            0,
-            0,
-            0,
-            address(0),
-            payable(address(msg.sender)),
-            approvalHashSig
+            address(safe),                  // to address
+            0,                              // value
+            data,                           // data
+            Enum.Operation.Call,            // operation
+            0,                              // safeTxGas
+            0,                              // baseGas
+            0,                              // gasPrice
+            address(0),                     // gasToken
+            payable(address(msg.sender)),   // refundReceiver
+            approvalHashSig                 // signature
         );
     }
 
@@ -219,14 +258,14 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
      * @return predicted The predicted address that the Safe proxy would have if deployed by the specified deployer
      * with the provided nonce.
      */
-    function predictSafeAddress(address[] memory admins, uint256 nonce) public pure returns (address predicted) {
+    function predictSafeAddress(address[] memory admins, uint256 nonce) public view returns (address predicted) {
         bytes memory initializer = abi.encodeWithSignature(
             "setup(address[],uint256,address,bytes,address,address,uint256,address)",
             admins,
             1, // threshold
             address(0),
             hex"00",
-            SafeSuiteLibV141.SAFE_CompatibilityFallbackHandler_ADDRESS,
+            safeLibAddresses.compatibilityFallbackHandlerAddress,
             address(0),
             0,
             address(0)
@@ -235,12 +274,12 @@ contract HoprNodeStakeFactory is HoprNodeStakeFactoryEvents, Ownable2Step {
         bytes32 predictedHash = keccak256(
             abi.encodePacked(
                 bytes1(0xff),
-                address(SafeSuiteLibV141.SAFE_SafeProxyFactory_ADDRESS),
+                address(safeLibAddresses.safeProxyFactoryAddress),
                 salt,
                 keccak256(
                     abi.encodePacked(
-                        SafeProxyFactory(SafeSuiteLibV141.SAFE_SafeProxyFactory_ADDRESS).proxyCreationCode(),
-                        uint256(uint160(SafeSuiteLibV141.SAFE_Safe_ADDRESS))
+                        SafeProxyFactory(safeLibAddresses.safeProxyFactoryAddress).proxyCreationCode(),
+                        uint256(uint160(safeLibAddresses.safeAddress))
                     )
                 )
             )
