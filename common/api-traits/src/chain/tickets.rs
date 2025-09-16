ccfecca41383cb842fbeecbb8808c8579f4a6dbb
NumberFour8 <lukas.pohanka@inina.net>
2025-09-16 15:55:20 +0200

    feat: add missing ticket info chain ops
    Signed-off-by: NumberFour8 <lukas.pohanka@inina.net>

--- common/api-traits/src/chain/tickets.rs
+++ common/api-traits/src/chain/tickets.rs
@@ -1,16 +1,27 @@
 use std::future::Future;
 
-pub use hopr_internal_types::prelude::RedeemableTicket;
+pub use hopr_internal_types::prelude::{RedeemableTicket, WinningProbability};
+use hopr_primitive_types::balance::HoprBalance;
 
 use crate::chain::ChainReceipt;
 
-/// On-chain operations with tickets.
+/// On-chain write operations with tickets.
 #[async_trait::async_trait]
-pub trait ChainTicketOperations {
+pub trait ChainWriteTicketOperations {
     type Error;
     /// Redeems a single ticket on-chain.
     async fn redeem_ticket(
         &self,
         ticket: RedeemableTicket,
     ) -> Result<impl Future<Output = Result<ChainReceipt, Self::Error>> + Send + '_, Self::Error>;
+}
+
+/// On-chain read operations with tickets.
+#[async_trait::async_trait]
+pub trait ChainReadTicketOperations {
+    type Error;
+    /// Retrieves the network-set minimum incoming ticket winning probability.
+    async fn minimum_incoming_ticket_win_prob(&self) -> Result<WinningProbability, Self::Error>;
+    /// Retrieves the network-set minimum ticket price.
+    async fn minimum_ticket_price(&self) -> Result<HoprBalance, Self::Error>;
 }
