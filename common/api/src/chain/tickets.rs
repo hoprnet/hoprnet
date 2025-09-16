1b9f212f9ac380b87f101264f37f70f4e610455e
Lukas <lukas.pohanka@inina.net>
2025-09-16 23:26:53 +0200

    feat: add chain operations traits into the new hopr-traits crate (#7495)
    Signed-off-by: NumberFour8 <lukas.pohanka@inina.net>

---
+++ common/api/src/chain/tickets.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+
+use futures::future::BoxFuture;
+pub use hopr_internal_types::prelude::{RedeemableTicket, WinningProbability};
+use hopr_primitive_types::balance::HoprBalance;
+
+use crate::chain::ChainReceipt;
+
+/// On-chain write operations with tickets.
+#[async_trait::async_trait]
+pub trait ChainWriteTicketOperations {
+    type Error: Error + Send + Sync + 'static;
+    /// Redeems a single ticket on-chain.
+    async fn redeem_ticket(
+        &self,
+        ticket: RedeemableTicket,
+    ) -> Result<BoxFuture<'_, Result<ChainReceipt, Self::Error>>, Self::Error>;
+}
+
+/// On-chain read operations with tickets.
+#[async_trait::async_trait]
+pub trait ChainReadTicketOperations {
+    type Error: Error + Send + Sync + 'static;
+    /// Retrieves the network-set minimum incoming ticket winning probability.
+    async fn minimum_incoming_ticket_win_prob(&self) -> Result<WinningProbability, Self::Error>;
+    /// Retrieves the network-set minimum ticket price.
+    async fn minimum_ticket_price(&self) -> Result<HoprBalance, Self::Error>;
+}
