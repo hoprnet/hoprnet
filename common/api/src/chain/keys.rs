74f8d015aba0a7e492d05457cd919896bcc2c847
Lukas <lukas.pohanka@inina.net>
2025-09-22 22:19:37 +0200

    feat: update hopr-api crate (#7515)

--- common/api/src/chain/keys.rs
+++ common/api/src/chain/keys.rs
@@ -10,13 +10,11 @@ use hopr_primitive_types::prelude::Address;
 #[async_trait::async_trait]
 pub trait ChainKeyOperations {
     type Error: Error + Send + Sync + 'static;
-    type Mapper: KeyIdMapper<HoprSphinxSuite, HoprSphinxHeaderSpec> + Clone;
+    type Mapper: KeyIdMapper<HoprSphinxSuite, HoprSphinxHeaderSpec> + Clone + Send + Sync + 'static;
     /// Translates [`Address`] into [`OffchainPublicKey`].
     async fn chain_key_to_packet_key(&self, chain: &Address) -> Result<Option<OffchainPublicKey>, Self::Error>;
     /// Translates [`OffchainPublicKey`] into [`Address`].
     async fn packet_key_to_chain_key(&self, packet: &OffchainPublicKey) -> Result<Option<Address>, Self::Error>;
-    /// Returns [mapper](KeyIdMapper) for offchain key IDs.
-    fn key_id_mapper(&self) -> Self::Mapper;
     /// Returns [mapper](KeyIdMapper) for offchain key IDs as a reference.
     fn key_id_mapper_ref(&self) -> &Self::Mapper;
 }
