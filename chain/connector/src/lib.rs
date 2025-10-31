95e8cd3c995e222afe42cc9d2261f870db370d2f
NumberFour8 <lukas.pohanka@inina.net>
2025-10-31 09:59:13 +0100

    refactor: minor code changes
    Signed-off-by: NumberFour8 <lukas.pohanka@inina.net>

--- chain/connector/src/lib.rs
+++ chain/connector/src/lib.rs
@@ -8,7 +8,6 @@ pub use backend::{Backend, InMemoryBackend, TempDbBackend};
 use hopr_chain_types::ContractAddresses;
 use hopr_crypto_types::prelude::ChainKeypair;
 use hopr_primitive_types::prelude::Address;
-use crate::errors::ConnectorError;
 
 pub type HoprBlokliConnector = HoprBlockchainConnector<TempDbBackend, blokli_client::BlokliClient, hopr_chain_types::payload::SafePayloadGenerator>;
 
@@ -24,9 +23,9 @@ pub fn create_hopr_blokli_connector(
     );
 
     let client = blokli_client::BlokliClient::new(
-          blokli_url.parse().map_err(|_| ConnectorError::InvalidArguments("invalid blokli url"))?,
+          blokli_url.parse().map_err(|_| errors::ConnectorError::InvalidArguments("invalid blokli url"))?,
           blokli_client::BlokliClientConfig::default()
     );
 
-    Ok(HoprBlokliConnector::new(chain_key.clone(), safe_address, client, TempDbBackend::new()?, payload_gen))
+    Ok(HoprBlockchainConnector::new(chain_key.clone(), safe_address, client, TempDbBackend::new()?, payload_gen))
 }
