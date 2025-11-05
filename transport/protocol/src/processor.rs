c6b2b3a4cc1c603ece6406179e836772f70af454
Tibor <9529609+Teebor-Choka@users.noreply.github.com>
2025-11-05 18:37:16 +0100

    Apply suggestion from @Teebor-Choka

--- transport/protocol/src/processor.rs
+++ transport/protocol/src/processor.rs
@@ -221,7 +221,7 @@ where
     ) -> Result<PacketSendAwaiter> {
         let (tx, rx) = futures::channel::oneshot::channel::<std::result::Result<(), PacketError>>();
 
-        tracing::debug!(?routing, "sent packet via routing");
+        tracing::trace!(?routing, "sending via packet routing");
         self.tx
             .clone()
             .send((data, routing, tx.into()))
