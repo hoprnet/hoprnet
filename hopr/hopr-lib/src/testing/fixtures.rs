1164da24148d409223833be4bbc4efb03174325f
NumberFour8 <lukas.pohanka@inina.net>
2025-11-12 16:52:18 +0100

    fix: update keys in fixtures
    Signed-off-by: NumberFour8 <lukas.pohanka@inina.net>

--- hopr/hopr-lib/src/testing/fixtures.rs
+++ hopr/hopr-lib/src/testing/fixtures.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use futures_time::future::FutureExt;
+use hex_literal::hex;
 use hopr_chain_connector::{
     BlockchainConnectorConfig, HoprBlockchainSafeConnector, create_trustful_hopr_blokli_connector,
     testing::{BlokliTestClient, BlokliTestStateBuilder, FullStateEmulator},
@@ -48,8 +49,6 @@ lazy_static! {
     static ref CLUSTER_MUTEX: Mutex<()> = Mutex::new(());
 }
 
-pub const SNAPSHOT_BASE: &str = "/tmp/hopr-tests";
-pub const PATH_TO_PROTOCOL_CONFIG: &str = "tests/protocol-config-anvil.json";
 pub const SWARM_N: usize = 3;
 
 pub fn exclusive_indexes<const N: usize>() -> [usize; N] {
@@ -66,13 +65,38 @@ pub fn exclusive_indexes<const N: usize>() -> [usize; N] {
 
 lazy_static::lazy_static! {
     static ref NODE_CHAIN_KEYS: Vec<ChainKeypair> = vec![
-
-    ];
-    static ref NODE_SAFES_MODULES: Vec<(Address, Address)> = vec![
-
+        ChainKeypair::from_secret(&hex!("76a4edbc3f595d4d07671779a0055e30b2b8477ecfd5d23c37afd7b5aa83781d")).unwrap(),
+        ChainKeypair::from_secret(&hex!("c90f09e849aa512be3dd007452977e32c7cfdc1e3de1a62bd92ba6592bcc9e90")).unwrap(),
+        ChainKeypair::from_secret(&hex!("40d4749a620d1a4278d030a3153b5b94d6fcd4f9677f6ce8e37e6ebb1987ad53")).unwrap(),
+        ChainKeypair::from_secret(&hex!("e539f1ac48270be4e84b6acfe35252df5e141a29b50ddb07b50670271bb574ee")).unwrap(),
+        ChainKeypair::from_secret(&hex!("9ab557eb14d8b081c7e1750eb87407d8c421aa79bdeb420f38980829e7dbf936")).unwrap(),
+        ChainKeypair::from_secret(&hex!("afba85b6cf1433e22d257f4ae2ef8e74317c4e18482e90e841abb77e3331ad58")).unwrap(),
+        ChainKeypair::from_secret(&hex!("4ba798854868f7f77975019ef5a3a89c6518a7ff5b1ac5b39f9ebb619b0f17da")).unwrap(),
+        ChainKeypair::from_secret(&hex!("5a4a67919f3b1bd09de351056a9cfd82054092648c4658e36621a46870a44c77")).unwrap(),
+        ChainKeypair::from_secret(&hex!("73680d318cca7f0a6280c21daee02cc13ba988b0de9be5d333bbc19003d1f86b")).unwrap(),
     ];
     static ref NODE_OFFCHAIN_KEYS: Vec<OffchainKeypair> = vec![
-
+        OffchainKeypair::from_secret(&hex!("71bf1f42ebbfcd89c3e197a3fd7cda79b92499e509b6fefa0fe44d02821d146a")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("c3659450e994f3ad086373440e4e7070629a1bfbd555387237ccb28d17acbfc8")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("4a14c5aeb53629a2dd45058a8d233f24dd90192189e8200a1e5f10069868f963")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("8c1edcdebfe508031e4124168bb4a133180e8ee68207a7946fcdc4ad0068ef0d")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("6075c595103667537c33cdb954e3e5189921cab942e5fc0ba9ec27fe6d7787d1")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("ca45a38bed988daadcebd5333abcbfd0dbb2ae5ed1917dbab8cd932970ba6b9b")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("7dddac7f5873d416e837a51351cc776b94799c7953ba9ab8d8541825fc342e94")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("dd5e0e05aea4c6a6b120e635be806b9c118123ab64f30b4210e9568a1272f617")).unwrap(),
+        OffchainKeypair::from_secret(&hex!("7c8fca94af22557421b5e4ee8a4532a77b4ee2ce5c15b410825ffe7b5b60462d")).unwrap(),
+    ];
+    // (Safe address, Module address)
+    static ref NODE_SAFES_MODULES: Vec<(Address, Address)> = vec![
+        ("7e641055ee5427572aafb1de11b56f0472f3af47".parse().unwrap(), "cd4d1e4c3a9acf604e6715d14cae64a165a381ec".parse().unwrap()),
+        ("e4d759ab6e1c57d5cc0b271c0bf5fa5137bcefd9".parse().unwrap(), "6fb8f33c1ac1a1c56a959680e8a14d918cbb2be7".parse().unwrap()),
+        ("1f835eb942f39dfac4c007bea41ce547de404f02".parse().unwrap(), "093364b60d2b4083958d779a6368ad3559985e38".parse().unwrap()),
+        ("65fec51266c3e5da55792d1a7a0700a5b246efe8".parse().unwrap(), "bbf4d38bfb0c80641a57937c44fcc42aa01c77bd".parse().unwrap()),
+        ("5af6633066297f257f0438f3d7a8c411ff7c823d".parse().unwrap(), "fa3a4eec4ea7c8404e2cd686a5842a0040fe5c67".parse().unwrap()),
+        ("5e99bb000c2a615e98ee5e9c1128e14b563ff497".parse().unwrap(), "0e536c87591767655f842025b5d5e2d178aade92".parse().unwrap()),
+        ("70416f2ad90b7773919bd3a822c7bb7d92b42b2f".parse().unwrap(), "3431fcd4ad1ed1ff4906069bb34279a3fd8145bc".parse().unwrap()),
+        ("a3d811f7efe65fcd10b7b97ce9bf85429ef657f1".parse().unwrap(), "0ad7675c28f93a161e4b2815326af7f0e866a14e".parse().unwrap()),
+        ("5eb2888c6184d9bea2d7a3ab478845b9aa5c812b".parse().unwrap(), "d8ede85de102862e2311d23263342730db18a770".parse().unwrap()),
     ];
 }
 
