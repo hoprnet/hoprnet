430823c8d7a8c0a21c6cfebf65869e88426d3834
NumberFour8 <lukas.pohanka@inina.net>
2025-12-02 14:59:31 +0100

    fix: transport tests
    Signed-off-by: NumberFour8 <lukas.pohanka@inina.net>

--- transport/protocol/tests/msg_ack_workflows.rs
+++ transport/protocol/tests/msg_ack_workflows.rs
@@ -4,7 +4,7 @@ use common::{random_packets_of_count, send_relay_receive_channel_of_n_peers};
 use serial_test::serial;
 
 #[serial]
-#[test_log::test(tokio::test(flavor = "multi_thread"))]
+#[test_log::test(tokio::test)]
 async fn test_packet_relayer_workflow_3_peers() -> anyhow::Result<()> {
     let packets = random_packets_of_count(5);
 
@@ -12,7 +12,7 @@ async fn test_packet_relayer_workflow_3_peers() -> anyhow::Result<()> {
 }
 
 #[serial]
-#[test_log::test(tokio::test(flavor = "multi_thread"))]
+#[test_log::test(tokio::test)]
 async fn test_packet_relayer_workflow_5_peers() -> anyhow::Result<()> {
     let packets = random_packets_of_count(5);
 
