efadce1cb3d941a4274d22e3dde97c2749ee2616
Lukas <lukas.pohanka@inina.net>
2026-01-13 15:07:55 +0100

    feat: improve nonce management in the TransactionSequencer (#7721)
    Signed-off-by: NumberFour8 <lukas.pohanka@inina.net>

--- chain/connector/src/connector/sequencer.rs
+++ chain/connector/src/connector/sequencer.rs
@@ -10,12 +10,12 @@ type TxRequest<T> = (
     futures::channel::oneshot::Sender<errors::Result<blokli_client::api::TxId>>,
 );
 
-/// Takes care of sequencing transactions, nonce management and submitting the transactions to the blockchain in-order.
+/// Takes care of sequencing transactions, nonce management, and submitting the transactions to the blockchain in-order.
+///
+/// This object is meant to be a singleton and cannot be cloned.
 pub struct TransactionSequencer<C, R> {
-    sender: Option<futures::channel::mpsc::Sender<TxRequest<R>>>,
-    nonce: std::sync::Arc<std::sync::atomic::AtomicU64>,
+    sender: futures::channel::mpsc::Sender<TxRequest<R>>,
     client: std::sync::Arc<C>,
-    signer: ChainKeypair,
 }
 
 const TX_QUEUE_CAPACITY: usize = 2048;
@@ -26,41 +26,51 @@ where
     R: SignableTransaction + Send + Sync + 'static,
 {
     pub fn new(signer: ChainKeypair, client: std::sync::Arc<C>) -> Self {
-        Self {
-            sender: None,
-            nonce: std::sync::Arc::new(std::sync::atomic::AtomicU64::new(1)),
-            client,
-            signer,
-        }
-    }
+        tracing::debug!(signer = %signer.public().to_address(), "starting transaction sequencer");
 
-    pub async fn start(&mut self) -> errors::Result<()> {
-        let tx_count = self
-            .client
-            .query_transaction_count(&self.signer.public().to_address().into())
-            .await?;
-        self.nonce.store(tx_count, std::sync::atomic::Ordering::SeqCst);
-
-        let chain_id = self.client.query_chain_info().await?.chain_id as u64;
-
-        let (sender, receiver) = futures::channel::mpsc::channel(TX_QUEUE_CAPACITY);
-        self.sender = Some(sender);
-
-        tracing::debug!(tx_count, signer = %self.signer.public().to_address(), "starting transaction sequencer");
-
-        let nonce_load = self.nonce.clone();
-        let nonce_inc = self.nonce.clone();
-        let client = self.client.clone();
-        let signer = self.signer.clone();
+        let client_clone = client.clone();
+        let (sender, receiver) = futures::channel::mpsc::channel::<TxRequest<R>>(TX_QUEUE_CAPACITY);
+        let chain_info_cache = std::sync::Arc::new(async_lock::OnceCell::new());
         hopr_async_runtime::prelude::spawn(
             receiver
-                .then(move |(tx, notifier)| {
-                    let client = client.clone();
+                .then(move |(tx, notifier): (R, _)| {
+                    let client = client_clone.clone();
                     let signer = signer.clone();
-                    let nonce = nonce_load.load(std::sync::atomic::Ordering::SeqCst);
+                    let chain_info_cache = chain_info_cache.clone();
                     async move {
-                        // We currently use fixed gas estimation.
-                        tx.sign_and_encode_to_eip2718(nonce, chain_id, GasEstimation::default().into(), &signer)
+                        let chain_id = match chain_info_cache
+                            .get_or_try_init(|| {
+                                client
+                                    .query_chain_info()
+                                    .map_err(ConnectorError::from)
+                                    .and_then(|ci| futures::future::ok(ci.chain_id as u64))
+                            })
+                            .await
+                        {
+                            Ok(chain_id) => {
+                                tracing::debug!(chain_id, "chain id initialized");
+                                *chain_id
+                            }
+                            Err(e) => return (Err(e), notifier),
+                        };
+
+                        // Currently use fixed gas estimation.
+                        let gas_estimation = GasEstimation::default();
+                        tracing::debug!(?gas_estimation, "gas estimation used for tx");
+
+                        let nonce = match client
+                            .query_transaction_count(&signer.public().to_address().into())
+                            .map_err(ConnectorError::from)
+                            .await
+                        {
+                            Ok(tx_count) => {
+                                tracing::debug!(tx_count, "transaction count retrieved");
+                                tx_count
+                            }
+                            Err(e) => return (Err(e), notifier),
+                        };
+
+                        tx.sign_and_encode_to_eip2718(nonce, chain_id, gas_estimation.into(), &signer)
                             .map_err(ConnectorError::from)
                             .and_then(move |tx| {
                                 tracing::debug!(nonce, "submitting transaction");
@@ -77,18 +87,6 @@ where
                     }
                 })
                 .for_each(move |(res, notifier)| {
-                    // The nonce is incremented when the transaction succeeded or failed due to on-chain
-                    // rejection.
-                    if res.is_ok()
-                        || res
-                            .as_ref()
-                            .is_err_and(|error| error.as_transaction_rejection_error().is_some())
-                    {
-                        let prev_nonce = nonce_inc.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-                        tracing::debug!(prev_nonce, ?res, "nonce incremented due to tx success or rejection");
-                    } else {
-                        tracing::debug!(?res, "nonce not incremented due to tx failure other than rejection");
-                    }
                     if notifier.send(res).is_err() {
                         tracing::debug!(
                             "failed to notify transaction result - the caller may not want to await the result \
@@ -100,7 +98,7 @@ where
                 .inspect(|_| tracing::warn!("transaction sequencer queue stopped")),
         );
 
-        Ok(())
+        Self { sender, client }
     }
 }
 
@@ -116,14 +114,10 @@ where
         transaction: R,
         timeout_until_finalized: std::time::Duration,
     ) -> errors::Result<impl Future<Output = errors::Result<blokli_client::api::types::Transaction>>> {
-        let mut sender = self
-            .sender
-            .clone()
-            .ok_or(ConnectorError::InvalidState("transaction sender not started"))?;
-
         let (notifier_tx, notifier_rx) = futures::channel::oneshot::channel();
 
-        sender
+        self.sender
+            .clone()
             .send((transaction, notifier_tx))
             .await
             .map_err(|_| ConnectorError::InvalidState("transaction queue dropped"))?;
@@ -146,125 +140,7 @@ where
 
 impl<C, R> Drop for TransactionSequencer<C, R> {
     fn drop(&mut self) {
-        if let Some(mut sender) = self.sender.take() {
-            // Causes the internally spawned task that drives the queue to terminate
-            sender.close_channel();
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use hopr_chain_types::{contract_addresses_for_network, payload::SafePayloadGenerator, prelude::PayloadGenerator};
-    use hopr_primitive_types::{prelude::*, traits::BytesRepresentable};
-
-    use super::*;
-    use crate::{
-        connector::tests::{MODULE_ADDR, PRIVATE_KEY_1},
-        testing::BlokliTestStateBuilder,
-    };
-
-    #[tokio::test]
-    async fn test_sequencer_should_increase_nonce_after_successful_tx() -> anyhow::Result<()> {
-        let blokli_client = BlokliTestStateBuilder::default()
-            .with_balances([([1u8; Address::SIZE].into(), HoprBalance::zero())])
-            .with_balances([([1u8; Address::SIZE].into(), XDaiBalance::zero())])
-            .with_balances([(
-                ChainKeypair::from_secret(&PRIVATE_KEY_1)?.public().to_address(),
-                XDaiBalance::new_base(10),
-            )])
-            .with_balances([(
-                ChainKeypair::from_secret(&PRIVATE_KEY_1)?.public().to_address(),
-                HoprBalance::new_base(1000),
-            )])
-            .with_hopr_network_chain_info("rotsee")
-            .build_dynamic_client(MODULE_ADDR.into());
-
-        let blokli_client = std::sync::Arc::new(blokli_client);
-
-        let mut tx_sequencer = TransactionSequencer::<_, <SafePayloadGenerator as PayloadGenerator>::TxRequest>::new(
-            ChainKeypair::from_secret(&PRIVATE_KEY_1)?,
-            blokli_client.clone(),
-        );
-        tx_sequencer.start().await?;
-
-        let safe_tx_gen = SafePayloadGenerator::new(
-            &ChainKeypair::from_secret(&PRIVATE_KEY_1)?,
-            contract_addresses_for_network("rotsee").unwrap().1,
-            MODULE_ADDR.into(),
-        );
-
-        let nonce_before = tx_sequencer.nonce.load(std::sync::atomic::Ordering::SeqCst);
-
-        tx_sequencer
-            .enqueue_transaction(
-                safe_tx_gen.transfer([1u8; Address::SIZE].into(), HoprBalance::new_base(100))?,
-                std::time::Duration::from_secs(2),
-            )
-            .await?
-            .await?;
-
-        assert!(
-            nonce_before < tx_sequencer.nonce.load(std::sync::atomic::Ordering::SeqCst),
-            "nonce should be incremented after successful tx"
-        );
-
-        insta::assert_yaml_snapshot!(*blokli_client.snapshot());
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_sequencer_should_increase_nonce_after_rejected_tx() -> anyhow::Result<()> {
-        let blokli_client = BlokliTestStateBuilder::default()
-            .with_balances([([1u8; Address::SIZE].into(), HoprBalance::zero())])
-            .with_balances([([1u8; Address::SIZE].into(), XDaiBalance::zero())])
-            .with_balances([(
-                ChainKeypair::from_secret(&PRIVATE_KEY_1)?.public().to_address(),
-                XDaiBalance::new_base(10),
-            )])
-            .with_balances([(
-                ChainKeypair::from_secret(&PRIVATE_KEY_1)?.public().to_address(),
-                HoprBalance::new_base(1000),
-            )])
-            .with_hopr_network_chain_info("rotsee")
-            .build_dynamic_client(MODULE_ADDR.into());
-
-        let blokli_client = std::sync::Arc::new(blokli_client);
-
-        let mut tx_sequencer = TransactionSequencer::<_, <SafePayloadGenerator as PayloadGenerator>::TxRequest>::new(
-            ChainKeypair::from_secret(&PRIVATE_KEY_1)?,
-            blokli_client.clone(),
-        );
-        tx_sequencer.start().await?;
-
-        let safe_tx_gen = SafePayloadGenerator::new(
-            &ChainKeypair::from_secret(&PRIVATE_KEY_1)?,
-            contract_addresses_for_network("rotsee").unwrap().1,
-            MODULE_ADDR.into(),
-        );
-
-        let nonce_before = tx_sequencer.nonce.load(std::sync::atomic::Ordering::SeqCst);
-
-        assert!(
-            tx_sequencer
-                .enqueue_transaction(
-                    safe_tx_gen.transfer([1u8; Address::SIZE].into(), HoprBalance::new_base(100000))?,
-                    std::time::Duration::from_secs(2)
-                )
-                .await?
-                .await
-                .is_err(),
-            "rejected tx should fail"
-        );
-
-        assert!(
-            nonce_before < tx_sequencer.nonce.load(std::sync::atomic::Ordering::SeqCst),
-            "nonce should be incremented after rejected tx"
-        );
-
-        insta::assert_yaml_snapshot!(*blokli_client.snapshot());
-
-        Ok(())
+        // Causes the internally spawned task that drives the queue to terminate
+        self.sender.close_channel();
     }
 }
