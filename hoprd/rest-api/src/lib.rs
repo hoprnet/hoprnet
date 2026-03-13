56d16660074aa8cbfb64f9d3ffdfb83161b862fb
NumberFour8 <lukas.pohanka@inina.net>
2026-03-13 21:22:17 +0100

    refactor: remove hopr-node-db and sqlite related code
    Signed-off-by: NumberFour8 <lukas.pohanka@inina.net>

--- hoprd/rest-api/src/lib.rs
+++ hoprd/rest-api/src/lib.rs
@@ -28,7 +28,6 @@ use axum::{
     routing::{delete, get, post},
 };
 use hopr_chain_connector::HoprBlockchainSafeConnector;
-use hopr_db_node::HoprNodeDb;
 use hopr_lib::{Address, Hopr, errors::HoprLibError};
 use hopr_network_graph::SharedChannelGraph;
 // pub use hopr_builder::config::{HOPR_TCP_BUFFER_SIZE, HOPR_UDP_BUFFER_SIZE, HOPR_UDP_QUEUE_SIZE};
