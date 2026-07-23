3980f34d15aa75a0a09ec61cca45459524c6dde8
Tibor <9529609+Teebor-Choka@users.noreply.github.com>
2026-07-23 12:46:35 +0200

    Update protocols/session/src/socket/ack_state.rs

--- protocols/session/src/socket/ack_state.rs
+++ protocols/session/src/socket/ack_state.rs
@@ -579,7 +579,7 @@ impl<const C: usize> SocketState<C> for AcknowledgementState<C> {
         // Since segments are re-sent via Control stream, they are not later fed again
         // into the ring buffer.
         if !ctx.rb_tx.push(segment.clone()) {
-            tracing::warn!(
+            tracing::trace!(
                 frame_id = segment.frame_id,
                 seq_idx = segment.seq_idx,
                 "segment dropped: ring buffer full"
